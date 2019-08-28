3505d52ba542a43f5a3d1328fa322ab38074e49b
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-28 13:21:44 +0300

    added solution for array list homework

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,52 +1,100 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    private static final int BASIC_CAPACITY = 10;
+    private static final int BASIC_SIZE = 0;
+    private int capacity;
+    private int size;
+    private Object[] values;
+
+    public ArrayList() {
+        capacity = BASIC_CAPACITY;
+        size = BASIC_SIZE;
+        values = new Object[capacity];
+    }
+
+    public  ArrayList(int capacity) {
+        this.capacity = capacity;
+        size = BASIC_SIZE;
+        values = new Object[capacity];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == capacity) {
+            grow();
+        }
+        values[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            values[i + 1] = values[i];
+        }
+        values[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return (T) values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index > size - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T res = (T) values[index];
+        System.arraycopy(values, index + 1, values, index, this.size - 1 - index);
+        this.size--;
+        return res;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if ((T) values[i] == t) {
+                return remove(i);
+            }
+        }
+        return remove(-1);
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
+    }
+
+    private void grow() {
+        capacity = capacity + (capacity >> 1);
+        values = Arrays.copyOf(values, capacity);
     }
 }
