0f764e4efa99733df1c5670ce296d3f1dd7ed969
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-29 14:30:58 +0300

    ArrayList class finished

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class ArrayList<T> implements List<T> {
     private T[] elements;
     private int size = 0;
-    private static final int DEFAULT_CAPACITY = 16;
+    private static final int DEFAULT_CAPACITY = 10;
 
     public ArrayList() {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
@@ -17,30 +17,25 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         if (size == elements.length) {
-            resize(size * 2);
+            resize(size + (size >> 1));
         }
         elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > size) {
+        if (index > size || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
         if (size == elements.length) {
-            resize(size * 2);
+            resize(size + (size >> 1));
         }
         if (index == size) {
             elements[size++] = value;
             return;
         }
-        T prevItem = elements[index];
+        System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
-        for (int i = index + 1; i <= size; i++) {
-            T currentItem = elements[i];
-            elements[i] = prevItem;
-            prevItem = currentItem;
-        }
         size++;
     }
 
@@ -53,7 +48,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index >= size) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
         return elements[index];
@@ -61,27 +56,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void set(T value, int index) {
-        if (index > size) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
-        if (size == elements.length) {
-            resize(size * 2);
-        }
         elements[index] = value;
-        if (index == size) {
-            size++;
-        }
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
         T item = elements[index];
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         elements[--size] = null;
         return item;
     }
