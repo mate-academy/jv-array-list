b1ad4549b8b76b2643ac6d42407aaae495d76573
TatianaPirch <pirch.tatiana@gmail.com>
2019-08-29 15:41:50 +0300

    fixes added

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -7,119 +7,104 @@ import java.util.Arrays;
  */
 public class ArrayList<T> implements List<T> {
 
-    private Object[] arrayList = new Object[10];
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
 
-    private void resizeArray(int deltaSize) {
-        Object[] newArrayList = Arrays.copyOf(arrayList, arrayList.length * 3 / 2 + deltaSize);
-        arrayList = newArrayList;
+    public ArrayList() {
+        elementData = new Object[INITIAL_CAPACITY];
+        size = 0;
     }
 
     private void resizeArray() {
-        Object[] newArrayList = Arrays.copyOf(arrayList, arrayList.length * 2);
-        arrayList = newArrayList;
+        Object[] newArrayList = Arrays.copyOf(elementData, elementData.length * 3 / 2);
+        elementData = newArrayList;
+    }
+
+    private void resizeArray(int deltaSize) {
+        Object[] newArrayList = Arrays.copyOf(elementData, elementData.length * 3 / 2 + deltaSize);
+        elementData = newArrayList;
     }
 
     @Override
     public void add(T value) {
-        if (size() == arrayList.length) {
+        if (size == elementData.length) {
             resizeArray();
         }
-        arrayList[size()] = value;
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > size()) {
+        if (index > size || index < 0) {
             throw new ArrayIndexOutOfBoundsException("You cannot add an item to this position! ("
                     + index + ")");
         }
-        if (size() == arrayList.length) {
+        if (size == elementData.length) {
             resizeArray();
         }
-        if (index == size()) {
-            arrayList[size()] = value;
-        } else {
-            for (int i = size(); i > index; i--) {
-                arrayList[i] = arrayList[i - 1];
-            }
-            arrayList[index] = value;
-        }
+        System.arraycopy(elementData, index, elementData, index + 1,
+                size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        int arraySize = size();
-        if (size() + list.size() >= arrayList.length) {
+        if (size() == elementData.length) {
             resizeArray(list.size());
         }
-        for (int i = 0; i <  list.size(); i++) {
-            arrayList[arraySize + i] = list.get(i);
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        if (index >= size()) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException("Item with this number does not exist!");
         }
-        return  (T)arrayList[index];
+        return  (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size()) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException("You cannot set an item to this position!");
         }
-        arrayList[index] = value;
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size()) {
+        if (index >= size || index < 0) {
             throw new ArrayIndexOutOfBoundsException("Item with this number does not exist!");
         }
-        T result = (T) arrayList[index];
-        if (index < size()) {
-            for (int i = index; i < size() - 1; i++) {
-                arrayList[i] = arrayList[i + 1];
-            }
-            arrayList[size() - 1] = null;
-        }
+        T result = (T) elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index,
+                size - index - 1);
+        size--;
         return result;
     }
 
     @Override
     public T remove(T t) {
-        T result = null;
-        for (int i = 0; i < size(); i++) {
-            if (arrayList[i] == t) {
-                result = (T) arrayList[i];
-                for (int j = i; j < size() - 1; j++) {
-                    arrayList[j] = arrayList[j + 1];
-                }
-                arrayList[size() - 1] = null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == t) {
+                return remove(i);
             }
         }
-        if (result == null) {
-            throw new ArrayIndexOutOfBoundsException("Item with this number does not exist!");
-        }
-        return result;
+        throw new ArrayIndexOutOfBoundsException("Item with this number does not exist!");
     }
 
     @Override
     public int size() {
-        int size = 0;
-        for (int i = 0; i < arrayList.length; i++) {
-            if (arrayList[i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return size() > 0 ? false : true;
+        return size == 0;
     }
 }
 
