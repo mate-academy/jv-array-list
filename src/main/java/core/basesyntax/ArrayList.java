44b95f8ba8e57ed33a0933d22ac0dd7d909e5336
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-29 14:36:41 +0300

    added solution for array list homework

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,26 +8,25 @@ import java.util.Arrays;
 public class ArrayList<T> implements List<T> {
 
     private static final int BASIC_CAPACITY = 10;
-    private static final int BASIC_SIZE = 0;
     private int capacity;
     private int size;
     private Object[] values;
 
     public ArrayList() {
         capacity = BASIC_CAPACITY;
-        size = BASIC_SIZE;
+        size = 0;
         values = new Object[capacity];
     }
 
     public  ArrayList(int capacity) {
         this.capacity = capacity;
-        size = BASIC_SIZE;
+        size = 0;
         values = new Object[capacity];
     }
 
     @Override
     public void add(T value) {
-        if (size == capacity) {
+        if (size == values.length) {
             grow();
         }
         values[size++] = value;
@@ -35,11 +34,9 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size - 1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        for (int i = size - 1; i >= index; i--) {
-            values[i + 1] = values[i];
+        indexCheck(index);
+        if (size - index >= 0) {
+            System.arraycopy(values, index, values, index + 1, size - index);
         }
         values[index] = value;
         size++;
@@ -54,23 +51,23 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
+        indexCheck(index);
         return (T) values[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index > size - 1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+        indexCheck(index);
         values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        T res = (T) values[index];
+        indexCheck(index);
+        T result = (T) values[index];
         System.arraycopy(values, index + 1, values, index, this.size - 1 - index);
         this.size--;
-        return res;
+        return result;
     }
 
     @Override
@@ -85,16 +82,22 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return this.size == 0;
+        return size == 0;
     }
 
     private void grow() {
         capacity = capacity + (capacity >> 1);
         values = Arrays.copyOf(values, capacity);
+    }
+
+    private void indexCheck(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 }
