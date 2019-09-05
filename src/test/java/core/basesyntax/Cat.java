3234385284be0637b3a381bfd14fcc6f4b318708
Sofasmile <sofiya.dyakonova@ukr.net>
2019-09-05 18:07:21 +0300

    added equals and hashcode to cat

--- src/test/java/core/basesyntax/Cat.java
+++ src/test/java/core/basesyntax/Cat.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Cat {
     private String name;
     private String color;
@@ -23,6 +25,24 @@ public class Cat {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name)
+                && Objects.equals(color, cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color);
     }
 
     @Override
