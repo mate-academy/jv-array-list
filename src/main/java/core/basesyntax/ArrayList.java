17c37081d3d50027cb4768162a442229ee7ae343
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-11 20:43:25 +0200

    Implement custom ArrayList.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,110 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Object[] array;
+    private Object[] temporary;
+
+    public ArrayList() {
+        array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public ArrayList(int capacity) {
+        array = new Object[capacity];
+    }
 
     @Override
     public void add(T value) {
-
+        ensureCapacity();
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndex(index);
+        ensureCapacity();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (list != null) {
+            size += list.size();
+            while (array.length <= size) {
+                ensureCapacity();
+            }
+            temporary = new Object[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                temporary[i] = list.get(i);
+            }
+            System.arraycopy(temporary, 0, array, size - list.size(), temporary.length);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        final Object removedElement = array[index];
+        System.arraycopy(array,index + 1, array, index,size - index - 1);
+        array[size] = null;
+        size--;
+        return (T)removedElement;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (t == array[i] || t != null && t.equals(array[i])) {
+                remove(i);
+                return t;
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (size >= array.length) {
+            temporary = array;
+            array = new Object[size * 3 / 2 + 1];
+            System.arraycopy(temporary, 0, array, 0, temporary.length);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 }
