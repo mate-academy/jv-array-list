75b352250f1a303a472a588c435fd714644559eb
MrNopi <Smart.like1998@gmail.com>
2019-12-12 16:32:17 +0300

    remove unnecessary array and change exception throw

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -9,19 +9,18 @@ import java.util.NoSuchElementException;
  */
 public class ArrayList<T> implements List<T> {
     private Object[] elementData;
-    private Object[] emptyData = {};
-    private int lastIndex = 0;
+    private int size = 0;
     private static final int DEFAULT_LIST_CAPACITY = 10;
 
     public ArrayList() {
-        elementData = emptyData;
+        elementData = new Object[]{};
     }
 
     public ArrayList(int length) {
         if (length > 0) {
             elementData = new Object[length];
         } else if (length == 0) {
-            elementData = emptyData;
+            elementData = new Object[]{};
         } else {
             throw new ArrayIndexOutOfBoundsException("Your index is lesser that 0");
         }
@@ -29,37 +28,36 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (elementData.length == 0 || lastIndex == elementData.length) {
-            elementData = grow();
+        if (elementData.length == 0 || size == elementData.length) {
+            grow();
             add(value);
         } else {
-            elementData[lastIndex++] = value;
+            elementData[size++] = value;
         }
     }
 
     @Override
     public void add(T value, int index) {
-        if (elementData.length == lastIndex) {
-            elementData = grow();
+        if (elementData.length == size) {
+            grow();
         }
-        if (index > -1 && index < lastIndex) {
-            add((T) elementData[lastIndex]);
-            for (int i = lastIndex - 1; i > index - 1; i--) {
-                if (i == index) {
-                    set(value, i);
-                } else {
-                    set((T) elementData[i - 1], i);
-                }
-            }
-        } else {
+        if (index < 0 || index > size) {
             throw new ArrayIndexOutOfBoundsException("Your index is lesser that 0");
+        }
+        add((T) elementData[size]);
+        for (int i = size - 1; i > index - 1; i--) {
+            if (i == index) {
+                set(value, i);
+            } else {
+                set((T) elementData[i - 1], i);
+            }
         }
     }
 
     @Override
     public void addAll(List<T> list) {
         if (list.size() > elementData.length) {
-            elementData = grow();
+            grow();
             addAll(list);
         } else {
             for (int i = 0; i < list.size(); i++) {
@@ -70,53 +68,50 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index > -1 && index < lastIndex) {
-            return (T) elementData[index];
-        } else {
+        if (index < 0 || index > size - 1) {
             throw new ArrayIndexOutOfBoundsException("Your index is lesser that 0");
         }
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index > -1 && index < lastIndex) {
-            if (index > elementData.length) {
-                elementData = grow();
-            }
-            elementData[index] = value;
-        } else {
+        if (index < 0 || index > size - 1) {
             throw new ArrayIndexOutOfBoundsException("Your index is lesser that 0");
         }
+        if (index > elementData.length) {
+            grow();
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
         T value = null;
-        if (elementData.length == lastIndex) {
-            elementData = grow();
+        if (elementData.length == size) {
+            grow();
         }
-        if (index > -1 && index < lastIndex) {
-            for (int i = index; i < lastIndex; i++) {
-                if (i == index) {
-                    value = (T) elementData[i];
-                }
-                set((T) elementData[i + 1], i);
-            }
-            lastIndex--;
-            return value;
-        } else {
+        if (index < 0 || index > size - 1) {
             throw new ArrayIndexOutOfBoundsException("Your index is lesser that 0");
         }
+        for (int i = index; i < size; i++) {
+            if (i == index) {
+                value = (T) elementData[i];
+            }
+            set((T) elementData[i + 1], i);
+        }
+        size--;
+        return value;
     }
 
     @Override
     public T remove(T t) {
         int i = 0;
-        for (; i < lastIndex; i++) {
+        for (; i < size; i++) {
             if (elementData[i] == t || elementData[i].equals(t)) {
                 break;
             }
-            if (i == lastIndex - 1) {
+            if (i == size - 1) {
                 throw new NoSuchElementException("Check your index");
             }
         }
@@ -125,29 +120,20 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return lastIndex;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        if (elementData == emptyData || lastIndex == 0) {
-            return true;
-        }
-        for (int i = 0; i < lastIndex; i++) {
-            if (elementData[i] != null) {
-                return false;
-            }
-        }
-        return true;
+        return size == 0;
     }
 
-    private Object[] grow() {
+    private void grow() {
         int oldCapacity = elementData.length;
         if (oldCapacity > 0) {
             int newCapacity = (elementData.length * 3) / 2 + 1;
-            return elementData = Arrays.copyOf(elementData, newCapacity);
+            elementData = Arrays.copyOf(elementData, newCapacity);
         }
-        return elementData = Arrays.copyOf(elementData,
-                Math.max(DEFAULT_LIST_CAPACITY, lastIndex + 1));
+        elementData = Arrays.copyOf(elementData, Math.max(DEFAULT_LIST_CAPACITY, size + 1));
     }
 }
