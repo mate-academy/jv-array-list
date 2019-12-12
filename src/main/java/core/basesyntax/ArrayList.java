f1f6b73af11b8287490bce902e02b347e6c469a1
Misha <mishabeheka@gmail.com>
2019-12-12 10:25:24 +0200

    Add methods : reSize, add(T value), add(T value, index), size.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,19 +1,39 @@
 package core.basesyntax;
 
+
+import java.util.Arrays;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    private static final int BEGINNER_CAPACITY = 10;
+    private Object[] list;
+    private int realCapacity;
+    private int size;
+
+    public ArrayList() {
+        this.list = new Object[BEGINNER_CAPACITY];
+        this.realCapacity = BEGINNER_CAPACITY;
+    }
+
     @Override
     public void add(T value) {
-
+        newSize();
+        list[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < list.length-1){
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        newSize();
+        list[index] = value;
+        size++;
     }
 
     @Override
@@ -23,7 +43,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        return (T)list[index];
     }
 
     @Override
@@ -43,11 +63,16 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
-
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+    private void newSize(){
+        if (size() == list.length){
+            realCapacity = realCapacity * 3 /2;
+        }
+        list = Arrays.copyOf(list, realCapacity);
     }
 }
