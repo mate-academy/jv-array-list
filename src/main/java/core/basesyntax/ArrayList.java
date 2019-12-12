4df92780f1ef499f9a328675fa3b9168a4e55a06
Roman Dubovskyi <rdubowskij@gmail.com>
2019-12-12 12:30:39 +0200

    renamed counter variable to "size" changed resize procedure, os it's resizing only when its full and into 1.5 prev size simplyfied arrays copy removed "this" keyword changed "addAll" method as you suggested

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /**
@@ -10,109 +9,94 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] array;
-    private int counter;
+    private int size;
 
     public ArrayList() {
-        this.array = new Object[DEFAULT_CAPACITY];
+        array = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        resize();
-        array[counter] = value;
-        counter++;
+        ensureCapacity();
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > array.length || index < 0) {
-            throw new ArrayIndexOutOfBoundsException(index);
-        }
-        Object[] tempArr = new Object[array.length + 1];
-        System.arraycopy(array, 0, tempArr, 0, index);
-        tempArr[index] = value;
-        System.arraycopy(array, index, tempArr, index + 1, array.length - index);
-        array = tempArr;
-        counter++;
+        checkIndex(index);
+        ensureCapacity();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            resize();
-            array[counter] = list.get(i);
-            counter++;
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
+        checkIndex(index);
         return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
+        checkIndex(index);
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index > array.length || index < 0) {
-            throw new ArrayIndexOutOfBoundsException(index);
-        }
-        Object[] tempArr = new Object[array.length - 1];
-        System.arraycopy(array, 0, tempArr, 0, index);
-        System.arraycopy(array, index + 1, tempArr, index, array.length - index - 1);
-        Object oldObj = array[index];
-        array = tempArr;
-        counter--;
+        checkIndex(index);
+        final Object oldObj = array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[size] = null;
+        size--;
         return (T) oldObj;
     }
 
     @Override
     public T remove(T t) {
-        boolean contains = false;
-        int index = 0;
-        Object oldObj = null;
-        for (int i = 0; i < array.length; i++) {
-            if (t != null && array[i] != null && t.equals(array[i])
-                    || t == array[i]) {
-                index = i;
-                oldObj = t;
-                contains = true;
+        for (int i = 0; i < size; i++) {
+            if (t == array[i] || t != null && t.equals(array[i])) {
+                remove(i);
+                return t;
             }
         }
-        if (contains) {
-            Object[] tempArr = new Object[array.length - 1];
-            System.arraycopy(array, 0, tempArr, 0, index);
-            System.arraycopy(array, index + 1, tempArr, index, array.length - index - 1);
-            array = tempArr;
-            counter--;
-        } else {
-            throw new NoSuchElementException();
-        }
-        return (T) oldObj;
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return counter;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        for (Object element : array) {
-            if (element != null) {
-                return false;
-            }
+        if (size == 0) {
+            return true;
         }
-        return true;
+        return false;
     }
 
-    private void resize() {
-        Object[] newArr = Arrays.copyOf(array, counter + 1);
-        array = newArr;
+    private void ensureCapacity() {
+        if (size >= array.length) {
+            Object[] tempArr = array;
+            array = new Object[size * 3 / 2 + 1];
+            System.arraycopy(tempArr, 0, array, 0, tempArr.length);
+        }
 
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
     }
 
 }
