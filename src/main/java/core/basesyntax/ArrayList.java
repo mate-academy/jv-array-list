2aff54d3d0efbf0948eb066cc226e4a63a1c0652
Dmitrii <zinchukdimaa@gmail.com>
2019-12-13 11:17:53 +0200

    added homeWork-7.1-Solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,97 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    public static final int DEFAULT_CAPACITY = 10;
+    private Object[] valueList;
+    private int sizeCounter;
+
+    public ArrayList() {
+        valueList = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        checkCapacity();
+        valueList[sizeCounter] = value;
+        sizeCounter++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkCapacity();
+        checkIndex(index);
+        System.arraycopy(valueList, index, valueList, index + 1, sizeCounter - index);
+        valueList[index] = value;
+        sizeCounter++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) valueList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        valueList[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        final Object oldObj = valueList[index];
+        System.arraycopy(valueList, index + 1, valueList, index, sizeCounter - index - 1);
+        valueList[sizeCounter] = null;
+        sizeCounter--;
+        return (T) oldObj;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < sizeCounter; i++) {
+            if (t == valueList[i] || t != null && t.equals(valueList[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return sizeCounter;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return sizeCounter == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= sizeCounter) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    private void checkCapacity() {
+        if (sizeCounter >= valueList.length) {
+            Object[] temp = valueList;
+            valueList = new Object[sizeCounter * 3 / 2 + 1];
+            System.arraycopy(temp, 0, valueList, 0, temp.length);
+        }
     }
 }
