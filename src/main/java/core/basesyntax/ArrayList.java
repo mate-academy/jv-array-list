070fc302b8a9c0c9a72871fd18a51cc31ce46cc7
Julianevinska <57527101+Julianevinska@users.noreply.github.com>
2019-12-13 02:39:42 +0200

    HW 7.1 - solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,94 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
+    private Object[] array;
+    private int count = 0;
+
+    public ArrayList() {
+        array = new Object[DEFAULT_SIZE];
+    }
 
     @Override
     public void add(T value) {
-
+        arraySize();
+        array[count++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        verification(index);
+        arraySize();
+        System.arraycopy(array, index, array, index + 1, count - index);
+        array[index] = value;
+        count++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            arraySize();
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        verification(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        verification(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        verification(index);
+        Object removedObject = array[index];
+        System.arraycopy(array, index + 1, array, index, count - index - 1);
+        count--;
+        return (T) removedObject;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size(); i++) {
+            if ((t == array[i]) || (t != null && array[i].equals(t))) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
+    }
+
+    public void verification(int index) {
+        if ((index < 0) || (index >= count)) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public void arraySize() {
+        if (count >= array.length) {
+            array = Arrays.copyOf(array, count * 3 / 2 + 1);
+        }
     }
 }
