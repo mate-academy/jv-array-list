dd5a032fead0d9410a43af1765361ee1338572b4
Vadim Chaika <proxik777@gmail.com>
2019-12-17 14:48:15 +0200

    java heap space in 23 line (arrayList = new Object[capacity];

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,119 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private int counter = 0;
+    private int capacity = 10;
+    private Object[] arrayList;
+
+    public ArrayList() {
+
+        arrayList = new Object[capacity];
+    }
+
+    public void ensureCapacity() {
+        if ((arrayList.length - counter) < 1) {
+            Object[] twinList = arrayList;
+            capacity = ((capacity * 3) / 2) + 1;
+            arrayList = new Object[capacity];
+            System.arraycopy(twinList, 0, arrayList, 0, counter);
+        }
+    }
+
+    public void checker(int index) {
+        if (index < 0 || index >= counter) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public boolean valueExists(T t) {
+        for (int i = 0; i < size(); i++) {
+            if (arrayList[i] == t || arrayList[i].equals(t)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void add(T value) {
-
+        ensureCapacity();
+        arrayList[counter] = value;
+        counter++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checker(index);
+        ensureCapacity();
+        System.arraycopy(arrayList, index, arrayList, index + 1, counter - index);
+        arrayList[index] = value;
+        counter++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < size(); i++) {
+            add(get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checker(index);
+        return (T) arrayList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checker(index);
+        arrayList[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checker(index);
+        Object toRemove = arrayList[index];
+        System.arraycopy(arrayList, index + 1, arrayList, index, counter - index);
+        counter--;
+        return (T) toRemove;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        try {
+            if (!valueExists(t)) {
+                throw new NoSuchElementException();
+            } else {
+                for (int i = 0; i < size(); i++) {
+                    if (t == arrayList[i] || t.equals(arrayList[i])) {
+                        remove(i);
+                    }
+                }
+            }
+            return t;
+        } catch (NullPointerException e) {
+            for (int i = 0; i < size(); i++) {
+                if (arrayList[i] == null) {
+                    remove(i);
+                }
+            }
+            return t;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return counter;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return counter == 0;
     }
 }
