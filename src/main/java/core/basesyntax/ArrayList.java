79e1b6c01a11cf7f5897e2c24a7a15310c97c3f7
ihor <krohmal.igor777@gmail.com>
2020-04-07 17:08:56 +0300

    implementation class ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,103 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    private T[] arrayList;
+    private int count = 0;
+    private int capacity = 10;
+
+    public ArrayList() {
+        arrayList = (T[]) new Object[capacity];
+    }
+
     @Override
     public void add(T value) {
-
+        if (count >= capacity) {
+            enlargeArray();
+        }
+        arrayList[count] = value;
+        count++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > count) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            System.arraycopy(arrayList, index, arrayList, index + 1, count - index);
+            arrayList[index] = value;
+            count++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < count) {
+            return arrayList[index];
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < count) {
+            arrayList[index] = value;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index > count) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            T element = arrayList[index];
+            System.arraycopy(arrayList, index + 1, arrayList, index, count - index);
+            count--;
+            return element;
+        }
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < count; i++) {
+            if (t == null && arrayList[i] == null
+                    || t != null && t.equals(arrayList[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
+    }
+
+    private void enlargeArray() {
+        capacity = capacity + (capacity >> 1);
+        T[] newArrayList = (T[]) new Object[capacity];
+        System.arraycopy(arrayList, 0, newArrayList, 0, count);
+        arrayList = newArrayList;
     }
 }
