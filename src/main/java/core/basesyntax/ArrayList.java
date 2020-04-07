51a3ef56ef7e6ee446880cda6a024887c5d87ee8
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-07 17:17:12 +0300

    Added solution for HW_7.1

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,97 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elementData;
+    private int lastElement;
+
+    public ArrayList() {
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+        lastElement = 0;
+    }
 
     @Override
     public void add(T value) {
-
+        ensureCapacity();
+        elementData[lastElement++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        indexInBoundsCheck(index);
+        System.arraycopy(elementData, index, elementData, index + 1, lastElement - index);
+        elementData[index] = value;
+        lastElement++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexInBoundsCheck(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        indexInBoundsCheck(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexInBoundsCheck(index);
+        T valueHolder = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, lastElement - index - 1);
+        elementData[--lastElement] = null;
+        return valueHolder;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        T valueHolder = null;
+        for (int i = 0; i < lastElement; i++) {
+            if (t == elementData[i] || (t != null && t.equals(elementData[i]))) {
+                valueHolder = remove(i);
+                return valueHolder;
+            }
+        }
+        throw new NoSuchElementException("Element " + t + " not found in the array");
     }
 
     @Override
     public int size() {
-        return 0;
+        return lastElement;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
+    }
+
+    private void ensureCapacity() {
+        if (lastElement + 1 >= elementData.length) {
+            int oldCapacity = elementData.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+    }
+
+    private void indexInBoundsCheck(int index) {
+        if (index < 0 || index >= lastElement) {
+            throw new ArrayIndexOutOfBoundsException("Illegal index: " + index + " .");
+        }
     }
 }
