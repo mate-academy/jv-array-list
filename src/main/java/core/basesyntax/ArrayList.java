16014e1697950b54fbb29763e23478fe331f085c
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-07 15:48:39 +0300

    added solution of hw-7.1

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,106 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public ArrayList(int capacity) {
+        elementData = (T[]) new Object[capacity];
+        size = 0;
+    }
 
     @Override
     public void add(T value) {
-
+        ensureCapacity();
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        assertIndex(index);
+        ensureCapacity();
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        assertIndex(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        assertIndex(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        assertIndex(index);
+        T element = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
+        elementData[--size] = null;
+        return element;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        T element = null;
+        for (int i = 0; i < size; i++) {
+            if (t == elementData[i] || t != null && t.equals(elementData[i])) {
+                element = elementData[i];
+                System.arraycopy(elementData, i + 1, elementData, i, size - i - 1);
+                elementData[--size] = null;
+            }
+        }
+        if (t != element && element == null) {
+            throw new NoSuchElementException("Такого элемента нет!");
+        }
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (size + 1 > elementData.length) {
+            T[] newElementData = (T[]) new Object[elementData.length + (elementData.length >> 1)];
+            System.arraycopy(elementData, 0, newElementData, 0, size);
+            elementData = newElementData;
+        }
+    }
+
+    private void assertIndex(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Указанный индекс превышает длину массива!");
+        }
     }
 }
