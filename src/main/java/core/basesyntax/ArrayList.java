ded83647096c8d541177b797b5d41a8fe8359899
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-07 13:12:25 +0300

    added solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,113 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private T[] arrayList;
+    private int size;
+
+    public ArrayList() {
+        arrayList = (T[]) new Object[2];
+        size = 0;
+    }
 
     @Override
     public void add(T value) {
-
+        reviewSize();
+        arrayList[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < size && index >= 0) {
+            reviewSize();
+            addingShift(index);
+            arrayList[index] = value;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return (T) arrayList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < size && index >= 0) {
+            reviewSize();
+            arrayList[index] = value;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T nedded = get(index);
+        removingShift(index);
+        return nedded;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < arrayList.length; i++) {
+            if (t == arrayList[i] || (t != null && t.equals(arrayList[i]))) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            return remove(index);
+        } else {
+            throw new java.util.NoSuchElementException();
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void reviewSize() {
+        if (size == arrayList.length) {
+            int newSize = (int) (arrayList.length * 1.5);
+            arrayList = Arrays.copyOf(arrayList, newSize);
+        }
+    }
+
+    private void removingShift(int index) {
+        for (int i = index; i < size - 1; i++) {
+            arrayList[i] = arrayList[i + 1];
+        }
+        size--;
+    }
+
+    private void addingShift(int index) {
+        for (int i = size; i > index; i--) {
+            arrayList[i] = arrayList[i - 1];
+        }
+        size++;
     }
 }
