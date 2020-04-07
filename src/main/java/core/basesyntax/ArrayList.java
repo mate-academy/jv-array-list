9fa482d18250d154dc5ca08c3cd8722969ecd79e
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-07 19:30:55 +0300

    added solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,101 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] array;
+    private int size;
+
+    public ArrayList() {
+        array = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        if (array.length == size) {
+            resize();
+        }
+        array[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        throwExceptionIfIndexOutOfBounds(index);
+        if (array.length == size) {
+            resize();
+        }
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        throwExceptionIfIndexOutOfBounds(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        throwExceptionIfIndexOutOfBounds(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        throwExceptionIfIndexOutOfBounds(index);
+        T t = (T) array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return t;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(array[i], t)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void throwExceptionIfIndexOutOfBounds(int index) {
+        if (index > size - 1 || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    private void resize() {
+        int oldCapacity = array.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        Object[] temp = array;
+        array = new Object[newCapacity];
+        System.arraycopy(temp, 0, array, 0, oldCapacity);
     }
 }
