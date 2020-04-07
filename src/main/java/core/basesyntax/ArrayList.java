1f0b8e815eedcf24bfe99d0a94de6234827587ee
Andrey Tanchak <tanchak12@gmail.com>
2020-04-07 21:48:24 +0300

    added hm-1

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,96 @@
 package core.basesyntax;
 
-/**
- * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
- * реалізації ArrayList (default capacity, newCapacity...)</p>
- */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 1;
+    private T[] massive;
+    private int capacity;
+
+    public ArrayList() {
+        capacity = 0;
+        massive = (T[]) new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void add(T value) {
-
+        maxCapacity();
+        massive[capacity] = value;
+        capacity++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkAvailable(index);
+        maxCapacity();
+        System.arraycopy(massive, index, massive,index + 1,capacity - index);
+        massive[index] = value;
+        capacity++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        int zero = 0;
+        while (zero < list.size()) {
+            T test = list.get(zero);
+            add(test);
+            zero++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < capacity && index >= 0) {
+            return (T) massive[index];
+        }
+        throw new ArrayIndexOutOfBoundsException("The index exceeds the array length!");
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkAvailable(index);
+        massive[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkAvailable(index);
+        T result = massive[index];
+        System.arraycopy(massive, index + 1, massive, index, capacity - index - 1);
+        capacity--;
+        return result;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < capacity; i++) {
+            if (t == massive[i]
+                    || t != null && t.equals(massive[i])) {
+                return remove(i);
+            }
+        }
+        throw new java.util.NoSuchElementException("No such element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return capacity;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return capacity == 0;
+    }
+
+    private void checkAvailable(int index) {
+        if (index < 0 || index >= capacity) {
+            throw new ArrayIndexOutOfBoundsException(index + " Index is out of bounds");
+        }
+    }
+
+    private void maxCapacity() {
+        if (capacity == massive.length) {
+            T[] newArr = (T[]) new Object[capacity + 1];
+            System.arraycopy(massive, 0, newArr, 0, capacity);
+            massive = newArr;
+        }
     }
 }
