b370e4c75d74e5a3aba4234359b647a644ec93b1
Maryna <kaijou88@gmail.com>
2020-04-08 15:43:35 +0300

    changed naming

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -12,31 +12,31 @@ public class ArrayList<T> implements List<T> {
     public static final int DEFAULT_CAPACITY = 10;
 
     private int capacity;
-    private int idx;
+    private int position;
     private Object[] elementData;
 
     public ArrayList() {
         capacity = DEFAULT_CAPACITY;
-        idx = 0;
+        position = 0;
         elementData = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if ((idx + 1) == capacity) {
+        if ((position + 1) == capacity) {
             grow();
         }
 
-        elementData[idx++] = value;
+        elementData[position++] = value;
 
     }
 
     @Override
     public void add(T value, int index) {
         check(index);
-        System.arraycopy(elementData, index, elementData, index + 1, idx - index);
+        System.arraycopy(elementData, index, elementData, index + 1, position - index);
         this.elementData[index] = value;
-        idx++;
+        position++;
     }
 
     @Override
@@ -62,14 +62,14 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         check(index);
         T oldValue = (T) elementData[index];
-        System.arraycopy(elementData, index + 1, elementData, index, idx - index);
-        idx--;
+        System.arraycopy(elementData, index + 1, elementData, index, position - index);
+        position--;
         return oldValue;
     }
 
     @Override
     public T remove(T t) {
-        for (int i = 0; i < idx; i++) {
+        for (int i = 0; i < position; i++) {
             if (t == elementData[i] || (t != null && t.equals(elementData[i]))) {
                 return remove(i);
             }
@@ -79,16 +79,16 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return idx;
+        return position;
     }
 
     @Override
     public boolean isEmpty() {
-        return idx == 0;
+        return position == 0;
     }
 
     private void check(int index) {
-        if (index >= idx || index < 0) {
+        if (index >= position || index < 0) {
             throw new ArrayIndexOutOfBoundsException(index + " out Of Bounds");
         }
     }
