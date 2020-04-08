c0c2fadd442c544724ccbc4940896ccc95935ba0
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-04-08 12:52:04 +0300

    code was refactored and was fixed methond addAll

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,73 +8,76 @@ import java.util.NoSuchElementException;
  */
 public class ArrayList<T> implements List<T> {
 
-    static final Integer MIN_CAPACITY = 10;
+    private static final Integer MIN_CAPACITY = 10;
     private T[] array;
-    private Integer elementData = 0;
+    private int elementDat;
 
     public ArrayList() {
         this.array = (T[]) new Object[MIN_CAPACITY];
+        this.elementDat = 0;
+    }
+
+    public ArrayList(int opacity) {
+        this.array = (T[]) new Object[opacity];
+        this.elementDat = 0;
     }
 
     @Override
     public void add(T value) {
-        grow(1, this.array.length);
-        this.array[elementData] = value;
-        elementData += 1;
+        grow();
+        this.array[this.elementDat] = value;
+        this.elementDat += 1;
     }
 
     @Override
     public void add(T value, int index) {
-        grow(1, this.array.length);
-        if (index < elementData) {
+        grow();
+        if (index < this.elementDat) {
             T[] cloneOfArray = this.array.clone();
             this.array[index] = value;
-            elementData += 1;
-            System.arraycopy(cloneOfArray, index, this.array, index + 1, elementData - index - 1);
-        } else if (index == elementData) {
+            this.elementDat += 1;
+            System.arraycopy(cloneOfArray, index, this.array, index + 1,
+                    this.elementDat - index - 1);
+        } else if (index == this.elementDat) {
             this.add(value);
-            elementData += 1;
+            this.elementDat += 1;
         } else {
-            throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + elementData);
+            throw new ArrayIndexOutOfBoundsException("Index: " + index
+                    + " Size: " + this.elementDat);
         }
     }
 
-    private void grow(int lengthOfElements, Integer length) {
-        if (elementData + lengthOfElements > this.array.length) {
+    private void grow() {
+        if (this.elementDat + 1 > this.array.length) {
             T[] cloneOfArray = this.array.clone();
-            this.array = (T[]) new Object[length + (length >> 1)];
+            this.array = (T[]) new Object[this.array.length + (this.array.length >> 1)];
             System.arraycopy(cloneOfArray, 0, this.array, 0, cloneOfArray.length);
         }
     }
 
     @Override
     public void addAll(List<T> list) {
-        while (this.array.length < elementData + list.size()) {
-            grow(list.size(), this.array.length);
-        }
         T[] bufferedArray = (T[]) new Object[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            bufferedArray[i] = list.get(i);
+            this.add(list.get(i));
         }
-        System.arraycopy(bufferedArray, 0, this.array, elementData, bufferedArray.length);
-        elementData += bufferedArray.length;
     }
 
     @Override
     public T get(int index) {
-        if (index < elementData) {
+        if (index < this.elementDat) {
             return this.array[index];
         }
-        throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + elementData);
+        throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + this.elementDat);
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < elementData) {
-            this.array[index] = value;
-        } else {
-            throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + elementData);
+        if (index >= this.elementDat) {
+            throw new ArrayIndexOutOfBoundsException("Index: " + index
+                    + " Size: " + this.elementDat);
         }
+        this.array[index] = value;
     }
 
     @Override
@@ -84,7 +87,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T t) {
-        for (int i = 0; i < elementData; i++) {
+        for (int i = 0; i < this.elementDat; i++) {
             if ((t == null && this.array[i] == null)
                     || this.array[i].equals(t)) {
                 T result = array[i];
@@ -96,32 +99,32 @@ public class ArrayList<T> implements List<T> {
     }
 
     private Integer cutting(int index) {
-        if (index < elementData) {
+        if (index < this.elementDat) {
             T[] bufferedMassive = this.array.clone();
             this.array = (T[]) new Object[this.array.length - 1];
-            elementData -= 1;
+            this.elementDat -= 1;
             System.arraycopy(bufferedMassive, 0, this.array, 0, index);
             System.arraycopy(bufferedMassive, index + 1, this.array, index,
                     this.array.length - index - 1);
             return index;
         }
-        throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + elementData);
+        throw new ArrayIndexOutOfBoundsException("Index: " + index + " Size: " + this.elementDat);
     }
 
     @Override
     public int size() {
-        return elementData;
+        return this.elementDat;
     }
 
     @Override
     public boolean isEmpty() {
-        return elementData == 0;
+        return this.elementDat == 0;
     }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder("[");
-        for (int i = 0; i < elementData; i++) {
+        for (int i = 0; i < this.elementDat; i++) {
             result.append(" " + this.array[i] + ",");
         }
         result.append("]");
