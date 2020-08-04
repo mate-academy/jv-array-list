e9d12f9212e424c138a31c00f22d57d5f38b8188
Leonid <rommel@ukr.net>
2020-08-04 14:13:07 +0300

    All methods wrote

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,114 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
-    @Override
-    public void add(T value) {
+    private static final int DEFAULT_CAPACITY = 10;
 
+    private Object[] elementData;
+
+    private int size;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     @Override
-    public void add(T value, int index) {
+    public void add(T value) {
+        if (size == elementData.length) {
+            elementData = grow(preferredNewCapacity());
+        }
+        elementData[size++] = value;
+    }
 
+
+    @Override
+    public void add(T value, int index) {
+        checkIndex(index);
+        if (size == elementData.length) {
+            elementData = grow(preferredNewCapacity());
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (size + list.size() > elementData.length) {
+            elementData = grow(size + list.size());
+        }
+        // К сожалению, тут нет возможности осуществить быстрое копирование
+        // с помощью System.arraysCopy так как наш кастомный лист не имеет метода toArray()
+        // и нам в любом случае придется перебирать его по одному элементу.
+        for (int i = 0; i < list.size(); i++) {
+            elementData[size++] = list.get(i);
+        }
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T get(int index) {
-        return null;
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elementData[index] = value;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T removed = (T) elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index);
+        elementData[--size] = null;
+        return removed;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == t || elementData[i].equals(t)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    private Object[] grow(int minCapacity) {
+        int newCapacity = Math.max(preferredNewCapacity(), minCapacity);
+        Object[] newElementData = new Object[newCapacity];
+        System.arraycopy(elementData, 0, newElementData, 0, size);
+        return newElementData;
+    }
+
+    private int preferredNewCapacity() {
+        int oldCapacity = elementData.length;
+        return oldCapacity + (oldCapacity >> 1);
     }
 }
