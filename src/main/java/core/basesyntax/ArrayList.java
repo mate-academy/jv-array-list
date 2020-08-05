453c007fc2003b10bb2743598035c31908a0da04
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-08-05 15:21:40 +0300

    Added solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,108 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int ONE_ELEMENT = 1;
+    private int size;
+    private T[] elementData;
+
+    public ArrayList() {
+        size = 0;
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            size = initialCapacity;
+            elementData = (T[]) new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            elementData = (T[]) new Object[0];
+        } else {
+            throw new RuntimeException("Number of sheet elements is incorrect");
+        }
+    }
+
+    public void checkingForArrayOutOfBounds(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public T[] ensureCapacity(int numberOfElements) {
+        if (elementData.length - 1 - size >= numberOfElements) {
+            return elementData;
+        }
+        T[] newArrayData = (T[]) new Object[elementData.length + (elementData.length >> 1)];
+        System.arraycopy(elementData, 0, newArrayData, 0, size);
+        return newArrayData;
+    }
 
     @Override
     public void add(T value) {
-
+        elementData = ensureCapacity(ONE_ELEMENT);
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        elementData = ensureCapacity(ONE_ELEMENT);
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkingForArrayOutOfBounds(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkingForArrayOutOfBounds(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkingForArrayOutOfBounds(index);
+        T removed = (T) elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index);
+        elementData[--size] = null;
+        return removed;
     }
 
     @Override
     public T remove(T t) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == t || elementData[i].equals(t)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Element not found");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0 ? true : false;
     }
 }
