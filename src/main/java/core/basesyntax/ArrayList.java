ebf102396734690140074f36c9f4985e45c82a08
Marina <maryna.voloshyna.11@gmail.com>
2020-08-05 21:58:45 +0300

    one

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,34 +5,64 @@ package core.basesyntax;
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
+    public int arraySize;
+    public Object[] values;
+    public int flag;
+
+    private ArrayList() {
+        arraySize = 0;
+        values = (T[]) new Object[10];
+    }
 
     @Override
     public void add(T value) {
-
+        if (isEnoughSpace() == false) {
+            resize();
+        }
+        values[arraySize] = value;
+        arraySize++;
     }
 
     @Override
     public void add(T value, int index) {
-
+      if(isEnoughSpace() == false){
+          resize();
+      }
+        System.arraycopy(values, index, values, index + 1, arraySize - index);
+        values[index] = value;
+        arraySize++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+   if(isEnoughSpaceForList(list.size())==false){
+        resize();
+        continue;
+   }
+      for(int i=0; i<list.size(); i++){
+          values[arraySize] = get(i);
+          arraySize++;
+      }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= values.length) {
+            return null;
+        }
+        return (T) values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+         if(index<values.length){
+             values[index] = value;
+         }
     }
 
     @Override
     public T remove(int index) {
+
         return null;
     }
 
@@ -43,11 +73,34 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+
+        return values.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+
+        return values.length == 0;
+    }
+
+
+    public boolean isEnoughSpace() {
+       return  (arraySize + 1 < values.length);
+    }
+
+    public boolean isEnoughSpaceForList(int listLength) {
+        return (listLength <= values.length - arraySize);
+
+    }
+
+
+    public void resize() {
+        Object[] oldValues = values;
+        int oldCapacity = values.length;
+        int newCapacity = (oldCapacity * 3) / 2 + 1;
+        values = (T[]) new Object[newCapacity];
+        arraySize = newCapacity;
+        // oldData - временное хранилище текущего массива с данными
+        System.arraycopy(oldValues, 0, values, 0, arraySize);
     }
 }
