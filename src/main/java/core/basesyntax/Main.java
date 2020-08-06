c5f1d776e6067d3e63e067e8b9921080117f1e92
m.mikhajlov <lizappy31@gmail.com>
2020-08-06 22:49:21 +0300

    hw8

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+    }
+}
