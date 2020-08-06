14d228fefd3f6add6445abc7c58029f1fa8bbb9a
Andriy <andriy.lagun@gmail.com>
2020-08-06 11:34:17 +0300

    Implemented solution for ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,53 +1,104 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свій ArrayList який імплементує інтерфейс List. Дотриматися основних вимог щодо
  * реалізації ArrayList (default capacity, newCapacity...)</p>
  */
 public class ArrayList<T> implements List<T> {
 
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elements;
+    private int size;
+    private int index;
+
+    ArrayList() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        if (index == elements.length) {
+            resize();
+        }
+        elements[index] = value;
+        index++;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index >= elements.length) {
+            resize();
+        }
+        System.arraycopy(elements, index, elements, index + 1, this.index - index);
+        elements[index] = value;
+        this.index++;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T element = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, this.index - index);
+        size--;
+        this.index--;
+        return element;
     }
 
     @Override
-    public T remove(T t) {
-        return null;
+    public T remove(T value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], value)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= this.index) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    @SuppressWarnings("checked")
+    private void resize() {
+        Object[] newArray = new Object[elements.length + (elements.length / 2 + 1)];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = (T[]) newArray;
     }
 }
