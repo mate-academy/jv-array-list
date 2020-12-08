0043688a5ceea1df658b5f4d27ece3055e1ebbfc
Tonitrus <grom4er@gmail.com>
2020-12-08 17:07:40 +0200

    Minimap representation of ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,31 +1,68 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] data;
+    private int size;
+    private int lastPutIndex = 0;
+
+    public ArrayList(int capacity) {
+        data = new Object[capacity];
+    }
+
+    public ArrayList() {
+        data = new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void add(T value) {
-
+        if (data.length == size) {
+            // скопировать старый, и добавить в новый. уведиченный размер на 1.5
+            changeCountValues();
+        } else {
+            add(value, lastPutIndex);
+            changeCountValues();
+        }
 
     }
 
     @Override
     public void add(T value, int index) {
+        if (data.length == size) {
+            // скопировать старый, и добавить в новый. уведиченный размер на 1.5
+            changeCountValues();
+        } else {
+            data[index] = value;
+            changeCountValues();
+        }
 
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (data.length - list.size() < 0) {
+            //скопировать старый, и добавить в новый. уведиченный размер на 1.5
+            changeCountValues();
+        } else {
+//скопировать с 1 во второй
+            changeCountValues();
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new RuntimeException(String.format("Index %d out of ArrayList.", index));
+        }
+        return (T) data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new RuntimeException(String.format("Index %d out of ArrayList.", index));
+        }
+        data[index] = value;
     }
 
     @Override
@@ -40,11 +77,16 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size <= 0;
+    }
+
+    private void changeCountValues() {
+        size = data.length;
+        lastPutIndex++;
     }
 }
