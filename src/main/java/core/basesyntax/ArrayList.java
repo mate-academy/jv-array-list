bc91ecd183c36b13010e41e452c7068bcd409e57
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-08 21:15:12 +0200

    Added constant, improved method addAll(), renamed array

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,18 +4,19 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private T[] things;
+    private static final double INCREMENT_BY_HALF = 1.5;
+    private T[] values;
     private int size;
 
     public ArrayList() {
-        this.things = (T[]) new Object[DEFAULT_CAPACITY];
+        this.values = (T[]) new Object[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
     private void resizing() {
-        T[] newArray = (T[]) new Object[(int) (things.length * 1.5)];
-        System.arraycopy(things,0, newArray, 0, size);
-        things = newArray;
+        T[] newArray = (T[]) new Object[(int) (values.length * INCREMENT_BY_HALF)];
+        System.arraycopy(values,0, newArray, 0, size);
+        values = newArray;
     }
 
     private T[] toArray(List<T> list) {
@@ -28,19 +29,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (size == things.length) {
+        if (size == values.length) {
             resizing();
         }
-        things[size] = value;
+        values[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (size == things.length) {
+        if (size == values.length) {
             resizing();
         }
-        System.arraycopy(things, index, things, index + 1, size - index);
+        System.arraycopy(values, index, values, index + 1, size - index);
         if (index == size) {
             add(value);
         } else {
@@ -51,10 +52,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        while (things.length < size + list.size()) {
-            resizing();
-        }
-        System.arraycopy(toArray(list), 0, things, size, list.size());
+        System.arraycopy(toArray(list), 0, values, size, list.size());
         size += list.size();
     }
 
@@ -63,7 +61,7 @@ public class ArrayList<T> implements List<T> {
         if (index < 0 || index >= size) {
             throw new ArrayIndexOutOfBoundsException("Incorrect index");
         }
-        return things[index];
+        return values[index];
     }
 
     @Override
@@ -71,7 +69,7 @@ public class ArrayList<T> implements List<T> {
         if (index < 0 || index >= size) {
             throw new ArrayIndexOutOfBoundsException("Incorrect index");
         }
-        things[index] = value;
+        values[index] = value;
     }
 
     @Override
@@ -79,9 +77,9 @@ public class ArrayList<T> implements List<T> {
         if (index < 0 || index >= size) {
             throw new ArrayIndexOutOfBoundsException("Incorrect index");
         }
-        T result = things[index];
-        System.arraycopy(things, index + 1, things, index, size - index);
-        things[--size] = null;
+        T result = values[index];
+        System.arraycopy(values, index + 1, values, index, size - index);
+        values[--size] = null;
         return result;
     }
 
@@ -90,16 +88,16 @@ public class ArrayList<T> implements List<T> {
         int index = -1;
         T value = null;
         for (int i = 0; i < size; i++) {
-            if (things[i] == t || things[i].equals(t)) {
+            if (values[i] == t || values[i].equals(t)) {
                 index = i;
-                value = things[i];
+                value = values[i];
                 break;
             }
         }
         if (index == -1) {
             throw new NoSuchElementException("Can't find element with same value");
         }
-        System.arraycopy(things, index + 1, things, index, size - index);
+        System.arraycopy(values, index + 1, values, index, size - index);
         size--;
         return value;
     }
