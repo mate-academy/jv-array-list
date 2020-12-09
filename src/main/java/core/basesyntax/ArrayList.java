5584d8a775a8825c0636fe69222dd8acf62b82b9
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-09 21:52:41 +0200

    implemented get(int index), addAll(List<T> list)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -43,12 +43,20 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-
+        while (list.size() + size > elementData.length) {
+            grow();
+        }
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size) {
+            throw new RuntimeException("Index out of size");
+        }
+        return elementData[index];
     }
 
     @Override
