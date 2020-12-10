fd3de7a73443b98d6ff9d7aa0db8b18e7a3edd09
engineer027 <dp270597dma@gmail.com>
2020-12-10 14:59:33 +0200

    fix method add

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -16,8 +16,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         widenArray();
-        values[size] = value;
-        size++;
+        values[size++] = value;
     }
 
     @Override
