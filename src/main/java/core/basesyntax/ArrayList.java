ae4b881f789524fec0c301ad1a21ea75424bfdec
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-10 17:52:52 +0200

    2) ReCommit - changed acces modifiers to private in all private methods, and moved all the private methods to the bottom of a code; - optimized the growCapacity() - now array increases dirrectly in 1.5 times, added whitespaces in arraycopy(); - removes "this" from methods; - optimized the names of checkIndex methods.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -71,7 +71,7 @@ public class ArrayList<T> implements List<T> {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("There is no such element in array!");
+        throw new NoSuchElementException("There is no such element in array!!");
     }
 
     @Override
