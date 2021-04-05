0fe1da34fab2927955fec903172fdff40d2fc553
Nikber5 <nikber198@gmail.com>
2021-04-05 16:17:50 +0300

    Few more changes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -40,9 +40,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        while (size + list.size() > content.length) {
-            content = resizeArray();
-        }
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
