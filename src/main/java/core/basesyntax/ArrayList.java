987bd9e41e5e802de46636a13ada063806978a12
IhorRome <ihor.rome@gmail.com>
2021-04-07 17:25:36 +0300

    fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,61 +1,47 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private static final int INDEX_NOT_FOUND = -1;
     private static final int DEFAULT_CAPACITY = 10;
     private T[] elements;
     private int size;
 
     public ArrayList() {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
-        size = 0;
     }
 
-    private T[] resize(int currentSize, int index) {
-        T[] newArray = (T[]) new Object[currentSize * 3 / 2];
-        System.arraycopy(elements, 0, newArray, 0, index);
-        System.arraycopy(elements, index, newArray, index + 1, elements.length - index);
-        return elements = newArray;
-    }
-
-    private T[] resize(int currentSize) {
-        T[] newArray = (T[]) new Object[currentSize * 3 / 2];
+    private T[] resize() {
+        T[] newArray = (T[]) new Object[elements.length * 3 / 2];
         System.arraycopy(elements, 0, newArray, 0, elements.length);
         return elements = newArray;
     }
 
-    private void checkIndex(int index, String message) {
+    private void checkIndex(int index) {
         if (!(index < size) || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(message);
+            throw new ArrayListIndexOutOfBoundsException("Given index is out of bound");
         }
     }
 
     @Override
     public void add(T value) {
         if (elements.length == size) {
-            elements = resize(elements.length);
+            elements = resize();
         }
-        this.elements[size] = value;
+        elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
         if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Can't add element by this index");
+            throw new ArrayListIndexOutOfBoundsException("Given index is out of bound");
         }
         if (elements.length == size) {
-            elements = resize(elements.length, index);
-        } else {
-            T[] newArray = (T[]) new Object[elements.length];
-            System.arraycopy(elements, 0, newArray, 0, index);
-            System.arraycopy(elements, index, newArray, index + 1, elements.length - index - 1);
-            elements = newArray;
+            elements = resize();
         }
-        this.elements[index] = value;
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
         size++;
     }
 
@@ -68,41 +54,33 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndex(index, "Can't get element by this index");
+        checkIndex(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, "Can't get element by this index");
-        this.elements[index] = value;
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index,"Can't remove element by this index");
+        checkIndex(index);
         final T removedElement = elements[index];
-        T[] newArray = (T[]) new Object[elements.length];
-        System.arraycopy(elements, 0, newArray, 0, index);
-        System.arraycopy(elements,index + 1, newArray, index, elements.length - index - 1);
-        elements = newArray;
+        System.arraycopy(elements,index + 1, elements, index, size - index);
         size--;
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        int elementsIndex = INDEX_NOT_FOUND;
-        for (int i = 0; i < elements.length; i++) {
-            if (Objects.equals(elements[i], element)) {
-                elementsIndex = i;
-                break;
+        for (int i = 0; i < size; i++) {
+            if (element == elements[i] || element != null && element.equals(elements[i])) {
+                return remove(i);
             }
         }
-        if (elementsIndex == INDEX_NOT_FOUND) {
-            throw new NoSuchElementException("There is no such element in list");
-        }
-        return remove(elementsIndex);
+        throw new NoSuchElementException("There is no such element in list");
     }
 
     @Override
