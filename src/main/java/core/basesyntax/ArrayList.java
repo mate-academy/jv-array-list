a326d316594cd39a061d4ab8b3fe2d2c7f24e012
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-09 17:42:43 +0300

    implement add and  get methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,15 +1,47 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Test");
+        arrayList.add(null);
+        arrayList.add("for");
+        arrayList.add("Mate");
+
+        System.out.println(arrayList.size());
+    }
+
+    //    it should have the default capacity, the internal array should grow 1.5 times when it is full, etc.
+    int capacity = 10;
+    T[] array = (T[]) new Object[capacity];
+    int size = 0;
 
     @Override
     public void add(T value) {
-
+        if (array.length < size) {
+            capacity = capacity + capacity / 2;
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (array.length < size) {
+            capacity = capacity + capacity / 2;
+        }
+        T[] copyArray = (T[]) new Object[capacity];
+        for (int i = 0; i < copyArray.length; i++) {
+            if (i < index) {
+                copyArray[i] = array[i];
+            } else if (i == index) {
+                copyArray[i] = value;
+            } else {
+                copyArray[i] = array[i - 1];
+            }
+        }
+        array = copyArray;
+        size++;
     }
 
     @Override
@@ -19,7 +51,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        return array[index];
     }
 
     @Override
@@ -39,7 +71,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
