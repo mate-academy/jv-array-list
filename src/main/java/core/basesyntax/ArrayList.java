bb6294a80e5d614ee08952b6ce69f34447cc67d8
Darina <to.darina.to>
2021-04-10 13:06:28 +0300

    mvn clean package

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    T[] elementData = (T[]) new Object[10];
+    private T[] elementData = (T[]) new Object[10];
     private int size;
 
     @Override
@@ -12,7 +12,7 @@ public class ArrayList<T> implements List<T> {
             elementData[size++] = value;
         } else {
             T[] newArray = (T[]) new Object[elementData.length + (elementData.length >> 1)];
-            System.arraycopy(elementData,0,newArray,0,size);
+            System.arraycopy(elementData, 0, newArray, 0, size);
             newArray[size++] = value;
             elementData = newArray;
         }
@@ -20,47 +20,31 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        Object [] newElementData;
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Wrong index");
         }
         if (index < elementData.length) {
-            System.arraycopy(elementData, index , elementData, index + 1, size - index);
+            System.arraycopy(elementData, index, elementData, index + 1, size - index);
             elementData[index] = value;
             size++;
-            if (index >=  elementData.length) {
-                newElementData = (T[]) new Object[elementData.length + (elementData.length >> 1)];
-                System.arraycopy(elementData,0,newElementData,0,index - 1);
-                newElementData [index] = value;
-                System.arraycopy(elementData,index,newElementData,index + 1,size - index);
+            if (index >= elementData.length) {
+                T[] copyElementData = (T[]) new Object[elementData.length
+                        + (elementData.length >> 1)];
+                System.arraycopy(elementData, 0, copyElementData, 0, index - 1);
+                copyElementData[index] = value;
+                System.arraycopy(elementData, index, copyElementData, index + 1, size - index);
                 size++;
-                elementData = (T[]) newElementData;
+                elementData = copyElementData;
             }
         } else {
-            elementData[size + 1] = value;
+            add(value);
         }
     }
 
     @Override
     public void addAll(List<T> list) {
-        //1 2 3
-        // oldArray = 1 2 3 4 5 6 7 8 9 []
-        // create new array -- copy oldArray --- oldArray + newList
-        //
-        T[] listArray = (T[]) new Object[list.size()];
-        Object [] addAll = new Object[listArray.length];
-        for(int i = 0; i < listArray.length; i++){
-            listArray[i] = list.get(i);
-        }
-        if (listArray.length + size > elementData.length) {
-            addAll = (T[]) new Object[elementData.length + (elementData.length >> 1)];
-            System.arraycopy(elementData, 0, addAll, 0, size);
-            System.arraycopy(elementData, 0, addAll, 0, addAll.length);
-            elementData = (T[]) addAll;
-            size += listArray.length;
-        } else {
-            System.arraycopy(listArray, 0, elementData, size, listArray.length);
-            size += listArray.length;
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
     }
 
@@ -94,24 +78,16 @@ public class ArrayList<T> implements List<T> {
     }
 
     @Override
-    public T remove(T element) { /// null
-        boolean elementFound = false;
-        T remove = null;
+    public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (element == null && elementData[i] == null) {
-                remove = remove(i);
-                return  remove;
-            }
-            if (element.equals(elementData[i]) && elementData[i] != null) {
-                remove = remove(i);
-                elementFound = true;
+            if (elementData[i] == element || elementData[i] != null
+                    && element.equals(elementData[i])) {
+                return remove(i);
             }
         }
-            if (elementFound == false) {
-                throw new NoSuchElementException("No such element");
-            }
-            return remove;
-        }
+        throw new NoSuchElementException("No such element");
+    }
+
     @Override
     public int size() {
         return size;
