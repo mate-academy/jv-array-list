2c0b267654889c9421bec8c0da67617c08101ea3
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-04-11 11:58:14 +0300

    checklist fix

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private Object[] items;
+    private T[] items;
     private int size;
     private int capacity;
 
     public ArrayList() {
-        items = new Object[MAX_ITEMS_NUMBER];
+        items = (T[]) new Object[MAX_ITEMS_NUMBER];
         capacity = MAX_ITEMS_NUMBER;
     }
 
@@ -24,7 +23,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        indexCheck(index < 0, "ArrayList out of bounds");
+        indexCheck(index < 0, "ArrayList out of bounds, negative index");
         indexCheck(index > size + 1, "ArrayList out of bounds");
         if (index == size) {
             add(value);
@@ -40,12 +39,6 @@ public class ArrayList<T> implements List<T> {
         size++;
     }
 
-    private void indexCheck(boolean b, String s) {
-        if (b) {
-            throw new ArrayListIndexOutOfBoundsException(s);
-        }
-    }
-
     @Override
     public void addAll(List<T> list) {
         while (size + list.size() > capacity) {
@@ -58,21 +51,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        indexCheck(index < 0, "ArrayList out of bounds");
+        indexCheck(index < 0, "ArrayList out of bounds, negative index");
         indexCheck(index > size - 1, "Array out of bounds");
         return (T) items[index];
     }
 
     @Override
     public void set(T value, int index) {
-        indexCheck(index < 0, "ArrayList out of bounds");
+        indexCheck(index < 0, "ArrayList out of bounds, negative index");
         indexCheck(index > size - 1, "ArrayList out of bounds");
         items[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        indexCheck(index < 0, "ArrayList out of bounds");
+        indexCheck(index < 0, "ArrayList out of bounds, negative index");
         indexCheck(index > size - 1, "ArrayList out of bounds");
         final T temp = (T) items[index];
         for (int i = index; i < size; i++) {
@@ -86,7 +79,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (items[i] == null && items[i] == null) {
+            if (items[i] == null && element == null) {
                 remove(i);
                 return element;
             } else if (items[i] == null) {
@@ -97,7 +90,7 @@ public class ArrayList<T> implements List<T> {
                 return element;
             }
         }
-        throw new NoSuchElementException();
+        throw new NoSuchElementException("No such element");
     }
 
     @Override
@@ -107,15 +100,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0;
     }
 
     private void grow() {
-        capacity *= 1.5;
-        items = Arrays.copyOf(items, capacity);
+        T[] tempArray = (T[]) new Object[(int) (items.length * 1.5)];
+        System.arraycopy(items, 0, tempArray, 0, items.length);
+        items = tempArray;
+        capacity = items.length;
+    }
+
+    private void indexCheck(boolean b, String s) {
+        if (b) {
+            throw new ArrayListIndexOutOfBoundsException(s);
+        }
     }
 }
