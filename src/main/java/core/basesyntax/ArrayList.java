1455f0a07c58c97bd7c5fd6f015fd9dfa2ce345c
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-12 00:01:27 +0300

    Alfa version.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,49 +1,175 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    private Object[] elementData = new Object[DEFAULT_CAPACITY];
+    private int size;
 
     @Override
     public void add(T value) {
+        add(value, elementData, size);
+    }
 
+    private void add(T value, Object[] elementData, int s) {
+        if (s == elementData.length) {
+            elementData = grow();
+        }
+        elementData[s] = value;
+        size = s + 1;
     }
 
     @Override
     public void add(T value, int index) {
+        rangeCheckForAdd(index);
+        final int s;
+        Object[] elementData;
+        if ((s = size) == (elementData = this.elementData).length) {
+            elementData = grow();
+        }
+        System.arraycopy(elementData, index,
+                elementData, index + 1,
+                s - index);
+        elementData[index] = value;
+        size = s + 1;
+    }
 
+    private Object[] grow(int minCapacity) {
+        return elementData = Arrays.copyOf(elementData,
+                newCapacity(minCapacity));
+    }
+
+    private Object[] grow() {
+        return grow(size + 1);
+    }
+
+    private int newCapacity(int minCapacity) {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity <= 0) {
+            if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+                return Math.max(DEFAULT_CAPACITY, minCapacity);
+            }
+            if (minCapacity < 0) {
+                throw new OutOfMemoryError();
+            }
+            return minCapacity;
+        }
+        return (newCapacity - MAX_ARRAY_SIZE <= 0)
+                ? newCapacity
+                : hugeCapacity(minCapacity);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) {
+            throw new OutOfMemoryError();
+        }
+        return (minCapacity > MAX_ARRAY_SIZE)
+                ? Integer.MAX_VALUE
+                : MAX_ARRAY_SIZE;
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Negative index");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        Object[] arrayForAdd = new Object[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arrayForAdd[i] = list.get(i);
+        }
+        int addLength = arrayForAdd.length;
+        Object[] elementData;
+        final int s;
+        if (addLength > (elementData = this.elementData).length - (s = size)) {
+            elementData = grow(s + addLength);
+        }
+        System.arraycopy(arrayForAdd, 0, elementData, s, addLength);
+        size = s + addLength;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Negative index!");
+        }
+        return elementData(index);
+    }
+
+    T elementData(int index) {
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Bad index");
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Bad index");
+        }
+        final Object[] es = elementData;
+        @SuppressWarnings("unchecked") T oldValue = (T) es[index];
+        fastRemove(es, index);
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        final Object[] es = elementData;
+        final int size = this.size;
+        int i = 0;
+        boolean isFound = false;
+        if (element == null) {
+            for (; i < size; i++) {
+                if (es[i] == null) {
+                    isFound = true;
+                    break;
+                }
+            }
+        } else {
+            for (; i < size; i++) {
+                if (element.equals(es[i])) {
+                    isFound = true;
+                    break;
+                }
+            }
+        }
+        if (isFound) {
+            fastRemove(es, i);
+            return element;
+        } else {
+            throw new NoSuchElementException("Element doesn`t exist");
+        }
+    }
+
+    private void fastRemove(Object[] es, int i) {
+        final int newSize;
+        if ((newSize = size - 1) > i) {
+            System.arraycopy(es, i + 1, es, i, newSize - i);
+        }
+        es[size = newSize] = null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
