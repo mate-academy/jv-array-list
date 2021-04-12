147d47437f8036bd54382b3d26d993550cdd66f5
SerKisJD <sergej.kislyakov@gmail.com>
2021-04-12 13:47:56 +0300

    2 renamed field, remade method 'add'

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,25 +8,27 @@ public class ArrayList<T> implements List<T> {
     private static final String INVALID_INDEX = "Invalid index!";
     private static final String INDEX_NOT_EXIST = "Index doesn't exist!";
     private int size;
-    private T[] array;
+    private T[] elements;
 
     public ArrayList() {
-        array = (T[]) new Object[DEFAULT_CAPACITY];
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
         changeArrayCapacity();
-        array[size] = value;
+        elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
         changeArrayCapacity();
-        indexExisting(index);
-        System.arraycopy(array, index, array, index + 1, size - index);
-        array[index] = value;
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_NOT_EXIST);
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
         size++;
     }
 
@@ -40,20 +42,20 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        return array[index];
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
         checkIndex(index);
-        array[index] = value;
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
         checkIndex(index);
-        T deletedIndex = array[index];
-        System.arraycopy(array, index + 1, array, index, size - index);
+        T deletedIndex = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index);
         size--;
         return deletedIndex;
     }
@@ -61,7 +63,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (array[i] != null && array[i].equals(element) || array[i] == element) {
+            if (elements[i] != null && elements[i].equals(element) || elements[i] == element) {
                 return remove(i);
             }
         }
@@ -85,16 +87,10 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void changeArrayCapacity() {
-        if (array.length == size) {
+        if (elements.length == size) {
             T[] resizedArray = (T[]) new Object[size + (size / 2)];
-            System.arraycopy(array, 0, resizedArray, 0, size);
-            array = resizedArray;
-        }
-    }
-
-    private void indexExisting(int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_NOT_EXIST);
+            System.arraycopy(elements, 0, resizedArray, 0, size);
+            elements = resizedArray;
         }
     }
 }
