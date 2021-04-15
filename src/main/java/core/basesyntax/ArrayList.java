2610a187262aa4486862281ddce4a2153149033d
DeStinyShell <dmitriy4ever97@gmail.com>
2021-04-15 21:51:32 +0300

    Test

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -7,6 +7,7 @@ public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_ARRAY_CAPACITY = 10;
     private T[] objectsArray = (T[]) new Object[]{};
     private int size;
+
     @Override
     public void add(T value) {
         add(value, size);
