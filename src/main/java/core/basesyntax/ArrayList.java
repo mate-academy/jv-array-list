1003b9236a624ab57e306913f1b66efd0eef4a36
mbelm <mbelmasg@gmail.com>
2021-05-23 15:49:51 +0300

    Solved

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,49 +1,99 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private int size = 0;
+    private T[] array = (T[]) new Object[10];
 
     @Override
     public void add(T value) {
-
+        if (size + 1 > array.length - 1) {
+            array = grow();
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            add(value);
+        } else if (index < size && index >= 0) {
+            if (size + 1 > array.length - 1) {
+                array = grow();
+            }
+            System.arraycopy(array, index, array, index + 1, size - index);
+            array[index] = value;
+            size++;
+        } else {
+            throw new core.basesyntax.ArrayListIndexOutOfBoundsException("Wrong index");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < size && index >= 0) {
+            return array[index];
+        } else {
+            throw new core.basesyntax.ArrayListIndexOutOfBoundsException("Wrong index!");
+        }
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < size && index >= 0) {
+            array[index] = value;
+        } else {
+            throw new core.basesyntax.ArrayListIndexOutOfBoundsException("Wrong index!");
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < size && index >= 0) {
+            T result = array[index];
+            System.arraycopy(array, index + 1, array, index, size - index);
+            size--;
+            return result;
+        } else {
+            throw new core.basesyntax.ArrayListIndexOutOfBoundsException("Wrong index!");
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int index = 0; index < size; index++) {
+            if (Objects.equals(array[index], element)) {
+                System.arraycopy(array, index + 1, array, index, size - index);
+                size--;
+                return element;
+            }
+        }
+        throw new NoSuchElementException("NoSuchElement");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private T[] grow() {
+        return Arrays.copyOf(array,array.length
+                + (array.length >> 2));
     }
 }
