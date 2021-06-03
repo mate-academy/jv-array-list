752bd564d1b952c3d5c3d14a761f46fd2d0f5b56
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-06-03 19:37:27 +0300

    list - array for objects; arraySize - how mane points of array is not equals null; methods: add(value) - for add a value in to last free points of array list; add(value, index) - for add a value in to index position of array list; addAll (list) - for add list in to end main list; get(index) - returns a value from a index position in the list; set(value, index) -  change a value in index position; remove(index) - delete index position adn return them value; remove(value) - delete and return value, if list has not a value return exception; size() - return amount points was used in list; isEmpty() - return value type boolean, and said list used or no; sizeControl - for change size list if it has not free positions; indexError(index) - return exception if list has not index position;

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,49 +1,112 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private int startLength = 10;
+    private T[] list = (T[]) (new Object[startLength]);
+    private int arraySize = 0;
 
     @Override
     public void add(T value) {
-
+        list[arraySize] = value;
+        arraySize++;
+        sizeControl();
+        return;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index <= size() && index >= 0) {
+            T temp = list[index];
+            list[index] = value;
+            for (int i = (index + 1); i < list.length; i++) {
+                value = list[i];
+                list[i] = temp;
+                temp = value;
+            }
+            arraySize++;
+            sizeControl();
+        } else {
+            indexError(index);
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexError(index);
+        return list[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        indexError(index);
+        list[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexError(index);
+        T removeValue = list[index];
+        for (int i = index; i < (list.length - 1); i++) {
+            list[i] = list[i + 1];
+        }
+        arraySize--;
+        return removeValue;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T value) {
+        for (int i = 0; i < arraySize; i++) {
+            if (value != null && value.equals(list[i])) {
+                T removeValue = list[i];
+                remove(i);
+                return removeValue;
+            }
+            if (value == null && value == list[i]) {
+                remove(i);
+                return null;
+            }
+        }
+        throw new NoSuchElementException("No found!!!");
     }
 
     @Override
     public int size() {
-        return 0;
+        return arraySize;
     }
 
     @Override
     public boolean isEmpty() {
+        if (arraySize == 0) {
+            return true;
+        }
         return false;
+    }
+
+    private void sizeControl() {
+        if (arraySize == list.length) {
+            T[] temp = (T[]) new Object[list.length + list.length / 2];
+            for (int i = 0; i < list.length; i++) {
+                temp[i] = list[i];
+            }
+            list = temp;
+        }
+    }
+
+    private void indexError(int index) {
+        if (index >= arraySize) {
+            throw new ArrayListIndexOutOfBoundsException("Index error!!!");
+        }
+        if (index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is negative!!!");
+        }
     }
 }
