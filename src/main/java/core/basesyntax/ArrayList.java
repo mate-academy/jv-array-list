eb21cdf8d729343a5a9282f36d6e75066467a5c8
Yura Lug <LUV97@ukr.net>
2021-06-11 12:41:50 +0300

    fix some things

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final int ZERO_INDEX = 0;
-    private static final int USUALLY_SIZE = 1;
+    private static final int USUAL_SIZE = 1;
     private int size;
     private Object[] elementData;
 
@@ -13,32 +13,9 @@ public class ArrayList<T> implements List<T> {
         elementData = new Object[DEFAULT_CAPACITY];
     }
 
-    private void checkSize(int length) {
-        int capacity = elementData.length;
-        while (length + size > capacity) {
-            capacity = (int) (capacity * 1.5);
-            Object [] inheritElementData = new Object[capacity];
-            System.arraycopy(elementData, 0, inheritElementData, 0, size);
-            elementData = new Object[capacity];
-            elementData = inheritElementData;
-        }
-    }
-
-    private void checkIndex(int index) {
-        if (index >= size || index < ZERO_INDEX) {
-            throw new ArrayListIndexOutOfBoundsException("Undefined index");
-        }
-    }
-
-    private void checkAddIndex(int index) {
-        if (index > size || index < ZERO_INDEX) {
-            throw new ArrayListIndexOutOfBoundsException("Undefined index");
-        }
-    }
-
     @Override
     public void add(T value) {
-        checkSize(USUALLY_SIZE);
+        checkSize(USUAL_SIZE);
         elementData[size] = value;
         size++;
     }
@@ -46,19 +23,14 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         checkAddIndex(index);
-        checkSize(USUALLY_SIZE);
-        if (index == size) {
-            elementData[size] = value;
-        } else if ((index < size) && (index >= ZERO_INDEX)) {
-            System.arraycopy(elementData, index, elementData, index + 1, size - index);
-            elementData[index] = value;
-        }
+        checkSize(USUAL_SIZE);
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        checkSize(list.size());
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
@@ -88,8 +60,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if ((elementData[i] == element) || ((elementData[i] != null)
-                    && (elementData[i].equals(element)))) {
+            if (elementData[i] == element || (elementData[i] != null
+                    && elementData[i].equals(element))) {
                 remove(i);
                 return element;
             }
@@ -107,5 +79,27 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void checkSize(int length) {
+        int capacity = elementData.length;
+        while (length + size > capacity) {
+            capacity = (int) (capacity * 1.5);
+            Object [] inheritElementData = new Object[capacity];
+            System.arraycopy(elementData, 0, inheritElementData, 0, size);
+            elementData = inheritElementData;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < ZERO_INDEX) {
+            throw new ArrayListIndexOutOfBoundsException("Undefined index");
+        }
+    }
+
+    private void checkAddIndex(int index) {
+        if (index > size || index < ZERO_INDEX) {
+            throw new ArrayListIndexOutOfBoundsException("Undefined index");
+        }
     }
 }
