d4c37487c1dde10ae5a5142b021cc5b287af2348
Vladyslav Gural <vladgural@gmail.com>
2021-08-26 13:12:38 +0300

    changed ternary operator to logical operator

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -55,8 +55,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (elements[i] == null ? element == null
-                    : elements[i].equals(element)) {
+            if (elements[i] == element
+                    || (elements[i] != null && elements[i].equals(element))) {
                 return remove(i);
             }
         }
