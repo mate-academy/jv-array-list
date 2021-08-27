a8ee7598e6750979536460d745d629b4f6b78e0a
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-08-27 20:39:44 +0300

    initial commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,24 +1,60 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_SIZE = 10;
+    private boolean arrayIsEmpty = true;
+    private T[] arrayData;
+    private int size;
+    private int currentSizeOfArray = INITIAL_SIZE;
+
+    @SuppressWarnings({"unchecked"})
+    public ArrayList() {
+        arrayData = (T[]) new Object[INITIAL_SIZE];
+        currentSizeOfArray = INITIAL_SIZE;
+    }
+
     @Override
     public void add(T value) {
+        if (checkForEmpty(value)) {
+            if (size < currentSizeOfArray - 1) {
+                arrayData[size] = value;
+            }
+        }
+    }
 
+    private boolean checkForEmpty(T value) {
+        if (arrayIsEmpty) {
+            arrayData[0] = value;
+            size++;
+            arrayIsEmpty = false;
+           return false;
+        }
+        return true;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (checkForEmpty(value)) {
+            if (index < currentSizeOfArray
+            && index == size) {
+                arrayData[index] = value;
+            }
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
 
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(index < size) {
+            return arrayData[index];
+        }
+        throw new ArrayListIndexOutOfBoundsException("element are not exist, please input right index");
     }
 
     @Override
@@ -38,11 +74,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size <= 0;
     }
 }
