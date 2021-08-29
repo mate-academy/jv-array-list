54ccd17c226b06bb94ae319985edf0f4dde5b0ca
Serhii Orlov <orlov30@gmail.com>
2021-08-29 20:18:26 +0300

    delete repeat code, rename a few variables, implement a new method - resize();

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,11 +4,10 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int INITIAL_CAPACITY = 10;
-    private int current_capacity = INITIAL_CAPACITY;
+    private static final double GROW_COEFFICIENT = 1.5;
+    private int currentCapacity = INITIAL_CAPACITY;
     private T[] elementData;
     private int size;
-    private static final double GROW_COEFICIENT = 1.5;
-
 
     public ArrayList() {
         elementData = (T[]) new Object[INITIAL_CAPACITY];
@@ -16,12 +15,8 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (size == current_capacity) {
-            current_capacity *= GROW_COEFICIENT;
-            T[] temporyArray = (T[]) new Object[size];
-            System.arraycopy(elementData, 0, temporyArray, 0, size);
-            elementData = (T[]) new Object[current_capacity];
-            System.arraycopy(temporyArray, 0, elementData, 0, size);
+        if (size == currentCapacity) {
+            resize();
         }
         elementData[size] = value;
         size++;
@@ -29,82 +24,79 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        T[] temporyArray = (T[]) new Object[size];
-        int temporyIndex = 0;
-        System.arraycopy(elementData, 0, temporyArray, 0, size);
-        if (size == current_capacity) {
-            current_capacity *= GROW_COEFICIENT;
-            T[] localTemporyArray = (T[]) new Object[size];
-            System.arraycopy(elementData, 0, localTemporyArray, 0, size);
-            elementData = (T[]) new Object[current_capacity];
-            System.arraycopy(localTemporyArray, 0, elementData, 0, size);
+        T[] temporaryArray = (T[]) new Object[size];
+        System.arraycopy(elementData, 0, temporaryArray, 0, size);
+        if (size == currentCapacity) {
+            resize();
         }
-//        if (size >= index && index >= 0) {
-//            temporyIndex = index;
-//        } else {
-//
-//        }
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index");
-        }
+        isIndexValid(index, "ADD");
         elementData[index] = value;
-        System.arraycopy(temporyArray, index, elementData, index + 1,
+        System.arraycopy(temporaryArray,
+                index,
+                elementData,
+                index + 1,
                 size - index);
-        //elementData[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        T[] temporyArray = (T[]) new Object[list.size()];
-        for (int i = 0; i < temporyArray.length; i++) {
-            temporyArray[i] = list.get(i);
+        T[] temporaryArray = (T[]) new Object[list.size()];
+        for (int i = 0; i < temporaryArray.length; i++) {
+            temporaryArray[i] = list.get(i);
         }
-        System.arraycopy(temporyArray, 0, elementData, size, list.size());
+        System.arraycopy(temporaryArray,
+                0,
+                elementData,
+                size,
+                list.size());
         size += list.size();
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index");
-        }
+        isIndexValid(index, "noADD");
         return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index");
-        }
+        isIndexValid(index, "noADD");
         elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (size <= index || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index");
-        }
+        isIndexValid(index, "noADD");
         T oldRecord = elementData[index];
-        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
+        System.arraycopy(elementData,
+                index + 1,
+                elementData,
+                index,
+                size - index - 1);
         size--;
         return oldRecord;
     }
 
     @Override
     public T remove(T element) {
-        int temporyIndex = -1;
+        int temporaryIndex = -1;
         T oldRecord = (T) new Object();
         for (int i = 0; i < size; i++) {
-            if (elementData[i] == element || (elementData[i] != null && elementData[i].equals(element))) {
-                temporyIndex = i;
+            if (elementData[i] == element
+                    || (elementData[i] != null
+                    && elementData[i].equals(element))) {
+                temporaryIndex = i;
                 oldRecord = elementData[i];
                 break;
             }
         }
-        if (temporyIndex != -1) {
-            System.arraycopy(elementData, temporyIndex + 1, elementData,
-                    temporyIndex, size - temporyIndex - 1);
+        if (temporaryIndex != -1) {
+            System.arraycopy(elementData,
+                    temporaryIndex + 1,
+                    elementData,
+                    temporaryIndex,
+                    size - temporaryIndex - 1);
             size--;
             return oldRecord;
         }
@@ -119,5 +111,20 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void resize() {
+        currentCapacity *= GROW_COEFFICIENT;
+        T[] temporaryArray = (T[]) new Object[size];
+        System.arraycopy(elementData, 0, temporaryArray, 0, size);
+        elementData = (T[]) new Object[currentCapacity];
+        System.arraycopy(temporaryArray, 0, elementData, 0, size);
+    }
+
+    private void isIndexValid(int index, String operation) {
+        if ((operation.equals("ADD") && (index > size || index < 0))
+                || (operation.equals("noADD") && (index < 0 || index >= size))) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index");
+        }
     }
 }
