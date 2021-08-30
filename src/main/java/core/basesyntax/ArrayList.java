927460b80cfbd20829ef1726a930b7feb85cb616
Sasha Hordenin <sg743148@gmail.com>
2021-08-30 17:24:09 +0300

    added all required changes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,61 +4,43 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private T[] array;
+    private T[] values;
     private int size;
-    private int minCapacity;
 
     public ArrayList() {
-        this.array = (T[]) new Object[DEFAULT_CAPACITY];
-        this.size = 0;
-        this.minCapacity = DEFAULT_CAPACITY;
+        values = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        this.array = grow(minCapacity);
-        array[size] = value;
+        grow();
+        values[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndexForAddMethod(index);
-        if (index + 1 <= minCapacity) {
-            this.array = grow(minCapacity);
-            T[] newArray = grow(minCapacity);
-            System.arraycopy(this.array, 0, newArray, 0, index);
-            newArray[index] = value;
-            System.arraycopy(this.array, index, newArray, index + 1, minCapacity - index - 1);
-            copyArray(newArray, this.array);
-            size++;
-        } else {
-            add(value);
-        }
-    }
-
-    private T[] grow(int minCapacity) {
-        T[] arrayWithNewCapacity = (T[]) new Object[getNewCapacity(minCapacity)];
-        copyArray(this.array, arrayWithNewCapacity);
-        return arrayWithNewCapacity;
-    }
-
-    private int getNewCapacity(int minCapacity) {
-        if (size + 1 > array.length) {
-            return this.minCapacity = minCapacity + (minCapacity >> 1);
-        }
-        return minCapacity;
-    }
-
-    private void checkIndexForAddMethod(int index) {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Index out of bonds exception for index "
                     + index);
         }
+        grow();
+        System.arraycopy(values, index, values, index + 1, size - index);
+        values[index] = value;
+        size++;
     }
 
-    private void copyArray(T[] arrayCopyFrom, T[] arrayCopyTo) {
-        System.arraycopy(arrayCopyFrom, 0, arrayCopyTo, 0, this.array.length);
+    private void grow() {
+        T[] arrayWithNewCapacity = (T[]) new Object[getNewCapacity(values.length)];
+        System.arraycopy(values, 0, arrayWithNewCapacity, 0, values.length);
+        values = arrayWithNewCapacity;
+    }
+
+    private int getNewCapacity(int currentCapacity) {
+        if (size + 1 > currentCapacity) {
+            return currentCapacity + (currentCapacity >> 1);
+        }
+        return currentCapacity;
     }
 
     @Override
@@ -71,29 +53,29 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         checkIndexForRemoveGetSetMethods(index);
-        return array[index];
+        return values[index];
     }
 
     @Override
     public void set(T value, int index) {
         checkIndexForRemoveGetSetMethods(index);
-        array[index] = value;
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
         checkIndexForRemoveGetSetMethods(index);
-        T removedElement = array[index];
-        System.arraycopy(array, index + 1, array, index, minCapacity - index - 1);
+        T removedElement = values[index];
+        System.arraycopy(values, index + 1, values, index, values.length - index - 1);
         size--;
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < array.length; i++) {
-            if ((array[i] == null && element == null)
-                    || (array[i] != null && array[i].equals(element))) {
+        for (int i = 0; i < values.length; i++) {
+            if ((values[i] == element)
+                    || (values[i] != null && values[i].equals(element))) {
                 return remove(i);
             }
         }
