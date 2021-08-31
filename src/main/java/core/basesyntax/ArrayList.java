837258e4d43a5044c0f503d2cd056c9323743327
Vitalii Mykh <vitaliymykh@gmail.com>
2021-08-31 12:42:30 +0300

    rewrite code with "System.arraycopy", instead of my custom method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
     private static final Object[] EMPTY_ELEMENTDATA = {};
-    public Object [] elementData;
+    private Object [] elementData;
     private int size;
 
     public ArrayList(int initialCapacity) {
@@ -18,18 +18,18 @@ public class ArrayList<T> implements List<T> {
                     + initialCapacity);
         }
     }
+
     public ArrayList() {
-        this.elementData = new Object[10];
+        this.elementData = new Object[DEFAULT_CAPACITY];
     }
 
     private Object[] grow(int minCapacity) {
-        Object[] biggerArray = new Object[minCapacity];
-        for(int i = 0; i < biggerArray.length; i ++) {
+        Object[] biggerArray = new Object[(int)(minCapacity * 1.5)];
+        for (int i = 0; i < elementData.length; i++) {
             biggerArray[i] = elementData[i];
         }
         return biggerArray;
     }
-
 
     @Override
     public void add(T value) {
@@ -39,33 +39,20 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         int potentialSize = size + 1;
-        if(!(indexInBoundsCheck(potentialSize, elementData.length ))){
-            elementData = grow((int)(potentialSize * 1.5));
+        if (potentialSize > elementData.length) {
+            elementData = grow(elementData.length);
         }
-        Object[] temporaryArray = new Object[elementData.length];
-        if(index == size) {
-            elementData[index] = value;
-            size ++;
-        } else if (index < size && index >= 0) {
-            for(int i = 0; i< size; i++){
-                if(i< index){
-                    temporaryArray[i] = elementData[i];
-                }if(i == index) {
-                    temporaryArray[i] = value;
-                } else {
-                    temporaryArray[i] = elementData[i-1];
-                }
-            }
-            size++;
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("invalid index passed");
-        }
+        indexInBoundsCheck(index, potentialSize);
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
+
     }
 
     @Override
     public void addAll(List<T> list) {
-        if((elementData.length - size) <= list.size()) {
-            grow((int)(list.size() + elementData.length * 1.5));
+        if ((elementData.length - size) <= list.size()) {
+            grow(elementData.length);
         }
         int j = 0;
         for (int i = size; j < list.size(); i++) {
@@ -73,65 +60,37 @@ public class ArrayList<T> implements List<T> {
             j++;
             size++;
         }
-
     }
 
     @Override
     public T get(int index) {
-        if(indexInBoundsCheck(index,size)) {
-            return (T) elementData[index];
-        }
-        throw new ArrayListIndexOutOfBoundsException("Can't get element with this index, try again");
+        indexInBoundsCheck(index,size);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if(indexInBoundsCheck(index,size)) {
-            elementData[index] = value;
-        }
-        throw new ArrayListIndexOutOfBoundsException("Can't set element into this index, try again");
-    }
-
-    public boolean indexInBoundsCheck(int index, int bounds) {
-        if( index < 0 || index >= bounds || bounds < 0) {
-          return false;
-        }
-        return  true;
+        indexInBoundsCheck(index,size);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        Object [] fixedArray = new Object[elementData.length];
-        if(indexInBoundsCheck(index, size)) {
-            for( int i = 0; i < fixedArray.length - 1; i++) {
-                if (i < index) {
-                    fixedArray[i] = elementData[i];
-                }
-               /* else if ( index = fixedArray.length - 1) {
-                    fixedArray[i] =
-                } */
-                else {
-                        fixedArray[i] = elementData[i + 1];
-                }
-            }
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("Can't remove element with this index, try again");
-        }
-        Object OldObject = elementData[index];
+        indexInBoundsCheck(index, size);
+        Object oldObject = elementData[index];
         size = size - 1;
-        elementData = fixedArray;
-        return (T) OldObject;
+        System.arraycopy(elementData, index + 1, elementData, index,size - index);
+        return (T) oldObject;
     }
 
     @Override
     public T remove(T element) {
-        int indexOfElement;
-            for( int i = 0; i < elementData.length; i++) {
-                if(elementData[i] == element || elementData[i] != null && elementData[i].equals(element)) {
-                    indexOfElement = i;
-                    remove(indexOfElement);
-                }
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == element || elementData[i] != null
+                    && elementData[i].equals(element)) {
+                return remove(i);
             }
+        }
         throw new NoSuchElementException("There is no such element in list, check again");
     }
 
@@ -143,5 +102,11 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    public void indexInBoundsCheck(int index, int bounds) {
+        if (index < 0 || index >= bounds || bounds < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Unreacheble index, please check again");
+        }
     }
 }
