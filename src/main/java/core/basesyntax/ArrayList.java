f567a5cd6a480dac98ce73e2ccff3a5fd8a6a20c
pustoj0 <pustoj0@gmail.com>
2021-08-31 10:31:30 +0300

    clean redundant imports

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
