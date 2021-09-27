eb985be57e24290e3b84ab80c4fae97b5775c2f0
ASelivonchyk <aselivonchyk@gmail.com>
2021-09-27 11:23:59 +0300

    Wrote an assignment jv-array-list. Override the methods of ArrayList class.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,93 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_ARRAY_SIZE = 10;
+    private Object[] array = new Object[INITIAL_ARRAY_SIZE];
+    private int size = 0;
+    
     @Override
     public void add(T value) {
-
+        resizeArray();
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size < index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Illegal index value. "
+                    + "No such index or index less than 0");
+        }
+        resizeArray();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        verificationOfIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        verificationOfIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        verificationOfIndex(index);
+        resizeArray();
+        T result = (T) array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return result;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == element || array[i] != null
+                    && array[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("No such element in array");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public void resizeArray() {
+        if (size == array.length - 1) {
+            array = Arrays.copyOf(array, array.length + 10);
+        }
+    }
+
+    public void verificationOfIndex(int index) {
+        if (size <= index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Illegal index value. "
+                    + "No such index or index less than 0");
+        }
     }
 }
+
