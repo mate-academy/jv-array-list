e4e61f1d73f76eee11b405fdc1af722e6fab80ac
Mykola Skrypka <mykolaskrypka@gmail.com>
2021-10-03 21:11:34 +0300

    the required methods were override

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,102 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] list = (T[]) new Object[DEFAULT_CAPACITY];
+    private int capacity = DEFAULT_CAPACITY;
+    private int size = 0;
+
+    private void grow() {
+        int oldCapacity = capacity;
+        capacity = (int) (capacity * 1.5);
+        T[] growList = (T[]) new Object[capacity];
+        System.arraycopy(list, 0, growList, 0, oldCapacity);
+        list = growList;
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == capacity) {
+            grow();
+        }
+        list[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index to add value!");
+        }
+        if (capacity == size) {
+            grow();
+        }
+        System.arraycopy(list, index, list, index + 1, size - index);
+        list[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        while (size + list.size() > capacity) {
+            grow();
+        }
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index > size - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index to get value!");
+        }
+        return list[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index > size - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index to remove value!");
+        }
+        list[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T removedElement;
+        if (index > size - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index to remove value!");
+        }
+        removedElement = list[index];
+        System.arraycopy(list, index + 1, list, index,size - 1 - index);
+        list[size - 1] = null;
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int ind = 0; ind < size; ind++) {
+            T el = list[ind];
+            if (el == element || (el != null && el.equals(element))) {
+                return remove(ind);
+            }
+        }
+        throw new NoSuchElementException("Wrong index to remove value!");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
+
 }
