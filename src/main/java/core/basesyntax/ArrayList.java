8db07d9d8b56fadec8dd9391e82f57fd4ff08cd1
LinkClink <shekaqwe@gmail.com>
2022-01-25 17:57:58 +0200

    optimisation code

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,13 +6,16 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int MAX_SIZE = 10;
     private int size;
-    private T[] values = (T[]) new Object[MAX_SIZE];
+    private T[] values;
+
+    public ArrayList() {
+        values = (T[]) new Object[MAX_SIZE];
+    }
 
     @Override
     public void add(T value) {
         resize();
-        values[size] = value;
-        size++;
+        values[size++] = value;
     }
 
     @Override
@@ -21,7 +24,7 @@ public class ArrayList<T> implements List<T> {
             resize();
             System.arraycopy(values, index, values, index + 1, size - index);
             values[index] = value;
-            ++size;
+            size++;
         } else {
             throw new ArrayListIndexOutOfBoundsException("Sorry received bad index");
         }
@@ -36,7 +39,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index < size & index >= 0) {
+        if (checkIndexIsCorrect(index)) {
             return values[index];
         } else {
             throw new ArrayListIndexOutOfBoundsException("Sorry received bad index");
@@ -45,7 +48,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void set(T value, int index) {
-        if (index < size & index >= 0) {
+        if (checkIndexIsCorrect(index)) {
             values[index] = value;
         } else {
             throw new ArrayListIndexOutOfBoundsException("Sorry this index is not exit");
@@ -54,11 +57,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        if (index < size & index >= 0) {
-            T obj = values[index];
+        if (checkIndexIsCorrect(index)) {
+            T objectRemoved = values[index];
             System.arraycopy(values, index + 1, values, index, size - index - 1);
             values[size--] = null;
-            return obj;
+            return objectRemoved;
         } else {
             throw new ArrayListIndexOutOfBoundsException("Sorry this index is not exit");
         }
@@ -66,24 +69,15 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        int index = 0;
-        int flag = 0;
-        T obj = (T) new Object();
         for (int i = 0; i < size; i++) {
             if (values[i] == element || values[i] != null && values[i].equals(element)) {
-                index = i;
-                flag = 1;
-                obj = values[i];
+                T objectRemoved = values[i];
+                System.arraycopy(values, i + 1, values, i, size - i - 1);
+                values[size--] = null;
+                return objectRemoved;
             }
         }
-        if (flag == 1) {
-            System.arraycopy(values, index + 1, values, index, size - index - 1);
-            size--;
-            values[size] = null;
-        } else {
-            throw new NoSuchElementException("Sorry this value does not exit");
-        }
-        return obj;
+        throw new NoSuchElementException("Sorry this value does not exit");
     }
 
     @Override
@@ -98,5 +92,9 @@ public class ArrayList<T> implements List<T> {
 
     private void resize() {
         values = Arrays.copyOf(values, values.length + 10);
+    }
+
+    private boolean checkIndexIsCorrect(int index) {
+        return index < size & index >= 0;
     }
 }
