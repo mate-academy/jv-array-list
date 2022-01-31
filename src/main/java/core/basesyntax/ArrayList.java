6cd7086edf653b5a490490f1561c6eb88274ddf2
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-31 14:39:49 +0200

    added fields "elementData", "DEFAULT_CAPACITY", "size" and created an empty constructor

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,13 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
     @Override
     public void add(T value) {
 
