3dab44c92f6d465319b2a56eba80ffe9e24a346c
Sergii Turchyn <turchyn13@gmail.com>
2022-04-15 23:18:50 +0300

    partly made methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,16 +1,46 @@
 package core.basesyntax;
 
+import java.sql.Struct;
+import java.util.Arrays;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] tempArray;
+    private int size;
+
+    public ArrayList() {
+        tempArray = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == tempArray.length) {
+            grow();
+        }
+        tempArray[size] = value;
+        size++;
     }
 
+    private void grow () {
+        int newLength = (int) (tempArray.length * 1.5);
+        T[] newTempArray = (T[]) new Object[newLength];
+        System.arraycopy(tempArray,0, newTempArray, 0, size);
+        tempArray = newTempArray;
+    }
+
+    // TODO check
     @Override
     public void add(T value, int index) {
-
+        IndexCheck(index + 1);   // need to be corrected, because index=-1  --- doesn't work correctly
+        if (size == tempArray.length) {
+            grow();
+        }
+        System.arraycopy(tempArray, index, tempArray, (index + 1), (size - index));
+        tempArray[index] = value;
+        size++;
     }
 
+    // TODO
     @Override
     public void addAll(List<T> list) {
 
@@ -18,19 +48,33 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        IndexCheck(index);
+        return tempArray[index];
     }
 
+    // TODO check
     @Override
     public void set(T value, int index) {
-
+        IndexCheck(index);
+        tempArray[index] = value;
+        size++;
     }
 
+    // TODO
     @Override
     public T remove(int index) {
+        IndexCheck(index);
+
         return null;
     }
 
+    private void IndexCheck(int index) {
+        if (index >= 0 && index < size) {
+            return;
+        } throw new ArrayListIndexOutOfBoundsException("Wrong index");
+    }
+
+    // TODO
     @Override
     public T remove(T element) {
         return null;
@@ -38,11 +82,18 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayList{" +
+                "tempArray=" + Arrays.toString(tempArray) +
+                '}';
     }
 }
