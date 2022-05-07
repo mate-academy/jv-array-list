57d508f1c498587ec3e7d56595e72a221465d3f3
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-08 00:43:10 +0300

    changed according to the comments

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,34 +4,34 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private static final int START_SIZE = 10;
-    private static final double INCREMENT = 1.5;
-    private Object[] baseObjects;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROWTH_COEFFICIENT = 1.5;
+    private Object[] elemets;
     private int size;
 
     public ArrayList() {
-        baseObjects = new Object[START_SIZE];
+        elemets = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == baseObjects.length) {
+        if (size == elemets.length) {
             resize();
         }
-        baseObjects[size] = value;
+        elemets[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
         if (index != 0) {
-            isInRange(index - 1, "Cannot use add for index ");
+            isInRange(index - 1);
         }
-        if (size == baseObjects.length) {
+        if (size == elemets.length) {
             resize();
         }
-        System.arraycopy(baseObjects, index, baseObjects, index + 1, size - index);
-        baseObjects[index] = value;
+        System.arraycopy(elemets, index, elemets, index + 1, size - index);
+        elemets[index] = value;
         size++;
     }
 
@@ -44,21 +44,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        isInRange(index, "Cannot use get for index ");
-        return (T) baseObjects[index];
+        isInRange(index);
+        return (T) elemets[index];
     }
 
     @Override
     public void set(T value, int index) {
-        isInRange(index, "Cannot use set for index ");
-        baseObjects[index] = value;
+        isInRange(index);
+        elemets[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        isInRange(index, "Cannot use remove for index ");
-        T element = (T) baseObjects[index];
-        System.arraycopy(baseObjects, index + 1, baseObjects, index, size - index - 1);
+        isInRange(index);
+        T element = (T) elemets[index];
+        System.arraycopy(elemets, index + 1, elemets, index, size - index - 1);
         size--;
         return element;
     }
@@ -66,8 +66,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(baseObjects[i], element)) {
-                System.arraycopy(baseObjects, i + 1, baseObjects, i, size - i - 1);
+            if (Objects.equals(elemets[i], element)) {
+                System.arraycopy(elemets, i + 1, elemets, i, size - i - 1);
                 size--;
                 return element;
             }
@@ -85,16 +85,16 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void isInRange(int index, String message) {
+    private void isInRange(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(message + index);
+            throw new ArrayListIndexOutOfBoundsException("Invalid index " + index);
         }
     }
 
     private void resize() {
-        int newCapacity = (int) (baseObjects.length * INCREMENT);
-        Object[] newObjects = new Object[newCapacity];
-        System.arraycopy(baseObjects, 0, newObjects, 0, baseObjects.length);
-        baseObjects = newObjects;
+        int capacity = (int) (elemets.length * GROWTH_COEFFICIENT);
+        Object[] objects = new Object[capacity];
+        System.arraycopy(elemets, 0, objects, 0, elemets.length);
+        elemets = objects;
     }
 }
