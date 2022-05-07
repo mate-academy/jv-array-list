2d8896aa9a127e91d872591057446d2808bbc420
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-05-07 20:10:11 +0300

    logic simplified, arraycopy used and redundant variables deleted

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,18 +1,11 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_CAPACITY = 10;
-    private Object[] arrayList = new Object[DEFAULT_ARRAY_SIZE];
+    private static final int DEFAULT_ARRAY_SIZE = 10;
+    private Object[] elements = new Object[DEFAULT_ARRAY_SIZE];
     private int size;
-
-    public void checkIndexAdd(int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index");
-        }
-    }
 
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
@@ -21,71 +14,70 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void resize() {
-        if (arrayList.length == size) {
-            arrayList = Arrays.copyOf(arrayList, (int) (size * 1.5));
+        if (elements.length == size) {
+            Object[] dummyList = new Object[(int) (size * 1.5)];
+            System.arraycopy(elements, 0, dummyList, 0, elements.length);
+            elements = dummyList;
         }
     }
 
     @Override
     public void add(T value) {
         resize();
-        arrayList[size] = value;
+        elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndexAdd(index);
-        for (int i = size; i > index; i--) {
-            resize();
-            arrayList[i] = arrayList[i - 1];
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index");
         }
-        arrayList[index] = value;
+        resize();
+        System.arraycopy(elements, index, elements, index + 1, elements.length - index - 1);
+        resize();
+        elements[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            arrayList[size] = list.get(i);
-            size++;
+            ArrayList.this.add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        checkIndexRemoveSet(index);
-        return (T) arrayList[index];
+        checkIndex(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndexRemoveSet(index);
-        arrayList[index] = value;
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndexRemoveSet(index);
-        T popped = (T) arrayList[index];
+        checkIndex(index);
         if (index == size - 1) {
-            arrayList[index] = null;
+            T result = (T) elements[index];
+            elements[index] = null;
             size--;
-            return popped;
+            return result;
         }
-        for (int i = index; i < size; i++) {
-            arrayList[i] = arrayList[i + 1];
-        }
-        arrayList[size] = null;
+        T result = (T) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index);
         size--;
-        return popped;
+        return result;
     }
 
     @Override
     public T remove(T element) {
-        T popped = null;
         for (int i = 0; i < size; i++) {
-            T currentObject = (T) arrayList[i];
+            T currentObject = (T) elements[i];
             if (currentObject == element
                     || (currentObject != null
                     && element != null
@@ -93,10 +85,7 @@ public class ArrayList<T> implements List<T> {
                 return remove(i);
             }
         }
-        if (popped == null) {
-            throw new NoSuchElementException("Element is not in the List");
-        }
-        return popped;
+        throw new NoSuchElementException("Element is not in the List");
     }
 
     @Override
