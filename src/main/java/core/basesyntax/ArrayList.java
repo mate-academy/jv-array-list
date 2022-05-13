3010b60cdce31858b766820a6e83cace98dd3b5a
Evhenii Vaskiv <evgenijvaskiv@gmail.com>
2022-05-13 17:07:48 +0300

    methods simplified, unnecessary variable removed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,102 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elements;
+    private int size;
+
+    public ArrayList() {
+        this.elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        grow();
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index should not be "
+                    + "bigger than size");
+        }
+        grow();
+        System.arraycopy(elements, index,
+                elements, index + 1,
+                size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if ((elements.length - size) < list.size()) {
+            grow();
+        }
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        exceptionBlock(index);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        exceptionBlock(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        exceptionBlock(index);
+        T oldValue = elements[index];
+        System.arraycopy(elements,index + 1, elements, index,
+                elements.length - (index + 1));
+        size--;
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element == elements[i] || (element != null && element.equals(elements[i]))) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no such element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        if (size == elements.length) {
+            int newCapacity = elements.length + (elements.length >> 1);
+            T[] newElements = (T[]) new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+    }
+
+    private void exceptionBlock(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: "
+                    + index + " size: " + size);
+        }
     }
 }
