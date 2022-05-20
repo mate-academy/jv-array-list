e7094fae6b0339d785692c9ef40dfb6bb350165b
gdp <gdp@zigzag.ints.net>
2022-05-20 15:48:35 +0300

    implement task
    - add implementation of interface List in class ArrayList;
    - data stores in array Object[] elementData;
    - field size has information about size;
    - add implementation of public methods in class ArrayList: add(), addAll(), get(), set(), remove(), size(), isEmpty()

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,133 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int GROW_BY = 2;
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            elementData = grow();
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        rangeCheckForAdd(index);
+        if (size == elementData.length) {
+            elementData = grow();
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (list == null || list.isEmpty()) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T get(int index) {
-        return null;
+        checkIndexInArray(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexInArray(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexInArray(index);
+        T oldValue = get(index);
+        fastRemove(elementData, index);
+        size--;
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index;
+        if ((index = findByValue(element)) == -1) {
+            throw new NoSuchElementException("Element not found: " + element);
+        }
+        return remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                s.append(", ");
+            }
+            s.append(elementData[i]);
+        }
+        return String.format("[%s] (%d)", s, size());
+    }
+
+    private Object[] grow() {
+        int newCapacity = elementData.length * GROW_BY;
+        return Arrays.copyOf(elementData, newCapacity);
+    }
+
+    private void fastRemove(Object[] data, int index) {
+        final int newSize;
+        if ((newSize = size - 1) > index) {
+            System.arraycopy(data, index + 1, data, index, newSize - index);
+        }
+        data[newSize] = null;
+    }
+
+    private void checkIndexInArray(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range: " + index);
+        }
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range: " + index);
+        }
+    }
+
+    private int findByValue(T value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(value, elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
