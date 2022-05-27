09f55b097cf586962df44aa823ae139baddf08ae
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-27 15:20:24 +0300

    Fix some part of mistakes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -2,21 +2,16 @@ package core.basesyntax;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
+@SuppressWarnings("unchecked")
 public class ArrayList<T> implements List<T> {
-    private static final int SOFT_MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
     private static final int DEFAULT_CAPACITY = 10;
-    private static final Object[] DEFAULT_ELEMENTS = {};
     private Object[] elements;
     private int size;
 
     public ArrayList() {
-        this.elements = new Object[DEFAULT_CAPACITY];
-    }
-
-    @SuppressWarnings("unchecked")
-    T elements(int index) {
-        return (T) elements[index];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
@@ -26,7 +21,12 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index, size);
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index passed to the method is negative or "
+                            + "bigger than size of ArrayList. "
+                            + "Index: " + index + ", Size: " + size);
+        }
         int s;
         Object[] elementData;
         if ((s = size) == (elementData = this.elements).length) {
@@ -44,33 +44,29 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        if (list == null) {
-            return;
-        }
-        for (int i = 0; i < ((ArrayList<T>) list).size; i++) {
+        for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        checkIndex(index, (size == 0) ? 0 : (size - 1));
-        return elements(index);
+        checkIndex(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, (size == 0) ? 0 : (size - 1));
+        checkIndex(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index, (size == 0) ? 0 : (size - 1));
-        final Object[] es = elements;
-
-        @SuppressWarnings("unchecked") T oldValue = (T) es[index];
-        final int newSize;
+        checkIndex(index);
+        Object[] es = elements;
+        T oldValue = (T) es[index];
+        int newSize;
         if ((newSize = size - 1) > index) {
             System.arraycopy(es, index + 1, es, index, newSize - index);
         }
@@ -81,10 +77,9 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         Object[] es = elements;
-
-        for (int i = 0; i < this.size; i++) {
-
-            if (equal(element, (T) es[i])) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(element, (T) es[i])
+                    || (element != null && element.equals((T) es[i]))) {
                 remove(i);
                 return element;
             }
@@ -104,7 +99,7 @@ public class ArrayList<T> implements List<T> {
 
     private Object[] grow(int minCapacity) {
         int oldCapacity = elements.length;
-        if (oldCapacity > 0 || elements != DEFAULT_ELEMENTS) {
+        if (oldCapacity > 0 || !Arrays.equals(elements, new Object[0])) {
             int newCapacity = newLength(oldCapacity, minCapacity - oldCapacity,
                     oldCapacity >> 1);
             return elements = Arrays.copyOf(elements, newCapacity);
@@ -115,7 +110,7 @@ public class ArrayList<T> implements List<T> {
 
     private int newLength(int oldLength, int minGrowth, int prefGrowth) {
         int prefLength = oldLength + Math.max(minGrowth, prefGrowth);
-        if (0 < prefLength && prefLength <= SOFT_MAX_ARRAY_LENGTH) {
+        if (0 < prefLength && prefLength <= 2147483640) {
             return prefLength;
         } else {
             return hugeLength(oldLength, minGrowth);
@@ -129,27 +124,17 @@ public class ArrayList<T> implements List<T> {
                     "Required array length " + oldLength + " + "
                             + minGrowth + " is too large");
         } else {
-            return Math.max(minLength, SOFT_MAX_ARRAY_LENGTH);
+            return Math.max(minLength, 2147483640);
         }
     }
 
-    private void checkIndex(int index, int size) {
-        if (index > size || index < 0) {
+    private void checkIndex(int index) {
+        int localSize = (size == 0) ? 0 : (size - 1);
+        if (index > localSize || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
                     "Index passed to the method is negative or "
                             + "bigger than size of ArrayList. "
                             + "Index: " + index + ", Size: " + size);
         }
     }
-
-    private boolean equal(T element1, T element2) {
-        if (element1 == element2) {
-            return true;
-        }
-        if (element2 == null || element1 == null) {
-            return false;
-        }
-        return element1.equals(element2);
-    }
-
 }
