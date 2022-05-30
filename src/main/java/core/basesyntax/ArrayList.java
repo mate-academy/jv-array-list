d13636ac1dbec056df4beed85b1bbdfddf5ba896
Dima Podakin <calombo.podakin@ukr.net>
2022-05-30 21:11:05 +0300

    created ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,102 @@
 package core.basesyntax;
 
-public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
+public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int size = 0;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(T value) {
+        grow();
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Cannot add value "
+                    + value + " with index "
+                    + index);
+        }
+        grow();
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Cannot give value from index: " + index);
+        }
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Cannot set index: " + index);
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Cannot remove element by index: "
+                    + index);
+        }
+        T removedElement = (T) elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, --size - index);
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        return remove(findElement(element));
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private int findElement(T element) {
+        int remElement = -1;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elementData[i], element)) {
+                remElement = i;
+            }
+        }
+        if (remElement < 0) {
+            throw new NoSuchElementException("Cannot find element: " + element);
+        }
+        return remElement;
+    }
+
+    private void grow() {
+        if (elementData.length <= size) {
+            Object[] newElementsData = new Object[elementData.length + (elementData.length >> 1)];
+            System.arraycopy(elementData, 0, newElementsData, 0, elementData.length);
+            elementData = newElementsData;
+        }
     }
 }
