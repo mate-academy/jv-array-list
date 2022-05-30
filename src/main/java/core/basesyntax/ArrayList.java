a96696b255a8d8f9700a717fe1d757fcacff5e85
Andrey Chumak <chumakandrey7@gmail.com>
2022-05-30 12:44:34 +0300

    Deleted unnecessary duplicates of System.arraycopy methods Replaced private methods after public

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,17 +5,92 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private int size = 0;
-    private Object[] objArray;
+    private Object[] values;
 
     public ArrayList() {
-        objArray = new Object[DEFAULT_CAPACITY];
+        values = new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public void add(T value) {
+        add(value, size);
+    }
+
+    @Override
+    public void add(T value, int index) {
+        if (size == values.length) {
+            values = grow();
+        }
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (index < size) {
+            int countElementsForCopying = size - index;
+            System.arraycopy(values, index, values, index + 1, countElementsForCopying);
+            values[index] = value;
+            size++;
+            return;
+        }
+        values[index] = value;
+        size++;
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        Object[] fromList = toArray(list);
+        for (Object obj : fromList) {
+            add((T) obj);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return (T) values[index];
+    }
+
+    @Override
+    public void set(T value, int index) {
+        checkIndex(index);
+        values[index] = value;
+
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+        final T removedItem = (T) values[index];
+        int countElementsForCopying = size - index - 1;
+        System.arraycopy(values, index + 1, values, index, countElementsForCopying);
+        values[--size] = null;
+        return removedItem;
+    }
+
+    @Override
+    public T remove(T element) {
+        for (int i = 0; i < size; i++) {
+            if (element == values[i] || element != null && element.equals(values[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There no such element in array");
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     private Object[] grow() {
-        int oldCapacity = objArray.length;
+        int oldCapacity = values.length;
         int newCapacity = oldCapacity + (oldCapacity >> 1);
         Object[] grown = new Object[newCapacity];
-        System.arraycopy(objArray, 0, grown, 0, size);
+        System.arraycopy(values, 0, grown, 0, size);
         return grown;
     }
 
@@ -32,84 +107,5 @@ public class ArrayList<T> implements List<T> {
         if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-    }
-
-    @Override
-    public void add(T value) {
-        add(value, size);
-    }
-
-    @Override
-    public void add(T value, int index) {
-        if (size == objArray.length) {
-            objArray = grow();
-        }
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
-        if (index < size) {
-            int sizeOfTempArr = size - index;
-            Object[] temporary = new Object[sizeOfTempArr];
-            System.arraycopy(objArray, index, temporary, 0, sizeOfTempArr);
-            objArray[index] = value;
-            System.arraycopy(temporary, 0, objArray, index + 1, sizeOfTempArr);
-            size++;
-            return;
-        }
-        objArray[index] = value;
-        size++;
-    }
-
-    @Override
-    public void addAll(List<T> list) {
-        Object[] fromList = toArray(list);
-        for (Object obj : fromList) {
-            add((T) obj);
-        }
-    }
-
-    @Override
-    public T get(int index) {
-        checkIndex(index);
-        return (T) objArray[index];
-    }
-
-    @Override
-    public void set(T value, int index) {
-        checkIndex(index);
-        objArray[index] = value;
-
-    }
-
-    @Override
-    public T remove(int index) {
-        checkIndex(index);
-        final T removedItem = (T) objArray[index];
-        int sizeOfTempArr = size - index - 1;
-        Object[] temporary = new Object[sizeOfTempArr];
-        System.arraycopy(objArray, index + 1, temporary, 0, sizeOfTempArr);
-        System.arraycopy(temporary, 0, objArray, index, sizeOfTempArr);
-        objArray[--size] = null;
-        return removedItem;
-    }
-
-    @Override
-    public T remove(T element) {
-        for (int i = 0; i < size; i++) {
-            if (element == objArray[i] || element != null && element.equals(objArray[i])) {
-                return remove(i);
-            }
-        }
-        throw new NoSuchElementException("There no such element in array");
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
     }
 }
