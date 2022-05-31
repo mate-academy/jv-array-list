0d2fafec63d0d9ece4466d871c2de0d2fdbfb42e
Yaroslav Lastivka <yaroslav0108@gmail.com>
2022-05-31 14:24:28 +0300

    ArrayList class was created

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,33 +4,35 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elementData;
+    private Object[] values;
     private int size;
 
     public ArrayList() {
-        elementData = new Object[DEFAULT_CAPACITY];
+        values = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == elementData.length) {
+        if (size == values.length) {
             grow();
         }
-        elementData[size] = value;
-        size = size + 1;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-        rangeCheckForAdd(index);
-        if (size == elementData.length) {
+        if (size == values.length) {
             grow();
         }
-        System.arraycopy(elementData, index,
-                elementData, index + 1,
-                size - index);
-        elementData[index] = value;
-        size = size + 1;
+        if (index != size) {
+            validindex(index);
+            System.arraycopy(values, index,
+                    values, index + 1,
+                    size - index);
+        }
+        values[index] = value;
+        size++;
     }
 
     @Override
@@ -42,34 +44,34 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndex(index, size);
-        return (T) elementData[index];
+        validindex(index);
+        return (T) values[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, size);
-        elementData[index] = value;
+        validindex(index);
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index,size);
-        Object[] es = elementData;
-        T oldValue = (T) es[index];
+        validindex(index);
+        Object[] oldValues = values;
+        T oldValue = (T) oldValues[index];
         final int newSize;
         if ((newSize = size - 1) > index) {
-            System.arraycopy(es, index + 1, es, index, newSize - index);
+            System.arraycopy(oldValues, index + 1, oldValues, index, newSize - index);
         }
-        es[size = newSize] = null;
+        oldValues[size = newSize] = null;
         return oldValue;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if ((elementData[i] != null && elementData[i].equals(element))
-                    || elementData[i] == element) {
+            if ((values[i] != null && values[i].equals(element))
+                    || values[i] == element) {
                 return remove(i);
             }
         }
@@ -87,22 +89,16 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void grow() {
-        if (elementData.length > 0) {
-            Object[] newelementData = new Object[elementData.length + (elementData.length >> 1)];
-            System.arraycopy(elementData, 0, newelementData, 0, elementData.length);
-            elementData = newelementData;
+        if (values.length > 0) {
+            Object[] newelementData = new Object[values.length + (values.length >> 1)];
+            System.arraycopy(values, 0, newelementData, 0, values.length);
+            values = newelementData;
         }
     }
 
-    private void checkIndex(int index, int size) {
+    private void validindex(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Passed index is invalid" + index);
-        }
-    }
-
-    private void rangeCheckForAdd(int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Passed index is invalid" + index);
+            throw new ArrayListIndexOutOfBoundsException("Passed index is invalid " + index);
         }
     }
 }
