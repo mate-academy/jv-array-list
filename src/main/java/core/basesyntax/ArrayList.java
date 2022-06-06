97f4c77b52f1334394904a12ceb696ee8a0168e0
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-06-07 00:10:07 +0200

    Created ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
     private T[] elementData;
     private int size;
@@ -10,59 +12,99 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (size + 1 == elementData.length) {
-            T[] newElementData = (T[]) new Object[elementData.length + elementData.length >> 1];
-            System.arraycopy(elementData, 0, newElementData, 0, size);
-            elementData = newElementData;
-        }
-        elementData[size] = value;
-        size ++;
+        ensureCapacity(size + 1);
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (size + 1 == elementData.length) {
-            T[] newElementData = (T[]) new Object[elementData.length + elementData.length >> 1];
-            System.arraycopy(elementData, 0, newElementData, 0, index);
-            System.arraycopy(elementData, index + 1, newElementData, index + 1, size - index - 1);
-            elementData = newElementData;
+        ensureCapacity(size + 1);
+        try {
+            System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("Can't add element to this index");
         }
-        elementData[index] = value;
-        size ++;
+            elementData[index] = value;
+            size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        ensureCapacity(size + list.size());
+        System.arraycopy(toArray(list), 0, elementData, size, list.size());
+        size += list.size();
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Element with this index doesn't exist");
+        }
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can't replace the element on the specified position");
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        try {
+        T removedElement = elementData[index];
+        removeElement(index);
+        return removedElement;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("There isn't element on this index");
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = 0;
+        try {
+            while (elementData[index] != element) {
+                index++;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException("There is no such element in specified list");
+        }
+        removeElement(index);
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return elementData[0] == null;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        while (minCapacity >= elementData.length) {
+            T[] newElementData = (T[]) new Object[elementData.length + (elementData.length >> 1)];
+            System.arraycopy(elementData, 0, newElementData, 0, size);
+            elementData = newElementData;
+        }
+    }
+
+    private void removeElement(int index) {
+        int numMoved = size - index - 1;
+        System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+        elementData[--size] = null;
+    }
+
+    private T[] toArray(List<T> list) {
+        T[] array = (T[]) new Object[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
     }
 }
