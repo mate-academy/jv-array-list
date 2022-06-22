593acb6e6b68d30f379e5860f50228ae75acc613
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-06-22 09:04:35 +0300

    created method : grow, rangeChekForAdd, add(value,index),size

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,59 @@
 package core.basesyntax;
+//import jdk.internal.util.ArraysSupport;
+
+import java.util.Arrays;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    transient T[] arrayData = (T[]) new Object[DEFAULT_CAPACITY];
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private int size;
+    private int cursor; // index of next element
+    private int lastReturned = -1;  // index of last element returned; -1 if no such
+    //private int expectedModCount = modCount;
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0)
+            throw new ArrayListIndexOutOfBoundsException("Index of bounds exception");
+    }
+
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = arrayData.length;
+        if (oldCapacity > 0 || arrayData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            int newCapacity = (int) (oldCapacity * 1.5);
+            return arrayData = Arrays.copyOf(arrayData, newCapacity);
+        } else {
+            return arrayData = (T[]) new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
+    }
+
     @Override
     public void add(T value) {
+        checkForComodification();
 
+        try {
+            int index = cursor;
+            ArrayList.this.add(value, index);
+            cursor = index + 1;
+            lastReturned = -1;
+            //expectedModCount = modCount;
+        } catch (IndexOutOfBoundsException ex) {
+            throw new ArrayListIndexOutOfBoundsException("Index of bounds exception");
+        }
     }
 
     @Override
     public void add(T value, int index) {
-
+        rangeCheckForAdd(index);
+        //modCount++;
+        final int s;
+        if ((s = size) == arrayData.length)
+            arrayData = (T[]) grow(size + 1);
+        System.arraycopy(arrayData, index,
+                arrayData, index + 1,
+                s - index);
+        arrayData[index] = value;
+        size = s + 1;
     }
 
     @Override
@@ -38,7 +83,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
