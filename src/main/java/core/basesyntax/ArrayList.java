4e330b35dcdd256a9bdbaf45a71536b54829f978
Nata <natserbo@gmail.com>
2022-06-24 18:50:42 +0300

    fill add method & create grow() method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,44 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Objects[] elementsData;
+    private Exception ArrayListIndexOutOfBoundsException;
+
+
+    public Objects[] grow(int currentCapasity){
+        int newCapacity = currentCapasity + (currentCapasity >> 1);
+        return elementsData = Arrays.copyOf(elementsData, newCapacity);
+    }
     @Override
     public void add(T value) {
-
+        if (size == elementsData.length) {
+            elementsData = grow(elementsData.length);
+        }
+        elementsData[size] = (Objects) value;
+        size++;
     }
 
     @Override
-    public void add(T value, int index) {
-
+    public void add(T value, int index) throws ArrayListIndexOutOfBoundsException {
+        if (index > size) {
+            try {
+                if (index > size) {
+                    throw core.basesyntax.ArrayListIndexOutOfBoundsException;
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (size == elementsData.length) {
+            elementsData = grow(elementsData.length);
+        }
+        elementsData[index] = (Objects) value;
+        size++;
     }
 
     @Override
