555522711bfe5d10c886458ce84700bf46d57e8d
Nata <natserbo@gmail.com>
2022-06-25 18:59:29 +0300

    fill get, set addAll, remove , size, isEmpty methods, edit add method, create ArrayList constructor

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,78 +1,97 @@
 package core.basesyntax;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private int size;
-    private Objects[] elementsData;
-    private Exception ArrayListIndexOutOfBoundsException;
+    private T[] elementsData;
 
+    public ArrayList() {
+        this.elementsData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
 
-    public Objects[] grow(int currentCapasity){
-        int newCapacity = currentCapasity + (currentCapasity >> 1);
+    public T[] grow(int currentCapacity) {
+        int newCapacity = currentCapacity + (currentCapacity >> 1);
         return elementsData = Arrays.copyOf(elementsData, newCapacity);
     }
+
     @Override
     public void add(T value) {
-        if (size == elementsData.length) {
-            elementsData = grow(elementsData.length);
-        }
-        elementsData[size] = (Objects) value;
-        size++;
+        add(value,size);
     }
 
     @Override
     public void add(T value, int index) throws ArrayListIndexOutOfBoundsException {
-        if (index > size) {
-            try {
-                if (index > size) {
-                    throw core.basesyntax.ArrayListIndexOutOfBoundsException;
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is bigger then ArrayList size");
         }
         if (size == elementsData.length) {
             elementsData = grow(elementsData.length);
         }
-        elementsData[index] = (Objects) value;
+        System.arraycopy(elementsData,index,elementsData,index + 1,size - index);
+        elementsData[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if ((list.size() + size) >= elementsData.length){
+            elementsData = grow(elementsData.length);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            elementsData[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
-    public T get(int index) {
-        return null;
+    public T get(int index) throws ArrayListIndexOutOfBoundsException {
+        if (index >= size || index < 0){
+            throw new ArrayListIndexOutOfBoundsException("Index is bigger then ArrayList size");
+        }
+        return elementsData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index >= size || index < 0){
+            throw new ArrayListIndexOutOfBoundsException("Index is bigger then ArrayList size");
+        }
+        elementsData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index >= size || index < 0){
+            throw new ArrayListIndexOutOfBoundsException("Index is bigger then ArrayList size");
+        }
+        T removedValue = elementsData[index];
+        System.arraycopy(elementsData,index + 1, elementsData, index, size - index - 1);
+        size--;
+        return removedValue;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T element) throws NoSuchElementException {
+        for (int i = 0; i < size; i++) {
+            if (elementsData[i] == null && elementsData[i] == element
+                    || elementsData[i] != null && elementsData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There are no elements in ArrayList");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
