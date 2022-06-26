a4e75e2185fa4a9955d4c6d870755c3cb5571d1f
Roman Voitalik <voitalik1@gmail.com>
2022-06-26 20:16:12 +0300

    add grow, get, set, size isEmpty methods.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,35 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData = new Object[DEFAULT_CAPACITY];
+    private int size;
+
+    public ArrayList() {
+
+        Object[] elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    private Object[] grow(int currentCapacity) {
+        int newCapacity = currentCapacity + (currentCapacity >> 1);
+        return Arrays.copyOf(elementData, newCapacity);
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            elementData = grow(size);
+        }
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < size) {
+            return;
+        }
     }
 
     @Override
@@ -18,31 +39,40 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("ArrayListIndexOutOfBoundsException");
+        }
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("ArrayListIndexOutOfBoundsException");
+        }
+        elementData[index] = value;
 
     }
 
     @Override
     public T remove(int index) {
+        size--;
         return null;
     }
 
     @Override
     public T remove(T element) {
+        size--;
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
