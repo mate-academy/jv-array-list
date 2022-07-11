863507175b21ebb25c1074df1c7c833b2e924cb1
Serhii Buria <sergeiburya@gmail.com>
2022-07-11 12:53:24 +0300

    added method to remove array element, checked implementation of class methodsÐ

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 5;
     private static final double STEP_INCREASING_ARRAY = 1.5;
     private T[] objects;
     private int size;
@@ -17,12 +19,13 @@ public class ArrayList<T> implements List<T> {
     public int size() {
         return size;
     }
+
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    private void grow() {
+    private void growArray() {
         if (size == objects.length) {
             T[] arraySours = objects;
             objects = (T[])new Object[(int)
@@ -30,15 +33,17 @@ public class ArrayList<T> implements List<T> {
             System.arraycopy(arraySours,0,objects,0,arraySours.length);
         }
     }
+
     private void checkOutOfBoundsException(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Out of bout exception for index: "
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds value: "
                     + index);
         }
     }
+
     @Override
     public void add(T value) {
-        grow();
+        growArray();
         objects[size] = value;
         size++;
     }
@@ -46,10 +51,10 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Out of bout exception ofr value :"
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds value :"
                     + value + " with index " + index);
         }
-        grow();
+        growArray();
         System.arraycopy(objects,index,objects,index + 1,objects.length - index - 1);
         objects[index] = value;
         size++;
@@ -57,37 +62,47 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public String toString() {
-        return "ArrayList{" +
-                "objects=" + Arrays.toString(objects) +
-                ", size=" + size +
-                '}';
+        return "ArrayList{"
+                + "objects="
+                + Arrays.toString(objects)
+                + ", size=" + size + '}';
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkOutOfBoundsException(index);
+        return objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkOutOfBoundsException(index);
+        objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkOutOfBoundsException(index);
+        T element = objects[index];
+        System.arraycopy(objects,index + 1, objects,index, objects.length - index - 1);
+        size--;
+        return element;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T value) {
+        for (int i = 0; i < size(); i++) {
+            if (Objects.equals(objects[i], value)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("No such value: " + value);
     }
-
-
-
 }
--- src/main/java/core/basesyntax/MainArrayList.java
+++ src/main/java/core/basesyntax/MainArrayList.java
@@ -1,26 +1,27 @@
 package core.basesyntax;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Random;
 
 public class MainArrayList {
     public static void main(String[] args) {
         ArrayList<String> stringArrayList = new ArrayList<>();
 
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 3;
         Random random = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 16; i++) {
             stringArrayList.add(new String(random.ints(leftLimit, rightLimit + 1)
                     .limit(targetStringLength)
-                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint,
+                            StringBuilder::append)
                     .toString()));
         }
         System.out.println(stringArrayList);
-
-
+        System.out.println(stringArrayList.get(14));
+        System.out.println(stringArrayList.get(15));
+        System.out.println(stringArrayList.remove(15));
+        System.out.println(stringArrayList);
     }
 }
