d08e6380aebfec2602b5ff0ff63e273d61fff364
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-28 14:45:05 +0300

    Task completed v.1.2 Changes were made taking into account the comments of the mentor

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -30,9 +30,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        if (list.size() >= elements.length) {
-            expand();
-        }
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
