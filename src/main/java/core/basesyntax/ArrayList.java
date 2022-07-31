871f73c9893724ee9979b74af1417a10da547967
Alina Nikiforova <0500302717a@gmail.com>
2022-07-31 10:39:54 +0200

    implementation ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,94 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int MAX_ITEMS_NUMBER = 6;
+    private Object[] items = new Object[MAX_ITEMS_NUMBER];
+    private int size;
+
     @Override
     public void add(T value) {
-
+        grow();
+        items[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index: " + index
+                    + " for size " + size);
+        }
+        grow();
+        if (size > index) {
+            System.arraycopy(items, index, items, index + 1, size - index);
+        }
+        items[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        verifyIndexOutOfBounds(index);
+        return (T) items[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        verifyIndexOutOfBounds(index);
+        items[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        verifyIndexOutOfBounds(index);
+        Object deletedItem = items[index];
+        System.arraycopy(items, index + 1, items, index, size - 1 - index);
+        size--;
+
+        return (T) deletedItem;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element != null && element.equals(items[i]) || element == items[i]) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Not found element: " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        if (size == items.length) {
+            Object[] newItems = new Object[items.length + (items.length >> 1)];
+            System.arraycopy(items, 0, newItems, 0, size);
+            items = newItems;
+        }
+    }
+
+    private void verifyIndexOutOfBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index: " + index
+                    + " for size " + size);
+        }
     }
 }
