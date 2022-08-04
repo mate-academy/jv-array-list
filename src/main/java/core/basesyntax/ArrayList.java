51940dd4dfdf3c23fec6be016a1bb14342c59c00
Nazar <litvik.12@gmail.com>
2022-08-04 16:38:42 +0300

    moved private methods to the end of class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,9 +5,90 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private final int DEFAULT_SIZE = 10;
-    private int size;
+    private static final int DEFAULT_SIZE = 10;
     private Object[] objects;
+    private int size;
+
+    public ArrayList() {
+        objects = new Object[DEFAULT_SIZE];
+    }
+
+    @Override
+    public void add(T value) {
+        checkingSizeOfList();
+        objects[size] = value;
+        size++;
+    }
+
+    @Override
+    public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index value. "
+                    + "Index out of bounds.");
+        }
+        checkingSizeOfList();
+        if (index != size) {
+            System.arraycopy(objects, index, objects, index + 1, size - index);
+        }
+        objects[index] = value;
+        size++;
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (validationIndex(index)) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index value. "
+                    + "Index out of bounds.");
+        }
+        return (T) objects[index];
+    }
+
+    @Override
+    public void set(T value, int index) {
+        if (validationIndex(index)) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index value. "
+                    + "Index out of bounds.");
+        }
+        objects[index] = value;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (validationIndex(index)) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index value. "
+                    + "Index out of bounds.");
+        }
+        Object object = objects[index];
+        removeObject(index);
+        return (T) object;
+    }
+
+    @Override
+    public T remove(T value) {
+        int index = findByValue(value);
+        if (index == -1) {
+            throw new NoSuchElementException("This value is missing in list");
+        }
+        removeObject(index);
+        return value;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     private void checkingSizeOfList() {
         if (size == objects.length) {
@@ -35,87 +116,4 @@ public class ArrayList<T> implements List<T> {
         }
         size--;
     }
-
-    public ArrayList() {
-        objects = new Object[DEFAULT_SIZE];
-    }
-
-    @Override
-    public void add(T value) {
-        checkingSizeOfList();
-        objects[size] = value;
-        size++;
-    }
-
-    @Override
-    public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Wrong index value. " +
-                    "Index out of bounds.");
-        }
-        checkingSizeOfList();
-        if (index != size) {
-            System.arraycopy(objects, index, objects, index + 1, size - index);
-        }
-        objects[index] = value;
-        size++;
-    }
-
-    @Override
-    public void addAll(List<T> list) {
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
-        }
-    }
-
-    @Override
-    public T get(int index) {
-        if (validationIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Wrong index value. " +
-                    "Index out of bounds.");
-        }
-        return (T) objects[index];
-    }
-
-    @Override
-    public void set(T value, int index) {
-        if (validationIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Wrong index value. " +
-                    "Index out of bounds.");
-        }
-        objects[index] = value;
-    }
-
-    @Override
-    public T remove(int index) {
-        if (validationIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Wrong index value. " +
-                    "Index out of bounds.");
-        }
-        Object object = objects[index];
-        removeObject(index);
-        return (T) object;
-    }
-
-    @Override
-    public T remove(T value) {
-        int index = findByValue(value);
-        if (index == -1) {
-            throw new NoSuchElementException("This value is missing in list");
-        }
-        removeObject(index);
-        return value;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-
 }
