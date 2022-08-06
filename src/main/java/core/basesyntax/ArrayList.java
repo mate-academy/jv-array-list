aeea22aab4801bdd011cbc50528e948735a75c64
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-08-06 17:53:39 +0200

    implement remove(index)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -50,17 +50,28 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-
+        checkIndex(index, size);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
+        checkIndex(index, size);
+        resizeIfFull();
+        elements[index] = value;
+        size++;
 
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index, size);
+        T removedElement = (T) elements[index];
+        //нужно взять весь массив после индекса (тоесть след на один)
+        // и скопировать на индекс - 1, остаточное место станет null
+        System.arraycopy(elements, index + 1, elements, index, size - index);
+        size--;
+        return removedElement;
     }
 
     @Override
