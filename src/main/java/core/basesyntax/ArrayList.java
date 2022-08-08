f8f0d2c58c834b55351d12cfd17056a43a8714af
max-bolotin <realprof.mx@gmail.com>
2022-08-08 15:21:21 +0300

    student notes have been taken into account

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,57 +4,56 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private static final String NULL_MARKER = "__Null_Value__";
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] objects = new Object[0];
+    private int size;
+    private int capacity;
+
+    public ArrayList() {
+        this.capacity = DEFAULT_CAPACITY;
+    }
 
     @Override
     public void add(T value) {
-        Object[] temp = new Object[objects.length];
-        System.arraycopy(objects, 0, temp, 0, objects.length);
-        objects = new Object[temp.length + 1];
-        System.arraycopy(temp, 0, objects, 0, temp.length);
-        objects[temp.length] = value == null ? (T) NULL_MARKER : value;
-        trimToSize();
+        if (size >= capacity) {
+            capacity = grow();
+        }
+        Object[] temp = new Object[size];
+        System.arraycopy(objects, 0, temp, 0, size);
+        objects = new Object[capacity];
+        System.arraycopy(temp, 0, objects, 0, size);
+        objects[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (size == capacity) {
+            capacity = grow();
+        }
         if (index > size() || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index doesn't exist: " + index);
         }
-        if (index == size()) {
+        if (index == size) {
             add(value);
             return;
         }
-        trimToSize();
-        Object[] res = new Object[size() + 1];
+        Object[] res = new Object[size + 1];
         if (index == 0) {
-            res[0] = value == null ? (T) NULL_MARKER : value;
+            res[0] = value;
         } else {
             System.arraycopy(objects, 0, res, index - 1, index);
-            res[index] = value == null ? (T) NULL_MARKER : value;
+            res[index] = value;
         }
-        System.arraycopy(objects, index, res, index + 1, objects.length - index);
-        objects = new Object[size() + 1];
+        System.arraycopy(objects, index, res, index + 1, size - index);
+        objects = new Object[capacity];
         System.arraycopy(res, 0, objects, 0, res.length);
+        size++;
     }
 
-    private void trimToSize() {
-        int endOfArrayIndex = 0;
-        boolean trimNeeded = false;
-        for (int i = 0; i < objects.length; i++) {
-            if (objects[i] == null) {
-                endOfArrayIndex = i;
-                trimNeeded = true;
-                break;
-            }
-        }
-        if (trimNeeded) {
-            Object[] shortArray = new Object[endOfArrayIndex];
-            System.arraycopy(objects, 0, shortArray, 0, endOfArrayIndex);
-            objects = new Object[endOfArrayIndex];
-            System.arraycopy(shortArray, 0, objects, 0, endOfArrayIndex);
-        }
+    private int grow() {
+        capacity += capacity >> 1;
+        return capacity;
     }
 
     @Override
@@ -72,7 +71,7 @@ public class ArrayList<T> implements List<T> {
             throw new ArrayListIndexOutOfBoundsException(
                     "No element with such index in list: " + index);
         }
-        return (T) (objects[index] == NULL_MARKER ? null : objects[index]);
+        return (T) (objects[index]);
     }
 
     @Override
@@ -81,7 +80,7 @@ public class ArrayList<T> implements List<T> {
             throw new ArrayListIndexOutOfBoundsException(
                     "No element with such index in list: " + index);
         }
-        objects[index] = value == null ? (T) NULL_MARKER : value;
+        objects[index] = value;
     }
 
     @Override
@@ -91,16 +90,16 @@ public class ArrayList<T> implements List<T> {
                     "No element with such index in list: " + index);
         }
         T deletedObject = (T) objects[index];
-        if (index == objects.length - 1) {
+        if (index == size - 1) {
             objects[index] = null;
         } else {
-            if (objects.length - (index + 1) >= 0) {
+            if (size - (index + 1) >= 0) {
                 System.arraycopy(objects, index + 1, objects,
-                        index + 1 - 1, objects.length - (index + 1));
+                        index + 1 - 1, size - (index + 1));
             }
-            objects[objects.length - 1] = null;
+            objects[size - 1] = null;
         }
-        trimToSize();
+        size--;
         return deletedObject;
     }
 
@@ -108,9 +107,7 @@ public class ArrayList<T> implements List<T> {
     public T remove(T element) {
         boolean elementFound = false;
         T removedObject = null;
-        if (element == null) {
-            element = (T) NULL_MARKER;
-        }
+
         for (int i = 0; i < objects.length; i++) {
             if (Objects.equals(objects[i], element)) {
                 removedObject = remove(i);
@@ -119,21 +116,20 @@ public class ArrayList<T> implements List<T> {
             }
         }
         if (elementFound) {
-            return removedObject == NULL_MARKER ? null : removedObject;
+            return removedObject;
         } else {
             throw new NoSuchElementException("No such element in list: "
-                    + (element == NULL_MARKER ? null : element));
+                    + element);
         }
     }
 
     @Override
     public int size() {
-        trimToSize();
-        return objects.length;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return objects.length == 0 || objects[0] == null;
+        return size == 0;
     }
 }
