fa2163029cd3ae497d1d0727c4ac0461b4bcbafb
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-08 18:54:38 +0300

    implement isEmpty method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final int ARRAY_FIRST_INDEX = 0;
+    private static final double GROWTH_INDEX = 1.5;
     private int size;
     private int capacity;
     private T[] values;
@@ -58,11 +59,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     private T[] grow() {
-        capacity = (int) (capacity * 1.5);
+        capacity = (int) (capacity * GROWTH_INDEX);
         T[] tempArray = (T[]) new Object[capacity];
         System.arraycopy(values, ARRAY_FIRST_INDEX, tempArray, ARRAY_FIRST_INDEX, size);
 
