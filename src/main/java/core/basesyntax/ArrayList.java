95b186d03825a46858906d6f6fdb1e4709557b3c
LubusUzh <lubus.uzh@gmail.com>
2022-08-08 13:40:53 +0300

    Add values for ArrayList and create empty Constructor

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,14 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] arrayList;
+    private int size;
+
+    public ArrayList() {
+        arrayList = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
 
