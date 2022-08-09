7bc6bb94be41a4dbe653e76cbfbec57f3682a572
max-bolotin <realprof.mx@gmail.com>
2022-08-09 17:06:59 +0300

    mentor's notes and checkstyle errors fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -9,7 +9,7 @@ public class ArrayList<T> implements List<T> {
     private int capacity;
 
     public ArrayList() {
-        this.capacity = DEFAULT_CAPACITY;
+        capacity = DEFAULT_CAPACITY;
         objects = new Object[DEFAULT_CAPACITY];
     }
 
@@ -24,103 +24,66 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (size == capacity) {
-            capacity = grow();
-        }
         if (index > size() || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index doesn't exist: " + index);
+        }
+        if (size == capacity) {
+            capacity = grow();
         }
         if (index == size) {
             add(value);
             return;
         }
-        Object[] res = new Object[size + 1];
-        if (index == 0) {
-            res[0] = value;
-        } else {
-            System.arraycopy(objects, 0, res, index - 1, index);
-            res[index] = value;
-        }
-        System.arraycopy(objects, index, res, index + 1, size - index);
-        objects = new Object[capacity];
-        System.arraycopy(res, 0, objects, 0, res.length);
+        System.arraycopy(objects, index, objects, index + 1, size - index);
+        objects[index] = value;
         size++;
-    }
-
-    private int grow() {
-        capacity += capacity >> 1;
-        Object[] temp = new Object[size];
-        System.arraycopy(objects, 0, temp, 0, size);
-        objects = new Object[capacity];
-        System.arraycopy(temp, 0, objects, 0, size);
-        return capacity;
     }
 
     @Override
     public void addAll(List<T> list) {
-        Object[] arr = new Object[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            arr[i] = list.get(i);
-            add((T) arr[i]);
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
+        indexCheck(index);
+        return (T) objects[index];
+    }
+
+    private void indexCheck(int index) {
         if (index > size() - 1 || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
                     "No element with such index in list: " + index);
         }
-        return (T) (objects[index]);
     }
 
     @Override
     public void set(T value, int index) {
-        if (index > size() - 1 || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "No element with such index in list: " + index);
-        }
+        indexCheck(index);
         objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index > size() - 1 || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "No element with such index in list: " + index);
-        }
-        T deletedObject = (T) objects[index];
-        if (index == size - 1) {
-            objects[index] = null;
-        } else {
-            if (size - (index + 1) >= 0) {
-                System.arraycopy(objects, index + 1, objects,
-                        index + 1 - 1, size - (index + 1));
-            }
-            objects[size - 1] = null;
-        }
+        indexCheck(index);
+        final T deletedObject = (T) objects[index];
+        System.arraycopy(objects, index + 1, objects, index, size - (index + 1));
+        objects[size - 1] = null;
         size--;
         return deletedObject;
     }
 
     @Override
     public T remove(T element) {
-        boolean elementFound = false;
-        T removedObject = null;
-
         for (int i = 0; i < objects.length; i++) {
             if (objects[i] == element || (element != null && element.equals(objects[i]))) {
-                removedObject = remove(i);
-                elementFound = true;
-                break;
+                return remove(i);
             }
         }
-        if (elementFound) {
-            return removedObject;
-        } else {
-            throw new NoSuchElementException("No such element in list: "
-                    + element);
-        }
+        throw new NoSuchElementException("No such element in list: "
+                + element);
     }
 
     @Override
@@ -131,5 +94,13 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private int grow() {
+        capacity += capacity >> 1;
+        Object[] temp = objects;
+        objects = new Object[capacity];
+        System.arraycopy(temp, 0, objects, 0, size);
+        return capacity;
     }
 }
