06c0f3351b02c42ed1c391387aa5aefea7b3d1ad
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-08-09 17:13:27 +0300

    constant magic number is created

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,8 +6,10 @@ public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final String MESSAGE = "There is not such element.";
     private static final String MESSAGE_OUT = "Out of bounds ";
+    private static final double NUMBER = 1.5;
     private int size;
     private Object[] elements;
+
 
     public ArrayList() {
         elements = new Object[DEFAULT_CAPACITY];
@@ -24,19 +26,13 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
+        checkIndex(index, size + 1);
         if (size == elements.length) {
             grow();
         }
-        checkIndex(index, size + 1);
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
-    }
-
-    public void checkIndex(int index, int size) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(MESSAGE_OUT + index);
-        }
     }
 
     @Override
@@ -80,7 +76,7 @@ public class ArrayList<T> implements List<T> {
                 return (T) removedObject;
             }
         }
-        throw new NoSuchElementException(MESSAGE);
+        throw new NoSuchElementException(MESSAGE + element);
     }
 
     @Override
@@ -93,9 +89,15 @@ public class ArrayList<T> implements List<T> {
         return size() == 0;
     }
 
-    public void grow() {
-        Object[] newElements = new Object[elements.length * 3 / 2];
+    private void grow() {
+        Object[] newElements = new Object[(int) (elements.length * NUMBER)];
         System.arraycopy(elements, 0, newElements, 0, size);
         elements = newElements;
+    }
+
+    private void checkIndex(int index, int size) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(MESSAGE_OUT + index);
+        }
     }
 }
