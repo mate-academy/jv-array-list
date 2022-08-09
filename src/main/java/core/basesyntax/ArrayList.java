fc6811efd3437b1bd0f7fa71f8498757d9efd149
Thousandfold <you@example.com>
2022-08-09 18:17:00 +0300

    override_methods_in_arrayLit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,102 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int CAPACITY = 10;
+    private Object[] row;
+    private int size;
+
+    public ArrayList() {
+        row = new Object[CAPACITY];
+    }
+
+    public void checkSize() {
+        if (size == row.length) {
+            Object[] newRow = new Object[row.length + row.length / 2];
+            System.arraycopy(row, 0,newRow, 0, size);
+            row = newRow;
+        }
+    }
+
+    public void indexExeption(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect input index: " + index);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        checkSize();
+        row[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect input index: " + index);
+        }
+        checkSize();
+        System.arraycopy(row, index, row,index + 1, size - index);
+        row[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexExeption(index);
+        return (T) row[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        indexExeption(index);
+        row[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexExeption(index);
+        T value = (T) row[index];
+        System.arraycopy(row, index + 1, row, index, size - index - 1);
+        size--;
+        return value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        indexOf(element);
+        T value = (T) row[indexOf(element)];
+        remove(indexOf(element));
+        return value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
+
+    public int indexOf(T element) {
+        int i = 0;
+        for ( ;i < size; i++) {
+            if (row[i] == element || row[i] != null && row[i].equals(element)) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException("Element " + element + " was not found!");
+    }
+
 }
