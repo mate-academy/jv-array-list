b34a6853e44eb74960d3445e41e386661c71ae6f
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-08-09 10:14:25 +0300

    remove redundant array

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -25,11 +25,9 @@ public class ArrayList<T> implements List<T> {
             add(value);
         } else {
             increase();
-            Object[] valuesTemp = new Object[size - index + 1];
-            System.arraycopy(values, index, valuesTemp, 0, valuesTemp.length);
+            System.arraycopy(values, index, values, index + 1, size - index);
             values[index] = value;
             size++;
-            System.arraycopy(valuesTemp, 0, values, index + 1, valuesTemp.length - 1);
         }
     }
 
