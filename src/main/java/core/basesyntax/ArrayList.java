4de8151b986b390ceea1198332ee27ec12c5f4c9
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-08-09 17:58:09 +0300

    Was deleted redundant constants

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,21 +3,17 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final String NO_POSITION_MESSAGE = "This position is not exist in list ";
-    private static final String NO_ELEMENT_MESSAGE = "There is no such element in list ";
-    private static int capacity = 10;
-    private static final int ONE = 1;
-    private static final int ZERO = 0;
+    private static final int CAPACITY = 10;
     private T[] items;
     private int size;
 
     public ArrayList() {
-        items = (T[]) new Object[capacity];
+        items = (T[]) new Object[CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == capacity) {
+        if (size == items.length) {
             grow();
         }
         items[size] = value;
@@ -27,20 +23,20 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         validIndex(value, index);
-        if (size == capacity) {
+        if (size == items.length) {
             grow();
         }
-        System.arraycopy(items, index, items, index + ONE, size - index);
+        System.arraycopy(items, index, items, index + 1, size - index);
         items[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        while (capacity < size + list.size()) {
+        while (items.length < size + list.size()) {
             grow();
         }
-        System.arraycopy(list.toArray(), ZERO, items, size, list.size());
+        System.arraycopy(list.toArray(), 0, items, size, list.size());
         size += list.size();
     }
 
@@ -60,18 +56,18 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         validIndex(index);
         T removedObject = items[index];
-        System.arraycopy(items, index + ONE, items, index, --size - index);
+        System.arraycopy(items, index + 1, items, index, --size - index);
         return removedObject;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = ZERO; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (items[i] == element || items[i] != null && items[i].equals(element)) {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException(NO_ELEMENT_MESSAGE + element);
+        throw new NoSuchElementException("There is no such element in list " + element);
     }
 
     @Override
@@ -81,26 +77,24 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        return size == ZERO;
+        return size == 0;
     }
 
     private void grow() {
-        int oldCapacity = capacity;
-        capacity += capacity >> ONE;
-        T[] newItems = (T[]) new Object[capacity];
-        System.arraycopy(items, ZERO, newItems, ZERO, oldCapacity);
+        T[] newItems = (T[]) new Object[items.length + items.length / 2];
+        System.arraycopy(items, 0, newItems, 0, items.length);
         items = newItems;
     }
 
     private void validIndex(int index) {
-        if (index < ZERO || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(NO_POSITION_MESSAGE + index);
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is not exist");
         }
     }
 
     private void validIndex(T value, int index) {
-        if (index < ZERO || index > size) {
-            throw new ArrayListIndexOutOfBoundsException(NO_POSITION_MESSAGE + index);
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is not exist");
         }
     }
 }
