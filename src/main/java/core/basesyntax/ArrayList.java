ef0858c7425be672b0e294cab9e65435978fe14c
Yaroslav Vashchenko <yaroslav.vashchenko@gmail.com>
2022-08-09 16:54:43 +0300

    first commit hw-array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,97 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elements.length) {
+            Object[] newObject = new Object[elements.length + elements.length / 2];
+            for (int i = 0; i < elements.length; i++) {
+                newObject[i] = elements[i];
+            }
+            elements = newObject;
+        }
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can't write an element to ArrayList");
+        }
+        if (size == elements.length) {
+            Object[] newObject = new Object[elements.length + elements.length / 2];
+            for (int i = 0; i < elements.length; i++) {
+                newObject[i] = elements[i];
+            }
+            elements = newObject;
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index > size() - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can't get an element");
+        }
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index > size() - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is out of bounds of the list size");
+        }
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index > size - 1 || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is out of bounds of the list size");
+        }
+        T removed = (T) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        return removed;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] != null && elements[i].equals(element) || elements[i] == element) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("No element in the list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
