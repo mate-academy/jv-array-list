42aeba7b2702f98725647a93d4e9a84cfb93059e
UmyaFamilia <umyafamilya1974@gmail.com>
2022-08-10 10:47:00 +0300

    should be good

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,84 +3,75 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_VOLUME = 10;
-    private static final String INDEX_ERROR_MESSAGE = "Can not to find such element";
-    private static final String INDEX_OUT_OF_THE_BROAD = "Index out of the broad";
-    private Object [] mainArray;
+    private static final int CAPATHITY = 10;
     private int size;
-    private Object removedObject;
+    private Object [] elements;
 
     public ArrayList() {
-        mainArray = new Object[DEFAULT_VOLUME];
-    }
-
-    @Override
-    public void add(T value) {
-        if (mainArray.length == size) {
-            grow();
-        }
-        mainArray[size] = value;
-        size++;
+        this.elements = new Object[CAPATHITY];
     }
 
     @Override
     public void add(T value, int index) {
-        checkFoExceptionIndex(index);
-        if (mainArray.length == size) {
-            grow();
-        }
-        Object [] bufferArray = new Object[mainArray.length + 1];
-        System.arraycopy(mainArray, 0, bufferArray, 0, index);
-        bufferArray[index] = value;
-        System.arraycopy(mainArray, index, bufferArray, index + 1, mainArray.length - index);
-        mainArray = bufferArray;
+        checkIndex(index,size + 1);
+        grow();
+        System.arraycopy(elements, index, elements,index + 1,size - index);
+
+        elements[index] = value;
         size++;
+    }
+
+    @Override
+    public void add(T value) {
+        grow();
+        elements[size] = value;
+        size++;
+    }
+
+    public void grow() {
+        if (elements.length == size) {
+            Object [] newArray = new Object[elements.length + elements.length / 2];
+            System.arraycopy(elements,0, newArray,0, size);
+            elements = newArray;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            this.add(list.get(i));
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        checkFoExceptionIndex(index);
-        if (index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_THE_BROAD);
-        }
-        return (T) mainArray[index];
+        checkIndex(index,size);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkFoExceptionIndex(index);
-        if (index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_THE_BROAD);
-        }
-        mainArray[index] = value;
+        checkIndex(index,size);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkFoExceptionIndex(index);
-        if (index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_THE_BROAD);
-        }
-        removedObject = mainArray[index];
-        removeIndex(index);
-        return (T) removedObject;
+        checkIndex(index,size);
+        T remuveelement = (T) elements[index];
+        System.arraycopy(elements,index + 1, elements, index,size - index - 1);
+        size--;
+        return remuveelement;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (element == mainArray[i] || element != null && element.equals(mainArray[i])) {
+            if (element != null && element.equals(elements[i]) || element == elements[i]) {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("Can not to find such element");
+        throw new NoSuchElementException("Not found element: " + element);
     }
 
     @Override
@@ -90,28 +81,12 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        return size() == 0;
+        return size == 0;
     }
 
-    public void grow() {
-        Object[] bufferArray = new Object[mainArray.length + mainArray.length / 2];
-        for (int i = 0; i < mainArray.length; i++) {
-            bufferArray[i] = mainArray[i];
-        }
-        mainArray = bufferArray;
-    }
-
-    public void removeIndex(int index) {
-        mainArray[index] = null;
-        if (index != size) {
-            System.arraycopy(mainArray, index + 1, mainArray, index, size - index - 1);
-        }
-        size--;
-    }
-
-    public void checkFoExceptionIndex(int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_ERROR_MESSAGE);
+    public void checkIndex(int index, int length) {
+        if (index < 0 || index >= length) {
+            throw new ArrayListIndexOutOfBoundsException("We are out of sithe array");
         }
     }
 }
