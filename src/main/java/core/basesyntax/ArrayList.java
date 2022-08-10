b8fd6d078b0ce0661c3fbfee3f6f9ea5c820b573
Julia Kononenko <munguryan13@gmail.com>
2022-08-10 07:57:26 +0300

    rewrite solution, used only 1 resize

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -12,81 +12,73 @@ public class ArrayList<T> implements List<T> {
         if (size < array.length) {
             array[size] = value;
             size++;
-        } else {
-            resize();
-            array[size] = value;
-            size++;
+        }
+        if (size >= array.length) {
+            Object[] tempArray = array;
+            array = new Object[(int) (array.length * 1.5)];
+            for (int i = 0; i < tempArray.length; i++) {
+                array[i] = tempArray[i];
+            }
         }
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
+        if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
                     String.format("Index %s out of bound for length %s", index, size));
         }
-        if (size == array.length) {
-            resizeByIndex();
-        } else {
-            System.arraycopy(array, index, array, index + 1, size - index);
-        }
+        resize();
+        System.arraycopy(array, index, array, index + 1, size - index);
         array[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        array = new Object[size + list.size()];
-        for (int i = 0; i < size; i++) {
-            array[i] = this.array[i];
-        }
         for (int i = 0; i < list.size(); i++) {
-            array[size + i] = list.get(i);
+            add(list.get(i));
         }
-        size = array.length;
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
+        if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
-                    String.format("Index %s out of bound for length %s", index, size)
-            );
+                    String.format("Index %s out of bound for length %s", index, size));
         }
         return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= size) {
+        if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
-                    String.format("Index %s out of bound for length %s", index, size)
-            );
+                    String.format("Index %s out of bound for length %s", index, size));
         }
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= size) {
+        if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
                     String.format("Index %s out of bound for length %s", index, size));
         }
-        Object deletedEl = array[index];
+        Object deletedElement = array[index];
         System.arraycopy(array, index + 1, array, index, size - 1 - index);
         size--;
-        return (T) deletedEl;
+        return (T) deletedElement;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if ((array[i] == element)
-                    || (element != null && element.equals(array[i]))) {
+            if (element != null && element.equals(array[i]) || element == array[i]) {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("Element " + element + " not found");
+        throw new NoSuchElementException("Can`t find " + element);
     }
 
     @Override
@@ -99,15 +91,12 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void resizeByIndex() {
-        Object[] newArray = new Object[(int) (array.length * 1.5)];
-        System.arraycopy(array, 0, newArray, 1, size);
-        array = newArray;
-    }
-
     private void resize() {
-        Object[] newArray = new Object[(int) (array.length * 1.5)];
-        System.arraycopy(array, 0, newArray, 0, size);
-        array = newArray;
+        if (size == array.length) {
+            Object[] newArray = new Object[(int) (size * 1.5)];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
     }
 }
+
