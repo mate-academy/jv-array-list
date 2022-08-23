b77518ea5f22268a0d5f4814c68675878ca932d6
Riki Maru <qui.eg0m@gmail.com>
2022-08-23 17:26:40 +0300

    addAll() method was fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -32,8 +32,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        int listSize = list.size();
-        for (int i = 0; i < listSize; i++) {
+        for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
