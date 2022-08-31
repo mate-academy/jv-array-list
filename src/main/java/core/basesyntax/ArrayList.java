54fa2d17be20eb86bb8a17eea102773b089d3b67
Alexander <liero2D@gmail.com>
2022-08-31 16:47:32 +0300

    added methods ArrayList. Please check. Thanks.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double INCREASE_CAPACITY = 1.5;
+    private T[] data;
+    private int size;
+
+    public ArrayList() {
+        data = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size + 1 >= data.length) {
+            grow();
+        }
+        data[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            data[size] = value;
+        }
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index is out of capacity");
+        }
+        if (size + 1 >= data.length) {
+            grow();
+        }
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T value = data[index];
+        System.arraycopy(data,index + 1, data, index, size - index - 1);
+        size--;
+        return value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = elementValue(element);
+        return remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        int newCapacity = (int) (data.length * INCREASE_CAPACITY);
+        T[] newArrayCapacity = (T[]) new Object[newCapacity];
+        System.arraycopy(data, 0, newArrayCapacity, 0, data.length);
+        data = newArrayCapacity;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index is out of capacity");
+        }
+    }
+
+    private int elementValue(T element) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == element || data[i] != null && data[i].equals(element)) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException("Element is not found:" + element);
     }
 }
