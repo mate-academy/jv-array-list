b39e1a83af89da821da7fcad402327a2a8443ca7
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-08-31 14:40:10 +0300

    initial commit some variables created

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,18 @@
 package core.basesyntax;
 
+
 public class ArrayList<T> implements List<T> {
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    transient Object[] elementData;
+    private static final int DEFAULT_CAPACITY = 10;
+
+
+    public ArrayList() {this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;}
+
+    //private Object[] grow() {
+    //    return grow(size + 1);
+    //}
     @Override
     public void add(T value) {
 
