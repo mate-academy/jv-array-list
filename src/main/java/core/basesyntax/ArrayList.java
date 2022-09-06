86d2fe448365e24a9e3a91d54a523cc44b51af9d
Serhii Rovenskyi <katapulta13@gmail.com>
2022-09-06 11:45:48 +0300

    Version 2. Mistakes were fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -2,40 +2,37 @@ package core.basesyntax;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private static final Object[] EMPTY_ELEMENTDATA = {};
-    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elementData = new Object[1];
-    private int modCount = 0;
-    private int size = 0;
+    private T[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void add(T value) {
-        modCount++;
-        if (size == elementData.length) {
-            elementData = grow();
+        if (size == elements.length) {
+            elements = (T[]) grow();
         }
-        elementData[size] = value;
+        elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
         rangeCheckForAdd(index);
-        modCount++;
-        final int s;
-        Object[] elementData;
-        if ((s = size) == (elementData = this.elementData).length) {
-            elementData = grow();
+        if (size == elements.length) {
+            elements = (T[]) grow();
         }
-        System.arraycopy(elementData, index,
-                elementData, index + 1,
-                s - index);
-        elementData[index] = value;
-        size = s + 1;
+        System.arraycopy(elements, index,
+                elements, index + 1,
+                size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
@@ -48,50 +45,34 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         rangeCheckForGet(index);
-        return (T) elementData[index];
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
         rangeCheckForGet(index);
-        T oldValue = (T) elementData[index];
-        elementData[index] = value;
+        T oldValue = (T) elements[index];
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
         rangeCheckForGet(index);
-        final Object[] es = elementData;
-
-        @SuppressWarnings("unchecked") T oldValue = (T) es[index];
-        fastRemove(es, index);
-
-        return oldValue;
+        T removedElement = elements[index];
+        System.arraycopy(elements, index + 1, elements, index,size - index - 1);
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        final Object[] es = elementData;
         final int size = this.size;
-        int i = 0;
-        found: {
-            if (element == null) {
-                for (; i < size; i++) {
-                    if (es[i] == null) {
-                        break found;
-                    }
-                }
-            } else {
-                for (; i < size; i++) {
-                    if (element.equals(es[i])) {
-                        break found;
-                    }
-                }
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], element)) {
+                return remove(i);
             }
-            throw new NoSuchElementException("Element wasn't found");
         }
-        fastRemove(es, i);
-        return element;
+        throw new NoSuchElementException("element: '" + element + "' does not exist in the list");
     }
 
     @Override
@@ -104,43 +85,14 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private Object[] grow(int minCapacity) {
-        return elementData = Arrays.copyOf(elementData,
-                newCapacity(minCapacity));
-    }
-
     private Object[] grow() {
-        return grow(size + 1);
-    }
-
-    private int newCapacity(int minCapacity) {
-        int oldCapacity = elementData.length;
+        int oldCapacity = elements.length;
         int newCapacity = oldCapacity + (oldCapacity >> 1);
-        if (newCapacity - minCapacity <= 0) {
-            if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
-                return Math.max(DEFAULT_CAPACITY, minCapacity);
-            }
-            if (minCapacity < 0) {
-                throw new OutOfMemoryError();
-            }
-            return minCapacity;
-        }
-        return (newCapacity - MAX_ARRAY_SIZE <= 0)
-                ? newCapacity
-                : hugeCapacity(minCapacity);
-    }
-
-    private static int hugeCapacity(int minCapacity) {
-        if (minCapacity < 0) {
-            throw new OutOfMemoryError();
-        }
-        return (minCapacity > MAX_ARRAY_SIZE)
-                ? Integer.MAX_VALUE
-                : MAX_ARRAY_SIZE;
+        return elements = Arrays.copyOf(elements,
+                newCapacity);
     }
 
     private void fastRemove(Object[] es, int i) {
-        modCount++;
         final int newSize;
         if ((newSize = size - 1) > i) {
             System.arraycopy(es, i + 1, es, i, newSize - i);
@@ -149,14 +101,14 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void rangeCheckForGet(int index) {
-        if ((index >= size) || (index < 0)) {
+        if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
                     + " doesn't exist in ArrayList. Please enter value from 0 to " + size);
         }
     }
 
     public void rangeCheckForAdd(int index) {
-        if ((index > size) || (index < 0)) {
+        if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
                     + " of bounds the ArrayList. Please enter value from 0 to " + size);
         }
--- src/main/java/core/basesyntax/Help.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Help {
-    public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.rangeCheckForAdd(5);
-    }
-}
