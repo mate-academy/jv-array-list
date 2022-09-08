2585fc9e325b1589995aac5931cda5cfd5891848
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-09-08 18:32:49 +0300

    code refactored after review

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -13,7 +13,9 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        grow();
+        if (size == elementData.length) {
+            grow();
+        }
         elementData[size] = value;
         size++;
     }
@@ -23,25 +25,21 @@ public class ArrayList<T> implements List<T> {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Such index " + index + " doesn't exist");
         }
-        grow();
+        if (size == elementData.length) {
+            grow();
+        }
         System.arraycopy(elementData, index, elementData, index + 1, size - index);
         elementData[index] = value;
         size++;
-    }
-
-    public Object[] toArray(List<T> list) {
-        Object[] arrayFromList = new Object[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            arrayFromList[i] = list.get(i);
-        }
-        return arrayFromList;
     }
 
     @Override
     public void addAll(List<T> list) {
         Object[] newArrayList = new Object[elementData.length + list.size()];
         System.arraycopy(elementData, 0, newArrayList, 0, size);
-        System.arraycopy(toArray(list), 0, newArrayList, size, list.size());
+        for (int i = 0; i < list.size(); i++) {
+            newArrayList[size+i] = list.get(i);
+        }
         elementData = newArrayList;
         size += list.size();
     }
@@ -49,20 +47,26 @@ public class ArrayList<T> implements List<T> {
     @Override
     @SuppressWarnings("unchecked")
     public T get(int index) {
-        rangeCheckForAdd(index);
+        if (index >= size || index < 0) {
+            rangeCheck(index);
+        }
         return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        rangeCheckForAdd(index);
+        if (index >= size || index < 0) {
+            rangeCheck(index);
+        }
         elementData[index] = value;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public T remove(int index) {
-        rangeCheckForAdd(index);
+        if (index >= size || index < 0) {
+            rangeCheck(index);
+        }
         T removedElement = (T) elementData[index];
         System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
         size--;
@@ -82,7 +86,7 @@ public class ArrayList<T> implements List<T> {
                 return element;
             }
         }
-        throw new NoSuchElementException();
+        throw new NoSuchElementException("Such element doesn't exist in this arrayList");
     }
 
     @Override
@@ -96,16 +100,13 @@ public class ArrayList<T> implements List<T> {
     }
 
     public void grow() {
-        if (size == elementData.length) {
             Object[] newArray = new Object[elementData.length + (elementData.length / 2)];
             System.arraycopy(elementData, 0, newArray, 0, size);
             elementData = newArray;
-        }
+
     }
 
-    private void rangeCheckForAdd(int index) {
-        if (index >= size || index < 0) {
+    private void rangeCheck(int index) {
             throw new ArrayListIndexOutOfBoundsException("Such index " + index + " doesn't exist");
-        }
     }
 }
