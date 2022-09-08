d8d4067c1759a4214d14e6d0696657607da72cfc
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-09-08 12:27:19 +0300

    code refactored after reading common mistakes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,18 +3,18 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private Object[] elements;
     private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
     private int size;
 
     public ArrayList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        elementData = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
         grow();
-        elements[size] = value;
+        elementData[size] = value;
         size++;
     }
 
@@ -24,14 +24,14 @@ public class ArrayList<T> implements List<T> {
             throw new ArrayListIndexOutOfBoundsException("Such index " + index + " doesn't exist");
         }
         grow();
-        System.arraycopy(elements, index, elements, index + 1, size - index);
-        elements[index] = value;
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
         size++;
     }
 
-    public Object[] listToArrayList(List<T> list) {
+    public Object[] toArray(List<T> list) {
         Object[] arrayFromList = new Object[list.size()];
-        for( int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             arrayFromList[i] = list.get(i);
         }
         return arrayFromList;
@@ -39,10 +39,10 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        Object[] newArrayList = new Object[elements.length + list.size()];
-        System.arraycopy(elements, 0, newArrayList, 0, size);
-        System.arraycopy(listToArrayList(list), 0, newArrayList, size, list.size());
-        elements = newArrayList;
+        Object[] newArrayList = new Object[elementData.length + list.size()];
+        System.arraycopy(elementData, 0, newArrayList, 0, size);
+        System.arraycopy(toArray(list), 0, newArrayList, size, list.size());
+        elementData = newArrayList;
         size += list.size();
     }
 
@@ -50,21 +50,21 @@ public class ArrayList<T> implements List<T> {
     @SuppressWarnings("unchecked")
     public T get(int index) {
         rangeCheckForAdd(index);
-        return (T) elements[index];
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
         rangeCheckForAdd(index);
-        elements[index] = value;
+        elementData[index] = value;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public T remove(int index) {
         rangeCheckForAdd(index);
-        T removedElement = (T) elements[index];
-        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        T removedElement = (T) elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
         size--;
         return removedElement;
     }
@@ -73,27 +73,17 @@ public class ArrayList<T> implements List<T> {
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
             if (element == null) {
-                if (elements[i] == null) {
+                if (elementData[i] == null) {
                     remove(i);
                     return element;
                 }
-            } else if (element.equals(elements[i])) {
+            } else if (element.equals(elementData[i])) {
                 remove(i);
                 return element;
             }
         }
         throw new NoSuchElementException();
     }
-
-    public boolean contains(T element) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i].equals(element)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 
     @Override
     public int size() {
@@ -106,10 +96,10 @@ public class ArrayList<T> implements List<T> {
     }
 
     public void grow() {
-        if (size == elements.length) {
-            Object[] newArray = new Object[elements.length + (elements.length / 2)];
-            System.arraycopy(elements, 0, newArray, 0, size);
-            elements = newArray;
+        if (size == elementData.length) {
+            Object[] newArray = new Object[elementData.length + (elementData.length / 2)];
+            System.arraycopy(elementData, 0, newArray, 0, size);
+            elementData = newArray;
         }
     }
 
