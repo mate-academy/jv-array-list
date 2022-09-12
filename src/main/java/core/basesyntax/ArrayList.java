41d4aaefaf66d8c684043d4491cb296cd6d4fed2
Mykyta Hulianytskyi <yadiver82@gmail.com>
2022-09-12 15:38:27 +0300

    v1.0 all overrided methods done, some errors should be corrected

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,109 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+@SuppressWarnings("unchecked")
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] arrayList;
+
+    private int size;
+
+    public ArrayList() {
+        arrayList = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        add(value, size);
     }
 
     @Override
     public void add(T value, int index) {
-
+        boundsCheck(index);
+        if (size == arrayList.length) {
+            arrayList = grow(size + 1);
+        }
+        System.arraycopy(arrayList, index,
+                arrayList, index + 1,
+                size - index);
+        arrayList[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        T[] a = (T[]) new Object[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            a[i] = list.get(i);
+        }
+        if (a.length > (arrayList.length - size)) {
+            arrayList = grow(size + a.length);
+        }
+        System.arraycopy(a, 0, arrayList, size, a.length);
+        size += a.length;
+    }
 
+    private T[] grow(int minCapacity) {
+        return arrayList = Arrays.copyOf(arrayList, arrayList.length >> 1);
     }
 
     @Override
     public T get(int index) {
-        return null;
+        boundsCheck(index);
+        return arrayList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        boundsCheck(index);
+        arrayList[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        boundsCheck(index);
+        T oldValue = arrayList[index];
+        fastRemove(index);
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < arrayList.length; i++) {
+            if (element == arrayList[i] || element.equals(arrayList[i])) {
+                T oldValue = arrayList[i];
+                fastRemove(i);
+                return oldValue;
+            }
+        }
+        throw new NoSuchElementException("there is no such element present:" + element);
+    }
+
+    private void fastRemove(int i) {
+        final int newSize;
+        if ((newSize = size - 1) > i) {
+            System.arraycopy(arrayList, i + 1, arrayList, i, newSize - i);
+        }
+        arrayList[size = newSize] = null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (size == 0);
+    }
+
+    private void boundsCheck(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("index: " + index
+                    + " is more than actual size: " + size);
+        }
     }
 }
