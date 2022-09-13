32c45c8bb5bf98fa0876c617df5f92d61a343390
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-13 22:46:50 +0300

    Optimize code

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,9 +3,7 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-
     private static final int INITIAL_CAPACITY = 10;
-
     private Object[] objects;
     private int size;
 
@@ -15,67 +13,61 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (size() == objects.length) {
+        if (size == objects.length) {
             grow();
         }
-        objects[size()] = value;
+        objects[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > size() || index < 0) {
+        if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index is invalid!");
         }
-        if (index == size() - 1 && index == size()) {
+        if (index == size - 1 && index == size()) {
             this.add(value);
         } else {
             shift(index);
             objects[index] = value;
             size++;
         }
+
     }
 
     @Override
     public void addAll(List<T> list) {
-        if (!list.isEmpty()) {
-            for (int i = 0; i < list.size(); i++) {
-                this.add(list.get(i));
-            }
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        if (checkIndex(index)) {
-            return (T) objects[index];
-        }
-        throw new ArrayListIndexOutOfBoundsException("Index is invalid!");
+        checkIndex(index);
+        return (T) objects[index];
+
     }
 
     @Override
     public void set(T value, int index) {
-        if (checkIndex(index)) {
-            objects[index] = value;
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("Index is invalid!");
-        }
+        checkIndex(index);
+        objects[index] = value;
+
     }
 
     @Override
     public T remove(int index) {
-        if (checkIndex(index)) {
-            Object element = objects[index];
-            collapse(index);
-            size--;
-            return (T) element;
-        }
-        throw new ArrayListIndexOutOfBoundsException("Index is invalid!");
+        checkIndex(index);
+        T removedElement = (T) objects[index];
+        System.arraycopy(objects, index + 1, objects, index, size - index - 1);
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (element != null) {
                 if (element.equals(objects[i])) {
                     return remove(i);
@@ -94,40 +86,29 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        return size() == 0;
+        return size == 0;
     }
 
-    private boolean checkIndex(int index) {
-        return index > 0 && index < size() || index == 0;
+    private void checkIndex(int index) {
+        if (!(index > 0 && index < size || index == 0)) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
     }
 
     private void grow() {
-        int newSize = size() + (size() >>> 1);
+        int newSize = size + (size >>> 1);
         Object[] newArray = new Object[newSize];
         System.arraycopy(objects, 0, newArray, 0, size());
         objects = newArray;
     }
 
     private void shift(int index) {
-        Object[] newArray = new Object[size() + 1];
+        Object[] newArray = new Object[size + 1];
         if (index == 0) {
             System.arraycopy(objects, 0, newArray, 1, size());
         } else {
             System.arraycopy(objects, 0, newArray, 0, index);
             System.arraycopy(objects, index, newArray, index + 1, size() - index);
-        }
-        objects = newArray;
-    }
-
-    private void collapse(int index) {
-        Object[] newArray = new Object[size() - 1];
-        if (index == 0) {
-            System.arraycopy(objects, 1, newArray, 0, size() - 1);
-        } else if (index == size() - 1) {
-            System.arraycopy(objects, 0, newArray, 0, size() - 1);
-        } else {
-            System.arraycopy(objects, 0, newArray, 0, index - 1);
-            System.arraycopy(objects, index + 1, newArray, index, size() - index - 1);
         }
         objects = newArray;
     }
