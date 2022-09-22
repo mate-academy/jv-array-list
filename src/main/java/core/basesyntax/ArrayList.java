afafa82c0ef921be0b9ef93e5955cd3efc43814a
DmytroChernenko <mitresko12@gmail.com>
2022-09-22 14:29:16 +0300

    counter to size
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,7 +4,7 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int counter;
+    private int size;
     private T[] values = (T[]) new Object[DEFAULT_CAPACITY];
 
     @Override
