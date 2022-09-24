e01ef4e55848604d7bc5686af1dbdf24e44015be
Natalya Shcheglova <uralgnome@gmail.com>
2022-09-24 22:31:56 +0200

    Mistakes were fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,61 +8,37 @@ public class ArrayList<T> implements List<T> {
     private int size;
 
     public ArrayList() {
-        this.elementData = (T[]) new Object[DEFAULT_CAPACITY];
-        this.size = 0;
-    }
-
-    public T[] grow(T value, int index) {
-        T[] newElementData = null;
-        if (size < elementData.length) {
-            newElementData = (T[]) new Object[elementData.length];
-        }
-        if (size == elementData.length) {
-            newElementData = (T[]) new Object[elementData.length + elementData.length / 2];
-        }
-        System.arraycopy(elementData, 0, newElementData, 0, index);
-        newElementData[index] = value;
-        System.arraycopy(elementData, index, newElementData, index + 1, size - index);
-        elementData = newElementData;
-        size++;
-        return elementData;
-    }
-
-    public void indexCheck(int index) {
-        if (index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index of value has to be more or equal 0");
-        }
-        if (index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index of value has to be less then arrayList size");
-        }
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size < elementData.length) {
-            elementData[size] = value;
-            size++;
-        } else {
-            grow(value, size);
+        if (size == elementData.length) {
+            grow();
         }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
         if (index == size) {
-            this.add(value);
-            return;
+            add(value);
+        } else {
+            indexCheck(index);
+            if (size == elementData.length) {
+                grow();
+            }
+            System.arraycopy(elementData, index, elementData, index + 1, size - index);
+            elementData[index] = value;
+            size++;
         }
-        indexCheck(index);
-        grow(value, index);
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            this.add(list.get(i));
+            add(list.get(i));
         }
     }
 
@@ -81,23 +57,17 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         indexCheck(index);
-        T[] newElementData = (T[]) new Object[elementData.length];
-        System.arraycopy(elementData, 0, newElementData, 0, index);
-        System.arraycopy(elementData, index + 1, newElementData, index, size - index - 1);
-        T value = elementData[index];
-        elementData = newElementData;
+        T removedElement = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
         size--;
-        return value;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        T removedElement = null;
         for (int i = 0; i < size; i++) {
             if (element == elementData[i] || element != null && element.equals(elementData[i])) {
-                removedElement = elementData[i];
-                remove(i);
-                return removedElement;
+                return remove(i);
             }
         }
         throw new NoSuchElementException("There is no such element in the ArrayList");
@@ -111,5 +81,18 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size <= 0;
+    }
+
+    private void grow() {
+        T[] newArray = (T[]) new Object[(int) (elementData.length * 1.5)];
+        System.arraycopy(elementData, 0, newArray, 0, size);
+        elementData = newArray;
+    }
+
+    private void indexCheck(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index of value has to be more or equal 0 and less then arrayList size");
+        }
     }
 }
