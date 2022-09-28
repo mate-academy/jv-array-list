10ef623abbbf1dd9f012f7f6a85f61ca372030a8
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2022-09-28 17:13:37 +0300

    fixes were made

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final double GROWTH_INDEX = 1.5;
     private static final int DEFAULT_CAPACITY = 10;
-    private int currentSize;
+    private int size;
     private T[] array;
 
     public ArrayList() {
@@ -14,22 +14,22 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (!checkSize()) {
+        if (!(size < array.length)) {
             resizeArray();
         }
-        array[currentSize] = value;
-        currentSize++;
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > currentSize) {
+        if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("index [" + index + "] is invalid");
         }
-        if (checkSize()) {
-            System.arraycopy(array, index, array, index + 1, currentSize - index);
+        if (size < array.length) {
+            System.arraycopy(array, index, array, index + 1, size - index);
             array[index] = value;
-            currentSize++;
+            size++;
         } else {
             resizeArray();
             add(value, index);
@@ -38,9 +38,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        while (list.size() > (array.length - currentSize)) {
-            resizeArray();
-        }
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
@@ -62,15 +59,14 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkIndex(index);
         T removedElement = array[index];
-        int newSize = currentSize - 1;
-        System.arraycopy(array, index + 1, array, index, newSize - index);
-        currentSize--;
+        System.arraycopy(array, index + 1, array, index, size - 1 - index);
+        size--;
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < currentSize; i++) {
+        for (int i = 0; i < size; i++) {
             if (element == array[i] || element != null && element.equals(array[i])) {
                 return remove(i);
             }
@@ -80,22 +76,18 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return currentSize;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return currentSize == 0;
+        return size == 0;
     }
 
     private void checkIndex(int index) {
-        if (index < 0 || index >= currentSize) {
+        if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("index [" + index + "] is invalid");
         }
-    }
-
-    private boolean checkSize() {
-        return currentSize < array.length;
     }
 
     private void resizeArray() {
