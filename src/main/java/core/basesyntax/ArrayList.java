834478aa6a2be2b3e8354732ca83f9a5b10fc8a4
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-29 18:28:43 +0300

    v1 own ArrayList implement

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,101 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROW_TIMES_VALUE = 1.5;
+    private Object[] data = new Object[DEFAULT_CAPACITY];
+    private int size = 0;
+
     @Override
     public void add(T value) {
-
+        if (data.length == this.size) {
+            resize();
+        }
+        data[this.size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        size++;
+        checkIndex(index);
+        if (data.length == this.size) {
+            resize();
+        }
+        for (int i = this.size - 2; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        this.data[index] = value;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        this.size--;
+        T removedData = (T) data[index];
+        for (int i = index; i < this.size; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        this.data[size] = null;
+        return removedData;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < this.size; i++) {
+            T currentElement = (T) this.data[i];
+            if (currentElement == element || currentElement != null
+                    && currentElement.equals(element)) {
+                this.remove(i);
+                return currentElement;
+            }
+        }
+        throw new NoSuchElementException("Cannot find element <"
+                + (element == null ? "null" : element.toString()) + ">");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new ArrayListIndexOutOfBoundsException("index <" + index
+                    + "> out of ArrayList current size bounds <" + this.size + ">");
+        }
+    }
+
+    private void resize() {
+        int newCapacity = (int)(data.length * GROW_TIMES_VALUE);
+        Object[] resizedData = new Object[newCapacity];
+        System.arraycopy(this.data, 0, resizedData, 0, this.data.length);
+        this.data = resizedData;
     }
 }
