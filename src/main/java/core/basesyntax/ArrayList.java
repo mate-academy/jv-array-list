c4647dcb438a5f9d11480f023b54017fee67969a
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-10-06 16:58:18 +0300

    I`t pass all tests, but i think i have terrible solve of this task

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,135 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double resizeCoefficient = 1.5;
+    private Object[] elementData;
+    private int numberOfObjects;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+        numberOfObjects = 0;
+    }
+
     @Override
     public void add(T value) {
+        ensureCapasity();
+        elementData[numberOfObjects] = value;
+        numberOfObjects++;
+    }
 
+    private void ensureCapasity() {
+        if (numberOfObjects >= elementData.length) {
+            int newSize =(int) (numberOfObjects * resizeCoefficient);
+            elementData = Arrays.copyOf(elementData, newSize);
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > numberOfObjects) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index");
+        }
+        ensureCapasity();
 
+        Object[] tempFirstPart = Arrays.copyOfRange(elementData, 0, index);
+        Object[] tempSecondPart = Arrays.copyOfRange(elementData, index, elementData.length);
+        int counterForSecondArray = 0;
+        for (int i = 0; i < elementData.length; i++) {
+            if (i == index) {
+                numberOfObjects++;
+                elementData[i] = value;
+                continue;
+            }
+            if (i < index) {
+                elementData[i] = tempFirstPart[i];
+                continue;
+            }
+            if (i > index) {
+                elementData[i] = tempSecondPart[counterForSecondArray];
+                counterForSecondArray++;
+                continue;
+            }
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for(int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
+        checkIndex(index);
+        elementData[index] = value;
+    }
 
+    private void checkIndex(int index) {
+        if (index < 0 || index >= numberOfObjects) {
+            throw new ArrayListIndexOutOfBoundsException("Out of bound execution");
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+
+        Object[] tempFirstPart = Arrays.copyOfRange(elementData, 0, index);
+        Object[] tempSecondPart = Arrays.copyOfRange(elementData, index+1, elementData.length);
+        numberOfObjects--;
+        Object result = elementData[index];
+        int counterForSecondArray = 0;
+        for (int i = 0; i < numberOfObjects; i++) {
+            if (i < tempFirstPart.length && i != index) {
+                elementData[i] = tempFirstPart[i];
+                continue;
+            }
+            if (i >= index && i < tempSecondPart.length) {
+                elementData[i] = tempSecondPart[counterForSecondArray];
+                counterForSecondArray++;
+                continue;
+            }
+        }
+        return (T) result;
     }
 
     @Override
     public T remove(T element) {
+        Object result = null;
+        for (int i = 0; i < elementData.length; i++) {
+            if (elementData[i] == null ? element == elementData[i] : elementData[i].equals(element)) {
+                result = elementData[i];
+                remove(i);
+                return (T) result;
+            }
+        }
+        if (result == null) {
+            throw new NoSuchElementException("Can`t find file " + element + " in list");
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return numberOfObjects;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if (numberOfObjects > 0) {
+            return false;
+        }
+        return true;
     }
 }
