e24d155470af4772f4ad788a8c53684f13a7638d
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-10 18:24:08 +0300

    rewrote code

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,33 +1,47 @@
 package core.basesyntax;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
+@SuppressWarnings("unchecked")
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elements = new Object[DEFAULT_CAPACITY];
-    private int index;
+    private int size;
+    private T[] elementData;
+
+    public ArrayList() {
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    private void grow() {
+        if (size == elementData.length) {
+            int oldCapacity = elementData.length;
+            if (oldCapacity > 0) {
+                int newCapacity = elementData.length + (elementData.length >> 1);
+                elementData = Arrays.copyOf(elementData, newCapacity);
+            }
+        }
+    }
 
     @Override
     public void add(T value) {
-        willResizeIfNeed();
-        elements[index] = value;
-        index++;
+        grow();
+        elementData[size] = value;
+        size += 1;
     }
 
     @Override
     public void add(T value, int index) {
-        exceptions(index, new Throwable().getStackTrace()[0].getMethodName());
-        if (elements[index] != null) {
-            final Object[] newElems = new Object[elements.length];
-            System.arraycopy(elements, 0, newElems, 0, index);
-            newElems[index] = value;
-            System.arraycopy(elements, index, newElems, index + 1, elements.length - index - 1);
-            elements = newElems;
-            this.index++;
-            willResizeIfNeed();
-            return;
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index is incorrect");
         }
-        add(value);
+        grow();
+        T[] newElementData = (T[]) new Object[elementData.length];
+        System.arraycopy(elementData, 0, newElementData, 0, index);
+        newElementData[index] = value;
+        System.arraycopy(elementData, index, newElementData, index + 1, size - index);
+        elementData = newElementData;
+        size += 1;
     }
 
     @Override
@@ -39,99 +53,68 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        exceptions(index, new Throwable().getStackTrace()[0].getMethodName());
-        if (index + 1 == elements.length) {
-            willResizeIfNeed();
-        }
-        return (T) elements[index];
-    }
-
-    private void willResizeIfNeed() {
-        if (this.index + 1 > elements.length) {
-            Object[] moreSizeArray = new Object[elements.length + elements.length / 2];
-            System.arraycopy(elements, 0, moreSizeArray, 0, elements.length);
-            elements = moreSizeArray;
-        }
+        rangeCheck(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        exceptions(index, new Throwable().getStackTrace()[0].getMethodName());
-        if (elements[index] != null) {
-            final Object[] newElems = new Object[elements.length];
-            System.arraycopy(elements, 0, newElems, 0, index);
-            newElems[index] = value;
-            System.arraycopy(elements, index + 1, newElems, index + 1, elements.length - index - 1);
-            elements = newElems;
-        }
+        rangeCheck(index);
+        T[] newElementData = (T[]) new Object[elementData.length];
+        System.arraycopy(elementData, 0, newElementData, 0, index);
+        newElementData[index] = value;
+        System.arraycopy(elementData, index + 1, newElementData, index + 1, size - index);
+        elementData = newElementData;
+
     }
 
     @Override
     public T remove(int index) {
-        exceptions(index, new Throwable().getStackTrace()[0].getMethodName());
-        final Object[] es = elements;
+        rangeCheck(index);
+        final Object[] es = elementData;
         T oldValue = (T) es[index];
         fastRemove(es, index);
-
         return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < index; i++) {
-            if (element != null) {
-                if (element.equals(elements[i])) {
+        if (element != null) {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elementData[i])) {
                     remove(i);
                     return element;
                 }
             }
         }
         if (element == null) {
-            index--;
+            size--;
             return element;
         }
-        throw new NoSuchElementException("We cant find this element " + element);
+        throw new NoSuchElementException("Can't find this element: " + element);
     }
 
     private void fastRemove(Object[] es, int i) {
         final int newSize;
-        if ((newSize = index - 1) > i) {
+        if ((newSize = size - 1) > i) {
             System.arraycopy(es, i + 1, es, i, newSize - i);
         }
-        es[index = newSize] = null;
+        es[size = newSize] = null;
     }
 
     @Override
     public int size() {
-        return index;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return elements[0] == null;
+        return size == 0;
     }
 
-    private void exceptions(int index, String methodName) {
-        if (methodName.equals("add")) {
-            if (index > this.index) {
-                throw new ArrayListIndexOutOfBoundsException("we cant initialise current index "
-                        + index + " while index " + this.index + " is null");
-            }
-            if (index < 0) {
-                throw new ArrayListIndexOutOfBoundsException("we cant add value on index "
-                        + index + "because this index is negative");
-            }
-            return;
-        }
-        if (index > this.index - 1) {
-            throw new ArrayListIndexOutOfBoundsException("we cant "
-                    + methodName + " value on current index "
-                    + index + " while index " + this.index + " is null");
-        }
-        if (index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("we cant "
-                    + methodName + " value on index "
-                    + index + " because current index is negative");
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index is incorrect");
         }
     }
 }
