e24f70958394968504725b65963e56e6a0d2eb67
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-10-11 14:48:12 +0300

    Override methods and create checkException and capacityIncrease methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,106 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    private void checkException(int index) {
+        if (size != 0 && (index >= size || index < 0)) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index
+                    + " out of bounds for length " + size);
+        }
+    }
+
+    public void capacityIncrease() {
+        Object[] newArray = new Object[size + size / 2];
+        System.arraycopy(elements,0, newArray, 0,elements.length);
+        elements = newArray;
+    }
+
     @Override
     public void add(T value) {
+        if (size >= DEFAULT_CAPACITY) {
+            capacityIncrease();
+        }
 
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index != size) {
+            checkException(index);
+        }
 
+        if (size >= DEFAULT_CAPACITY) {
+            capacityIncrease();
+        }
+
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkException(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkException(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkException(index);
+        T removedElement = (T) elements[index];
+        if (index + 1 < size) {
+            System.arraycopy(elements, index + 1, elements, index, size - index);
+        } else {
+            System.arraycopy(elements, index, elements, index, size - index);
+        }
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], element)) {
+                return remove(i);
+            }
+        }
+
+        throw new NoSuchElementException("There is no such element"
+                + "[" + element + "] in ArrayList");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size > Integer.MAX_VALUE ? Integer.MAX_VALUE : size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
