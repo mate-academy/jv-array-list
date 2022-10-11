5b00ecb78762fff77a83f3d53d3a6eddb09dc3fa
yehbreus <yeh.breus@gmail.com>
2022-10-11 15:11:41 +0300

    implemented ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,106 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final String ARRAY_LIST_IOOB_EXCEPTION_TEXT
+            = "No element with such index";
+    private static final int WRONG_INDEX = -1;
+    private T[] data;
+    private int size;
+
+    public ArrayList() {
+        this.data = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == data.length) {
+            grow();
+        }
+        data[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(ARRAY_LIST_IOOB_EXCEPTION_TEXT);
+        }
+        if (size == data.length) {
+            grow();
+        }
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T oldValue = data[index];
+        size--;
+        System.arraycopy(data, index + 1, data, index, size - index);
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = indexOf(element);
+        if (index != -1) {
+            return remove(index);
+        }
+        throw new NoSuchElementException("No such element: " + element + " in ArrayList");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        T[] temp = data;
+        data = (T[]) new Object[size + (size >> 1)];
+        System.arraycopy(temp, 0, data, 0, size);
+    }
+
+    public void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(ARRAY_LIST_IOOB_EXCEPTION_TEXT);
+        }
+    }
+
+    public int indexOf(T value) {
+        for (int i = 0; i < size; i++) {
+            if (value == data[i] || value != null && value.equals(data[i])) {
+                return i;
+            }
+        }
+        return WRONG_INDEX;
     }
 }
