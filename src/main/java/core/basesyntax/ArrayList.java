5c30a4b227e8574ff13e36498806b15a1d52c8c4
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-10-11 09:49:33 +0300

    Removed class Objects

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int MAX_CAPACITY = 10;
@@ -66,7 +65,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element,obj[i])) {
+            if ((element == obj[i]) || (element != null && element.equals(obj[i]))) {
                 return remove(i);
             }
         }
