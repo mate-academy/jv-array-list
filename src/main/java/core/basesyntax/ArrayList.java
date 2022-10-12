d235ea533a0a1fed515c8dc482df98eab2078d0f
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-10-12 11:29:01 +0300

    Realize methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,102 @@
 package core.basesyntax;
 
-public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+import java.util.NoSuchElementException;
 
+public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE=10;
+    private static final double AMMOUT_OF_INCREASE=1.5;
+    private Object[] array;
+    private  int currentSize;
+
+    public ArrayList() {
+    array = new Object[DEFAULT_SIZE];
     }
 
     @Override
-    public void add(T value, int index) {
+    public void add(T value) {
+        if (currentSize == array.length) {
+            increaseSize();
+        }
+        array[currentSize] = value;
+        currentSize++;
+    }
 
+
+    @Override
+    public void add(T value, int index) {
+        if (index < 0 || index > currentSize) {
+            throw new ArrayListIndexOutOfBoundsException();
+        }
+        if (currentSize == array.length) {
+            increaseSize();
+        }
+        System.arraycopy(array, index, array,index + 1, currentSize - index);
+        array[index] = value;
+        currentSize++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) array[index];
     }
+
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+
+        checkIndex(index);
+        Object deletedEl = array[index];
+        System.arraycopy(array, index + 1, array, index, currentSize - 1 - index);
+        currentSize--;
+        return (T) deletedEl;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < currentSize; i++) {
+            if (element != null && element.equals(array[i]) || element == array[i]) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
+
 
     @Override
     public int size() {
-        return 0;
+
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+
+        return currentSize == 0;
+    }
+    private void checkIndex(int index) {
+        if (index >= currentSize || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    String.format("Index %s out of bound for length %s", index, currentSize));
+        }
+    }
+
+    public void increaseSize() {
+        Object[] newArray = (T[]) new Object[(int) (currentSize * 1.5)];
+        System.arraycopy(array, 0, newArray, 0, currentSize);
+        array = newArray;
     }
 }
--- src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
+++ src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
 public class ArrayListIndexOutOfBoundsException extends RuntimeException {
+    public ArrayListIndexOutOfBoundsException() {
+        super();
+    }
+
     public ArrayListIndexOutOfBoundsException(String message) {
         super(message);
     }
