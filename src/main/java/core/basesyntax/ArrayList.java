546401be04ee95ef9ab69cf9cf098ed874e09efc
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-10-12 12:00:30 +0300

    created ArrayList class, overrode required methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,116 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private T[] array;
+
+    public ArrayList() {
+        this.array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        int currentSize = size;
+        if (currentSize == array.length) {
+            array = (T[]) resize();
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkForAdd(index);
+        int currentSize = size;
+        if (currentSize == array.length) {
+            array = (T[]) resize();
+        }
+        System.arraycopy(array, index, array, index + 1,
+                    currentSize - index);
+        array[index] = value;
+        size = currentSize + 1;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        int currentSize = size;
+        int index = 0;
+        int listSize = list.size();
+        if (listSize > (array.length - currentSize)) {
+            array = (T[]) resize();
+        }
+        size = currentSize + listSize;
+        for (int i = currentSize; i < size; i++) {
+            array[i] = list.get(index);
+            index++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkForGetAndSet(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkForGetAndSet(index);
+        T oldValue = array[index];
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkForGetAndSet(index);
+        T oldValue = array[index];
+        int newSize;
+        if ((newSize = size - 1) > index) {
+            System.arraycopy(array, index + 1, array, index, newSize - index);
+        }
+        array[size = newSize] = null;
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element || array[i] != null && array[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no such element in the list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can't add new value. "
+                    + "Index:" + index + ". Size: " + size);
+        }
+    }
+
+    private void checkForGetAndSet(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Out of bounds. "
+                    + "Index:" + index + ". Size: " + size);
+        }
+    }
+
+    private Object[] resize() {
+        int newCapacity = size + (size >> 1);
+        return array = Arrays.copyOf(array, newCapacity);
     }
 }
--- src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
+++ src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public class ArrayListIndexOutOfBoundsException extends RuntimeException {
     public ArrayListIndexOutOfBoundsException(String message) {
-        super(message);
+
     }
+
 }
+
