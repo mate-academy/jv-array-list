8263bbccb7f429dddc001b714c564307e74b2583
Oleksandr Drobot <drob4@ukr.net>
2022-10-28 07:56:58 +0300

    implemented methods according to the task(corrected)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,40 +4,19 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private T[] defaultArray;
+    private T[] array;
     private int size;
 
     public ArrayList() {
-        defaultArray = (T[]) new Object[DEFAULT_CAPACITY];
-    }
-
-    private T[] getNewCapacityArray(T[] array) {
-        int oldCapacity = array.length;
-        int newCapacity = oldCapacity + (oldCapacity >> 1);
-        T[] newCapacityArray = (T[]) new Object[newCapacity];
-        System.arraycopy(array, 0, newCapacityArray, 0, oldCapacity);
-        return newCapacityArray;
-    }
-
-    private T[] addValueByIndex(T[] array, int index) {
-        System.arraycopy(array, index, array, index + 1, size - index);
-        return array;
-    }
-
-    private T[] removeValueByIndex(T[] array, int index) {
-        System.arraycopy(array, index + 1, array, index, size - index - 1);
-        return array;
+        array = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == defaultArray.length) {
-            defaultArray = getNewCapacityArray(defaultArray);
-            defaultArray[size] = value;
-            size++;
-            return;
+        if (size == array.length) {
+            getNewSizeArray();
         }
-        defaultArray[size] = value;
+        array[size] = value;
         size++;
     }
 
@@ -46,53 +25,44 @@ public class ArrayList<T> implements List<T> {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("The index is out of range!");
         }
-        if (size == defaultArray.length) {
-            defaultArray = getNewCapacityArray(defaultArray);
+        if (size == array.length) {
+            getNewSizeArray();
         }
         if (index < size) {
-            defaultArray = addValueByIndex(defaultArray, index);
-            defaultArray[index] = value;
-            size++;
-            return;
+            System.arraycopy(array, index, array, index + 1, size - index);
         }
-        defaultArray[index] = value;
+        array[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            defaultArray[size] = list.get(i);
+            array[size] = list.get(i);
             size++;
-            if (size == defaultArray.length) {
-                defaultArray = getNewCapacityArray(defaultArray);
+            if (size == array.length) {
+                getNewSizeArray();
             }
         }
     }
 
     @Override
     public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("The index is out of range!");
-        }
-        return defaultArray[index];
+        checkIndex(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("The index is out of range!");
-        }
-        defaultArray[index] = value;
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("The index is out of range!");
-        }
-        T removedElement = defaultArray[index];
-        defaultArray = removeValueByIndex(defaultArray, index);
+        checkIndex(index);
+        T removedElement = array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
         size--;
         return removedElement;
     }
@@ -100,8 +70,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (element == defaultArray[i] || element != null && element.equals(defaultArray[i])) {
-                defaultArray = removeValueByIndex(defaultArray, i);
+            if (element == array[i] || element != null && element.equals(array[i])) {
+                System.arraycopy(array, i + 1, array, i, size - i - 1);
                 size--;
                 return element;
             }
@@ -117,5 +87,18 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void getNewSizeArray() {
+        int newSize = size + (size >> 1);
+        T[] newArray = (T[]) new Object[newSize];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("The index is out of range!");
+        }
     }
 }
