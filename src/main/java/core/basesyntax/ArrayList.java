8f6e35d38c0406191f05aa49d4f73b77eb4a58ed
Dmitri Kolos <skil@bigmir.net>
2022-10-30 15:04:56 +0200

    ArrayList solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double CAPACITY_MULTIPLIER = 1.5;
+    private T[] arrayList;
+    private int size = 0;
+
+    public ArrayList() {
+        arrayList = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void resizeArray() {
+        if (arrayList.length == size) {
+            T[] newArrayList = (T[]) new Object[(int) (arrayList.length * CAPACITY_MULTIPLIER)];
+            System.arraycopy(arrayList,0, newArrayList, 0, arrayList.length);
+            arrayList = newArrayList;
+        }
+    }
+
+    public void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index: index should be "
+                    + "inclusively between 0 and " + size);
+        }
+    }
+
+    public void checkIndexAddByIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index: index should be not "
+                    + "negative and not greater than " + size);
+        }
+    }
+
     @Override
     public void add(T value) {
+        resizeArray();
+        arrayList[size] = value;
+        size++;
 
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexAddByIndex(index);
+        resizeArray();
+        System.arraycopy(arrayList, index, arrayList, index + 1, size - index);
+        arrayList[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
 
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return arrayList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        arrayList[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T removedValue = arrayList[index];
+        System.arraycopy(arrayList, index + 1, arrayList, index, arrayList.length - size);
+        size--;
+        return removedValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element == arrayList[i] || element != null && element.equals(arrayList[i])) {
+                T removedValue = arrayList[i];
+                System.arraycopy(arrayList, i + 1, arrayList, i, arrayList.length - size);
+                size--;
+                return removedValue;
+            }
+        }
+        throw new NoSuchElementException("There is no such element in the list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
