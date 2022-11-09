6a12e98215f857cc59f86d12c112d4c00ed4ca5c
Yurii Trokhymenko <uta1965_@ukr.net>
2022-11-09 09:22:38 +0100

    added fixed after review

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -7,15 +7,13 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int INIT_SIZE = 10;
     private static final double ZOOM_SIZE = 1.5;
-    private static final String BUG_INDEX = "The specified index does not exist";
-    private static final String BUG_FOUND = "Specified element not found";
     private Object[] elementData = new Object[INIT_SIZE];
     private int size = 0;
 
     @Override
     public void add(T value) {
         if (elementData.length == size) {
-            resize(lengthZoom());
+            resize();
         }
         elementData[size] = value;
         size++;
@@ -23,11 +21,13 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(BUG_INDEX);
+        if (index == size) {
+            add(value);
+            return;
         }
+        checkIndexForAdd(index);
         if (elementData.length == size) {
-            resize(lengthZoom());
+            resize();
         }
         System.arraycopy(elementData, index, elementData, index + 1, size - index);
         elementData[index] = value;
@@ -37,12 +37,6 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void addAll(List<T> list) {
         int numNew = list.size();
-        if (numNew == 0) {
-            return;
-        }
-        if (numNew > (elementData.length - size)) {
-            resize((size + numNew));
-        }
         for (int i = 0; i < numNew; i++) {
             add(list.get(i));
         }
@@ -50,58 +44,40 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(BUG_INDEX);
-        }
+        checkIndex(index);
         return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(BUG_INDEX);
-        }
+        checkIndex(index);
         elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(BUG_INDEX);
-        }
-        final Object[] es = elementData;
-        T oldValue = (T) es[index];
-        fastRemove(es, index);
+        checkIndex(index);
+        T oldValue = (T) elementData[index];
+        fastRemove(elementData, index);
         return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        final Object[] es = elementData;
         int index = -1;
-        if (element == null) {
-            for (int i = 0; i < size; i++) {
-                if (es[i] == null) {
-                    index = i;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (element.equals(es[i])) {
-                    index = i;
-                    break;
-                }
+        for (int i = 0; i < size; i++) {
+            if (((element == null) && (elementData[i] == null))
+                    || ((element != null) && (element.equals(elementData[i])))) {
+                index = i;
+                break;
             }
         }
         try {
-            T result = (T) es[index];
+            T result = (T) elementData[index];
         } catch (IndexOutOfBoundsException e) {
-            throw new NoSuchElementException(BUG_FOUND);
+            throw new NoSuchElementException("Specified element not found: " + element);
         }
-        T oldValue = (T) es[index];
-        fastRemove(es, index);
-        return oldValue;
+        return remove(index);
     }
 
     @Override
@@ -120,11 +96,8 @@ public class ArrayList<T> implements List<T> {
                 + ", size=" + size + '}';
     }
 
-    public int lengthZoom() {
-        return (int) (elementData.length * ZOOM_SIZE);
-    }
-
-    public void resize(int newLength) {
+    public void resize() {
+        int newLength = (int) (elementData.length * ZOOM_SIZE);
         Object[] newArray = new Object[newLength];
         System.arraycopy(elementData, 0, newArray, 0, size);
         elementData = newArray;
@@ -136,5 +109,19 @@ public class ArrayList<T> implements List<T> {
             System.arraycopy(es, i + 1, es, i, newSize - i);
         }
         es[size = newSize] = null;
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index
+                    + ", Size: " + size);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index does not exist, Index: "
+                    + index + ", Size: " + size);
+        }
     }
 }
