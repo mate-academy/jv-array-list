f9b26d61d9e923579b3e1ad60e6481c77c5b79cc
Ilya Samilyak <samilyakilya@gmail.com>
2022-11-11 19:10:40 +0200

    HW-ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,106 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private T[] array;
+
+    public ArrayList() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
+        grow();
+        array[size] = value;
+        size++;
+    }
 
+    private void grow() {
+        if (array.length == size) {
+            T[] newArray = (T[]) new Object[(int) (array.length * 1.5)];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = (T[]) newArray;
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: "
+                    + index + " doesn't exist or less than 0");
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: "
+                    + index + " doesn't exist or less than 0");
+        }
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexForAdd(index);
+        grow();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        grow();
+        T removedElement = (T) array[index];
+        System.arraycopy(array, index + 1, array, index, size - index);
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = 0;
+        T elem = array[index];
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(element, array[i])) {
+                elem = array[i];
+                index = i;
+                remove(index);
+                return elem;
+            }
+        }
+        throw new NoSuchElementException("Can't find the element " + elem + " in this list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
