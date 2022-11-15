b73313f84a4b7ffb5833d6fff55f6a945fea0651
Sander <alex.kapusniak@gmail.com>
2022-11-15 14:39:21 +0200

    finish worked arraylist

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,122 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_ARRAY_LENGTH = 6;
+    private T[] array = (T[]) new Object[DEFAULT_ARRAY_LENGTH];
+    private int size = 0;
+
     @Override
     public void add(T value) {
-
+        resize();
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        checkIndexForAddByIndex(index);
+        resize();
 
+        if (index < size) {
+            System.arraycopy(array, index, array, index + 1, array.length - index - 1);
+        }
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        return removeElementFromList(index);
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index;
+        try {
+            index = findElementForRemove(element);
+        } catch (NoSuchMethodException e) {
+            throw new NoSuchElementException();
+        }
+        return removeElementFromList(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private T removeElementFromList(int index) {
+        T result = array[index];
+        if (index == size - 1) {
+            size--;
+            return result;
+        }
+        System.arraycopy(array, index + 1, array, index, array.length - index - 1);
+        size--;
+        return result;
+    }
+
+    private int findElementForRemove(T element) throws NoSuchMethodException {
+        boolean isPresent = false;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (Objects.equals(array[i], element)) {
+                isPresent = true;
+                index = i;
+                break;
+            }
+        }
+        if (!isPresent) {
+            throw new NoSuchMethodException();
+        }
+        return index;
+    }
+
+    private void checkIndexForAddByIndex(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range List");
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range List");
+        }
+    }
+
+    private void resize() {
+        if (array.length <= size) {
+            int newSize = (int) (array.length * 1.5);
+            array = Arrays.copyOf(array, newSize);
+        }
     }
 }
