dbf33edd4831b81bd60bb6ce72ab4bea5ee8e3e3
Artem Bordichuk <artyombordichuk@gmail.com>
2022-11-26 19:46:59 +0200

    Code refactor: All required methods was required. Made my custom 'ArrayList' behave the same as 'java.util.ArrayList'.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,128 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] objects = new Object[DEFAULT_CAPACITY];
+    private int size = 0;
+
+    private Object[] grow() {
+        return grow(size + 1);
+    }
+
+    private Object[] grow(int minCapacity) {
+        return objects = Arrays.copyOf(objects, newCapacity(minCapacity));
+    }
+
+    private int newCapacity(int minCapacity) {
+        int oldCapacity = objects.length;
+        return (int) (oldCapacity + (oldCapacity * 1.5));
+    }
+
     @Override
     public void add(T value) {
-
+        if (size < objects.length) {
+            objects[size] = value;
+            size++;
+        } else {
+            objects = grow();
+            if (size < objects.length) {
+                objects[size] = value;
+                size++;
+            }
+        }
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("The index is bigger than"
+                   + " array size or less than null");
+        }
+        if (size == objects.length) {
+            objects = grow();
+        }
+        System.arraycopy(objects, index,
+                objects, index + 1,
+                size - index);
+        objects[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        Object[] inputObjects = new Object[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            inputObjects[i] = list.get(i);
+        }
+        if ((objects.length - size) < inputObjects.length) {
+            objects = grow();
+        }
+        System.arraycopy(inputObjects, 0, objects, size, inputObjects.length);
+        size += inputObjects.length;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index > size - 1) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounce exception");
+        }
+        return (T) objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || (index > size - 1)) {
+            throw new ArrayListIndexOutOfBoundsException("Index out bounds exception");
+        }
+        objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        final int newSize;
+        Object oldObject;
+
+        if (index < 0 || (index > size - 1)) {
+            throw new ArrayListIndexOutOfBoundsException("The index is bigger than "
+                   + "array size or less than null");
+        }
+
+        if (objects[index] == null) {
+            oldObject = null;
+        } else {
+            oldObject = objects[index];
+        }
+
+        if ((newSize = size - 1) > index) {
+            System.arraycopy(objects, index + 1, objects, index, size - index);
+        }
+        objects[newSize] = null;
+        size = newSize;
+        return (T) oldObject;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(element, objects[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("The object " + element
+                + " didn't found in collection");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (size == 0);
     }
 }
