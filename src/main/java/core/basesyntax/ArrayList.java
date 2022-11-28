f07f17b8ec39082a81eaa53b580986dd8014938e
Artem Bordichuk <artyombordichuk@gmail.com>
2022-11-28 17:41:11 +0200

    Code refactor: 1. Left only one method 'grow()'. The input parameter 'int minCapacity' was removed. 2. The 'grow()' method was refactored to 'void'. 3. The 'newCapacity()' method was renamed to 'countNewCapacity()'. 4. The constant field 'INCREASING_ARGUMENT' was created. We use that inside the method 'countNewCapacity' instead of hardcoding double value - 1.5. 5. The body of the method 'add()' was refactored and its logic was simplified. 6. The body of the method 'addAll()' was refactored and its logic was simplified. 7. The custom method 'ckeckIndex()' for checking input index value was created. We use that method inside methods 'get()' by index, 'set()' value by index, 'remove()' object by index. 8. Firstly, public methods were ordered, and then private methods.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,44 +6,28 @@ import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double INCREASING_ARGUMENT = 1.5;
     private Object[] objects = new Object[DEFAULT_CAPACITY];
     private int size = 0;
 
-    private Object[] grow() {
-        return grow(size + 1);
-    }
-
-    private Object[] grow(int minCapacity) {
-        return objects = Arrays.copyOf(objects, newCapacity(minCapacity));
-    }
-
-    private int newCapacity(int minCapacity) {
-        int oldCapacity = objects.length;
-        return (int) (oldCapacity + (oldCapacity * 1.5));
-    }
-
     @Override
     public void add(T value) {
-        if (size < objects.length) {
-            objects[size] = value;
-            size++;
-        } else {
-            objects = grow();
-            if (size < objects.length) {
-                objects[size] = value;
-                size++;
-            }
+        if (objects.length == size) {
+            grow();
         }
+        objects[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("The index is bigger than"
-                   + " array size or less than null");
+                    + " array size or less than null");
         }
+
         if (size == objects.length) {
-            objects = grow();
+            grow();
         }
         System.arraycopy(objects, index,
                 objects, index + 1,
@@ -54,30 +38,20 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        Object[] inputObjects = new Object[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            inputObjects[i] = list.get(i);
+            add(list.get(i));
         }
-        if ((objects.length - size) < inputObjects.length) {
-            objects = grow();
-        }
-        System.arraycopy(inputObjects, 0, objects, size, inputObjects.length);
-        size += inputObjects.length;
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index > size - 1) {
-            throw new ArrayListIndexOutOfBoundsException("Index out of bounce exception");
-        }
+        checkIndex(index);
         return (T) objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || (index > size - 1)) {
-            throw new ArrayListIndexOutOfBoundsException("Index out bounds exception");
-        }
+        checkIndex(index);
         objects[index] = value;
     }
 
@@ -86,10 +60,7 @@ public class ArrayList<T> implements List<T> {
         final int newSize;
         Object oldObject;
 
-        if (index < 0 || (index > size - 1)) {
-            throw new ArrayListIndexOutOfBoundsException("The index is bigger than "
-                   + "array size or less than null");
-        }
+        checkIndex(index);
 
         if (objects[index] == null) {
             oldObject = null;
@@ -124,5 +95,21 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return (size == 0);
+    }
+
+    private void grow() {
+        objects = Arrays.copyOf(objects, countNewCapacity());
+    }
+
+    private int countNewCapacity() {
+        int oldCapacity = objects.length;
+        return (int) (oldCapacity + (oldCapacity * INCREASING_ARGUMENT));
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayListIndexOutOfBoundsException("Index: "
+                    + index + " out of size: " + size);
+        }
     }
 }
