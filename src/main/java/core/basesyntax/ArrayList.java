0c010daa6d27e45383a4d65e12b07286d3d223f2
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2022-12-04 10:31:35 +0200

    Added constants all methods were overridden method increaseArray was created

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,108 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int CAPACITY = 10;
+    private static final double INCREASE_RATE = 1.5;
+    private T[] array;
+    private int size;
+
+    public ArrayList() {
+        array = (T[]) new Object[CAPACITY];
+    }
+
+    public void increaseArray() {
+        T[] arrayCopy = (T[]) new Object[(int) (array.length * INCREASE_RATE)];
+        System.arraycopy(array, 0, arrayCopy, 0, array.length);
+        array = arrayCopy;
+    }
+
     @Override
     public void add(T value) {
-
+        if (size() >= array.length) {
+            increaseArray();
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size()) {
+            throw new ArrayListIndexOutOfBoundsException("Index passed to the method is invalid");
+        }
+        if (size() >= array.length) {
+            increaseArray();
+        }
+        if (index < size()) {
+            System.arraycopy(array, index, array, index + 1, size() - index);
+            array[index] = value;
+            size++;
+        }
+        if (index == size()) {
+            add(value);
+        }
 
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        T value = null;
+        if (index < 0 || index >= size()) {
+            throw new ArrayListIndexOutOfBoundsException("Index passed to the method is invalid");
+        } else {
+            value = array[index];
+        }
+        return value;
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index >= size()) {
+            throw new ArrayListIndexOutOfBoundsException("Index passed to the method is invalid");
+        } else {
+            array[index] = value;
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Object removeElement;
+        if (index < 0 || index > size()) {
+            throw new ArrayListIndexOutOfBoundsException("Index passed to the method is invalid");
+        } else {
+            removeElement = get(index);
+            System.arraycopy(array, index + 1, array, index, size - index - 1);
+            size--;
+        }
+        return (T) removeElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size(); i++) {
+            if (array[i] != null && array[i].equals(element) || array[i] == element) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Element not found");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 }
