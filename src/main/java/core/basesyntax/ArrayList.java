3cdd442855e291ded6f64e6c4ab8a5cc61aa4876
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-12-12 17:00:23 +0200

    Created constant values. Created an empty array and int size, for future checks for size of the array. Created new method Arraylist to give an empty array default size. Created arrayCopy to increase the length of the array in case it's size will reach the limit. Created method checkIndex to check if given index is exists, otherwise throw the exception. Override the add method. Checked for array size, if it is equals to 10, increased it to 15. Otherwise, added to created array value and increased the size by 1. Override add method with index. Checked if index exists, if not, throw the exception. Then if size equals to the length of the values array (10) - increase array length to 15, (called arrayCopy method). After that coppy the value and index to created array. Increased size by 1. Override addAll method. with for loop added every single element of the given List of elements/values. Override get method. Checked if the index exists with checkIndex method, then returned requested index in created array. Override set method. Checked if the index exists with checkIndex method, then replaced/added given value with value in created array with the same index. Override remove(index) method. Checked if the index exists with checkIndex method, then found value by given index, and removed it plus decreased size by 1. Override remove(element) method. With for loop, checked if there is such element in our array, if so, removed element and decreased size by 1. Otherwise, throw an exception. Override size method. It should return int size; Override isEmpty method. It's boolean, if it is true, made it return size 0.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,100 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double INCREASE_RATE = 1.5;
+    private T[] values;
+    private int size;
+
+    public ArrayList() {
+        values = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void arrayCopy() {
+        values = Arrays.copyOf(values, (int) (size * INCREASE_RATE));
+    }
+
+    public void checkIndex(int index) throws ArrayListIndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("This index does not exist");
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == values.length) {
+            arrayCopy();
+        }
+        values[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("This index does not exist");
+        }
 
+        if (size == values.length) {
+            arrayCopy();
+        }
+
+        System.arraycopy(values, index, values, index + 1, size - index);
+        values[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T removedValue = values[index];
+        System.arraycopy(values, index + 1, values, index, size - index - 1);
+        size--;
+        return removedValue;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T element) throws NoSuchElementException {
+        for (int i = 0; i <= size; i++) {
+            if (Objects.equals(values[i], element)) {
+                size--;
+                System.arraycopy(values, i + 1, values, i, size - i);
+                return element;
+            }
+        }
+        throw new NoSuchElementException("There is no such value in the array");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
