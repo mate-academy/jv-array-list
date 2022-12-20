4e05945ef7adfc18689931a6b586734eb97439fa
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-20 22:27:42 +0200

    second commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,20 +3,19 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final double CAPACITY_COEFFICIENT = 1.5;
+    private static final double GROWTH_COEFFICIENT = 1.5;
     private static final int DEFAULT_CAPACITY = 10;
-    private static final int ARRAY_START_POSITION_INDEX = 0;
-    private Object[] elementData;
+    private Object[] elements;
     private int size;
 
     public ArrayList() {
-        elementData = new Object[DEFAULT_CAPACITY];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        grow();
-        elementData[size] = value;
+        checkCapacity();
+        elements[size] = value;
         size++;
     }
 
@@ -27,17 +26,17 @@ public class ArrayList<T> implements List<T> {
             return;
         }
         checkIndex(index);
-        grow();
-        System.arraycopy(elementData, index, elementData,
+        checkCapacity();
+        System.arraycopy(elements, index, elements,
                 index + 1, size - index);
-        elementData[index] = value;
+        elements[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            grow();
+            checkCapacity();
             add(list.get(i));
         }
     }
@@ -45,20 +44,20 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        return (T) elementData[index];
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
         checkIndex(index);
-        elementData[index] = value;
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
         checkIndex(index);
-        Object removeElement = elementData[index];
-        System.arraycopy(elementData, index + 1, elementData,
+        Object removeElement = elements[index];
+        System.arraycopy(elements, index + 1, elements,
                 index, size - index - 1);
         size--;
         return (T) removeElement;
@@ -67,8 +66,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (element != null && element.equals(elementData[i])
-                    || element == null && elementData[i] == null) {
+            if (element != null && element.equals(elements[i])
+                    || element == null && elements[i] == null) {
                 return remove(i);
             }
         }
@@ -85,14 +84,18 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void grow() {
-        if (size == elementData.length) {
-            int newCapacity = (int) (elementData.length * CAPACITY_COEFFICIENT);
-            Object[] newElementData = new Object[newCapacity];
-            System.arraycopy(elementData, ARRAY_START_POSITION_INDEX, newElementData,
-                    ARRAY_START_POSITION_INDEX, size);
-            elementData = newElementData;
+    private void checkCapacity() {
+        if (size == elements.length) {
+            grow();
         }
+    }
+
+    private void grow() {
+        int newCapacity = (int) (elements.length * GROWTH_COEFFICIENT);
+        Object[] newElementData = new Object[newCapacity];
+        System.arraycopy(elements, 0, newElementData,
+                0, size);
+        elements = newElementData;
     }
 
     private void checkIndex(int index) {
