938a68e05d48a7a59eccaa2e7616d79ae4f325f0
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-20 21:24:57 +0200

    first commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,103 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final double CAPACITY_COEFFICIENT = 1.5;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int ARRAY_START_POSITION_INDEX = 0;
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        grow();
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            add(value);
+            return;
+        }
+        checkIndex(index);
+        grow();
+        System.arraycopy(elementData, index, elementData,
+                index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            grow();
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        Object removeElement = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData,
+                index, size - index - 1);
+        size--;
+        return (T) removeElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element != null && element.equals(elementData[i])
+                    || element == null && elementData[i] == null) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("This element was not found " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void grow() {
+        if (size == elementData.length) {
+            int newCapacity = (int) (elementData.length * CAPACITY_COEFFICIENT);
+            Object[] newElementData = new Object[newCapacity];
+            System.arraycopy(elementData, ARRAY_START_POSITION_INDEX, newElementData,
+                    ARRAY_START_POSITION_INDEX, size);
+            elementData = newElementData;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid Index: " + index);
+        }
     }
 }
