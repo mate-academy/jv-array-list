01058247ad57cf54e48a8e02c13e2f3573ceb730
Дмитро <dima.smyk99.19@gmail.com>
2022-12-21 16:33:20 +0200

    change condition without Object

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
 
@@ -71,7 +70,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, values[i])) {
+            if (element == values[i] || element != null && element.equals(values[i])) {
                 return remove(i);
             }
         }
