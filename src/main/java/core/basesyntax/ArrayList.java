03367e766693a999f54ad4bb7ce73648b1ae5cbf
Serhii Berdychevskyi <sirko120690@mail.com>
2022-12-26 14:17:27 +0200

    Added first version hw jv-array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,101 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final double DEFAULT_COEFFICIENT = 1.5;
+    private static final int DEFAULT_ARRAY_LENGTH = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_ARRAY_LENGTH];
+    }
+
     @Override
     public void add(T value) {
-
+        resizing();
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "The index passed to the method is invalid");
+        }
+        resizing();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexValidity(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexValidity(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexValidity(index);
+        return doRemove(index);
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < elements.length; i++) {
+            if (element == elements[i] || element != null && element.equals(elements[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no such element present");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndexValidity(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "The index passed to the method is invalid");
+        }
+    }
+
+    private void resizing() {
+        if (size >= elements.length) {
+            int arrayLength = (int) (elements.length * DEFAULT_COEFFICIENT);
+            Object[] arrayNew = new Object[arrayLength];
+            System.arraycopy(elements, 0, arrayNew, 0, elements.length);
+            elements = arrayNew;
+        }
+    }
+
+    private T doRemove(int index) {
+        T value = (T) elements[index];
+        size--;
+        System.arraycopy(elements, index + 1, elements, index, size - index);
+        return value;
     }
 }
