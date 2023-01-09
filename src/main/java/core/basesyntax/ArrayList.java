a014ceadd41a375339c2cec83ffbfd6a58ebb1c4
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-09 23:02:52 +0200

    feat: jv-array-list #comment review issues wes fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,30 +1,23 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private T[] storage = (T[]) new Object[DEFAULT_CAPACITY];
-    private int size = 0;
+    private int size;
 
     @Override
     public void add(T value) {
         if (size == storage.length) {
-            T[] newStorage = (T[]) new Object[storage.length + (storage.length >> 1)];
-            for (int i = 0; i < storage.length; i++) {
-                newStorage[i] = storage[i];
-            }
-            storage = newStorage;
+            increaseCapacity();
         }
         storage[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index is out of array size");
-        }
+        indexValidation(index, false);
 
         if (index == size) {
             add(value);
@@ -32,21 +25,10 @@ public class ArrayList<T> implements List<T> {
         }
 
         if (size == storage.length) {
-            T[] newStorage = (T[]) new Object[storage.length + (storage.length >> 1)];
-            for (int i = 0; i < index; i++) {
-                newStorage[i] = storage[i];
-            }
-            newStorage[index] = value;
-            for (int i = index; i < storage.length; i++) {
-                newStorage[i + 1] = storage[i];
-            }
-            storage = newStorage;
-        } else {
-            for (int i = size - 1; i >= index; i--) {
-                storage[i + 1] = storage[i];
-            }
-            storage[index] = value;
+            increaseCapacity();
         }
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = value;
         size++;
     }
 
@@ -63,30 +45,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index is out of array size");
-        }
+        indexValidation(index, true);
         return storage[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index is out of array size");
-        }
-
+        indexValidation(index, true);
         storage[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index is out of array size");
-        }
+        indexValidation(index, true);
         T value = storage[index];
-        for (int i = index + 1; i < size; i++) {
-            storage[i - 1] = storage[i];
-        }
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
         storage[--size] = null;
         return value;
     }
@@ -95,7 +68,7 @@ public class ArrayList<T> implements List<T> {
     public T remove(T element) {
         int index = -1;
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, storage[i])) {
+            if (element == null ? storage[i] == null : element.equals(storage[i])) {
                 index = i;
                 break;
             }
@@ -114,5 +87,19 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void increaseCapacity() {
+        T[] newStorage = (T[]) new Object[storage.length + (storage.length >> 1)];
+        System.arraycopy(storage,0, newStorage, 0, storage.length);
+        storage = newStorage;
+    }
+
+    private void indexValidation(int index, boolean isSizeEqualToIndexForbidden) {
+        if ((isSizeEqualToIndexForbidden ? index >= size : index > size) || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+              "Index is out of array size. Index: " + index + ". Size: " + size + "."
+            );
+        }
     }
 }
