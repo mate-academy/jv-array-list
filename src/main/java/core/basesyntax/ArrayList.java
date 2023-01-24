1612f8d6e695d7cbdcbf302cc3e2bd90c52db75d
Kateryna <katerynachi5@gmail.com>
2023-01-24 14:34:29 +0100

    Override methods of ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,116 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROW_FACTOR = 1.5;
+    private T[] array;
+    private int size;
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ArrayList(int initialCapacity) {
+        array = (T[]) new Object[initialCapacity];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == array.length) {
+            growArray();
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Can't add the value, index is out of bounds");
+        }
+        if (size == array.length) {
+            growArray();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        return rearrangeElements(index);
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (array[i] == null) {
+                    return rearrangeElements(i);
+                }
+            }
+            return null;
+        }
+        for (int i = 0; i < size; i++) {
+            if (array[i] != null && array[i].equals(element)) {
+                return rearrangeElements(i);
+            }
+        }
+        throw new NoSuchElementException("No such element in the list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growArray() {
+        int newSize = (int) (array.length * GROW_FACTOR);
+        array = Arrays.copyOf(array, newSize);
+    }
+
+    private T rearrangeElements(int index) {
+        T removedElement = array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array [i + 1];
+        }
+        size--;
+        return removedElement;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Can't operate with the value, index is out of bounds");
+        }
     }
 }
