e7ef290ff2664df23e3aafcc832ff4a15d97cd9d
Maxim Skory <devisrush@gmail.com>
2023-02-02 00:31:50 +0200

    add() and get() methods were implemented. grow() and lengthCheck() methods were added

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,37 +3,53 @@ package core.basesyntax;
 import java.util.Arrays;
 
 public class ArrayList<T> implements List<T> {
-    private static final int ARRAY_LENGTH = 10;
+    private static final int ARRAY_DEFAULT_LENGTH = 10;
     private Object[] array;
     private int currentSize;
 
     public ArrayList() {
-        array = new Object[ARRAY_LENGTH];
+        array = new Object[ARRAY_DEFAULT_LENGTH];
         currentSize = 0;
     }
 
     @Override
     public void add(T value) {
-        if (currentSize == array.length) {
-            array = Arrays.copyOf(array, currentSize + (currentSize >> 1));
-        }
+        lengthCheck();
         array[currentSize] = value;
         currentSize++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        try {
+            lengthCheck();
+            Object[] arrayTail = Arrays.copyOfRange(array, index, currentSize);
+            array[index] = value;
+            System.arraycopy(arrayTail, 0, array, index + 1, arrayTail.length);
+            currentSize++;
+        } catch (RuntimeException e) {
+            throw new ArrayListIndexOutOfBoundsException("Index doesn't exist");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        Object[] listArr = new Object[list.size()];
+        lengthCheck();
+        for (int i = 0; i < list.size(); i++) {
+            listArr[i] = list.get(i);
+        }
+        System.arraycopy(listArr, 0, array, currentSize, listArr.length);
+        System.out.println(array);
     }
 
     @Override
     public T get(int index) {
-        return (T) array[index];
+        try {
+            return (T) array[index];
+        } catch (RuntimeException e) {
+            throw new ArrayListIndexOutOfBoundsException("Index can't be negative");
+        }
     }
 
     @Override
@@ -53,11 +69,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private void lengthCheck() {
+        if (currentSize == array.length) {
+            grow();
+        }
+    }
+
+    private void grow() {
+        array = Arrays.copyOf(array, currentSize + (currentSize >> 1));
     }
 }
