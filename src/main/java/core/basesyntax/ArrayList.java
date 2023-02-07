edd23c9cadcd6eff8ed4538edd945f2d23ac45a0
Artur Muliukov <arthur.muliukov@gmail.com>
2023-02-07 13:21:48 +0100

    Solved 1.0

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,135 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    static final int MAX_SIZE = 10;
+    private T[] array;
+    private int size;
+
+    public ArrayList() {
+        array = (T[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void add(T value) {
-
+        if (array.length == size) {
+            array = grow(array);
+        }
+        array[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index do not exist");
+        }
+        if (array.length == size) {
+            array = grow(array);
+        }
+        fastValueAdd(value, index);
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            array[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexExistCheck(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        indexExistCheck(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexExistCheck(index);
+        final T result = array[index];
+        fastRemove(index);
+        return result;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        final T result;
+        final int elementIndex;
+        final int newSize = size - 1;
+
+        if (element == null) {
+            for (int i = 0; i <= newSize; i++) {
+                if (array[i] == null) {
+                    result = array[i];
+                    elementIndex = i;
+                    fastRemove(elementIndex);
+                    return result;
+                }
+                if (i == newSize) {
+                    throw new NoSuchElementException("Element do not exist");
+                }
+            }
+        } else {
+            for (int i = 0; i <= newSize; i++) {
+                if (Objects.equals(array[i], element)) {
+                    result = array[i];
+                    elementIndex = i;
+                    fastRemove(elementIndex);
+                    return result;
+                }
+            }
+        }
+        throw new NoSuchElementException("Element do not exist");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size <= 0;
+    }
+
+    public T[] grow(T[] oldarray) {
+        final int newLength = (int) (oldarray.length * 1.5);
+        T[] newArray = (T[]) new Object[newLength];
+
+        for (int i = 0; i < oldarray.length; i++) {
+            newArray[i] = oldarray[i];
+        }
+        return newArray;
+    }
+
+    public void fastRemove(int index) {
+        final int newSize = size - 1;
+
+        System.arraycopy(array, index + 1, array, index, newSize - index);
+        size--;
+        array[size] = null;
+    }
+
+    public void indexExistCheck(int index) {
+        final int newSize = size - 1;
+
+        if (index < 0 || index > newSize) {
+            throw new ArrayListIndexOutOfBoundsException("Index do not exist");
+        }
+    }
+
+    public void fastValueAdd(T value, int index) {
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 }
