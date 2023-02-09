97c160b5e1265b9af8e97bc5f94aa52f3cbf254c
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-02-09 20:24:37 +0200

    required changes are made

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -2,25 +2,28 @@ package core.basesyntax;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
 
     private static final int DEFAULT_CAPACITY = 10;
-    private static final Object[] DEFAULT_ARRAYLIST = {};
     private Object[] objectArray;
 
     private int size;
 
+    private void checkIndex(int index) {
+        if ((size - 1) < index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can`t set,get or remove element, /n"
+                    + "index " + index + " is out of range 0 - " + (size - 1));
+        }
+    }
+
     public ArrayList() {
-        objectArray = DEFAULT_ARRAYLIST;
+        objectArray = new Object[DEFAULT_CAPACITY];
     }
 
     public ArrayList(int initialCapacity) {
-        if (initialCapacity > 0) {
+        if (initialCapacity >= 0) {
             this.objectArray = new Object[initialCapacity];
-        } else if (initialCapacity == 0) {
-            this.objectArray = DEFAULT_ARRAYLIST;
         } else {
             throw new IllegalArgumentException("Illegal Capacity: "
                     + initialCapacity);
@@ -32,19 +35,19 @@ public class ArrayList<T> implements List<T> {
         add(value, size);
     }
 
+    public void grow() {
+        objectArray = Arrays.copyOf(objectArray, objectArray.length
+                + (objectArray.length >> 1));
+    }
+
     @Override
     public void add(T value, int index) {
         if (size < index || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Cen`t add element. "
+            throw new ArrayListIndexOutOfBoundsException("Can`t add element. "
                     + "Index " + index + " is out of range 0 - " + size);
         }
-        if (objectArray.length == 0) {
-            objectArray = new Object[DEFAULT_CAPACITY];
-        }
-
         if (objectArray.length < size + 1) {
-            objectArray = Arrays.copyOf(objectArray, objectArray.length
-                    + (objectArray.length >> 1));
+            grow();
         }
         System.arraycopy(objectArray, index, objectArray, index + 1,
                 (objectArray.length - 1) - index);
@@ -62,29 +65,20 @@ public class ArrayList<T> implements List<T> {
     @SuppressWarnings("unchecked")
     @Override
     public T get(int index) {
-        if (index > (size - 1) || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Cen`t get element. "
-                    + "Index " + index + " is out of range 0 - " + (size - 1));
-        }
+        checkIndex(index);
         return (T) objectArray[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index > (size - 1) || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Can`t set element. "
-                    + "Index " + index + " is out of range 0 - " + (size - 1));
-        }
+        checkIndex(index);
         objectArray[index] = value;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public T remove(int index) {
-        if (index > (size - 1) || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Can`t remove element. "
-                    + "Index " + index + " is out of range 0 - " + (size - 1));
-        }
+        checkIndex(index);
         T result = (T) objectArray[index];
         System.arraycopy(objectArray, index + 1, objectArray, index,
                 (objectArray.length - 1) - index);
@@ -95,7 +89,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(objectArray[i], element)) {
+            if (element == objectArray[i] || element != null && element.equals(objectArray[i])) {
                 return remove(i);
             }
         }
