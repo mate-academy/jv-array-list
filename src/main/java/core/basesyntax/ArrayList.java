9540c037600b5bd6694850ea262d0a2dcecc3301
Valeriy M <valeriym250@gmail.com>
2023-02-17 11:32:03 +0200

    Implemented methods:  "public void add"  element, add with index, add All  "get" by element and by index  "set" by index  "remove" by elemet and by index  "size" which return list size  "is empty" - checks is Array class empty  "grow" (private) - this method calls when we addind some element in ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,131 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private Object[] objects = new Object[0];
+
     @Override
     public void add(T value) {
-
+        objects = grow((T[]) (objects));
+        if (objects.length != 0) {
+            objects[objects.length - 1] = value;
+        } else {
+            objects = grow((T[]) (objects));
+            objects[0] = value;
+        }
     }
 
     @Override
     public void add(T value, int index) {
-
+        try {
+            if (objects.length == 0) {
+                objects = grow((T[]) (objects));
+                objects[0] = value;
+            } else {
+                Object [] clonedArray = new Object[objects.length + 1];
+                for (int i = 0; i <= index; i++) {
+                    clonedArray[i] = (i == index) ? value : objects[i];
+                }
+                for (int i = index; i < objects.length; i++) {
+                    clonedArray[i + 1] = objects[i];
+                }
+                objects = grow((T[]) (objects));
+                objects = clonedArray;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("No such index");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        try {
+            return (T) (objects[index]);
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("Element does not exist");
+        }
     }
 
     @Override
     public void set(T value, int index) {
+        try {
+            objects[index] = value;
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("Such index does not exist");
+        }
 
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Object removed = -1;
+        try {
+            if (objects.length != 0) {
+                Object [] temp = new Object[objects.length - 1];
+                removed = objects[index];
+                for (int i = 0; i < index; i++) {
+                    temp[i] = objects[i];
+                }
+                if (index != objects.length - 1) {
+                    for (int i = index + 1; i < objects.length; i++) {
+                        temp[i - 1] = objects[i];
+                    }
+                }
+                objects = temp;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException("Such index does not exist");
+        }
+        return (T) (removed);
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < objects.length; i++) {
+            index = (Objects.equals(element, objects[i])) ? i : index;
+        }
+        if (index == -1) {
+            throw new NoSuchElementException("Such element does not exist");
+        }
+        return remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return objects.length;
     }
 
     @Override
     public boolean isEmpty() {
+        if (objects.length == 0) {
+            return true;
+        }
+        for (int i = 0; i < objects.length; i++) {
+            if (Objects.equals(objects[i], "")
+                    || Objects.equals(objects[i], null)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private T[] grow(T[] inputArray) {
+        int value = inputArray.length + 1;
+        Object [] startArray = new Object[value];
+        if (inputArray.length > 0) {
+            startArray = Arrays.copyOf(inputArray, value);
+        }
+        return (T[]) (startArray);
     }
 }
