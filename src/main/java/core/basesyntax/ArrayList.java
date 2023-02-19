43b6ac7560e6ef5e30da59b9a9ae1c99ca0cc45d
kromchk <kromchyk@yahoo.com>
2023-02-19 21:47:38 +0200

    solution of array-list task #2

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int START_SIZE = 10;
-    private static final float INCREASE_VOLUME = 1.5f;
     private static final String OF_BOUND_MESSAGE = "Out of bound index in ArrayList";
     private static final String OF_ABSENT_MESSAGE = "Element of ArrayList ain't present";
     private T[] storage;
-    private int pointer;
+    private int size;
 
     public ArrayList() {
         this.storage = (T[]) new Object[START_SIZE];
@@ -16,68 +16,58 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (storage.length == pointer) {
+        if (storage.length == size) {
             increaseSize();
         }
-        storage[pointer++] = value;
+        storage[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > pointer) {
+        if (!adding(value, index)) {
             throw new ArrayListIndexOutOfBoundsException(OF_BOUND_MESSAGE);
         }
-        List<T> tempList = new ArrayList<>();
-        for (int i = index; i < pointer; i++) {
-            tempList.add(get(i));
-        }
-        storage[index] = value;
-        pointer = index + 1;
-        addAll(tempList);
     }
 
     @Override
     public void addAll(List<T> list) {
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
+        if (list != null) {
+            for (int i = 0; i < list.size(); i++) {
+                add(list.get(i));
+            }
         }
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= pointer) {
+        T result = getting(index);
+        if (result != null && result.getClass().getGenericSuperclass() == null) {
             throw new ArrayListIndexOutOfBoundsException(OF_BOUND_MESSAGE);
         }
-        return storage[index];
+        return result;
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= pointer) {
+        if (!setting(value, index)) {
             throw new ArrayListIndexOutOfBoundsException(OF_BOUND_MESSAGE);
         }
-        storage[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= pointer) {
+        T result = removing(index);
+        if (result == null) {
             throw new ArrayListIndexOutOfBoundsException(OF_BOUND_MESSAGE);
         }
-        T result = storage[index];
-        System.arraycopy(storage, index + 1, storage, index, pointer - index - 1);
-        storage[--pointer] = null;
         return result;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < pointer; i++) {
-            if (storage[i] == element || (storage[i] != null && storage[i].equals(element))) {
-                T result = storage[i];
-                System.arraycopy(storage, i + 1, storage, i, pointer - i - 1);
-                storage[--pointer] = null;
-                return result;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i], element)) {
+                return removing(i);
             }
         }
         throw new NoSuchElementException(OF_ABSENT_MESSAGE);
@@ -85,16 +75,53 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return pointer;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return pointer == 0;
+        return size == 0;
+    }
+
+    private boolean adding(T value, int index) {
+        if (index >= 0 && index <= size) {
+            List<T> tempList = null;
+            if (index < size) {
+                tempList = new ArrayList<>();
+                for (int i = index; i < size; i++) {
+                    tempList.add(get(i));
+                }
+            }
+            size = index;
+            add(value);
+            addAll(tempList);
+        }
+        return index >= 0 && index < size;
+    }
+
+    private T getting(int index) {
+        return index < 0 || index >= size ? (T) new Object() : storage[index];
+    }
+
+    private T removing(int index) {
+        T result = null;
+        if (index >= 0 && index < size) {
+            result = storage[index];
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            storage[--size] = null;
+        }
+        return result;
+    }
+
+    private boolean setting(T value, int index) {
+        if (index >= 0 && index < size) {
+            storage[index] = value;
+        }
+        return index >= 0 && index < size;
     }
 
     private void increaseSize() {
-        int extendSize = (int) (INCREASE_VOLUME * pointer);
+        int extendSize = size + (size >> 1);
         T[] tempStorage = (T[]) new Object[extendSize];
         System.arraycopy(storage, 0, tempStorage, 0, storage.length);
         storage = tempStorage;
