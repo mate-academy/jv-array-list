8cbaa39e99999adf4b6526138bb06ccb6ceeaddb
Maksym Horielikov <hmv.fermats@gmail.com>
2023-02-20 19:27:35 +0200

    commit after mentor review

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,15 +4,18 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
+    private static final String ADD_MESSAGE_EXCEPTION = "Can't add to index ";
+    private static final String REMOVE_MESSAGE_EXCEPTION = "Can't remove to index ";
+    private static final String SET_MESSAGE_EXCEPTION = "Can't set to index ";
+    private static final String GET_MESSAGE_EXCEPTION = "Can't get to index ";
+    private static final String NO_SUCH_MESSAGE = "No such element in array list";
     private static final int DEFAULT_CAPACITY = 10;
-    private int currentCapacity;
     private T[] listArray;
     private int size = 0;
 
     @SuppressWarnings("unchecked")
     public ArrayList() {
         this.listArray = (T[]) new Object[DEFAULT_CAPACITY];
-        currentCapacity = DEFAULT_CAPACITY;
     }
 
     @Override
@@ -22,34 +25,31 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index, "Can't add to index " + index, true);
-        if (size == currentCapacity) {
+        checkIndexInclusiveSize(index, ADD_MESSAGE_EXCEPTION + index);
+        if (size == listArray.length) {
             resizeListArray(index);
         } else {
-            if (size > 0) {
-                System.arraycopy(listArray, index, listArray, index + 1, size - index);
-            }
+            System.arraycopy(listArray, index, listArray, index + 1, size - index);
         }
         listArray[index] = value;
         size++;
     }
 
-    private void checkIndex(int index, String message, boolean isInclusiveSize) {
-        if (isInclusiveSize) {
-            if (!(index >= 0 && index <= size)) {
-                throw new ArrayListIndexOutOfBoundsException(message);
-            }
-        } else {
-            if (!(index >= 0 && index < size)) {
-                throw new ArrayListIndexOutOfBoundsException(message);
-            }
+    private void checkIndex(int index, String message) {
+        if (!(index >= 0 && index < size)) {
+            throw new ArrayListIndexOutOfBoundsException(message);
+        }
+    }
+
+    private void checkIndexInclusiveSize(int index, String message) {
+        if (!(index >= 0 && index <= size)) {
+            throw new ArrayListIndexOutOfBoundsException(message);
         }
     }
 
     @SuppressWarnings("unchecked")
     private void resizeListArray(int index) {
-        currentCapacity = currentCapacity + (currentCapacity >> 1);
-        T[] temp = (T[]) new Object[currentCapacity];
+        T[] temp = (T[]) new Object[listArray.length + (listArray.length >> 1)];
         System.arraycopy(listArray, 0, temp, 0, index);
         System.arraycopy(listArray, index, temp, index + 1, size - index);
         listArray = temp;
@@ -57,29 +57,28 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        if (list == null) {
-            return;
-        }
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
+        if (list != null) {
+            for (int i = 0; i < list.size(); i++) {
+                add(list.get(i));
+            }
         }
     }
 
     @Override
     public T get(int index) {
-        checkIndex(index, "Can't get to index " + index, false);
+        checkIndex(index, GET_MESSAGE_EXCEPTION + index);
         return listArray[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, "Can't set to index " + index, false);
+        checkIndex(index, SET_MESSAGE_EXCEPTION + index);
         listArray[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index, "Can't remove to index " + index, false);
+        checkIndex(index, REMOVE_MESSAGE_EXCEPTION + index);
         T temp = listArray[index];
         if (index < size - 1) {
             System.arraycopy(listArray, index + 1, listArray, index, size - index - 1);
@@ -95,7 +94,7 @@ public class ArrayList<T> implements List<T> {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("No such element in array list");
+        throw new NoSuchElementException(NO_SUCH_MESSAGE);
     }
 
     @Override
