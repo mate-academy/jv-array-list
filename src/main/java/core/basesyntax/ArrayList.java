1107ab40697a342caab44da97bb108be6fbe4a0c
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-20 02:08:03 +0200

    draft

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,60 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_SIZE = 10;
+
+    private static final Object[] EMPTY_DATA = {};
+
+    private Object[] data = new Object[INITIAL_SIZE];
+    private int size;
+
+    private boolean addElement(T element) {
+        ensureCapacity(INITIAL_SIZE + 1);
+        data[size++] = element;
+        return true;
+    }
+
+    private void grow() {
+        int oldCapacity = data.length;
+        int newCapacity = oldCapacity + (oldCapacity / 2);
+        if (newCapacity < 0) {
+            newCapacity = Integer.MAX_VALUE;
+        }
+        data = Arrays.copyOf(data, newCapacity);
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = data.length;
+        int newCapacity = (oldCapacity * 3) / 2 + 1;
+        if (minCapacity > oldCapacity) {
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            data = Arrays.copyOf(data, newCapacity);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == data.length) {
+            grow();
+        }
+        data[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Can't add to index" + index);
+        }
+        if (size == data.length) {
+            grow();
+        }
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = value;
+        size++;
     }
 
     @Override
