3cf74c766f9747fb2f5cb2b0b958d91e09af68bd
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-02-20 16:41:39 +0200

    Solved task Created ArrayList class which implemented the List interface. Overrided the methods required

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,107 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    public static final double INCREASE_COEFF = 1.5;
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elements;
+    private int currentSize;
+
+    public ArrayList() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        increaseArrayIfNeed();
+        elements[currentSize] = value;
+        currentSize++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > currentSize || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Input index : " + index
+                    + " less than ArrayList size: " + currentSize);
+        } else if (index == currentSize) {
+            add(value);
+            return;
+        }
+        increaseArrayIfNeed();
+        System.arraycopy(elements, index, elements, index + 1, currentSize - index);
+        elements[index] = value;
+        currentSize++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexLimits(index);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexLimits(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexLimits(index);
+        if (index == currentSize - 1) {
+            T elementToReturn = elements[index];
+            elements[index] = null;
+            currentSize--;
+            return elementToReturn;
+        } else {
+            T elementToReturn = elements[index];
+            System.arraycopy(elements, index + 1, elements, index, currentSize - index);
+            currentSize--;
+            return elementToReturn;
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < currentSize; i++) {
+            if (Objects.equals(element, elements[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currentSize == 0;
+    }
+
+    private void increaseArrayIfNeed() {
+        if (elements.length == currentSize) {
+            T[] temp = elements;
+            elements = (T[]) new Object[(int)(currentSize * INCREASE_COEFF)];
+            System.arraycopy(temp, 0, elements, 0, currentSize);
+        }
+    }
+
+    private void checkIndexLimits(int index) {
+        if (index >= currentSize || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Input index : " + index
+                    + " more than ArrayList size: " + currentSize);
+        }
     }
 }
