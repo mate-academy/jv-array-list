72f22b877f3fb390feeb8e0312ac1c39f82bab2f
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-20 19:38:15 +0200

    implement own ArrayList methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,104 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_LIST_CAPACITY = 10;
+    private static final int HALF_FROM_DEFAULT_LIST_CAPACITY = DEFAULT_LIST_CAPACITY / 2;
+    private T[] list;
+    private int size;
+
+    public ArrayList() {
+        list = (T[]) new Object[DEFAULT_LIST_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (checkSize()) {
+            resizeList();
+        }
+        list[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size < index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Can't add " + value + " index: "
+                    + index + "was incorrect!");
+        }
+        if (size == index) {
+            add(value);
+            return;
+        } else if (checkSize()) {
+            resizeList();
+        }
+        System.arraycopy(list, index, list, index + 1, size - index);
+        list[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return list[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        list[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T listWithRemoveElement = list[index];
+        System.arraycopy(list,index + 1, list, index, size - index - 1);
+        size--;
+        return listWithRemoveElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element == list[i] || (list[i] != null) && list[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException(element + ": not found in list!");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private boolean checkSize() {
+        return size == list.length;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index!");
+        }
+    }
+
+    public void resizeList() {
+        T[] array = (T[]) new Object[list.length + HALF_FROM_DEFAULT_LIST_CAPACITY];
+        System.arraycopy(list, 0, array, 0, list.length);
+        list = array;
     }
 }
