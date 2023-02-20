bd43b60ce2178dfb4f6332611efa6c5816b44d7a
De1eF <jatfun58@gmail.com>
2023-02-20 16:35:56 +0200

    Push for linking add(value, index) problem

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,120 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    private int size;
+
     @Override
     public void add(T value) {
+        if (size > elementData.length) {
+            grow();
+        }
+        elementData[size++] = value;
+    }
 
+    private void log() {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == null) {
+                System.out.println("null");
+                return;
+            }
+            System.out.println(elementData[i].toString());
+        }
+        System.out.println("----------");
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Given index is not valid for size: " + size);
+        }
+        if (size == elementData.length) {
+            grow();
+            T[] oldElementData = elementData;
+            for (int i = index; i < size; i++) {
+                elementData[i + 1] = oldElementData[i];
+            }
+        }
+        elementData[index] = value;
+        size++;
+        log();
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+    }
 
+    private void grow() {
+        T[] oldElementData = elementData;
+        elementData = (T[]) new Object[(int)(elementData.length * 1.5f)];
+        for (int i = 0; i < oldElementData.length; i++) {
+            elementData[i] = oldElementData[i];
+        }
+        System.out.println("----------grew to " + this.elementData.length + "----------");
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Given index is not valid for size: " + size);
+        }
+        T result = null;
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
+                result = elementData[i];
+            }
+        }
+        return result;
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Given index is not valid for size: " + size);
+        }
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
+                elementData[i] = value;
+            }
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Given index is not valid for size: " + size);
+        }
+        T deleted = elementData[index];
+        T[] temp = elementData;
+        for (int i = index + 1; i < size; i++) {
+            elementData[i - 1] = temp[i];
+        }
+        size--;
+        return deleted;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new ArrayListIndexOutOfBoundsException("Given index is not valid for size: " + size);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
---
+++ src/main/java/core/basesyntax/Test.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(i + "",0);
+        }
+    }
+}
