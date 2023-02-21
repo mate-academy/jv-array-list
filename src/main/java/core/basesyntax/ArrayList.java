780458c5ad8fb354d4a74757c44f86d9796bacdd
OlehSarapuk <olehsarapuk@gmail.com>
2023-02-21 18:05:25 +0200

    added informative messages to exceptions

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -22,7 +22,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("index you want to add value is incorrect "
+            throw new ArrayListIndexOutOfBoundsException("index" + index + "you want to add value is incorrect "
                     + "(greater than number of elements or less than zero)");
         } else {
             shiftRightAndAddElement(value, index);
@@ -63,7 +63,7 @@ public class ArrayList<T> implements List<T> {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("no such element in list");
+        throw new NoSuchElementException("no such element" + element.toString() + "in list");
     }
 
     @Override
@@ -99,7 +99,7 @@ public class ArrayList<T> implements List<T> {
 
     private void checkIfIndexCorrect(int index) {
         if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("index you want to reach is incorrect "
+            throw new ArrayListIndexOutOfBoundsException("index" + index + "you want to reach is incorrect "
                     + "(greater or equal than number of elements or less than zero)");
         }
     }
