e3cc7ca94ef52ba1ab72f8ec3044f92fbe3f88c7
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-21 18:27:15 +0200

    Create my implementation of ArrayList with all overrided methods like add() addAll() remove() etc.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,118 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final double MULTIPLICATION_CAPACITY = 1.5;
+    private static final String EXCEPTION_MESSAGE = "Index of the bounds. Check your input index";
+    private T[] values;
+    private int size;
+    private int capacity;
+
+    public ArrayList() {
+        size = 0;
+        capacity = 10;
+        values = (T[]) new Object[capacity];
+    }
+
     @Override
     public void add(T value) {
-
+        grow();
+        values[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        indexOnRange(index);
+        grow();
+        for (int i = size; i > index; i--) {
+            values[i] = values[i - 1];
+        }
+        values[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexOnRangeWithEqualsSize(index);
+        return values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
-    }
-
-    @Override
-    public T remove(int index) {
-        return null;
+        indexOnRangeWithEqualsSize(index);
+        T oldValue = values[index];
+        values[index] = value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (values[i] == null) {
+                if (element == null) {
+                    T removed = values[i];
+                    remove(i);
+                    return removed;
+                }
+            } else {
+                if (values[i].equals(element)) {
+                    T removed = values[i];
+                    remove(i);
+                    return removed;
+                }
+            }
+        }
+        throw new NoSuchElementException("No such element");
+    }
+
+    @Override
+    public T remove(int index) {
+        indexOnRangeWithEqualsSize(index);
+        T removed = values[index];
+        for (int i = index; i < size - 1; i++) {
+            values[i] = values[i + 1];
+        }
+        T newRemoved = removed;
+        values[size - 1] = null;
+        size--;
+        return newRemoved;
+    }
+
+    private void indexOnRange(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void indexOnRangeWithEqualsSize(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void grow() {
+        if (size == capacity) {
+            capacity *= MULTIPLICATION_CAPACITY;
+            values = Arrays.copyOf(values, capacity);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
--- src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
+++ src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
@@ -3,5 +3,6 @@ package core.basesyntax;
 public class ArrayListIndexOutOfBoundsException extends RuntimeException {
     public ArrayListIndexOutOfBoundsException(String message) {
         super(message);
+
     }
 }
