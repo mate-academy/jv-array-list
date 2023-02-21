c05f07192929d4a3043e4eec4d7eff777da41e23
Sanyk Volodymyr <sanikvladimir@gmail.com>
2023-02-21 17:53:58 +0200

    fixed indicated mistakes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -14,7 +14,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        resizeArray();
+        checkCapacity();
         elements[size] = value;
         size++;
     }
@@ -22,7 +22,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         checkIndex(index, size + 1);
-        resizeArray();
+        checkCapacity();
         System.arraycopy(elements, index, elements, index + 1, (size - index));
         elements[index] = value;
         size++;
@@ -50,9 +50,10 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         checkIndex(index, size);
-        T removedElement = elements[index];
+        final T removedElement = elements[index];
         System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         size--;
+        elements[size] = null;
         return removedElement;
     }
 
@@ -64,7 +65,7 @@ public class ArrayList<T> implements List<T> {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("No Such Element");
+        throw new NoSuchElementException("There is no " + element + " Element");
     }
 
     @Override
@@ -79,15 +80,19 @@ public class ArrayList<T> implements List<T> {
 
     private void checkIndex(int index, int size) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Index Out Of Bounds");
+            throw new ArrayListIndexOutOfBoundsException("Index:" + index + ", Out Of Bounds");
+        }
+    }
+
+    private void checkCapacity() {
+        if (elements.length == size) {
+            resizeArray();
         }
     }
 
     private void resizeArray() {
-        if (elements.length == size) {
-            T[] extendedArray = (T[]) new Object[(int) (elements.length * AUGMENTER_FACTOR)];
-            System.arraycopy(elements, 0, extendedArray, 0, size);
-            elements = extendedArray;
-        }
+        T[] extendedArray = (T[]) new Object[(int) (elements.length * AUGMENTER_FACTOR)];
+        System.arraycopy(elements, 0, extendedArray, 0, size);
+        elements = extendedArray;
     }
 }
