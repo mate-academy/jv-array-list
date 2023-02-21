7a76b2ed9e546917ed6dd4a0d327f6d204cce64e
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-02-21 16:12:22 +0200

    fixed methods and done recomposition

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,104 +1,74 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
-public class ArrayList<T> implements List<T> {
-
-    private static final double GROW_INDEX = 1.5;
+public class ArrayList<E> implements List<E> {
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] data;
+    private E[] data;
     private int size;
 
     public ArrayList() {
-        data = new Object[DEFAULT_CAPACITY];
+        data = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
-    public void add(T value) {
-        checkFreeSpace();
+    public void add(E value) {
+        if (isStorageFull()) {
+            grow();
+        }
         data[size] = value;
         size++;
     }
 
     @Override
-    public void add(T value, int index) {
-
+    public void add(E value, int index) {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Can't add element: " + value
                     + " on the position: " + index);
+        } else if (isStorageFull()) {
+            grow();
         }
-        checkFreeSpace();
         System.arraycopy(data, index, data, index + 1, size - index);
         data[index] = value;
         size++;
     }
 
     @Override
-    public void addAll(List<T> list) {
+    public void addAll(List<E> list) {
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
 
     @Override
-    public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index : " + index + " is not valid.");
-        }
-        return (T) data[index];
+    public E get(int index) {
+        checkIndexBounds(index);
+        return data[index];
     }
 
     @Override
-    public void set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Can't set value. Index "
-                    + index + " doesn't exist.");
-        }
+    public void set(E value, int index) {
+        checkIndexBounds(index);
         data[index] = value;
     }
 
     @Override
-    public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Can't remove element from position: "
-                    + index);
-        }
-        return makeCopyAndDelete(index);
+    public E remove(int index) {
+        checkIndexBounds(index);
+        E deletingData = data[index];
+        System.arraycopy(data, index + 1, data, index, size - index - 1);
+        size--;
+        return deletingData;
     }
 
     @Override
-    public T remove(T element) {
+    public E remove(E element) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == element || data[i] != null && data[i].equals(element)) {
+            if (areEqual(data[i], element)) {
                 return remove(i);
             }
         }
         throw new NoSuchElementException("Can't remove non-existing element: " + element);
-    }
-
-    private void checkFreeSpace() {
-        if (size == data.length) {
-            data = grow();
-        }
-    }
-
-    private Object[] grow() {
-        int newCapacity = (int) (data.length * GROW_INDEX);
-        return data = Arrays.copyOf(data, newCapacity);
-
-    }
-
-    private T makeCopyAndDelete(int index) {
-        Object[] elementsToCopy = new Object[(size - 1) - index];
-        int indexNextAfterRemoved = index + 1;
-        for (int i = 0; i < elementsToCopy.length; i++) {
-            elementsToCopy[i] = data[indexNextAfterRemoved++];
-        }
-        T deletedElement = (T) data[index];
-        System.arraycopy(elementsToCopy, 0, data, index, elementsToCopy.length);
-        size--;
-        return deletedElement;
     }
 
     @Override
@@ -109,5 +79,26 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private boolean isStorageFull() {
+        return size() == data.length;
+    }
+
+    private void grow() {
+        int newCapacity = (data.length >> 1) + data.length;
+        E[] biggerData = (E[]) new Object[newCapacity];
+        System.arraycopy(data, 0, biggerData, 0, data.length);
+        data = biggerData;
+    }
+
+    private void checkIndexBounds(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index : " + index + " is out of bounds.");
+        }
+    }
+
+    private boolean areEqual(E a, E b) {
+        return a == b || a != null && a.equals(b);
     }
 }
