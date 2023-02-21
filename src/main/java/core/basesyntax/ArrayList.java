299454324d18833e7f4fddbe182c7b9edc90d4f2
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-02-21 12:18:37 +0200

    fixed solution hw-10-jv-array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,37 +1,33 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double GROWING_COEFFICIENT = 1.5;
-    private static final int NON_EXISTENT_INDEX = -1;
-    private Object[] values;
-    private int lengthCounter;
-    private int currentCapacity;
+    private T[] values;
+    private int size;
 
     public ArrayList() {
         values = (T[]) new Object[DEFAULT_CAPACITY];
-        currentCapacity = DEFAULT_CAPACITY;
     }
 
     @Override
     public void add(T value) {
-        grow(currentCapacity);
-        values[lengthCounter] = value;
-        lengthCounter++;
+        grow(values.length);
+        values[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > lengthCounter || index < 0) {
+        if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds");
         }
-        grow(currentCapacity);
-        System.arraycopy(values, index, values, index + 1, lengthCounter - index);
+        grow(values.length);
+        System.arraycopy(values, index, values, index + 1, size - index);
         values[index] = value;
-        lengthCounter++;
+        size++;
     }
 
     @Override
@@ -57,66 +53,48 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkIndex(index);
         final T value = (T) values[index];
-        int newLengthCounter = lengthCounter - 1;
+        int newLengthCounter = size - 1;
         if (newLengthCounter >= index) {
             System.arraycopy(values, index + 1, values, index, newLengthCounter - index);
         }
         values[newLengthCounter] = null;
-        lengthCounter--;
+        size--;
         return value;
     }
 
     @Override
     public T remove(T element) {
-        int index = NON_EXISTENT_INDEX;
-        if (element == null) {
-            lengthCounter--;
-            return null;
-        }
+        int index = -1;
         for (int i = 0; i < values.length; i++) {
-            if (Objects.equals(values[i], element)) {
-                index = i;
+            if (values[i] == element || values[i] != null && values[i].equals(element)) {
+                return remove(i);
             }
         }
-        checkElement(element, index);
-        checkIndex(index);
-        T value = (T) values[index];
-        remove(index);
-        return value;
+        throw new NoSuchElementException("The element " + element + " is not exist");
     }
 
     @Override
     public int size() {
-        return lengthCounter;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        if (lengthCounter == 0) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     public void grow(int oldCapacity) {
-        if (lengthCounter == currentCapacity) {
+        if (size == values.length) {
             int newCapacity = (int) (oldCapacity * GROWING_COEFFICIENT);
             T[] newValues = (T[]) new Object[newCapacity];
             System.arraycopy(values, 0, newValues, 0, values.length);
             values = newValues;
-            currentCapacity = values.length;
         }
     }
 
     public void checkIndex(int index) {
-        if (index >= lengthCounter || index < 0) {
+        if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds");
-        }
-    }
-
-    public void checkElement(T element, int index) {
-        if (index < 0) {
-            throw new NoSuchElementException("The element " + element + " is not exist");
         }
     }
 }
