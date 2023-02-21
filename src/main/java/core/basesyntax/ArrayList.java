e6ec3e70658eede2f220b911c3b2c39c06be2a49
yuriy49 <yuriy121991@gmail.com>
2023-02-21 22:46:17 +0200

    checkSize add improve style

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double INCREASE_SIZE = 1.5;
     private int size;
     private T[] data;
 
@@ -14,26 +15,18 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        add(value, size);
+        checkSize(size, data);
+        data[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndexFoAdd(index);
-        if (size == data.length) {
-            grow();
-        }
+        checkIndexToAdd(index);
+        checkSize(size, data);
         System.arraycopy(data, index, data, index + 1, size - index);
         data[index] = value;
         size++;
-    }
-
-    public void checkIndexFoAdd(int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Index of the bounds "
-                    + size + " Check your input index!"
-                    + index);
-        }
     }
 
     @Override
@@ -45,19 +38,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkInvalidIndex(index);
+        checkIndex(index);
         return data[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkInvalidIndex(index);
+        checkIndex(index);
         data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkInvalidIndex(index);
+        checkIndex(index);
         T item = data[index];
         System.arraycopy(data, index + 1, data, index, size - 1 - index);
         size--;
@@ -85,16 +78,30 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void grow() {
-        T[] newItems = (T[]) new Object[(int) (data.length * 1.5)];
+        T[] newItems = (T[]) new Object[(int) (data.length * INCREASE_SIZE)];
         System.arraycopy(data, 0, newItems, 0, size);
         data = newItems;
     }
 
-    private void checkInvalidIndex(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index of the bounds "
-                    + size + " Check your input index!"
+                    + size + " Check your input index! "
                     + index);
+        }
+    }
+
+    private void checkIndexToAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index of the bounds "
+                    + size + " Check your input index! "
+                    + index);
+        }
+    }
+
+    private void checkSize(int size, T[] data) {
+        if (size == data.length) {
+            grow();
         }
     }
 }
