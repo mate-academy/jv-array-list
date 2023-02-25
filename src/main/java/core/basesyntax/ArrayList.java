a438b9f3e61d6d92cdcf778bf8df23345d4a7cee
Volodymyr Sakalosh <sva2005@ukr.net>
2023-02-25 15:42:28 +0200

    Fixed method checkIndex(int index)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -87,7 +87,7 @@ public class ArrayList<T> implements List<T> {
 
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Index not valid");
+            throw new ArrayListIndexOutOfBoundsException("Not valid index: " + index);
         }
     }
 
