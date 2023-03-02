e21e7cade6ab9d30b171d8e201435bf6cc1f62ff
Denys Yastrebov <programmer_p@outlook.com>
2023-03-02 16:05:53 +0200

    firstTry

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,43 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
+    private int size = 0;
+    private int maxSize = 0;
+    private Object[] elementData;
+
     @Override
     public void add(T value) {
-
+        if (this.isEmpty()) {
+            elementData = new Object[DEFAULT_SIZE];
+            maxSize = DEFAULT_SIZE;
+        } else {
+            if (this.size == maxSize) {
+                maxSize = maxSize + maxSize / 2;
+                Object[] oldElementData = elementData;
+                elementData = new Object[maxSize];
+                elementData = oldElementData;
+            }
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < size) {
+          Object[] oldElementData = new Object[size];
+          oldElementData = elementData;
+          for (int i = 0; i < index; i++) {
+              elementData[i] = oldElementData[i];
+          }
+          elementData[index] = value;
+          for (int i = index + 1; i < maxSize; i++) {
+              elementData[i] = oldElementData[i-1];
+          }
+        }
     }
 
     @Override
@@ -18,31 +47,45 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        final Object[] es = elementData;
+
+        T oldValue = (T) es[index];
+        fastRemove(es, index);
+
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void fastRemove(Object[] es, int i) {
+        final int newSize;
+        if ((newSize = size - 1) > i)
+            System.arraycopy(es, i + 1, es, i, newSize - i);
+        es[size = newSize] = null;
     }
 }
