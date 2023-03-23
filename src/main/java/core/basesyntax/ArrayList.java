9b94114805bc40e6c9074aa5c17141adc61a822c
irynamatveieva <irina.m2004@icloud.com>
2023-03-23 11:17:04 +0200

    simplified the code in some methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] arrayOfElements;
-    private int sizeOfElements = 0;
+    private int size = 0;
 
     public ArrayList() {
         this.arrayOfElements = new Object[DEFAULT_CAPACITY];
@@ -13,15 +13,15 @@ public class ArrayList<T> implements List<T> {
 
     private Object[] growIfArrayFull() {
         Object[] grownArray = arrayOfElements;
-        if (sizeOfElements == arrayOfElements.length) {
+        if (size == arrayOfElements.length) {
             grownArray = new Object[(int) (arrayOfElements.length * 1.5)];
-            System.arraycopy(arrayOfElements, 0, grownArray, 0, sizeOfElements);
+            System.arraycopy(arrayOfElements, 0, grownArray, 0, size);
         }
         return grownArray;
     }
 
     private void checkForValidIndexValue(int index) {
-        if (index < 0 || index > sizeOfElements - 1) {
+        if (index < 0 || index > size - 1) {
             throw new ArrayListIndexOutOfBoundsException("Invalid value of index.");
         }
     }
@@ -29,54 +29,28 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         arrayOfElements = growIfArrayFull();
-        for (int i = 0; i < arrayOfElements.length; i++) {
-            if (arrayOfElements[i] == null && i >= sizeOfElements) {
-                arrayOfElements[i] = value;
-                sizeOfElements++;
-                break;
-            }
-        }
+        arrayOfElements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
         arrayOfElements = growIfArrayFull();
-        for (int i = 0; i < arrayOfElements.length; i++) {
-            if (i == index && arrayOfElements[i] != null) {
-                Object[] resultedArray = new Object[arrayOfElements.length + 1];
-                System.arraycopy(arrayOfElements, 0, resultedArray, 0, i);
-                resultedArray[index] = value;
-                sizeOfElements++;
-                System.arraycopy(arrayOfElements, index,
-                        resultedArray, index + 1, sizeOfElements - index);
-                arrayOfElements = resultedArray;
-                break;
-            }
-            if (arrayOfElements[i] == null && index >= sizeOfElements) {
-                arrayOfElements[index] = value;
-                sizeOfElements++;
-                break;
-            }
-        }
-        if (index > sizeOfElements || index < 0) {
+        if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Invalid value of index.");
         }
+        for (int i = size; i > index; i--) {
+            arrayOfElements[i] = arrayOfElements[i - 1];
+        }
+        arrayOfElements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        Object[] newArray = new Object[sizeOfElements + list.size()];
-        System.arraycopy(arrayOfElements, 0, newArray, 0, sizeOfElements);
-        int j = 0;
-        for (int i = sizeOfElements; i < newArray.length; i++) {
-            while (j < list.size()) {
-                newArray[i] = list.get(j);
-                sizeOfElements++;
-                j++;
-                break;
-            }
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
-        arrayOfElements = newArray;
     }
 
     @Override
@@ -94,41 +68,31 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         checkForValidIndexValue(index);
-        Object[] resultedArray = new Object[sizeOfElements - 1];
-        System.arraycopy(arrayOfElements, 0, resultedArray, 0, index);
-        System.arraycopy(arrayOfElements, index + 1,
-                resultedArray, index, sizeOfElements - index - 1);
         T removedValue = get(index);
-        arrayOfElements = resultedArray;
-        sizeOfElements--;
+        System.arraycopy(arrayOfElements, index + 1,
+                arrayOfElements, index, size - index - 1);
+        size--;
         return removedValue;
     }
 
     @Override
     public T remove(T element) {
-        int index = 0;
-        boolean valueExist = false;
-        for (int i = 0; i < arrayOfElements.length; i++) {
+        for (int i = 0; i < size; i++) {
             if ((arrayOfElements[i] != null && arrayOfElements[i].equals(element))
                     || arrayOfElements[i] == element) {
-                index = i;
-                valueExist = true;
-                break;
+                return remove(i);
             }
         }
-        if (!valueExist) {
-            throw new NoSuchElementException("There is no such element present in the list.");
-        }
-        return remove(index);
+        throw new NoSuchElementException("There is no such element present in the list.");
     }
 
     @Override
     public int size() {
-        return sizeOfElements;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return sizeOfElements == 0;
+        return size == 0;
     }
 }
