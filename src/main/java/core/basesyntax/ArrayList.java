f539248ebaf103b1c8a22eb543ecb2f322d9bce0
irynamatveieva <irina.m2004@icloud.com>
2023-03-23 14:00:47 +0200

    made exceprions more informative and use method to copy array

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,24 +5,16 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] arrayOfElements;
-    private int size = 0;
+    private int size;
 
     public ArrayList() {
         this.arrayOfElements = new Object[DEFAULT_CAPACITY];
     }
 
-    private Object[] growIfArrayFull() {
-        Object[] grownArray = arrayOfElements;
-        if (size == arrayOfElements.length) {
-            grownArray = new Object[(int) (arrayOfElements.length * 1.5)];
-            System.arraycopy(arrayOfElements, 0, grownArray, 0, size);
-        }
-        return grownArray;
-    }
-
-    private void checkForValidIndexValue(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index > size - 1) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid value of index.");
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index
+                    + ". Size: " + size);
         }
     }
 
@@ -37,11 +29,10 @@ public class ArrayList<T> implements List<T> {
     public void add(T value, int index) {
         arrayOfElements = growIfArrayFull();
         if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid value of index.");
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index
+                    + ". Size: " + size);
         }
-        for (int i = size; i > index; i--) {
-            arrayOfElements[i] = arrayOfElements[i - 1];
-        }
+        System.arraycopy(arrayOfElements, index, arrayOfElements, index + 1, size - index);
         arrayOfElements[index] = value;
         size++;
     }
@@ -55,19 +46,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkForValidIndexValue(index);
+        checkIndex(index);
         return (T) arrayOfElements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkForValidIndexValue(index);
+        checkIndex(index);
         arrayOfElements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkForValidIndexValue(index);
+        checkIndex(index);
         T removedValue = get(index);
         System.arraycopy(arrayOfElements, index + 1,
                 arrayOfElements, index, size - index - 1);
@@ -94,5 +85,14 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private Object[] growIfArrayFull() {
+        Object[] grownArray = arrayOfElements;
+        if (size == arrayOfElements.length) {
+            grownArray = new Object[(int) (arrayOfElements.length * 1.5)];
+            System.arraycopy(arrayOfElements, 0, grownArray, 0, size);
+        }
+        return grownArray;
     }
 }
