ac34d2d34b2c7c5617cff928c7391a23be39621c
Rule <amirparadice@gmail.com>
2023-03-24 00:08:26 -0400

    1.Created private static fields and two necessary parametres 2.Declare the size of standart Array 3.Made grow method and checkIndex 4.Overrided methods one by one. 5.Importing NoSuchElementException.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,101 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    public static final int DEFAULT_SIZE = 10;
+    public static final double GROW_RATE = 1.5;
+    private int size;
+    private T[] elementData;
+
+    public ArrayList() {
+        this.elementData = (T[]) new Object[DEFAULT_SIZE];
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            add(value);
+            return;
+        }
+        checkI(index);
+        growIfArrayFull();
+        System.arraycopy(elementData,index,elementData,index + 1,size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkI(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkI(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkI(index);
+        T deletedElement = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
+        size--;
+        return deletedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == null && element == null
+                    || elementData[i] != null && elementData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("element " + element + " doesn't exist");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growIfArrayFull() {
+        if (size == elementData.length) {
+            int newObjectsSize = (int) (size * GROW_RATE);
+            T[] newSize = (T[]) new Object[newObjectsSize];
+            System.arraycopy(this.elementData,0, newSize, 0,size);
+            this.elementData = newSize;
+        }
+    }
+
+    private void checkI(int index) {
+        if (index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("index is less than " + 0);
+        }
+        if (index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("index is exceeding " + size);
+        }
     }
 }
