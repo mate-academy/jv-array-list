9453ecb1ca21e541bde8b977afdda37a2219eb74
Artem <artemenko.as@gmail.com>
2023-04-28 13:08:03 +0300

    Added methods for indexOutOfRange, indexOutOfRangeForAdd and resize. Method addAll is now less complex. Shifting elements with System.arraycopy.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_ARRAY_LIST_CAPACITY = 10;
@@ -17,24 +17,16 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         if (size == elements.length) {
-            int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
-            T[] newElements = (T[]) new Object[newCapacity];
-            System.arraycopy(elements, 0, newElements, 0, elements.length);
-            elements = newElements;
+            resize();
         }
         elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        indexOutOfRange(index);
+        indexOutOfRangeForAdd(index);
         if (size == elements.length) {
-            int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
-            T[] newElements = (T[]) new Object[newCapacity];
-            System.arraycopy(elements, 0, newElements, 0, index);
-            newElements[index] = value;
-            System.arraycopy(elements, index, newElements, index + 1, size - index);
-            elements = newElements;
+            resize(value, index);
         } else {
             for (int i = size - 1; i >= index; i--) {
                 elements[i + 1] = elements[i];
@@ -48,50 +40,32 @@ public class ArrayList<T> implements List<T> {
     public void addAll(List<T> list) {
         int remainingCapacity = elements.length - size;
         if (list.size() > remainingCapacity) {
-            int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
-            T[] newElements = Arrays.copyOf(elements, newCapacity);
-            System.arraycopy(elements, 0, newElements, 0, elements.length);
-            elements = newElements;
+            resize();
         }
         for (int i = 0; i < list.size(); i++) {
-            if (size == elements.length) {
-                int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
-                elements = (T[]) new Object[newCapacity];
-            }
-            elements[size++] = list.get(i);
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index must be between 0 and " + (elements.length - 1)
-            );
-        }
+        indexOutOfRange(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index must be between 0 and " + (elements.length - 1)
-            );
-        }
+        indexOutOfRange(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index must be between 0 and " + (size - 1)
-            );
-        }
+        indexOutOfRange(index);
         final T removedElement = elements[index];
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
+        int elementsMoved = size - index - 1;
+        if (elementsMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, elementsMoved);
         }
         size--;
         elements[size] = null;
@@ -100,19 +74,10 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        if (element == null) {
-            size--;
-            return null;
-        }
         for (int i = 0; i < size; i++) {
-            if (element.equals(elements[i])) {
-                final T removedElement = elements[i];
-                for (int j = i; j < size - 1; j++) {
-                    elements[j] = elements[j + 1];
-                }
-                elements[size] = null;
-                size--;
-                return removedElement;
+            if (Objects.equals(element, elements[i])) {
+                return remove(i);
+
             }
         }
         throw new NoSuchElementException("There is no such value");
@@ -128,10 +93,34 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void indexOutOfRange(int index) {
+    private void indexOutOfRangeForAdd(int index) {
         String msg = "Index must be between 0 and ";
         if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException(msg + (elements.length - 1));
+            throw new ArrayListIndexOutOfBoundsException(msg + (size - 1));
         }
+    }
+
+    private void indexOutOfRange(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index must be between 0 and " + (size - 1)
+            );
+        }
+    }
+
+    private void resize() {
+        int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
+        T[] newElements = (T[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+
+    private void resize(T value, int index) {
+        int newCapacity = (int) (elements.length * ARRAY_MULTIPLIER);
+        T[] newElements = (T[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, newElements, 0, index);
+        newElements[index] = value;
+        System.arraycopy(elements, index, newElements, index + 1, size - index);
+        elements = newElements;
     }
 }
