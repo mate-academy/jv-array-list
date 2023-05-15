cebbb66d3c2f1b50d8dc0cc62ff6b2ba9ea1b38d
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2023-05-15 23:05:16 +0300

    some methods were filled

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,19 +1,21 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double RESIZE_FACTOR = 1.5;
-    private T[] elements;
+    private T[] dataArray;
     int size = 0;
 
     public ArrayList() {
-        elements = (T[]) new Object[DEFAULT_CAPACITY];
+        dataArray = (T[]) new Object[DEFAULT_CAPACITY];
     }
-
-
 
     @Override
     public void add(T value) {
+        growIfArrayFull();
+        dataArray[size++] = value;
     }
 
     @Override
@@ -27,8 +29,10 @@ public class ArrayList<T> implements List<T> {
     }
 
     @Override
-    public T get(int index) {
-        return null;
+    public T get(int index) throws ArrayListIndexOutOfBoundsException {
+        checkIndexEqualsSize(index);
+        checkIndex(index);
+        return (T) dataArray[index];
     }
 
     @Override
@@ -38,21 +42,54 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        return null;
+        get(index);
+        T removedValue = dataArray[index];
+        size--;
+        System.arraycopy(dataArray, index + 1, dataArray, index, size - index);
+        return removedValue;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T element) throws  NoSuchElementException {
+        for (int i = 0; i < size; i++) {
+            if (dataArray[i] == element || dataArray[i] != null &&
+            dataArray[i].equals(element)) {
+                size--;
+                return dataArray[i];
+            }
+        }
+        throw new NoSuchElementException("No such elelement" + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growIfArrayFull() {
+        if (size == dataArray.length) {
+            int oldLength = size;
+            int newLength = (int) (oldLength * RESIZE_FACTOR);
+            T[] newDataArray = (T[]) new Object[newLength];
+            System.arraycopy(dataArray, 0, newDataArray, 0, size);
+            dataArray = newDataArray;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is wrong: " + index);
+        }
+    }
+
+    private void checkIndexEqualsSize(int index) {
+        if (index == size) {
+            throw new ArrayListIndexOutOfBoundsException("Index is wrong: " + index);
+        }
     }
 }
