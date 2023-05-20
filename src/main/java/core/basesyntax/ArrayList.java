b04ed782aa807598570827e99fe1f285952df638
pochtalon <pochtalon1010@gmail.com>
2023-05-20 17:17:35 +0300

    Implemented List interface; Created 3 constants; Created 3 variables "size", "capacity" and "array" Created additional methods "grow", "legalBound"

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,124 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int START_CAPACITY = 10;
+    private static final int EMPTY = 0;
+    private static final double GROW = 1.5;
+    private int size;
+    private int capacity;
+    private T[] array;
+
+    public ArrayList() {
+        array = (T[]) new Object[START_CAPACITY];
+        size = EMPTY;
+        capacity = START_CAPACITY;
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == capacity) {grow();}
+        array[size] = value;
+        size++;
+    }
+    private void grow() {
+        capacity = (int) (capacity * GROW);
+        T[] newArray = (T[]) new Object[capacity];
+        System.arraycopy(array, 0, newArray,0, size);
+        array = newArray;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("IndexOutOfBounds");
+        } else {
+            if (size == capacity) { grow(); }
+            T[] newArray = (T[]) new Object[capacity];
+            System.arraycopy(array, 0, newArray,0, index);
+            newArray[index] = value;
+            System.arraycopy(array, index, newArray,index + 1, size - index);
+            array = newArray;
+            size++;
+        }
+    }
 
+    private boolean legalBound(int index) {
+        if (index < 0 || index > size - 1) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (size + list.size() > capacity) {grow();}
+        T[] acceptedArray =  (T[]) new Object[list.size()];
+        for (int i = 0; i < acceptedArray.length; i++) {
+            acceptedArray[i] = list.get(i);
+        }
+        System.arraycopy(acceptedArray, 0, array, size, acceptedArray.length);
+        size += acceptedArray.length;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (legalBound(index)) {
+            throw new ArrayListIndexOutOfBoundsException("IndexOutOfBounds");
+        } else {
+            return array[index];
+        }
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (legalBound(index)) {
+            throw new ArrayListIndexOutOfBoundsException("IndexOutOfBounds");
+        } else {
+            array[index] = value;
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (legalBound(index)) {
+            throw new ArrayListIndexOutOfBoundsException("IndexOutOfBounds");
+        } else {
+            T result = array[index];
+            size--;
+            System.arraycopy(array, index + 1, array, index, size - index);
+            return result;
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        T result = null;
+        int index = 0;
+        for (int i = 0; i < size; i++){
+            if (array[i] == element || array[i] != null && array[i].equals(element)) {
+                result = element;
+                index = i;
+                break;
+            }
+        }
+        if (result == null && element != null) {
+            throw new NoSuchElementException();
+        } else {
+            size--;
+            System.arraycopy(array, index + 1, array, index, size - index);
+            return result;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == EMPTY;
     }
 }
