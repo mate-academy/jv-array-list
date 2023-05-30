96d76f4c2e4280a3e1d9de43cb7329dcc6cd1638
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-05-30 21:04:57 +0300

    fix coments

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,26 +1,20 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double NUMBER_INCREAD_ARRAY = 1.5;
-    private Object[] elements;
+    private T[] elements;
     private int size;
 
     public ArrayList() {
         this.elements = (T[]) new Object[DEFAULT_CAPACITY];
-        this.size = 0;
     }
 
     @Override
     public void add(T value) {
-        if (elements.length == size) {
-            Object[] newArray = new Object[(int) (elements.length * NUMBER_INCREAD_ARRAY)];
-            System.arraycopy(elements, 0, newArray, 0, size);
-            elements = newArray;
-        }
+        resizedIfNeeded();
         elements[size] = value;
         size++;
     }
@@ -30,11 +24,7 @@ public class ArrayList<T> implements List<T> {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("index are incorect");
         }
-        if (elements.length == size) {
-            Object[] newArray = new Object[(int) (elements.length * NUMBER_INCREAD_ARRAY)];
-            System.arraycopy(elements, 0, newArray, 0, size);
-            elements = newArray;
-        }
+        resizedIfNeeded();
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
@@ -56,25 +46,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("index are incorect");
-        }
+        checkIndex(index);
         return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("index are incorect");
-        }
+        checkIndex(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("index are incorect");
-        }
+        checkIndex(index);
         T removedIndex = (T) elements[index];
         System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         size--;
@@ -83,21 +67,27 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        int indexOfElement = -1;
-        for (int i = 0; i < elements.length; i++) {
-            if (Objects.equals(elements[i], element)) {
-                indexOfElement = i;
-                break;
+        for (int i = 0; i < size; i++) {
+            if ((elements[i] == null && element == null)
+                    || (elements[i] != null && elements[i].equals(element))) {
+                return remove(i);
             }
         }
-        if (indexOfElement == -1) {
-            throw new NoSuchElementException("elements not found");
+        throw new NoSuchElementException("Element not found: " + element);
+    }
+
+    public void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("index are incorect");
         }
-        T removedElements = (T) elements[indexOfElement];
-        System.arraycopy(elements, indexOfElement + 1, elements, indexOfElement,
-                size - indexOfElement - 1);
-        size--;
-        return removedElements;
+    }
+
+    public void resizedIfNeeded() {
+        if (elements.length == size) {
+            Object[] newArray = new Object[(int) (elements.length * NUMBER_INCREAD_ARRAY)];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = (T[]) newArray;
+        }
     }
 
     @Override
