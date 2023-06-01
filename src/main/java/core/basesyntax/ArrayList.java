3e7773445a6d2207bac5100ae8cbcac21f68ed28
Sieryi D <hunterst@ukr.net>
2023-06-01 18:05:53 +0300

    second version (with many changes)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,27 +6,15 @@ public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private T[] elementArray;
     private int size;
-    private int capacity;
 
     public ArrayList() {
-        capacity = DEFAULT_CAPACITY;
-        elementArray = (T[]) new Object[capacity];
-    }
-
-    public T[] elementArrayPlusSize(T[] elementArray, int increasingInSizeByInt) {
-        int futureSize = size + increasingInSizeByInt;
-        while (futureSize >= capacity) {
-            capacity += (capacity >> 1);
-        }
-        T[] arrayPlusSize = (T[]) new Object[capacity];
-        System.arraycopy(elementArray, 0, arrayPlusSize, 0, size);
-        return arrayPlusSize;
+        elementArray = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == capacity) {
-            elementArray = elementArrayPlusSize(elementArray, 1);
+        if (size == elementArray.length) {
+            elementArray = grow(elementArray, 1);
         }
         elementArray[size] = value;
         size++;
@@ -34,81 +22,61 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index >= 0 && index <= size) {
-            if (size == capacity) {
-                elementArray = elementArrayPlusSize(elementArray, 1);
-            }
-            size++;
-            System.arraycopy(elementArray, index, elementArray, index + 1, size - index - 1);
-            elementArray[index] = value;
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
+        checkIndexEqualsSize(index);
+        if (size == elementArray.length) {
+            elementArray = grow(elementArray, 1);
         }
+        size++;
+        System.arraycopy(elementArray, index, elementArray, index + 1, size - index - 1);
+        elementArray[index] = value;
     }
 
     @Override
     public void addAll(List<T> list) {
         int listSize = list.size();
-        int oldSize = size;
-        if (size + listSize >= capacity) {
-            elementArray = elementArrayPlusSize(elementArray, listSize);
+        if (size + listSize >= elementArray.length) {
+            elementArray = grow(elementArray, listSize);
         }
         for (int a = 0; a < listSize; a++) {
-            elementArray[oldSize + a] = list.get(a);
-            size++;
+            add(list.get(a));
         }
     }
 
     @Override
     public T get(int index) {
-        if (index >= 0 && index < size) {
-            return elementArray[index];
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
-        }
+        checkIndex(index);
+        return elementArray[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= 0 && index < size) {
-            elementArray[index] = value;
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
-        }
+        checkIndex(index);
+        elementArray[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= 0 && index < size) {
-            size--;
-            T deletedElement = elementArray[index];
-            System.arraycopy(elementArray, index + 1, elementArray, index, size - index);
-            elementArray[size] = null;
-            return deletedElement;
-        } else {
-            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
-        }
+        checkIndex(index);
+        size--;
+        T deletedElement = elementArray[index];
+        System.arraycopy(elementArray, index + 1, elementArray, index, size - index);
+        elementArray[size] = null;
+        return deletedElement;
     }
 
     @Override
     public T remove(T element) {
         int numberDeletedElement = -1;
-        T deletedElement = null;
         boolean deleteElement = false;
         for (int a = 0; a < size; a++) {
             if (element == elementArray[a] || element != null && element.equals(elementArray[a])) {
-                deletedElement = elementArray[a];
                 numberDeletedElement = a;
                 deleteElement = true;
                 break;
             }
         }
         if (deleteElement == true) {
-            System.arraycopy(elementArray, numberDeletedElement + 1, elementArray,
-                    numberDeletedElement, size - 1 - numberDeletedElement);
-            elementArray[size - 1] = null;
-            size--;
-            return deletedElement;
+            return remove(numberDeletedElement);
         } else {
             throw new NoSuchElementException("this element is missing from the T[] elementArray");
         }
@@ -121,9 +89,30 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
+        return size == 0;
     }
+
+    private T[] grow(T[] elementArray, int increasingInSizeByInt) {
+        int futureSize = size + increasingInSizeByInt;
+        int newCapacity = elementArray.length;
+        while (futureSize >= newCapacity) {
+            newCapacity += (newCapacity >> 1);
+        }
+        T[] arrayPlusSize = (T[]) new Object[newCapacity];
+        System.arraycopy(elementArray, 0, arrayPlusSize, 0, size);
+        return arrayPlusSize;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
+        }
+    }
+
+    private void checkIndexEqualsSize(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("this index is not correction");
+        }
+    }
+
 }
