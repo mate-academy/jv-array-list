edfb3b80e2e3b24a38f14e3bf87c4bcff2da17ad
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-06-06 00:36:19 +0300

    added remove and get methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
 
     private Object[] internalStorage;
     private int size;
@@ -22,6 +23,9 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
+        if (size > 0 && index <= size - 1) {
+            return (T) internalStorage[index - 1];
+        }
         return null;
     }
 
@@ -32,12 +36,23 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
+        if (size > 0) {
+            if (index == size - 1) {
+                internalStorage[index] = null;
+                size--;
+            }
+            if (index < size - 1) {
+                System.arraycopy(internalStorage, index + 1, internalStorage, index, size - 1 - index);
+                size--;
+            }
+            return (T) internalStorage[index];
+        }
         return null;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        return remove(indexOf(element));
     }
 
     @Override
@@ -52,5 +67,14 @@ public class ArrayList<T> implements List<T> {
 //            return true;
 //        }
 //        return false;
+    }
+
+    private int indexOf(T element) {
+        for (int i = 0; i < size; i++) {
+            if (internalStorage[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
