d3bd69d1d90affe0d62373b250903e68ff6af14b
Sieryi D <hunterst@ukr.net>
2023-06-06 13:59:07 +0300

    add changes from 06.06 (changed method grow() etc.)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROW_FACTOR = 1.5;
     private T[] elementArray;
     private int size;
 
@@ -14,28 +15,25 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         if (size == elementArray.length) {
-            //elementArray = grow(elementArray, 1);
-            elementArray = grow();
+            grow();
         }
-        elementArray[size] = value;
-        size++;
+        elementArray[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndexEqualsSize(index);
         if (size == elementArray.length) {
-            elementArray = grow();
+            grow();
         }
         size++;
+        checkIndex(index);
         System.arraycopy(elementArray, index, elementArray, index + 1, size - index - 1);
         elementArray[index] = value;
     }
 
     @Override
     public void addAll(List<T> list) {
-        int listSize = list.size();
-        for (int a = 0; a < listSize; a++) {
+        for (int a = 0; a < list.size(); a++) {
             add(list.get(a));
         }
     }
@@ -83,26 +81,15 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private T[] grow() {
-        int futureSize = size + 1;
-        int newCapacity = elementArray.length;
-        while (futureSize >= newCapacity) {
-            newCapacity += (newCapacity >> 1);
-        }
-        T[] arrayPlusSize = (T[]) new Object[newCapacity];
-        System.arraycopy(elementArray, 0, arrayPlusSize, 0, size);
-        return arrayPlusSize;
+    private void grow() {
+        int newCapacity = (int) (elementArray.length * GROW_FACTOR);
+        T[] resizedArray = (T[]) new Object[newCapacity];
+        System.arraycopy(elementArray, 0, resizedArray, 0, size);
+        elementArray = resizedArray;
     }
 
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("index " + index
-                    + " is not correction. size = " + size);
-        }
-    }
-
-    private void checkIndexEqualsSize(int index) {
-        if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("index " + index
                     + " is not correction. size = " + size);
         }
