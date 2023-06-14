894a23db536ec7ff9990e5aca7ea26b5e3e46d92
Yura Khilko <yurakhilko@gmail.com>
2023-06-14 10:44:29 +0300

    added fixes after review.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,24 +6,23 @@ public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double RESIZE_COEFFICIENT = 1.5;
     private Object[] array;
-    private Object[] tempArray;
-    private int size = 0;
+    private int size;
 
     public ArrayList(int capacity) {
         if (capacity > 0) {
-            this.array = new Object[capacity];
+            array = new Object[capacity];
         } else {
-            this.array = new Object[DEFAULT_CAPACITY];
+            array = new Object[DEFAULT_CAPACITY];
         }
     }
 
     public ArrayList() {
-        this.array = new Object[DEFAULT_CAPACITY];
+        array = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (this.size + 1 > array.length) {
+        if (size + 1 > array.length) {
             resizeArray(array);
         }
         array[size] = value;
@@ -32,47 +31,43 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Wrong index " + index);
-        } else if (index == size) {
-            this.add(value);
-        } else {
-            tempArray = new Object[size - index];
-            System.arraycopy(array, index, tempArray, 0, size - index);
-            this.set(value, index);
-            size = index + 1;
-            for (Object tempArrayRow : tempArray) {
-                this.add((T) tempArrayRow);
-            }
+        if (index == size) {
+            add(value);
+            return;
         }
-
+        checkIndex(index);
+        // case when added value into the middle of array:
+        Object lastElement = array[size - 1];
+        System.arraycopy(array, index, array, index + 1, size - index - 1);
+        set(value, index);
+        add((T) lastElement);
     }
 
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
-            this.add(list.get(i));
+            add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        indexCheck(index);
+        checkIndex(index);
         return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        indexCheck(index);
+        checkIndex(index);
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        indexCheck(index);
+        checkIndex(index);
         T value;
         value = (T) array[index];
-        System.arraycopy(array,index + 1, array, index, size - index - 1);
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
         array[array.length - 1] = null;
         size--;
         return value;
@@ -80,41 +75,25 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        boolean isFound = false;
         for (int i = 0; i < array.length; i++) {
-            if (this.equalsRow(array[i], element)) {
+            if (equalsRow(array[i], element)) {
                 remove(i);
-                isFound = true;
-                break;
+                return element;
             }
         }
-        if (isFound) {
-            return element;
-        } else {
-            throw new NoSuchElementException("No such element " + element);
-        }
+        throw new NoSuchElementException("No such element " + element);
     }
 
     private void resizeArray(Object[] array) {
-        tempArray = array;
         this.array = new Object[(int) (array.length * RESIZE_COEFFICIENT)];
-        System.arraycopy(tempArray, 0, this.array, 0, tempArray.length);
+        System.arraycopy(array, 0, this.array, 0, array.length);
     }
 
     public <T> boolean equalsRow(T element1, T element2) {
-        if (element1 == element2) {
-            return true;
-        }
-        if (element1 == null || element2 == null) {
-            return false;
-        }
-        if (element1.getClass().equals(element2.getClass())) {
-            return element1.equals(element2);
-        }
-        return false;
+        return element1 == element2 || element1 != null && element1.equals(element2);
     }
 
-    public void indexCheck(int index) {
+    public void checkIndex(int index) {
         if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Wrong index " + index);
         }
@@ -122,7 +101,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     @Override
