8b1f1b9bfd5b374661583af39f737d95e29b04be
Anton Uzhva <anton.itec91@gmail.com>
2023-06-19 18:14:48 +0300

    Initial commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,42 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_CAPICITY = 10;
+    private int size;
+    private Object[] elementData;
+
+    public ArrayList() {
+        elementData = new Object[INITIAL_CAPICITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            growElementData();
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size == index) {
+            add(value);
+            return;
+        }
+        if (index > -1 && index < size) {
+            if (size + 1 == elementData.length) {
+                growElementData();
+            }
+            System.arraycopy(elementData, index, elementData, index + 1, size - index);
+            elementData[index] = value;
+            size++;
+            return;
+        }
+        throw new ArrayListIndexOutOfBoundsException("Wrong index!");
     }
 
     @Override
@@ -18,31 +46,51 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index == size) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index!");
+        }
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index == size) {
+            throw new ArrayListIndexOutOfBoundsException("Wrong index!");
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T elementToReturn = get(index);
+        System.arraycopy(elementData, index + 1, elementData, index, size - index);
+        size--;
+        return elementToReturn;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no element such element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growElementData() {
+        Object[] increasedArr = new Object[(int) (elementData.length * 1.5)];
+        System.arraycopy(elementData, 0, increasedArr, 0, elementData.length);
+        elementData = increasedArr;
     }
 }
