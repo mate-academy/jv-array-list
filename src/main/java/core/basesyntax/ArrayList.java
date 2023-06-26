81b6a80ee9420669bfcfb3fa22a142925c01a68a
User <Monsterhunter@ukr.net>
2023-06-26 20:23:24 +0300

    Created ArrayList class that implements List interface changed Overrides

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,353 @@
 package core.basesyntax;
 
+import java.util.*;
+
 public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROWTH_FACTOR = 1.5;
+    private Object[] elements;
+    private int size;
 
+    public ArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
-    @Override
-    public void add(T value, int index) {
-
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = (int) (oldCapacity * GROWTH_FACTOR);
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
     }
 
-    @Override
-    public void addAll(List<T> list) {
-
+    private void checkIndexBounds(int index) {
+        if (index < 0 || index >= size)
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
     }
 
-    @Override
-    public T get(int index) {
-        return null;
-    }
-
-    @Override
-    public void set(T value, int index) {
-
-    }
-
-    @Override
-    public T remove(int index) {
-        return null;
-    }
-
-    @Override
-    public T remove(T element) {
-        return null;
+    private void checkElementExistence(T element) {
+        if (!contains(element))
+            throw new NoSuchElementException("Element not found: " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], o))
+                return true;
+        }
         return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (cursor >= size)
+                    throw new NoSuchElementException();
+                @SuppressWarnings("unchecked")
+                T element = (T) elements[cursor];
+                cursor++;
+                return element;
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <E> E[] toArray(E[] a) {
+        if (a.length < size) {
+            return (E[]) Arrays.copyOf(elements, size, a.getClass());
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    @Override
+    public boolean add(T element) {
+        ensureCapacity(size + 1);
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    @Override
+    public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        if (size == elements.length) {
+            ensureCapacity(size + 1);
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = value;
+        size++;
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        Object[] objects = list.toArray();
+        for (Object object : objects) {
+            add((T) object);
+        }
+
+    }
+
+    @Override
+    public T remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], o)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Element not found: " + o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object element : c) {
+            if (!contains(element))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        ensureCapacity(size + c.size());
+        int i = size;
+        for (T element : c) {
+            elements[i] = element;
+            i++;
+        }
+        size += c.size();
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        checkIndexBounds(index);
+        ensureCapacity(size + c.size());
+        int numElementsToShift = size - index;
+        if (numElementsToShift > 0) {
+            System.arraycopy(elements, index, elements, index + c.size(), numElementsToShift);
+        }
+        for (T element : c) {
+            elements[index++] = element;
+            size++;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (c.contains(elements[i])) {
+                remove(i);
+                modified = true;
+                i--;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elements[i])) {
+                remove(i);
+                modified = true;
+                i--;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(elements, 0, size, null);
+        size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndexBounds(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public void set(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        checkIndexBounds(index);
+        elements[index] = value;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        checkIndexBounds(index);
+        T previousElement = (T) elements[index];
+        elements[index] = element;
+        return previousElement;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        checkIndexBounds(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndexBounds(index);
+        T removedElement = (T) elements[index];
+        int numElementsToShift = size - index - 1;
+        if (numElementsToShift > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numElementsToShift);
+        }
+        elements[--size] = null;
+        return removedElement;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], o))
+                return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(elements[i], o))
+                return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return listIterator(0);
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        checkIndexBounds(index);
+        return new ListIterator<T>() {
+            private int cursor = index;
+            private int lastReturnedIndex = -1;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (cursor >= size)
+                    throw new NoSuchElementException();
+                lastReturnedIndex = cursor;
+                @SuppressWarnings("unchecked")
+                T element = (T) elements[cursor++];
+                return element;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return cursor > 0;
+            }
+
+            @Override
+            public T previous() {
+                if (cursor <= 0)
+                    throw new NoSuchElementException();
+                lastReturnedIndex = --cursor;
+                @SuppressWarnings("unchecked")
+                T element = (T) elements[cursor];
+                return element;
+            }
+
+            @Override
+            public int nextIndex() {
+                return cursor;
+            }
+
+            @Override
+            public int previousIndex() {
+                return cursor - 1;
+            }
+
+            @Override
+            public void remove() {
+                if (lastReturnedIndex < 0)
+                    throw new IllegalStateException();
+                ArrayList.this.remove(lastReturnedIndex);
+                if (lastReturnedIndex < cursor)
+                    cursor--;
+                lastReturnedIndex = -1;
+            }
+
+            @Override
+            public void set(T element) {
+                if (lastReturnedIndex < 0)
+                    throw new IllegalStateException();
+                ArrayList.this.set(lastReturnedIndex, element);
+            }
+
+            @Override
+            public void add(T element) {
+                ArrayList.this.add(cursor++, element);
+                lastReturnedIndex = -1;
+            }
+        };
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        checkIndexBounds(fromIndex);
+        checkIndexBounds(toIndex - 1);
+        ArrayList<T> subList = new ArrayList<>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            subList.add(get(i));
+        }
+        return subList;
     }
 }
--- src/main/java/core/basesyntax/List.java
+++ src/main/java/core/basesyntax/List.java
@@ -1,15 +1,44 @@
 package core.basesyntax;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public interface List<T> {
-    void add(T value);
+    boolean contains(Object o);
+
+    Iterator<T> iterator();
+
+    Object[] toArray();
+
+    @SuppressWarnings("unchecked")
+    <E> E[] toArray(E[] a);
+
+    boolean add(T value);
 
     void add(T value, int index);
 
     void addAll(List<T> list);
 
+    boolean containsAll(Collection<?> c);
+
+    boolean addAll(Collection<? extends T> c);
+
+    boolean addAll(int index, Collection<? extends T> c);
+
+    boolean removeAll(Collection<?> c);
+
+    boolean retainAll(Collection<?> c);
+
+    void clear();
+
     T get(int index);
 
     void set(T value, int index);
+
+    T set(int index, T element);
+
+    void add(int index, T element);
 
     T remove(int index);
 
@@ -18,4 +47,14 @@ public interface List<T> {
     int size();
 
     boolean isEmpty();
+
+    int indexOf(Object o);
+
+    int lastIndexOf(Object o);
+
+    ListIterator<T> listIterator();
+
+    ListIterator<T> listIterator(int index);
+
+    List<T> subList(int fromIndex, int toIndex);
 }
