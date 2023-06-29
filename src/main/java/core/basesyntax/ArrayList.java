fed88a27ba37f9269b190002eabadbec469fcfff
Anna Poplavska <semachka100201@gmail.com>
2023-06-29 13:58:05 +0200

    Mistakes fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,98 +1,74 @@
 package core.basesyntax;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_SIZE = 10;
     private T[] array;
     private int size;
-    private int maxSize;
 
     public ArrayList() {
         array = (T[]) new Object[DEFAULT_SIZE];
-        size = 0;
-        maxSize = DEFAULT_SIZE;
     }
 
     @Override
     public void add(T value) {
-        if (size >= maxSize) {
-            array = grow(array);
-        }
+        grow();
         array[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (size >= maxSize) {
-            array = grow(array);
-        }
-        if (checkIndex(index)) {
-            T[] lastPart = (T[]) new Object[size - index];
-            System.arraycopy(array, index, lastPart, 0, lastPart.length);
-            array[index] = value;
-            size++;
-            System.arraycopy(lastPart, 0, array, index + 1, lastPart.length);
-            return;
-        } else if (index == size) {
+        if (index == size) {
             add(value);
             return;
         }
-        throw new ArrayListIndexOutOfBoundsException("Index out of bounds of ArrayList.");
+        grow();
+        checkIndex(index);
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        while (size + list.size() > maxSize) {
-            array = grow(array);
+        while (size + list.size() > array.length) {
+            grow();
         }
-        for (int i = size; i < size + list.size(); i++) {
-            array[i] = list.get(i - size);
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
-        size += list.size();
     }
 
     @Override
     public T get(int index) {
-        if (checkIndex(index)) {
-            return array[index];
-        }
-        throw new ArrayListIndexOutOfBoundsException("Index out of bounds of ArrayList.");
+        checkIndex(index);
+        return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (checkIndex(index)) {
-            array[index] = value;
-            return;
-        }
-        throw new ArrayListIndexOutOfBoundsException("Index out of bounds of ArrayList.");
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (checkIndex(index)) {
-            size--;
-            T removed = array[index];
-            System.arraycopy(array, index + 1, array, index, size - index);
-            array[size] = null;
-            return removed;
-        }
-        throw new ArrayListIndexOutOfBoundsException("Index out of bounds of ArrayList.");
+        checkIndex(index);
+        T removed = array[index];
+        System.arraycopy(array, index + 1, array, index, --size - index);
+        array[size] = null;
+        return removed;
     }
 
     @Override
     public T remove(T element) {
-        int index = -1;
         for (int i = 0; i < size; i++) {
             if (element == array[i] || element != null && element.equals(array[i])) {
-                index = i;
-                break;
+                return remove(i);
             }
-        }
-        if (index > -1) {
-            return remove(index);
         }
         throw new NoSuchElementException("There is no such element in array.");
     }
@@ -107,15 +83,18 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private T[] grow(T[] array) {
-        int oldSize = maxSize;
-        maxSize = maxSize + (maxSize / 2);
-        T[] newArray = (T[]) new Object[maxSize];
-        System.arraycopy(array, 0, newArray, 0, oldSize);
-        return newArray;
+    private void grow() {
+        int oldSize = array.length;
+        int newSize = oldSize + oldSize / 2;
+        if (size >= array.length) {
+            array = Arrays.copyOf(array, newSize);
+        }
     }
 
-    private boolean checkIndex(int index) {
-        return index < size && index >= 0 ? true : false;
+    private void checkIndex(int index) {
+        if (index < size && index >= 0) {
+            return;
+        }
+        throw new ArrayListIndexOutOfBoundsException("Index out of bounds of ArrayList.");
     }
 }
