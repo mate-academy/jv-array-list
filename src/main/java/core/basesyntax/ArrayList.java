03d25c835942959a21c7051ce5d53548b670ad22
Sergei Kudelin <chertterner686@gmail.com>
2023-06-29 20:06:56 +0300

    first commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,117 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Object[] elements;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        int newSize = (int) (elements.length * 1.5);
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size " + index);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size] = value;
+        ++size;
     }
 
     @Override
     public void add(T value, int index) {
-
+        rangeCheckForAdd(index);
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        System.arraycopy(elements, index,
+                elements, index + 1,
+                size - index);
+        elements[index] = value;
+        ++size;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (list.size() > elements.length - size) {
+            ensureCapacity();
+        }
+        for (int index = size, listIndex = 0; index < (size + list.size()); ++index, ++listIndex) {
+            elements[index] = list.get(listIndex);
+        }
+        size += list.size();
     }
 
     @Override
     public T get(int index) {
-        return null;
+        rangeCheck(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        rangeCheck(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        rangeCheck(index);
+        Object item = elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return (T) item;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        Object item = null;
+        int index = 0;
+        for (; index < size; ++index) {
+            if (elements[index] != null && elements[index].equals(element)) {
+                item = remove(index);
+                break;
+            } else if (elements[index] == element) {
+                item = remove(index);
+                break;
+            }
+        }
+        if (index == size) {
+            throw new NoSuchElementException("There is no element like this!!!");
+        }
+        return (T) item;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
