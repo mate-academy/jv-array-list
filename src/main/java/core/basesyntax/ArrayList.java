c831113ba545654bde3ad2a24962d21dfe830fe9
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-07-02 18:58:41 +0300

    Version2-Stas (fixed bugs)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -89,14 +89,15 @@ public class ArrayList<T> implements List<T> {
 
     private int indexOf(T element) {
         for (int i = 0; i < size; i++) {
-            if ((element == null && elementData[i] == null) || (element != null && element.equals(elementData[i]))) {
+            if ((element == null && elementData[i] == null)
+                    || (element != null && element.equals(elementData[i]))) {
                 return i;
             }
         }
         return -1;
     }
 
-    public void checkIndex (int index) {
+    public void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
         }
