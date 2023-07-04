eae8404cc57c05893089b922c20a4d495f497ade
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-07-04 16:45:17 +0300

    first commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,9 +1,31 @@
 package core.basesyntax;
 
+
+
 public class ArrayList<T> implements List<T> {
+    private Object[] array;
+    private int size;
+
+    public ArrayList() {
+        array = new Object[10];
+        size = 0;
+    }
+
     @Override
     public void add(T value) {
+        if (size == array.length) {
+            expandArray();
+        }
+        array[size == 0 ? 0 : size - 1] = value;
+        size++;
+    }
 
+    private void expandArray() {
+        Object[] newArray = new Object[array.length + array.length >> 1];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
     }
 
     @Override
@@ -38,7 +60,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
