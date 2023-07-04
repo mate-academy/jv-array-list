00247db8019d5fa849326e19dbf5190bafa903a0
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-07-04 17:31:29 +0300

    added a part of solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -16,7 +16,7 @@ public class ArrayList<T> implements List<T> {
         if (size == array.length) {
             expandArray();
         }
-        array[size == 0 ? 0 : size - 1] = value;
+        array[size] = value;
         size++;
     }
 
@@ -30,32 +30,65 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-
+        checkIndex(index);
+        if (size == array.length) {
+            expandArray();
+        }
+        for (int i = size; i > index; i--){
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T removedItem = (T) array[index];
+        for (int i = size; i > index; i--) {
+            array[i-1] = array[i];
+        }
+        return removedItem;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("There is no element at index " + index);
+        }
+        if (index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("You can't input a negative index. "
+                    + index + " is below zero!");
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        T removedItem = element;
+        for (int i = 0; i < array.length; i++) {
+            T castedObject = (T) array[i];
+            if (castedObject.equals(element)) {
+                removedItem = remove(i);
+            }
+        }
+        return removedItem;
     }
 
     @Override
@@ -65,6 +98,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
