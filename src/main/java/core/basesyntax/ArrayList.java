164ffc133864c1555ff808bbe3b0d972a5e20a9b
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-10 13:26:48 +0300

    crtMthds ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.Objects;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE_ARRAY = 10;
+    private static final double INCREASE_SIZE_NUM = 1.5;
+    private T[] elementArray = (T[]) new Object[DEFAULT_SIZE_ARRAY];
+    private int size;
+
     @Override
     public void add(T value) {
-
+        checkCapacity();
+        elementArray[size] = value;
+        ++size;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexAdd(index);
+        checkCapacity();
+        System.arraycopy(elementArray, index, elementArray, index + 1, size - index);
+        elementArray[index] = value;
+        ++size;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); ++i) {
+            add(list.get(i));
+        }
+    }
 
+    @Override
+    public void increase() {
+        int newSize = (int) (elementArray.length * INCREASE_SIZE_NUM);
+        T[] increaseArray = (T[]) new Object[newSize];
+        System.arraycopy(elementArray, 0, increaseArray, 0, elementArray.length);
+        elementArray = increaseArray;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return elementArray[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elementArray[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T rmElement = (T) elementArray[index];
+        --size;
+        System.arraycopy(elementArray, index + 1, elementArray, index, size - index);
+        elementArray[size] = null;
+        return rmElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        return remove(findObject(element));
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkCapacity() {
+        if (size == elementArray.length) {
+            increase();
+        }
+    }
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds ");
+        }
+    }
+
+    private void checkIndexAdd(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds ");
+        }
+    }
+
+    private int findObject(T object) {
+        for (int i = 0; i < size; ++i) {
+            if (Objects.equals(elementArray[i], object)) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException("This " + object + "does not exist in array");
     }
 }
--- src/main/java/core/basesyntax/List.java
+++ src/main/java/core/basesyntax/List.java
@@ -18,4 +18,6 @@ public interface List<T> {
     int size();
 
     boolean isEmpty();
+
+    void increase();
 }
