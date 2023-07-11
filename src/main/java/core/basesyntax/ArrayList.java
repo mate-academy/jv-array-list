a626b841170df99e8b1b0362bebbcfbdd0ccc926
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-07-11 20:38:27 +0300

    created custom ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,106 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double INCREASE_COEFFICIENT = 1.5;
+    private static final double EMPTY_SIZE = 0;
+    private T[] objects = (T[]) new Object[DEFAULT_CAPACITY];
+    private int size;
+
     @Override
     public void add(T value) {
-
+        checkIfArrayIsFull();
+        objects[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIfArrayIsFull();
+        checkBoundForAdding(index);
+        for (int i = objects.length - 1; i > index; i--) {
+            objects[i] = objects[i - 1];
+        }
+        objects[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            objects[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkForBounds(index);
+        return objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkForBounds(index);
+        objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkForBounds(index);
+        T temp = objects[index];
+        for (int j = index; j < size - 1; j++) {
+            objects[j] = objects[j + 1];
+        }
+        size--;
+        return temp;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if ((objects[i] != null && objects[i].equals(element)) || (objects[i] == null)) {
+                remove(i);
+                return element;
+            }
+        }
+        throw new NoSuchElementException("There is no " + element + " in list");
+    }
+
+    public void checkForBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+    }
+
+    public void checkBoundForAdding(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is out of bounds");
+        }
+    }
+
+    public void checkIfArrayIsFull() {
+        if (size == objects.length) {
+            grow();
+        }
+    }
+
+    public void grow() {
+        int newSize = (int) (objects.length * INCREASE_COEFFICIENT);
+        T[] oldArray = objects;
+        objects = (T[]) new Object[newSize];
+        System.arraycopy(oldArray, 0, objects, 0, size);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == EMPTY_SIZE;
     }
 }
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("First");
+        arrayList.add("Second");
+        arrayList.add("Third");
+        System.out.println(arrayList.size());
+    }
+}
