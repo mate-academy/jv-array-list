fe6c4ce09bf33efbde1c510711e7223064a460c2
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-11 09:19:28 +0300

    created ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,141 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int NATURAL_NUMBER_BORDERLINE = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double CAPACITY_MULTIPLY_BY = 1.5;
+    private static final String NEGATIVE_INDEX_ERROR = "Index should be natural number";
+    private static final String INDEX_OUT_OF_SIZE_ERROR = "Index out of list length";
+
+    private T[] data;
+    private int size;
+
+    public ArrayList() {
+        data = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public ArrayList(int capacity) {
+        if (capacity > 0) {
+            data = (T[]) new Object[capacity];
+        } else if (capacity == 0) {
+            data = (T[]) new Object[]{};
+        } else {
+            throw new ArrayListIndexOutOfBoundsException(NEGATIVE_INDEX_ERROR);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if (size < data.length) {
+            data[size] = value;
+            size++;
+        } else {
+            this.ensureCapacity();
+            this.add(value);
+        }
     }
 
     @Override
     public void add(T value, int index) {
-
+        isIndexNatural(index);
+        if (index > size) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_SIZE_ERROR);
+        }
+        if (size < data.length) {
+            for (int i = size; i > index; i--) {
+                data[i] = data [i - 1];
+            }
+            data[index] = value;
+            size++;
+        } else {
+            this.ensureCapacity();
+            this.add(value, index);
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (size + list.size() > data.length) {
+            this.ensureCapacity();
+            this.addAll(list);
+        }
+        for (int i = size; i < (size + list.size()); i++) {
+            data[i] = list.get(i - size);
+        }
+        size += list.size();
     }
 
     @Override
     public T get(int index) {
-        return null;
+        isIndexNatural(index);
+        if (index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_SIZE_ERROR);
+        }
+        return data[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        isIndexNatural(index);
+        if (index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_SIZE_ERROR);
+        }
+        data[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        isIndexNatural(index);
+        if (index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_SIZE_ERROR);
+        }
+        final T result = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null;
+        size--;
+        return result;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if ((data[i] != null && data[i].equals(element))
+                    || (data[i] == null && element == null)) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            throw new NoSuchElementException(INDEX_OUT_OF_SIZE_ERROR);
+        }
+        return this.remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size() == 0;
     }
+
+    private void ensureCapacity() {
+        T[] newData = (T[]) new Object[(int)(size * CAPACITY_MULTIPLY_BY)];
+        System.arraycopy(data, 0, newData, 0, size);
+        data = newData;
+    }
+
+    private void isIndexNatural(int index) {
+        if (index < NATURAL_NUMBER_BORDERLINE) {
+            throw new ArrayListIndexOutOfBoundsException(NEGATIVE_INDEX_ERROR);
+        }
+    }
+
 }
