2e464bd53a5b7aa22336f15dcbe7135f9bf16480
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-11 21:45:47 +0300

    changed 0 capacity constructor's length, removed redundant lines, added second error text for isIndexValid() method, changed constant's names and value, created checkIndex() method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,10 +4,10 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private static final double CAPACITY_MULTIPLY_BY = 1.5;
-    private static final String NEGATIVE_INDEX_ERROR = "Index should be natural number";
-    private static final String INDEX_OUT_OF_SIZE_ERROR = "Index out of list length";
-
+    private static final double CAPACITY_MULTIPLIER = 1.5;
+    private static final String NEGATIVE_INDEX_MESSAGE = "Index should be natural number";
+    private static final String INDEX_OUT_OF_SIZE_ERROR = "Index out of bounds";
+    private static final String ELEMENT_DOES_NOT_EXISTS = "Can't find such element";
     private T[] data;
     private int size;
 
@@ -19,15 +19,15 @@ public class ArrayList<T> implements List<T> {
         if (capacity > 0) {
             data = (T[]) new Object[capacity];
         } else if (capacity == 0) {
-            data = (T[]) new Object[]{};
+            data = (T[]) new Object[DEFAULT_CAPACITY];
         } else {
-            throw new IllegalArgumentException(NEGATIVE_INDEX_ERROR);
+            throw new IllegalArgumentException(NEGATIVE_INDEX_MESSAGE);
         }
     }
 
     @Override
     public void add(T value) {
-        if (size >= data.length) {
+        if (checkIndex()) {
             increaseCapacity();
         }
         data[size] = value;
@@ -37,7 +37,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         isIndexValid(index);
-        if (size >= data.length) {
+        if (checkIndex()) {
             increaseCapacity();
         }
         System.arraycopy(data, index, data, index + 1, size - index);
@@ -78,16 +78,17 @@ public class ArrayList<T> implements List<T> {
     public T remove(T element) {
         int index = -1;
         for (int i = 0; i < size; i++) {
-            if ((data[i] != null && data[i].equals(element))
-                    || (data[i] == null && element == null)) {
+            T item = data[i];
+            if ((item != null && item.equals(element))
+                    || (item == null && element == null)) {
                 index = i;
                 break;
             }
         }
         if (index == -1) {
-            throw new NoSuchElementException(INDEX_OUT_OF_SIZE_ERROR);
+            throw new NoSuchElementException(ELEMENT_DOES_NOT_EXISTS);
         }
-        return this.remove(index);
+        return remove(index);
     }
 
     @Override
@@ -101,15 +102,19 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void increaseCapacity() {
-        T[] newData = (T[]) new Object[(int)(size * CAPACITY_MULTIPLY_BY)];
+        T[] newData = (T[]) new Object[(int)(size * CAPACITY_MULTIPLIER)];
         System.arraycopy(data, 0, newData, 0, size);
         data = newData;
     }
 
     private void isIndexValid(int index) {
         if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_SIZE_ERROR);
+            throw new ArrayListIndexOutOfBoundsException(
+                    (index < 0) ? NEGATIVE_INDEX_MESSAGE : INDEX_OUT_OF_SIZE_ERROR);
         }
     }
 
+    private boolean checkIndex() {
+        return size >= data.length;
+    }
 }
