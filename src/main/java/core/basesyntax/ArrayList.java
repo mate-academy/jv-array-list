24381c034940d2199bd0fd06e831183581718985
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-07-12 12:43:01 +0300

    simplified addAll() method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -56,10 +56,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        int newLength = size + list.size();
-        if (newLength >= elementData.length) {
-            elementData = grow();
-        }
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
