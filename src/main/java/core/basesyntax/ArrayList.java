c53c69913b0fadb6be682d70dc2f873bfe997909
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-07-12 12:52:02 +0300

    fixed bad naming of variables and methods, added an implementation of Iterable interface

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,37 +1,38 @@
 package core.basesyntax;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private T[] elements;
-    private int currentCapacity;
-    private int realCapacity;
+    private int capacity;
+    private int currentSize;
 
     public ArrayList() {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
-        realCapacity = DEFAULT_CAPACITY;
+        capacity = DEFAULT_CAPACITY;
     }
 
     @Override
     public void add(T value) {
         growIfArrayFull();
-        elements[currentCapacity] = value;
-        currentCapacity++;
+        elements[currentSize] = value;
+        currentSize++;
     }
 
     @Override
     public void add(T value, int index) {
-        isIndexValidForAdding(index);
-        if (index == currentCapacity) {
+        isIndexValidForAddingValues(index);
+        if (index == currentSize) {
             add(value);
         } else {
-            int partAfterIndexSize = currentCapacity - index;
+            int partAfterIndexSize = currentSize - index;
             T[] partAfterIndex = (T[]) new Object[partAfterIndexSize];
             System.arraycopy(elements, index, partAfterIndex, 0, partAfterIndexSize);
 
             //Increase the size because of adding one element
-            currentCapacity = currentCapacity + 1;
+            currentSize++;
             //Check if the modified capacity is valid
             growIfArrayFull();
             elements[index] = value;
@@ -41,42 +42,40 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
+        for (T element : list) {
+            add(element);
         }
     }
 
     @Override
     public T get(int index) {
-        isIndexValidForUsing(index);
+        isIndexValid(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        isIndexValidForUsing(index);
+        isIndexValid(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        isIndexValidForUsing(index);
+        isIndexValid(index);
         T oldValue = elements[index];
+        if (index == currentSize - 1) {
+            currentSize--;
+            T[] shortedArrayOfElements = (T[]) new Object[currentSize];
 
-        if (index == currentCapacity - 1) {
-            int newCapacity = currentCapacity - 1;
-            T[] shortedArrayOfElements = (T[]) new Object[newCapacity];
-
-            System.arraycopy(elements, 0, shortedArrayOfElements, 0, newCapacity);
+            System.arraycopy(elements, 0, shortedArrayOfElements, 0, currentSize);
             elements = shortedArrayOfElements;
-            currentCapacity--;
         } else {
-            int partAfterIndexSize = currentCapacity - index;
+            int partAfterIndexSize = currentSize - index;
             T[] partAfterIndex = (T[]) new Object[partAfterIndexSize];
 
             System.arraycopy(elements, index + 1, partAfterIndex, 0, partAfterIndexSize);
             System.arraycopy(partAfterIndex, 0, elements, index, partAfterIndexSize);
-            currentCapacity--;
+            currentSize--;
         }
         return oldValue;
     }
@@ -92,42 +91,60 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return currentCapacity;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return currentCapacity == 0;
+        return currentSize == 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int position;
+            @Override
+            public boolean hasNext() {
+                return position < currentSize;
+            }
+
+            @Override
+            public T next() {
+                T element = elements[position];
+                position++;
+                return element;
+            }
+        };
     }
 
     private void growIfArrayFull() {
-        if (realCapacity <= currentCapacity) {
-            int modifiedCapacity = realCapacity + (realCapacity >> 1);
+        if (capacity <= currentSize) {
+            int modifiedCapacity = capacity + (capacity >> 1);
             T[] newElementsStorage = (T[]) new Object[modifiedCapacity];
 
-            System.arraycopy(elements, 0, newElementsStorage, 0, realCapacity);
+            System.arraycopy(elements, 0, newElementsStorage, 0, capacity);
             elements = newElementsStorage;
-            realCapacity = modifiedCapacity;
+            capacity = modifiedCapacity;
             growIfArrayFull();
         }
     }
 
-    private void isIndexValidForUsing(int index) {
-        if (index >= currentCapacity || index < 0) {
+    private void isIndexValid(int index) {
+        if (index >= currentSize || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("The index - " + index
                                                         + " is invalid to use!");
         }
     }
 
-    private void isIndexValidForAdding(int index) {
-        if (index > currentCapacity || index < 0) {
+    private void isIndexValidForAddingValues(int index) {
+        if (index > currentSize || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("The index - " + index
                                                         + " is invalid to add to the arraylist!");
         }
     }
 
     private int getIndexOfValue(T value) {
-        for (int i = 0; i < currentCapacity; i++) {
+        for (int i = 0; i < currentSize; i++) {
             if ((value == elements[i]) || (value != null && value.equals(elements[i]))) {
                 return i;
             }
