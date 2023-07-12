5af5a5b70a1dfd653b117e7a9c7ed96572400868
pafmortim <pafmortim@gmail.com>
2023-07-12 05:29:08 +0300

    added custom ArrayList implementation

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,98 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_LIST_SIZE = 10;
+    private T[] values;
+    private int currentListSize = 0;
+
+    public ArrayList() {
+        values = (T[]) new Object[DEFAULT_LIST_SIZE];
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        values[currentListSize++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkAdditionRange(index);
+        growIfArrayFull();
+        System.arraycopy(values, index, values, index + 1, currentListSize - index);
+        values[index] = value;
+        currentListSize++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexRange(index);
+        return values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexRange(index);
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexRange(index);
+        T removeElement = values[index];
+        currentListSize--;
+        System.arraycopy(values,index + 1,values,index,currentListSize - index);
+        return removeElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < values.length; i++) {
+            if (element != null && element.equals(values[i]) || element == values[i]) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no value you looking for");
     }
 
     @Override
     public int size() {
-        return 0;
+        return currentListSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currentListSize == 0;
+    }
+
+    public void growIfArrayFull() {
+        if (currentListSize == values.length) {
+            int oldCapacity = values.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            values = Arrays.copyOf(values, newCapacity);
+        }
+    }
+
+    private void checkAdditionRange(int index) {
+        if (index < 0 || index > currentListSize) {
+            throw new ArrayListIndexOutOfBoundsException(index
+                    + " is out of bounds for adding to the list");
+        }
+    }
+
+    private void checkIndexRange(int index) {
+        if (index < 0 || index >= currentListSize) {
+            throw new ArrayListIndexOutOfBoundsException(index + " is out of bounds");
+        }
     }
 }
