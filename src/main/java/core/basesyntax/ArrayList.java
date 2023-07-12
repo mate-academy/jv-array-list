5b7b185c3a5fe5436ddfa52dfd872f35d2ab0769
Ihor <kalaurdelta96@gmail.com>
2023-07-12 15:43:20 +0300

    implement interface iterable, changed logic addAll() method, eliminated code duplication, create new method getElementIndex();

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,36 +1,39 @@
 package core.basesyntax;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double GROWTH_FACTOR = 1.5;
-    private transient Object[] elements;
+    private T[] elements;
     private int size;
 
     public ArrayList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        elements = (T[])new Object[DEFAULT_CAPACITY];
     }
 
     public ArrayList(int initCapacity) {
-        if (initCapacity <= 0) {
+        if (initCapacity < 0) {
             throw new IllegalArgumentException(
                     "Can't create array with negative length");
         }
-        elements = new Object[initCapacity];
+        elements = (T[])new Object[initCapacity];
     }
 
     @Override
     public void add(T value) {
-        growIfNeeded();
+        growIfFuel();
         elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        addElementArrayListIndexOutOfBoundException(index);
-        growIfNeeded();
+        if (index != size) {
+            checkIndexOutOfBoundException(index);
+        }
+        growIfFuel();
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
@@ -38,50 +41,36 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        growIfNeeded(list);
-        int listSize = list.size();
-        for (int i = 0; i < listSize; i++) {
-            elements[size + i] = list.get(i);
+        for (T element: list) {
+            add(element);
         }
-        size += listSize;
     }
 
     @Override
     public T get(int index) {
-        arrayListIndexOutOfBoundException(index);
-        return (T)elements[index];
+        checkIndexOutOfBoundException(index);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        arrayListIndexOutOfBoundException(index);
+        checkIndexOutOfBoundException(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        arrayListIndexOutOfBoundException(index);
-        T removedElement = (T)elements[index];
-        removeElement(index);
+        checkIndexOutOfBoundException(index);
+        T removedElement = elements[index];
+        fastRemoveElement(index);
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        int indexRemovedElement = -1;
-        for (int i = 0; i < size; i++) {
-            if (elements[i] != null
-                    && elements[i].equals(element)
-                    || elements[i] == element) {
-                indexRemovedElement = i;
-                break;
-            }
-        }
-        if (indexRemovedElement == -1) {
-            throw new NoSuchElementException("Can't find element: " + element);
-        }
-        T removedElement = (T)elements[indexRemovedElement];
-        removeElement(indexRemovedElement);
+        int indexRemovedElement = getElementIndex(element);
+        T removedElement = elements[getElementIndex(element)];
+        fastRemoveElement(indexRemovedElement);
         return removedElement;
     }
 
@@ -95,42 +84,63 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void growIfNeeded() {
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return (index < elements.length && elements[index] != null);
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return elements[index++];
+            }
+        };
+    }
+
+    private void growIfFuel() {
         if (elements.length == 1 && elements.length == size) {
-            Object[] newArray = new Object[DEFAULT_CAPACITY];
+            T[] newArray = (T[])new Object[DEFAULT_CAPACITY];
             System.arraycopy(elements, 0, newArray, 0, size);
             elements = newArray;
         }
         if (elements.length == size) {
-            Object[] newArray = new Object[(int)((double)elements.length * GROWTH_FACTOR)];
+            int newCapacity = (int)(elements.length * GROWTH_FACTOR);
+            T[] newArray = (T[]) new Object[newCapacity];
             System.arraycopy(elements, 0, newArray, 0, size);
             elements = newArray;
         }
     }
 
-    private void growIfNeeded(List<T> list) {
-        if (list != null && elements.length < size + list.size()) {
-            Object[] newArray = new Object[size + list.size()];
-            System.arraycopy(elements, 0, newArray, 0, size);
-            elements = newArray;
-        }
-    }
-
-    private void addElementArrayListIndexOutOfBoundException(int index) {
-        if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    "Index: " + index + " out of array size: " + size);
-        }
-    }
-
-    private void arrayListIndexOutOfBoundException(int index) {
+    private void checkIndexOutOfBoundException(int index) {
         if (index >= size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException(
                     "Index: " + index + " out of array size: " + size);
         }
     }
 
-    private void removeElement(int index) {
+    private int getElementIndex(T element) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] != null
+                    && elements[i].equals(element)
+                    || elements[i] == element) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            throw new NoSuchElementException("Can't find element: " + element);
+        }
+        return index;
+    }
+
+    private void fastRemoveElement(int index) {
         if (index == elements.length - 1) {
             elements[size - 1] = null;
         } else {
