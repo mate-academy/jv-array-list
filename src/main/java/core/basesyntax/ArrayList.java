9af9ebdbccdae765a503ba391be615c83f99cafe
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-07-13 00:15:31 +0300

    Style fixed, checkCapacity() and isIndexInRange() methods made private, etc.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -9,7 +9,6 @@ public class ArrayList<T> implements List<T> {
     private T[] array;
 
     public ArrayList() {
-        this.currentSize = 0;
         this.array = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
@@ -23,7 +22,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value, int index) {
         checkCapacity();
-        indexInRange(index, currentSize + 1);
+        isIndexInRange(index, currentSize + 1);
         System.arraycopy(array, index, array, index + 1, currentSize - index);
         array[index] = value;
         currentSize++;
@@ -38,20 +37,20 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        indexInRange(index, currentSize);
+        isIndexInRange(index, currentSize);
         return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        indexInRange(index, currentSize);
+        isIndexInRange(index, currentSize);
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
         checkCapacity();
-        indexInRange(index, currentSize);
+        isIndexInRange(index, currentSize);
         final T removeElement = array[index];
         System.arraycopy(array, index + 1, array, index, currentSize - index);
         array[currentSize - 1] = null;
@@ -62,9 +61,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < currentSize; i++) {
-            if (element == array[i] || (element != null && element.equals(array[i]))) {
-                remove(i);
-                return element;
+            if (element == array[i] || element != null && element.equals(array[i])) {
+                return remove(i);
             }
         }
         throw new NoSuchElementException("No such element in array! : " + element);
@@ -80,19 +78,18 @@ public class ArrayList<T> implements List<T> {
         return currentSize == 0;
     }
 
-    public void checkCapacity() {
+    private void checkCapacity() {
         if (array.length == currentSize) {
-            T[] tmpArray = array;
+            T[] newArray = array;
             array = (T[]) new Object[(int) (array.length * INCREASING_COEFFICIENT)];
-            System.arraycopy(tmpArray, 0, array, 0, currentSize);
+            System.arraycopy(newArray, 0, array, 0, currentSize);
         }
     }
 
-    public void indexInRange(int index, int maxBound) {
+    private void isIndexInRange(int index, int maxBound) {
         if (index >= maxBound || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Index ["
                     + index + "] out of array range!");
         }
     }
 }
-
