5bb2306c19c1b103936ceb7386ca726b5d040f70
literandltx <romanskovronn@gmail.com>
2023-07-19 00:50:51 +0300

    added methods: add, add by index, get by index

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,14 +1,45 @@
 package core.basesyntax;
 
-public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+import java.util.Arrays;
 
+public class ArrayList<T> implements List<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
-    public void add(T value, int index) {
+    public void add(T value) {
+        if (size == elementData.length) {
+            elementData = expand(elementData);
+        }
 
+        elementData[size] = value;
+        size++;
+    }
+
+
+    @Override
+    public void add(T value, int index) {
+        if (index > size || index < 0 ) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index");
+        }
+
+        if (size == elementData.length) {
+            elementData = expand(elementData);
+        }
+
+        System.arraycopy(elementData, index,
+                elementData, index + 1,
+                size - index);
+        elementData[index] = value;
+
+        size++;
     }
 
     @Override
@@ -18,7 +49,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0 ) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index");
+        }
+
+        return (T) elementData[index];
     }
 
     @Override
@@ -38,11 +73,20 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
+
+    private Object[] expand(Object[] elementData) {
+        Object[] newStorageKey = new Object[elementData.length + (elementData.length >> 1)];
+
+        System.arraycopy(elementData, 0, newStorageKey, 0, elementData.length);
+
+        return newStorageKey;
+    }
+
 }
