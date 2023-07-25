5cf67802c3890f19f25ca2e603fee18b620d40de
yermilov.m <yermilov.m@hotmail.com>
2023-07-25 20:25:25 +0200

    /Initial commit/

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,48 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+  @Override
+  public void add(T value) {
 
-    }
+  }
 
-    @Override
-    public void add(T value, int index) {
+  @Override
+  public void add(T value, int index) {
 
-    }
+  }
 
-    @Override
-    public void addAll(List<T> list) {
+  @Override
+  public void addAll(List<T> list) {
 
-    }
+  }
 
-    @Override
-    public T get(int index) {
-        return null;
-    }
+  @Override
+  public T get(int index) {
+    return null;
+  }
 
-    @Override
-    public void set(T value, int index) {
+  @Override
+  public void set(T value, int index) {
 
-    }
+  }
 
-    @Override
-    public T remove(int index) {
-        return null;
-    }
+  @Override
+  public T remove(int index) {
+    return null;
+  }
 
-    @Override
-    public T remove(T element) {
-        return null;
-    }
+  @Override
+  public T remove(T element) {
+    return null;
+  }
 
-    @Override
-    public int size() {
-        return 0;
-    }
+  @Override
+  public int size() {
+    return 0;
+  }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
+  @Override
+  public boolean isEmpty() {
+    return false;
+  }
 }
