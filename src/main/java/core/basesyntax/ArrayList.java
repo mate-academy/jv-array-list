bfbea7b47617c60bd38fd823b4f80e6da12c1f46
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-08-13 17:54:50 +0300

    ArrayList created,overriden required methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,100 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elements;
+    private int size = 0;
+
+    public ArrayList() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public ArrayList(int userDefinedCapacity) {
+        elements = (T[]) new Object[userDefinedCapacity];
+    }
+
     @Override
     public void add(T value) {
-
+        growArrayIfFull();
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        ifIndexOutOfBounds(index, size);
+        growArrayIfFull();
+        if (index < size && size < elements.length) {
+            System.arraycopy(elements, index, elements, index + 1, size);
+        }
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        ifIndexOutOfBounds(index, size - 1);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        ifIndexOutOfBounds(index, size - 1);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        ifIndexOutOfBounds(index, size);
+        T elementRemoved = get(index);
+        if (index < size - 1) {
+            System.arraycopy(elements, index + 1, elements, index, size - index);
+        }
+        size--;
+        return elementRemoved;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element || (elements[i] != null && elements[i].equals(element))) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public void ifIndexOutOfBounds(int index, int upperBound) {
+        if (index > upperBound || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    new IndexOutOfBoundsException().getMessage());
+        }
+    }
+
+    private void growArrayIfFull() {
+        if (elements.length == size) {
+            T[] increasedElements = (T[]) new Object[elements.length + elements.length / 2];
+            System.arraycopy(elements, 0, increasedElements, 0, size);
+            elements = increasedElements;
+        }
     }
 }
