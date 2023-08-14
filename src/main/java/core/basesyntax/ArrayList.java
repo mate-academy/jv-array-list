a051a12ea6c3e2e93b9b4aaa400d16eb30cdd197
Mykola Horianin <anyavic2379@gmail.com>
2023-08-14 19:06:50 +0300

    Created the ArrayList class, implemented the List interface. Redefined methods.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] values;
+    private int size;
+
+    public ArrayList() {
+        values = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == values.length) {
+            arrayGrowth(size);
+        }
+        values[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkRangeForIndexForAdd(index);
+        if (size == values.length) {
+            arrayGrowth(size);
+        }
+        System.arraycopy(values, index, values, index + 1, size - index);
+        values[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkRangeForIndex(index);
+        return values[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkRangeForIndex(index);
+        values[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkRangeForIndex(index);
+        final T elementRemove = values[index];
+        int newSize;
+        newSize = size - 1;
+        if (newSize > index) {
+            System.arraycopy(values, index + 1, values, index, newSize - index);
+        }
+        size--;
+        return elementRemove;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (values[i] == element || (values[i] != null && values[i].equals(element))) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Element does not exist");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkRangeForIndexForAdd(int index) {
+        if (index != 0 && (index < 0 || index > size)) {
+            throw new ArrayListIndexOutOfBoundsException("The index does not exist");
+        }
+    }
+
+    private void checkRangeForIndex(int index) {
+        if (index != 0 && (index < 0 || index >= size)) {
+            throw new ArrayListIndexOutOfBoundsException("The index does not exist");
+        }
+    }
+
+    private void arrayGrowth(int index) {
+        if (index >= values.length) {
+            int newLength = values.length + values.length / 2;
+            values = Arrays.copyOf(values, newLength);
+        }
     }
 }
