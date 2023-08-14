1e14cc79b3479e93f31d927b16c6aa9c24a90224
Olena Stankevych <yelenastankevych@gmail.com>
2023-08-14 18:55:52 +0300

    implemented ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,114 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int index;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+        index = -1;
+    }
+
     @Override
     public void add(T value) {
-
+        if (isNeedToResize()) {
+            grow();
+        }
+        elements[++index] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexToAdd(index);
+        if (isNeedToResize()) {
+            grow();
+        }
+        System.arraycopy(elements, index, elements, index + 1, size() - index);
+        elements[index] = value;
+        this.index++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexToGet(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndexToGet(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexToGet(index);
+        T toRemove = (T) elements[index];
+        if (!isLastIndex(index)) {
+            System.arraycopy(elements, index + 1, elements, index, this.index - index);
+        }
+        elements[this.index--] = null;
+        return toRemove;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        return remove(getIndexByValue(element));
     }
 
     @Override
     public int size() {
-        return 0;
+        return index + 1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() <= 0;
+    }
+
+    private boolean isNeedToResize() {
+        return size() >= elements.length;
+    }
+
+    private void grow() {
+        int newLength = size() + (size() >> 1);
+        elements = Arrays.copyOf(elements, newLength);
+    }
+
+    private void checkIndexToAdd(int index) {
+        if (index > size() || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index");
+        }
+    }
+
+    private void checkIndexToGet(int index) {
+        if (index >= size() || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index");
+        }
+    }
+
+    private boolean isLastIndex(int index) {
+        return index == this.index;
+    }
+
+    private int getIndexByValue(T value) {
+        for (int i = 0; i < size(); i++) {
+            T current = (T) elements[i];
+            if ((current != null && current.equals(value)) || current == value) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException("No such value in list");
     }
 }
