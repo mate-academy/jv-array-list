15d81fc1612a48ce3d8f718db9fe70cd99666aac
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-08-22 12:22:21 +0300

    Redundant methods are deleted

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private int size;
-
     private Object[] elements;
 
     public ArrayList() {
@@ -20,17 +19,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (checkAddIndex(index)) {
-            growIfFull();
-            elements = addResize(index);
-            elements[index] = value;
-            size++;
-        }
-    }
-
-    private Object[] addResize(int index) {
+        checkAddIndex(index);
+        growIfFull();
         System.arraycopy(elements, index, elements, index + 1, size - index);
-        return elements;
+        elements[index] = value;
+        size++;
     }
 
     @Override
@@ -50,7 +43,6 @@ public class ArrayList<T> implements List<T> {
     public void set(T value, int index) {
         checkIndex(index);
         elements[index] = value;
-
     }
 
     @Override
@@ -67,14 +59,9 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkIndex(index);
         T removeValue = (T) elements[index];
-        elements = removeResize(index);
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         elements[--size] = null;
         return removeValue;
-    }
-
-    private Object[] removeResize(int index) {
-        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
-        return elements;
     }
 
     @Override
@@ -88,7 +75,7 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void growIfFull() {
-        if (isFull()) {
+        if (size == elements.length) {
             int newCapacity = size + (size >> 1);
             Object[] newElements = new Object[newCapacity];
             System.arraycopy(elements, 0, newElements, 0, size);
@@ -96,23 +83,17 @@ public class ArrayList<T> implements List<T> {
         }
     }
 
-    private boolean checkIndex(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index > size - 1) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
                     + " out of bounds for length " + size);
         }
-        return true;
     }
 
-    private boolean checkAddIndex(int index) {
+    private void checkAddIndex(int index) {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
                     + " out of bounds for length " + size);
         }
-        return true;
-    }
-
-    private boolean isFull() {
-        return size >= elements.length;
     }
 }
