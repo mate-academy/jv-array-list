c0e9ab644f8a2befb636eeef14e61a9413575528
Petro Smoliar <smp22pl@gmail.com>
2023-09-04 16:05:10 +0200

    Implemented a custom ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,63 +6,52 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int INITIAL_CAPACITY = 10;
     private static final double GROWTH_FACTOR = 1.5;
-    private Object[] array = new Object[INITIAL_CAPACITY];
-    private int size = 0;
+    private T[] array;
+    private int size;
+
+    public ArrayList() {
+        this.array = (T[]) new Object[INITIAL_CAPACITY];
+    }
 
     @Override
     public void add(T value) {
-        if (size == array.length) {
-            array = Arrays.copyOf(array, (int) (array.length * GROWTH_FACTOR));
-        }
+        resizeArrayIfNeeded();
         array[size] = value;
         size++;
-
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
+        if (isInvalidIndexAdd(index)) {
             throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
+        } else {
+            Object[] arrayPart2 = Arrays.copyOfRange(array, index, size);
+            resizeArrayIfNeeded();
+            System.arraycopy(arrayPart2, 0, array, index + 1, size - index);
+            array[index] = value;
+            size++;
         }
-        if (size == array.length) {
-            array = Arrays.copyOf(array, (int) (array.length * GROWTH_FACTOR));
-        }
-        for (int i = size; i > index; i--) {
-            array[i] = array[i - 1];
-        }
-        array[index] = value;
-        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        int quantity = size + list.size();
-        int arrayLength = array.length;
-        if (quantity > arrayLength) {
-            while (quantity >= arrayLength) {
-                arrayLength = (int) (arrayLength * GROWTH_FACTOR);
-            }
-            array = Arrays.copyOf(array, arrayLength);
-        }
-        int index = size;
         for (int i = 0; i < list.size(); i++) {
-            array[index] = list.get(i);
-            index++;
+            add(list.get(i));
         }
-        size += list.size();
     }
 
     @Override
     public T get(int index) {
-        if (index >= size || index < 0) {
+        if (isInvalidIndex(index)) {
             throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
+        } else {
+            return (T) array[index];
         }
-        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size || index < 0) {
+        if (isInvalidIndex(index)) {
             throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
         } else {
             array[index] = value;
@@ -71,7 +60,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        if (index >= size || index < 0) {
+        if (isInvalidIndex(index)) {
             throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
         } else {
             T removedElement = (T) array[index];
@@ -108,5 +97,19 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void resizeArrayIfNeeded() {
+        if (size >= array.length) {
+            array = Arrays.copyOf(array, (int) (array.length * GROWTH_FACTOR));
+        }
+    }
+
+    private boolean isInvalidIndex(int index) {
+        return index < 0 || index >= size;
+    }
+
+    private boolean isInvalidIndexAdd(int index) {
+        return index < 0 || index > size;
     }
 }
