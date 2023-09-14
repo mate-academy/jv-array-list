18c4c2dfa8719fced679ca7108a66d8652c29b85
VladSchweps <vladschweps1@gmail.com>
2023-09-14 18:01:08 +0300

    Added solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,110 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROWTH_SIZE = 1.5;
+    private static final int FIRST_POSITION_ARRAY = 0;
+    private T[] elements;
+    private int size;
+
+    public ArrayList() {
+        this.elements = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
+        resizedIfNeeded();
+        elements[size] = value;
+        size++;
 
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index to add: " + index);
+        }
+        resizedIfNeeded();
+        System.arraycopy(
+                elements, index,
+                elements, index + 1,
+                size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index to get: " + index);
+        }
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index to set: " + index);
+        }
+        elements[index] = value;
 
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index for remove: " + index);
+        }
+        T removedElement = get(index);
+        System.arraycopy(
+                elements, index + 1,
+                elements, index,
+                size - index - 1);
+        elements[--size] = null;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == null) {
+                if (element == null) {
+                    return remove(i);
+                }
+            } else if (elements[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void resizedIfNeeded() {
+        if (elements.length == size) {
+            Object[] newArray = new Object[(int) (elements.length * GROWTH_SIZE)];
+            System.arraycopy(
+                    elements, FIRST_POSITION_ARRAY,
+                    newArray, FIRST_POSITION_ARRAY,
+                    size);
+            elements = (T[]) newArray;
+        }
     }
 }
