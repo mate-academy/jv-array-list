229632f51e613504e639e6e3f1ea74bc506d5642
Mykhailo Vorobets <misha.shark6969@gmail.com>
2023-09-20 22:51:01 +0300

    implemented interface List in class ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROW_COEFFICIENT = 1.5;
     private Object[] elements;
     private int size;
 
@@ -13,15 +14,15 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        resizeIdNeeded();
+        resizeIfNeeded();
         elements[size] = value;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index,size + 1);
-        resizeIdNeeded();
+        checkAddIndex(index);
+        resizeIfNeeded();
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
@@ -30,26 +31,26 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void addAll(List<T> list) {
         Object[] objects = convertListInArray(list);
-        resizeIdNeeded(size + objects.length);
+        resizeIfNeeded(size + objects.length);
         System.arraycopy(objects, 0, elements, size, objects.length);
         size = size + objects.length;
     }
 
     @Override
     public T get(int index) {
-        checkIndex(index, size);
+        checkGetSetRemoveIndex(index);
         return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, size);
+        checkGetSetRemoveIndex(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index, size);
+        checkGetSetRemoveIndex(index);
         T removeElement = (T) elements[index];
         System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         size--;
@@ -71,25 +72,27 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void resizeIdNeeded() {
-        if (elements.length == size) {
-            resizeIdNeeded(size << 1);
-        } else {
-            resizeIdNeeded(size);
-        }
+    private void resizeIfNeeded() {
+        resizeIfNeeded((int) (size * GROW_COEFFICIENT));
     }
 
-    private void resizeIdNeeded(int arrayLength) {
+    private void resizeIfNeeded(int arrayLength) {
         if (elements.length < arrayLength) {
             Object[] newArray = new Object[arrayLength];
-            System.arraycopy(elements, 0,newArray,0, size);
+            System.arraycopy(elements, 0, newArray, 0, size);
             elements = newArray;
         }
     }
 
-    private void checkIndex(int index, int length) {
-        if (index < 0 || index >= length) {
-            throw new ArrayListIndexOutOfBoundsException("index is not valid");
+    private void checkAddIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("index is not valid " + index);
+        }
+    }
+
+    public void checkGetSetRemoveIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new ArrayListIndexOutOfBoundsException("index is not valid  " + index);
         }
     }
 
@@ -103,7 +106,7 @@ public class ArrayList<T> implements List<T> {
     }
 
     private boolean objectEquals(T value1, T value2) {
-        if (value1 == null && value2 == null) {
+        if (value1 == value2) {
             return true;
         }
         return value1 != null && value1.equals(value2);
