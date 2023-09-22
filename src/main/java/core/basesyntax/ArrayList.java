a4d6b4b608f31548da63ba5b864782ec155adcfa
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-09-22 17:00:00 +0200

    Made code more clean and readable

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,114 +4,104 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     public static final double GROW_VALUE = 1.5;
-    private int maxSize = 10;
-    private int actualSize = 0;
-    private Object[] array = new Object[maxSize];
-    private Object[] tempArray;
+    private static final int DEFAULT_SIZE = 10;
+    private int size = 0;
+    private T[] array;
+    private T[] tempArray;
+
+    public ArrayList() {
+        array = (T[]) new Object[DEFAULT_SIZE];
+        tempArray = (T[]) new Object[DEFAULT_SIZE];
+    }
 
     @Override
     public void add(T value) {
-        if (actualSize == maxSize) {
+        if (size == array.length) {
             grow();
         }
-        array[actualSize] = value;
-        actualSize++;
+        array[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index > actualSize || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index doesn't exist");
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index
+                    + " doesn't exist, array size is " + array.length);
         }
-        if (actualSize == maxSize) {
+        if (size == array.length) {
             grow();
         }
-        tempArray = new Object[maxSize];
-        System.arraycopy(array, 0, tempArray, 0, index);
+        tempArray = array.clone();
         tempArray[index] = value;
-        System.arraycopy(array, index, tempArray, index + 1, actualSize - index);
-        actualSize++;
-        array = tempArray;
+        System.arraycopy(array, index, tempArray, index + 1, size - index);
+        size++;
+        array = tempArray.clone();
     }
 
     @Override
     public void addAll(List<T> list) {
-        int requiredSize = maxSize - actualSize + list.size();
-        while (requiredSize > maxSize) {
-            grow();
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
-        tempArray = new Object[maxSize];
-        System.arraycopy(array, 0, tempArray, 0, actualSize);
-        System.arraycopy(toArray(list), 0, tempArray, actualSize, list.size());
-        actualSize = actualSize + list.size();
-        array = tempArray;
     }
 
     @Override
     public T get(int index) {
-        if (index >= actualSize || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index doesn't exist");
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index
+                    + " doesn't exist, array size is " + array.length);
         }
         return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= actualSize || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index doesn't exist");
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index
+                    + " doesn't exist, array size is " + array.length);
         }
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= actualSize || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index doesn't exist");
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index
+                    + " doesn't exist, array size is " + array.length);
         }
         final T removedItem = (T) array[index];
-        tempArray = new Object[maxSize];
-        actualSize--;
-        System.arraycopy(array, 0, tempArray, 0, index);
-        System.arraycopy(array, index + 1, tempArray, index, actualSize - index);
-        array = tempArray;
+        size--;
+        tempArray = array.clone();
+        System.arraycopy(array, index + 1, tempArray, index, size - index);
+        array = tempArray.clone();
         return removedItem;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < array.length; i++) {
-            if ((array[i] != null && array[i].equals(element))
-                    || (array[i] == null && element == null)) {
+            if ((array[i] != null && array[i].equals(element)) || array[i] == element) {
                 remove(i);
                 return element;
             }
         }
-        throw new NoSuchElementException("There is no such element in array");
+        throw new NoSuchElementException("There is no element " + element + " in array");
     }
 
     @Override
     public int size() {
-        return actualSize;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return actualSize == 0;
+        return size == 0;
     }
 
-    public void grow() {
-        maxSize = (int) (maxSize * GROW_VALUE);
-        tempArray = new Object[maxSize];
-        System.arraycopy(array, 0, tempArray, 0, actualSize);
-        array = new Object[maxSize];
-        System.arraycopy(tempArray, 0, array, 0, actualSize);
-    }
-
-    public T[] toArray(List<T> list) {
-        Object[] array = new Object[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i);
-        }
-        return (T[]) array;
+    private void grow() {
+        tempArray = (T[]) new Object[(int) (array.length * GROW_VALUE)];
+        System.arraycopy(array, 0, tempArray, 0, size);
+        array = (T[]) new Object[(int) (array.length * GROW_VALUE)];
+        System.arraycopy(tempArray, 0, array, 0, size);
     }
 }
