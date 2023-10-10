a74b200ee66697657e8b32ea6cdfa2c95f936035
Hlib Kulikov <forzen123212@proton.me>
2023-10-10 18:11:07 +0300

    Realization of ArrayList.java Class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,109 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int currentSize;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        elementData[currentSize++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndex(index, currentSize);
+        growIfArrayFull();
+        if (index == currentSize) {
+            elementData[currentSize++] = value;
+            return;
+        }
+        int tailLength = currentSize - index;
+        System.arraycopy(elementData, index, elementData, index + 1, tailLength);
+        elementData[index] = value;
+        currentSize++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        int minLength = Math.max(list.size() + currentSize, DEFAULT_CAPACITY);
+        grow(minLength);
+        for (int i = 0; i < list.size(); i++) {
+            elementData[currentSize++] = list.get(i);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index, currentSize - 1);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index, currentSize - 1);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index, currentSize - 1);
+        T elemToRemove = (T) elementData[index];
+        if (index == currentSize) {
+            elementData[index] = null;
+            return elemToRemove;
+        }
+        int tailLength = currentSize - index - 1;
+        System.arraycopy(elementData, index + 1, elementData, index, tailLength);
+        currentSize--;
+        return elemToRemove;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < currentSize; i++) {
+            if (Objects.equals(element, elementData[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Cant find the  " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currentSize == 0;
+    }
+
+    private void growIfArrayFull() {
+        if (currentSize == elementData.length) {
+            grow(elementData.length);
+        }
+    }
+
+    private void grow(int minLength) {
+        int newCapacity = (int) (minLength + (minLength * 1.5));
+        Object[] tmp = new Object[newCapacity];
+        System.arraycopy(elementData, 0, tmp, 0, currentSize);
+        elementData = tmp;
+    }
+
+    private void checkIndex(int index, int upperBound) {
+        if (index < 0 || index > upperBound) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    String.format("Index %d out of bounds for size %d", index, currentSize));
+        }
     }
 }
