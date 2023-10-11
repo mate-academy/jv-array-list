5d3c309536900ba10c2e6340251b0bde945b69fa
Anton Dudar <dudar.anton4uk@gmail.com>
2023-10-11 17:55:17 +0300

    added constant for invalid index message v1

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -8,7 +8,8 @@ public class ArrayList<T> implements List<T> {
     private static final int START_SIZE = 0;
     private static final int INDEX_LOWER_BOUND = 0;
     private static final double CAPACITY_MULTIPLIER = 1.5;
-    private static final String INVALID_INDEX_MESSAGE = "The provided index is invalid. Please double-check it. Index: ";
+    private static final String INVALID_INDEX_MESSAGE = "The provided index is invalid. "
+            + "Please double-check it. Index: ";
     private Object[] elementData;
     private int size;
 
