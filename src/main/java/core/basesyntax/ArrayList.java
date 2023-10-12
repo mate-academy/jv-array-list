374a43017d20f115e85798589ce7d442844a2b70
Igor Korolevich <igorkorolj@gmail.com>
2023-10-12 18:13:11 +0300

    fixed code

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -18,14 +18,14 @@ public class ArrayList<T> implements List<T> {
     private static final String MESSAGE_AT_REMOVE_NO_SUCH_ELEMENT_EXCEPTION = "Cant remove element "
             + "because there is no such element in the list :";
     private T[] values;
-    private int usedSpace;
+    private int size;
 
     public ArrayList() {
-        values = (T[])new Object[MAX_ITEMS_NUMBER];
+        values = (T[]) new Object[MAX_ITEMS_NUMBER];
     }
 
     public void add(T value) {
-        values[usedSpace++] = value;
+        values[size++] = value;
         growIfArrayFull();
     }
 
@@ -33,63 +33,52 @@ public class ArrayList<T> implements List<T> {
         checkIfIndexOutOfBoundsException(MESSAGE_AT_ADD_EXCEPTION,
                 index);
         growIfArrayFull();
-        addValueToIndex(index, value);
+        System.arraycopy(values, index, values, index + ACCURACY_OF_ONE, size - index);
+        values[index] = value;
+        size++;
     }
 
     private void growIfArrayFull() {
-        if (usedSpace == values.length - ACCURACY_OF_ONE) {
-            int oldCapacity = values.length;
-            int newCapacity = oldCapacity + (oldCapacity >> GROWTH_FACTOR);
+        if (size == values.length) {
+            int newCapacity = size + (size >> GROWTH_FACTOR);
             Object[] tempValues = new Object[newCapacity];
-            System.arraycopy(values, INITIAL_POSITION, tempValues, INITIAL_POSITION, usedSpace);
-            values = (T[])new Object[newCapacity];
-            values = (T[]) tempValues;
+            System.arraycopy(values, INITIAL_POSITION, tempValues, INITIAL_POSITION, size);
+            values = (T[])tempValues;
         }
-    }
-
-    private void grow() {
-        int oldCapacity = values.length;
-        int newCapacity = oldCapacity + (oldCapacity >> GROWTH_FACTOR);
-        Object[] tempValues = new Object[newCapacity];
-        System.arraycopy(values, INITIAL_POSITION, tempValues, INITIAL_POSITION, usedSpace);
-        values = (T[])new Object[newCapacity];
-        values = (T[])tempValues;
     }
 
     public void addAll(List<T> list) {
-        while ((values.length - usedSpace) < list.size()) {
-            grow();
-        }
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
 
     public T get(int index) {
-        checkIfIndexOutOfBoundsException(MESSAGE_AT_GET_EXCEPTION ,
+        checkIfIndexOutOfBoundsException(MESSAGE_AT_GET_EXCEPTION,
                 index + ACCURACY_OF_ONE);
         return values[index];
     }
 
     public void set(T value, int index) {
-        checkIfIndexOutOfBoundsException(MESSAGE_AT_SET_EXCEPTION ,
+        checkIfIndexOutOfBoundsException(MESSAGE_AT_SET_EXCEPTION,
                 index + ACCURACY_OF_ONE);
         values[index] = value;
     }
 
     public T remove(int index) {
-        checkIfIndexOutOfBoundsException(MESSAGE_AT_REMOVE_EXCEPTION,
-                index + ACCURACY_OF_ONE);
-        T value = values[index];
-        removeValueFromIndex(index);
-        return value;
+        checkIfIndexOutOfBoundsException(MESSAGE_AT_REMOVE_EXCEPTION, index + ACCURACY_OF_ONE);
+        T removedValue = values[index];
+        System.arraycopy(values, index + ACCURACY_OF_ONE,
+                values, index, size - index - ACCURACY_OF_ONE);
+        size--;
+        return removedValue;
     }
 
     public T remove(T element) {
         for (int i = 0; i < values.length; i++) {
             if (values[i] == element || values[i] != null && values[i].equals(element)) {
                 T value = values[i];
-                removeValueFromIndex(i);
+                remove(i);
                 return value;
             }
         }
@@ -97,28 +86,16 @@ public class ArrayList<T> implements List<T> {
     }
 
     public int size() {
-        return usedSpace;
+        return size;
     }
 
     public boolean isEmpty() {
-        return usedSpace == 0;
-    }
-
-    private void addValueToIndex(int index, T value) {
-        System.arraycopy(values, index, values, index + ACCURACY_OF_ONE, usedSpace - index);
-        values[index] = value;
-        usedSpace++;
-    }
-
-    private void removeValueFromIndex(int index) {
-        System.arraycopy(values, INITIAL_POSITION, values, INITIAL_POSITION, index);
-        System.arraycopy(values, index + ACCURACY_OF_ONE, values, index, usedSpace - index);
-        usedSpace--;
+        return size == 0;
     }
 
     private void checkIfIndexOutOfBoundsException(String message, int index) {
-        if (index > usedSpace || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(message + usedSpace);
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(message + size);
         }
     }
 }
