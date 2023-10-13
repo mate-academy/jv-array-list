f3bba57c44b3d93c61becd05440373aa0e0f838f
Myself <wetball74@gmail.com>
2023-10-13 17:26:45 +0300

    jv arraylist first attempt

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
+    private Object[] objects;
+    private int size;
+
+    public ArrayList() {
+        objects = new Object[DEFAULT_SIZE];
+    }
+
+    public void boostIfSizeBigger(int minSize) {
+        if (minSize > objects.length) {
+            int newCapacity = objects.length + (objects.length >> 1);
+            if (newCapacity < minSize) {
+                newCapacity = minSize;
+            }
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(objects, 0, newArray, 0, objects.length);
+            objects = newArray;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (size <= index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index " + index);
+        }
+    }
+
     @Override
     public void add(T value) {
+        boostIfSizeBigger(size + 1);
+        objects[size++] = value;
 
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size < index || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index " + index);
+        }
+        boostIfSizeBigger(size + 1);
+        System.arraycopy(objects, index, objects, index + 1, size - index);
+        objects[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        boostIfSizeBigger(size + list.size());
+        for (int i = 0; i < list.size(); i++) {
+            objects[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        boostIfSizeBigger(index);
+        T returnValue = (T) objects[index];
+        if (index == size - 1) {
+            objects[--size] = null;
+            return returnValue;
+        }
+        System.arraycopy(objects, index + 1, objects, index, size - index - 1);
+        objects[--size] = null;
+        return returnValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            T currentValue = get(i);
+            if (currentValue == element || currentValue != null && currentValue.equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("e");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
