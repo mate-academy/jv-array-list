d4dd435401111dfd2c95a65c321c8c4439e7fba7
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-10-13 20:44:41 +0300

    Fixes with mentor's comments

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,14 +4,14 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final String NOT_FOUND_INDEX_MESSAGE = "Not found index: ";
+    private static final String NOT_FOUND_VALUE_MESSAGE = "Not found element with value: ";
     private Object[] storage = new Object[DEFAULT_CAPACITY];
     private int size = 0;
 
     @Override
     public void add(T value) {
-        if (size == storage.length) {
-            increaseStorage();
-        }
+        increaseStorage();
         storage[size] = value;
         size++;
     }
@@ -20,20 +20,14 @@ public class ArrayList<T> implements List<T> {
     public void add(T value, int index) {
         size++;
         validateIndex(index);
-        if (size + 1 == storage.length) {
-            increaseStorage();
-        }
+        increaseStorage();
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = value;
     }
 
     @Override
     public void addAll(List<T> list) {
-        int listSize = list.size();
-        if ((listSize + size) > storage.length) {
-            increaseStorage();
-        }
-        for (int i = 0; i < listSize; i++) {
+        for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
@@ -53,12 +47,9 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         validateIndex(index);
-        int tailLength = size - index;
-        T value = get(index);
-        if (tailLength > 0) {
-            System.arraycopy(storage, index + 1, storage, index, tailLength - 1);
-            storage[size - 1] = null;
-        }
+        final T value = get(index);
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+        storage[size - 1] = null;
         size--;
         return value;
     }
@@ -82,16 +73,18 @@ public class ArrayList<T> implements List<T> {
 
     private void validateIndex(int index) {
         if ((index < 0) || (index > size - 1)) {
-            throw new ArrayListIndexOutOfBoundsException("Not found index: " + index);
+            throw new ArrayListIndexOutOfBoundsException(NOT_FOUND_INDEX_MESSAGE + index);
         }
     }
 
     private void increaseStorage() {
-        int oldCapacity = storage.length;
-        int newCapacity = oldCapacity + (oldCapacity >> 1);
-        Object [] increasedStorage = new Object[newCapacity];
-        System.arraycopy(storage, 0, increasedStorage, 0, size);
-        storage = increasedStorage;
+        if (size == storage.length) {
+            int oldCapacity = storage.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            Object [] increasedStorage = new Object[newCapacity];
+            System.arraycopy(storage, 0, increasedStorage, 0, size);
+            storage = increasedStorage;
+        }
     }
 
     private int indexOf(T value) {
@@ -101,7 +94,6 @@ public class ArrayList<T> implements List<T> {
                 return i;
             }
         }
-        throw new NoSuchElementException("Not found element with value: "
-                + value);
+        throw new NoSuchElementException(NOT_FOUND_VALUE_MESSAGE + value);
     }
 }
