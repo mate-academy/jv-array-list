177b01059b155523c872ea4525833e0c9651831d
Vadym Hurnik <vad.com.z@gmail.com>
2023-10-13 20:05:10 +0300

    added constants, removed method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,50 +4,18 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
+    private static final int SIZE_INCREASE = 1;
+    private static final int RIGHT_SHIFT = 1;
     private static final int DEFAULT_CAPACITY = 10;
     private static final int INDEX_IS_NOT_PRESENT = -1;
+    private static final String NO_SUCH_ELEMENT_MESSAGE =
+            "There is no such element [%h] in the list";
+    private static final String OUT_OF_BOUNDS_MESSAGE = "Array size is %d. Index %d out of bounds.";
     private Object[] arrayList;
     private int currentSize;
 
     public ArrayList() {
         arrayList = new Object[DEFAULT_CAPACITY];
-    }
-
-    private Object[] grow() {
-        return Arrays.copyOf(arrayList, currentSize + (currentSize >> 1));
-    }
-
-    private void checkSize() {
-        if (currentSize + 1 > arrayList.length) {
-            arrayList = grow();
-        }
-    }
-
-    private void checkSize(List<T> list) {
-        int expectedSize = list.size() + currentSize;
-        if (expectedSize > arrayList.length) {
-            do {
-                arrayList = grow();
-            } while (expectedSize < arrayList.length);
-        }
-    }
-
-    private void checkIndex(int index) {
-        if (index > currentSize || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(
-                    String.format("Array size is %d. Index %d out of bounds.",
-                            currentSize, index));
-        }
-    }
-
-    private int indexOfElement(T element) {
-        for (int i = 0; i < currentSize; i++) {
-            if (arrayList[i] == element
-                    || arrayList[i] != null && arrayList[i].equals(element)) {
-                return i;
-            }
-        }
-        return INDEX_IS_NOT_PRESENT;
     }
 
     private T removeElementByIndex(int index) {
@@ -75,7 +43,6 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        checkSize(list);
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
@@ -103,8 +70,7 @@ public class ArrayList<T> implements List<T> {
     public T remove(T element) {
         int index = indexOfElement(element);
         if (index == INDEX_IS_NOT_PRESENT) {
-            throw new NoSuchElementException(
-                    "There is no such element [" + element + "] in the list");
+            throw new NoSuchElementException(String.format(NO_SUCH_ELEMENT_MESSAGE, element));
         }
         return removeElementByIndex(index);
     }
@@ -117,5 +83,32 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return currentSize == 0;
+    }
+
+    private Object[] grow() {
+        return Arrays.copyOf(arrayList, currentSize + (currentSize >> RIGHT_SHIFT));
+    }
+
+    private void checkSize() {
+        if (currentSize + SIZE_INCREASE > arrayList.length) {
+            arrayList = grow();
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index > currentSize || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    String.format(OUT_OF_BOUNDS_MESSAGE, currentSize, index));
+        }
+    }
+
+    private int indexOfElement(T element) {
+        for (int i = 0; i < currentSize; i++) {
+            if (arrayList[i] == element
+                    || arrayList[i] != null && arrayList[i].equals(element)) {
+                return i;
+            }
+        }
+        return INDEX_IS_NOT_PRESENT;
     }
 }
