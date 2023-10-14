11a84ca6e4500a1a476013f3a7281d797cbc0dc5
Mateusz Rostek <17rostek17@gmail.com>
2023-10-14 14:48:01 +0200

    Added constants/field, constructors, and following methods: add, remove, size, isEmpty, growIfArrayFull

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,9 +1,29 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
+    private static final double GROWTH_MULTIPLIER = 1.5;
+    private T[] arrayUnderneath;
+    private int size;
+
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Capacity can't a minus number!");
+        }
+        arrayUnderneath = (T[]) new Object[initialCapacity];
+    }
+
+    public ArrayList() {
+        this(DEFAULT_SIZE);
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        arrayUnderneath[size] = value;
+        size++;
     }
 
     @Override
@@ -33,16 +53,41 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        return null;
+        int indexToRemove = -1;
+        T foundedElement = null;
+        for (int i = 0; i < size; i++) {
+            if (element.equals(arrayUnderneath[i])) {
+                foundedElement = arrayUnderneath[i];
+                break;
+            }
+            indexToRemove++;
+        }
+        if (indexToRemove == -1) {
+            throw new NoSuchElementException("No such element: " + element);
+        }
+        for (int i = indexToRemove; i < size; i++) {
+            arrayUnderneath[i] = arrayUnderneath[i + 1];
+        }
+        size--;
+        return foundedElement;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growIfArrayFull() {
+        if (size == arrayUnderneath.length) {
+            int newArraySize = (int) Math.round(arrayUnderneath.length * GROWTH_MULTIPLIER);
+            T[] newArray = (T[]) new Object[newArraySize];
+            System.arraycopy(arrayUnderneath, 0, newArray, 0, arrayUnderneath.length);
+            arrayUnderneath = newArray;
+        }
     }
 }
