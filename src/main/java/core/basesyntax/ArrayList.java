00720df130cdf5c1fc841e099a0a7b27b76ef215
Jakub Sekowski <sekowskijakub@gmail.com>
2023-10-17 11:16:46 +0200

    Fixes applied: - removed unnecessary empty line - renamed method to checkIndex(int index) - util class no longer in use - isEmpty method fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,11 +1,8 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-
     private static final int DEFAULT_SIZE_OF_ARRAY = 10;
     private static final double ARRAY_SIZE_MULTIPLIER = 1.5;
     private int size;
@@ -29,8 +26,8 @@ public class ArrayList<T> implements List<T> {
             return;
         }
         extendArray();
-        indexCorrect(index);
-        System.arraycopy(arrayElements, index, arrayElements, index + 1, size - index);
+        checkIndex(index);
+        arrayElements = copyArray(index, index + 1, size - index);
         arrayElements[index] = value;
         size++;
     }
@@ -44,21 +41,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        indexCorrect(index);
+        checkIndex(index);
         return arrayElements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        indexCorrect(index);
+        checkIndex(index);
         arrayElements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        indexCorrect(index);
+        checkIndex(index);
         T removedElement = arrayElements[index];
-        System.arraycopy(arrayElements, index + 1, arrayElements, index, size - index - 1);
+        arrayElements = copyArray(index + 1, index, size - index - 1);
         size--;
         return removedElement;
     }
@@ -66,7 +63,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, arrayElements[i])) {
+            if (equals(element, arrayElements[i])) {
                 return remove(i);
             }
         }
@@ -80,14 +77,10 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0;
     }
 
-    private void indexCorrect(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index is incorrect for this array size.");
         }
@@ -95,7 +88,33 @@ public class ArrayList<T> implements List<T> {
 
     private void extendArray() {
         if (arrayElements.length == size) {
-            arrayElements = Arrays.copyOf(arrayElements, (int) (size * ARRAY_SIZE_MULTIPLIER));
+            arrayElements = copyToBiggerArray(arrayElements, (int) (size * ARRAY_SIZE_MULTIPLIER));
         }
+    }
+
+    private T[] copyToBiggerArray(T[] elements, int newCapacity) {
+        T[] result = (T[]) new Object[newCapacity];
+        for (int i = 0; i < elements.length; i++) {
+            result[i] = elements[i];
+        }
+        return result;
+    }
+
+    private Boolean equals(T firstElement, T secondElement) {
+        return (firstElement == secondElement)
+                || (firstElement != null && firstElement.equals(secondElement));
+    }
+
+    private T[] copyArray(int srcPos, int destPos, int length) {
+        int counter = 0;
+        T[] temporary = (T[]) new Object[destPos + length + 1];
+        for (int i = 0; i < destPos; i++) {
+            temporary[i] = arrayElements[i];
+        }
+        while (counter < length) {
+            temporary[destPos + counter] = arrayElements[srcPos + counter];
+            counter++;
+        }
+        return temporary;
     }
 }
