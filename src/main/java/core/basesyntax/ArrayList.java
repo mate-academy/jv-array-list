5c140dcfdb03e7fccb2f2732d40df6ac8ec0edec
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-19 20:06:41 +0300

    Create my own ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,114 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] DEFAULT_CAPACITY_EMPTY_ELEMENT_DATA = {};
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        elementData = DEFAULT_CAPACITY_EMPTY_ELEMENT_DATA;
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            elementData = grow(size + 1);
+        }
+        elementData[size] = value;
+        size = size + 1;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index " + index + " out of bounds for length" + elementData.length);
+        }
+        if (size == elementData.length) {
+            elementData = grow(size + 1);
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size = size + 1;
     }
 
     @Override
     public void addAll(List<T> list) {
+        elementData = grow(size + list.size());
+        for (int i = 0; i < list.size(); i++) {
+            elementData[size + i] = list.get(i);
+        }
+        size = size + list.size();
+    }
 
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        if (oldCapacity > 0 || elementData != DEFAULT_CAPACITY_EMPTY_ELEMENT_DATA) {
+            int newCapacity = Math.max((int)(elementData.length * 1.5f), minCapacity);
+            Object[] newElementData = new Object[newCapacity];
+            System.arraycopy(elementData, 0, newElementData, 0, elementData.length);
+            elementData = newElementData;
+            return elementData;
+        } else {
+            return elementData = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkToCorrectIndex(index);
+        return elementData(index);
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkToCorrectIndex(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkToCorrectIndex(index);
+        final T removedElement = elementData(index);
+        System.arraycopy(elementData, index + 1, elementData, index, size - 1 - index);
+        elementData[size - 1] = null;
+        size = size - 1;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == element
+                    || elementData[i] != null && elementData[i].equals(element)) {
+                remove(i);
+                return element;
+            }
+        }
+        throw new NoSuchElementException("ArrayList don't have such element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private T elementData(int index) {
+        return (T) elementData[index];
+    }
+
+    private void checkToCorrectIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index " + index + " out of bounds for length" + elementData.length);
+        }
     }
 }
