09b67dbcc3ee5edb7f7b73fb1bef57b3b950c96b
AdamMudrak <mudrets2000@gmail.com>
2023-11-01 21:21:24 +0200

    Delete capacity, apply improvement for addAll

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -15,32 +15,28 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         growIfArrayFull();
-        values[capacity++] = value;
-        size++;
+        values[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
         growIfArrayFull();
-        capacity++;
+        size++;
         checkIndex(index);
-        if (capacity == 2) {
+        if (size == 2) {
             moveElementsToTheRight(1);
-        } else if (capacity > 2) {
-            for (int currentIndex = capacity - 2; currentIndex > index; currentIndex--) {
+        } else if (size > 2) {
+            for (int currentIndex = size - 2; currentIndex > index; currentIndex--) {
                 moveElementsToTheRight(currentIndex);
             }
         }
         values[index] = value;
-        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        T[] tempValues = (T[]) new Object[list.size()];
-        for (int i = 0; i < tempValues.length; i++) {
-            tempValues[i] = list.get(i);
-            add(tempValues[i]);
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
     }
 
@@ -65,9 +61,8 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        for (int index = 0; index < capacity; index++) {
-            if (element == values[index]
-                    || (element != null && element.equals(values[index]))) {
+        for (int index = 0; index < size; index++) {
+            if (element == values[index] || (element != null && element.equals(values[index]))) {
                 return searchAndHoldRemovedElement(element, index);
             }
         }
@@ -85,11 +80,11 @@ public class ArrayList<T> implements List<T> {
     }
 
     private void growIfArrayFull() {
-        if (capacity == maxListSize) {
+        if (size == maxListSize) {
             maxListSize = (int) (maxListSize + maxListSize * 0.5);
             T[] tempValues = values;
             values = (T[]) new Object[maxListSize];
-            System.arraycopy(tempValues, 0, values, 0, capacity);
+            System.arraycopy(tempValues, 0, values, 0, size);
         }
     }
 
@@ -102,19 +97,18 @@ public class ArrayList<T> implements List<T> {
 
     private T searchAndHoldRemovedElement(T element, int index) {
         int nextElementIndex = index + 1;
-        while (nextElementIndex < capacity) {
+        while (nextElementIndex < size) {
             values[index++] = values[nextElementIndex++];
         }
         values[index] = null;
-        capacity--;
         size--;
         return element;
     }
 
     private void checkIndex(int index) {
-        if (index < 0 || index >= capacity) {
-            throw new ArrayListIndexOutOfBoundsException("Index " + index
-                    + " is Out of bounds for this ArrayList!");
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index "
+                    + index + " is Out of bounds for this ArrayList!");
         }
     }
 }
