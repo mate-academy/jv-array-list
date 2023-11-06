6aef0de8e67f74c4b2965bae21f86e4de373bf6e
Roman Kuzyk <kuzya228337@mail.com>
2023-11-06 22:16:56 +0200

    Constructor (ArrayList()): The class has a constructor that initializes an array of objects with a default capacity of 10 and sets the size to 0. ensureCapacity(int minCapacity) Method: This private method ensures that the internal array has sufficient capacity to accommodate new elements. It checks the minimum required capacity, calculates the new capacity as 1.5 times the old capacity, and adjusts it if necessary. The elements array is then resized using Arrays.copyOf(). add(T value) Method: This method adds an element to the end of the list. It checks the capacity and, if necessary, resizes the array before adding the element. add(T value, int index) Method: This method inserts an element at a specific index in the list. It checks if the index is within a valid range, resizes the array if needed, and shifts elements to make space for the new one. addAll(List<T> list) Method: This method adds all elements from another list to the end of the current list by iterating through the elements of the given list and calling the add(T value) method for each. get(int index) Method: This method retrieves an element from the list at the specified index. It performs boundary checks to ensure the index is within a valid range. set(T value, int index) Method: This method updates the value of an element at the specified index in the list. It also checks the index for validity. remove(int index) Method: This method removes an element from the list at the specified index and returns the removed element. It adjusts the array to fill the gap left by the removed element. remove(T element) Method: This method removes the first occurrence of the specified element from the list by iterating through the list and using the remove(int index) method when a match is found. size() Method: This method returns the current size (number of elements) of the list. isEmpty() Method: This method checks whether the list is empty by verifying if the size is 0.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,109 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = (int) (oldCapacity * 1.5);
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        ensureCapacity(size + 1);
+        elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index " + index);
+        }
 
+        ensureCapacity(size + 1);
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range " + index);
+        }
+
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range " + index);
+        }
 
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of range " + index);
+        }
+
+        final T removed = (T) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+
+        elements[size - 1] = null;
+        size--;
+        return removed;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if ((element == null && elements[i] == null)
+                    || (element != null && element.equals(elements[i]))) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("Element not found: " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
