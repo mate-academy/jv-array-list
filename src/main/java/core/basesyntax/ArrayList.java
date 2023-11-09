6ee798d716d5963bed8555ba0e6f59b4d18e3218
mister.Sokor <mister.sokor@gmail.com>
2023-11-08 23:12:21 -0600

    the code was optimized and duplicated parts were deleted, using one method checkIndexBounds(index);

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -20,9 +20,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
+        checkIndexBoundsForAdd(index);
         ensureCapacity();
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
@@ -38,25 +36,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (!checkIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
+        checkIndexBounds(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (!checkIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
+        checkIndexBounds(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (!checkIndex(index)) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
+        checkIndexBounds(index);
         T removedValue = elements[index];
         if (index < size - 1) {
             System.arraycopy(elements, index + 1, elements, index, size - index - 1);
@@ -66,17 +58,10 @@ public class ArrayList<T> implements List<T> {
     }
 
     public T remove(T element) {
-        if (element == null) {
-            for (int i = 0; i < size; i++) {
-                if (elements[i] == null) {
-                    return remove(i);
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (element.equals(elements[i])) {
-                    return remove(i);
-                }
+        for (int i = 0; i < size; i++) {
+            if ((element == null && elements[i] == null)
+                    || (element != null && element.equals(elements[i]))) {
+                return remove(i);
             }
         }
         throw new NoSuchElementException("Element not found: " + element);
@@ -92,22 +77,24 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void checkIndexBounds(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
-    }
-
-    private boolean checkIndex(int index) {
-        return (index >= 0 && index < size);
-    }
-
     private void ensureCapacity() {
         if (size == elements.length) {
             int newCapacity = (int) (elements.length * GROWTH_FACTOR);
             T[] newElements = (T[]) new Object[newCapacity];
             System.arraycopy(elements, 0, newElements, 0, size);
             elements = newElements;
+        }
+    }
+
+    private void checkIndexBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
+        }
+    }
+
+    private void checkIndexBoundsForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
         }
     }
 }
