81a46e708df7fba86e6cd8d73e6cb6e9f4d0679b
MilianCode <milian.code@gmail.com>
2023-11-21 12:07:56 +0100

    Fixed mistakes in jv-array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,18 +1,12 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITYY = 10;
     private static final double INCREMENT_INDEX = 1.5;
-    private Object[] array;
-    private int size;
-
-    public ArrayList() {
-        array = new Object[DEFAULT_CAPACITYY];
-        size = 0;
-    }
+    private T[] array = (T[]) new Object[DEFAULT_CAPACITYY];
+    private int size = 0;
 
     @Override
     public void add(T value) {
@@ -33,14 +27,6 @@ public class ArrayList<T> implements List<T> {
         size++;
     }
 
-    private void resizeArray() {
-        if (array.length == size) {
-            Object[] newArray = new Object[(int) (size * INCREMENT_INDEX)];
-            System.arraycopy(array, 0, newArray, 0, size);
-            array = newArray;
-        }
-    }
-
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
@@ -51,7 +37,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        return (T) array[index];
+        return array[index];
     }
 
     @Override
@@ -63,7 +49,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         checkIndex(index);
-        T removedElement = (T) array[index];
+        T removedElement = array[index];
         System.arraycopy(array, index + 1, array, index, size - index - 1);
         size--;
         return removedElement;
@@ -71,10 +57,13 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(array[i], element)) {
+        int i = 0;
+        for (Object elementFromArr : array) {
+            if (elementFromArr == element || (elementFromArr != null
+                    && elementFromArr.equals(element))) {
                 return remove(i);
             }
+            i++;
         }
         throw new NoSuchElementException("There is no such element in the list");
     }
@@ -87,6 +76,14 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void resizeArray() {
+        if (array.length == size) {
+            T[] newArray = (T[]) new Object[(int) (size * INCREMENT_INDEX)];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
     }
 
     private void checkIndex(int index) {
