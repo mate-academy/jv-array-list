1e2d7ce34eab7696bbad6ce17882138e21eef8ed
AlexL <alexlada86@gmail.com>
2023-12-30 04:30:31 +0100

    solved task

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,106 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private T[] elementData;
+    private int size;
+
+    public ArrayList() {
+        this.elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    private T[] grow() {
+        return elementData = Arrays.copyOf(elementData, (int) (elementData.length * 1.5));
+    }
+
+    private void checkArrayListIndexOutOfBoundsException(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index "
+                    + index + " out of bounds size " + size);
+        }
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            grow();
+        }
+        this.elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size == elementData.length) {
+            grow();
+        }
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index "
+                    + index + " out of bounds size " + size);
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        this.elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkArrayListIndexOutOfBoundsException(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkArrayListIndexOutOfBoundsException(index);
+        this.elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkArrayListIndexOutOfBoundsException(index);
+        T value = elementData[index];
+        if (index + 1 != size) {
+            System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
+        }
+        size -= 1;
+        return value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(elementData[i], element)) {
+                System.arraycopy(elementData, i + 1, elementData, i, size - 1 - i);
+                size -= 1;
+                return element;
+            }
+        }
+        throw new NoSuchElementException("Element " + element + " not found");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if (size != 0) {
+            return false;
+        }
+        return true;
     }
 }
