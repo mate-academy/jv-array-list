6af358a25ad5b81f1ce896e2087bc5b8da249bac
kvjohnny <kvjohnny.van@gmail.com>
2024-01-19 22:46:13 +0200

    overrided methods in ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,121 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
+@SuppressWarnings("unchecked")
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double NEW_ARRAY_CAPACITY_FACTOR = 1.5;
+    private int size;
+    private Object[] elementData;
+
+    public ArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        checkCapacity();
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        rangeCheckForAdd(index);
+        checkCapacity();
+        System.arraycopy(elementData, index,
+                elementData, index + 1,
+                size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            checkCapacity();
+            elementData[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(index + "is invalid");
+        }
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(index + "is invalid");
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T oldElementData;
+        try {
+            oldElementData = (T) elementData[index];
+            rangeCheckForAdd(index);
+            int amountOfCopiedNumbers = size - index - 1;
+            System.arraycopy(elementData, index + 1, elementData, index, amountOfCopiedNumbers);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayListIndexOutOfBoundsException(index + "is invalid");
+        }
+        --size;
+        return oldElementData;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int amountOfElement = 0;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == null && element == null
+                    || elementData[i] != null && elementData[i].equals(element)) {
+                amountOfElement++;
+                int amountOfCopiedNumbers = size - i - 1;
+                System.arraycopy(elementData, i + 1, elementData, i, amountOfCopiedNumbers);
+            }
+        }
+        if (amountOfElement == 0) {
+            throw new NoSuchElementException("no such element" + element + "present.");
+        }
+        --size;
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void rangeCheckForAdd(int index) throws ArrayIndexOutOfBoundsException {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(index + "is invalid");
+        }
+    }
+
+    private void grow() {
+        int newArrayCapacity = (int) (elementData.length * NEW_ARRAY_CAPACITY_FACTOR);
+        Object[] newArrayList = new Object[newArrayCapacity];
+        System.arraycopy(elementData, 0,
+                newArrayList, 0, size);
+        elementData = newArrayList;
+    }
+
+    private void checkCapacity() {
+        if (size == elementData.length) {
+            grow();
+        }
     }
 }
