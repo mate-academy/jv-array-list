06a335894a56dc7a936acf01451d86b51df84cfe
Danylo Kishchenko <amfich@gmail.com>
2024-01-31 00:51:01 +0200

    implemented custom ArrayList class with required methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,114 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elementData;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public ArrayList() {
+        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            increaseCapacity();
+        }
+        elementData[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(indexOutOfBoundsMsg(index));
+        }
+        if (!(size < elementData.length)) {
+            increaseCapacity();
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        if (list != null) {
+            for (int i = 0; i < list.size(); i++) {
+                add(list.get(i));
+            }
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T removedElement = elementData[index];
+        int numberOfElementsToMove = size - index - 1;
+        if (numberOfElementsToMove > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, numberOfElementsToMove);
+        }
+        elementData[--size] = null;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        if (element != null) {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elementData[i])) {
+                    return remove(i);
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return remove(i);
+                }
+            }
+        }
+        throw new NoSuchElementException("Can't remove element " + element + " from the list");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void increaseCapacity() {
+        int newCapacity = size + (size >> 1);
+        T[] oldElementData = elementData;
+        elementData = (T[]) new Object[newCapacity];
+        System.arraycopy(oldElementData, 0, elementData, 0, oldElementData.length);
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(indexOutOfBoundsMsg(index));
+        }
+    }
+
+    private String indexOutOfBoundsMsg(int index) {
+        return "Illegal index: " + index + ", for the list size: " + size;
     }
 }
