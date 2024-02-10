cee5a0e9805f77b6be9aa3b13cb6a7a0e497427f
Igor Hunder <gunderigorv@gmail.com>
2024-02-10 18:16:23 +0200

    Created a custom ArrayList class

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,8 +4,8 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private static final int EXPANSION_FACTOR1 = 1;
-    private static final int EXPANSION_FACTOR2 = 2;
+    private static final int EXPANSION_FACTOR = 2;
+    private static final int OFFSET = 1;
     private T[] elements;
     private int size;
 
@@ -16,20 +16,16 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void add(T value) {
         growArrayIfRequred();
-        elements[size] = value;
-        size++;
+        elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
         if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index + size);
         }
         growArrayIfRequred();
-        T[] newElements = (T[]) new Object[elements.length + EXPANSION_FACTOR1];
-        System.arraycopy(elements, 0, newElements, 0, index);
-        System.arraycopy(elements, index, newElements, index + EXPANSION_FACTOR1, size - index);
-        this.elements = newElements;
+        System.arraycopy(elements, index, elements, index + OFFSET, size - index);
         elements[index] = value;
         size++;
     }
@@ -57,9 +53,7 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkIndexBounds(index);
         T removedElement = elements[index];
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
+        System.arraycopy(elements, index + OFFSET, elements, index, size - index - OFFSET);
         size--;
         return removedElement;
     }
@@ -67,7 +61,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         int index = indexOf(element);
-        if (index != -1) {
+        if (index != -OFFSET) {
             return remove(index);
         }
         throw new NoSuchElementException("Element not found: " + element);
@@ -83,29 +77,25 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    public int indexOf(T element) {
+    private int indexOf(T element) {
         for (int i = 0; i < size; i++) {
-            if (element == null) {
-                if (elements[i] == null) {
-                    return i;
-                }
-            } else if (element.equals(elements[i])) {
+            if (element == null && elements[i] == null
+                    || element != null && element.equals(elements[i])) {
                 return i;
             }
         }
-
-        return -1;
+        return -OFFSET;
     }
 
-    public void checkIndexBounds(int index) {
+    private void checkIndexBounds(int index) {
         if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index + size);
         }
     }
 
-    public void growArrayIfRequred() {
+    private void growArrayIfRequred() {
         if (size >= elements.length) {
-            T[] newElements = (T[]) new Object[elements.length * EXPANSION_FACTOR2];
+            T[] newElements = (T[]) new Object[elements.length * EXPANSION_FACTOR];
             System.arraycopy(elements, 0, newElements, 0, elements.length);
             elements = newElements;
         }
