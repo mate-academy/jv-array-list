53f1d9c3378756a6a8f9aca49c768eaabeb5d1c6
Mykyta Alforov <nikalforov@gmail.com>
2024-02-26 17:01:41 +0200

    Changes to ArrayList class according to suggestions.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,30 +1,27 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double GROW_FACTOR = 1.5;
     private int size;
-    private Object[] dataArray;
+    private T[] dataArray;
 
     public ArrayList() {
-        this.dataArray = new Object[DEFAULT_CAPACITY];
-        size = 0;
+        this.dataArray = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        ensureCapacity(size + 1);
-        dataArray[size] = value;
-        size++;
+        ensureCapacity();
+        dataArray[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
         addIndexCheck(index);
-        ensureCapacity(size + 1);
+        ensureCapacity();
         System.arraycopy(dataArray, index, dataArray, index + 1, dataArray.length - index - 1);
         dataArray[index] = value;
         size++;
@@ -41,7 +38,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         boundsCheck(index);
-        return (T) dataArray[index];
+        return dataArray[index];
     }
 
     @Override
@@ -53,21 +50,19 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(int index) {
         boundsCheck(index);
-        final T oldValue = (T) dataArray[index];
+        final T oldValue = dataArray[index];
         System.arraycopy(dataArray, index + 1, dataArray, index, dataArray.length - index - 1);
-        dataArray[size - 1] = null;
-        size--;
+        dataArray[--size] = null;
         return oldValue;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(dataArray[i], element)) {
-                final T oldValue = (T) dataArray[i];
+            if (dataArray[i] == element || (dataArray[i] != null && dataArray[i].equals(element))) {
+                final T oldValue = dataArray[i];
                 System.arraycopy(dataArray, i + 1, dataArray, i, dataArray.length - i - 1);
-                dataArray[size - 1] = null;
-                size--;
+                dataArray[--size] = null;
                 return oldValue;
             }
         }
@@ -86,28 +81,34 @@ public class ArrayList<T> implements List<T> {
 
     private void boundsCheck(int index) {
         if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(index, size);
+            throw new ArrayListIndexOutOfBoundsException(getOutOfBoundsMessage(index));
         }
     }
 
     private void addIndexCheck(int index) {
         if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException(index, size);
+            throw new ArrayListIndexOutOfBoundsException(getOutOfBoundsMessage(index));
         }
     }
 
-    private void ensureCapacity(int minCapacity) {
-        if (minCapacity >= dataArray.length) {
-            grow(minCapacity);
+    private void ensureCapacity() {
+        if (size == dataArray.length) {
+            grow(size + 1);
         }
     }
 
     private void grow(int minCapacity) {
         int oldCapacity = dataArray.length;
-        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        int newCapacity = (int) (oldCapacity * GROW_FACTOR);
         if (newCapacity - minCapacity < 0) {
             newCapacity = minCapacity;
         }
-        dataArray = Arrays.copyOf(dataArray, newCapacity);
+        T[] newDataArray = (T[]) new Object[newCapacity];
+        System.arraycopy(dataArray, 0, newDataArray, 0, dataArray.length);
+        dataArray = newDataArray;
+    }
+
+    private String getOutOfBoundsMessage(int index) {
+        return "Index: " + index + ", Size: " + size;
     }
 }
--- src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
+++ src/main/java/core/basesyntax/ArrayListIndexOutOfBoundsException.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class ArrayListIndexOutOfBoundsException extends RuntimeException {
-    public ArrayListIndexOutOfBoundsException(int index, int size) {
-        super("Index: " + index + "; size: " + size);
+    public ArrayListIndexOutOfBoundsException(String message) {
+        super(message);
     }
 }
