66eb7a88c5e3f274b22138c78a30b270d744fd63
Mykyta Alforov <nikalforov@gmail.com>
2024-02-26 14:27:55 +0200

    Implemented List interface in ArrayList class.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,113 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Object[] dataArray;
+
+    public ArrayList() {
+        this.dataArray = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void add(T value) {
-
+        ensureCapacity(size + 1);
+        dataArray[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        addIndexCheck(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(dataArray, index, dataArray, index + 1, dataArray.length - index - 1);
+        dataArray[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
 
     }
 
     @Override
     public T get(int index) {
-        return null;
+        boundsCheck(index);
+        return (T) dataArray[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        boundsCheck(index);
+        dataArray[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        boundsCheck(index);
+        final T oldValue = (T) dataArray[index];
+        System.arraycopy(dataArray, index + 1, dataArray, index, dataArray.length - index - 1);
+        dataArray[size - 1] = null;
+        size--;
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(dataArray[i], element)) {
+                final T oldValue = (T) dataArray[i];
+                System.arraycopy(dataArray, i + 1, dataArray, i, dataArray.length - i - 1);
+                dataArray[size - 1] = null;
+                size--;
+                return oldValue;
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void boundsCheck(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(index, size);
+        }
+    }
+
+    private void addIndexCheck(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(index, size);
+        }
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity >= dataArray.length) {
+            grow(minCapacity);
+        }
+    }
+
+    private void grow(int minCapacity) {
+        int oldCapacity = dataArray.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0) {
+            newCapacity = minCapacity;
+        }
+        dataArray = Arrays.copyOf(dataArray, newCapacity);
     }
 }
