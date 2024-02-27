e4cb5bb204b60500ca98cbbcff1cc04eeeadf87f
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-27 17:29:04 +0200

    changed positions to constance

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_CAPACITY = 10;
     private static final double CAPACITY_MULTIPLIER = 1.5;
+    private static final int DEFAULT_CAPACITY = 10;
 
     private T[] elementsData;
     private int size;
