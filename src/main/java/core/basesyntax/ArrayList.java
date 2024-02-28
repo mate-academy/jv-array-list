5f9b37c0b2ca9c1d760ae71767da4b688c3b24e2
Denys <Eidenn005@gmail.com>
2024-02-28 19:40:10 +0200

    Make suggested changes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double GROW_INDEX = 1.5;
-    private static final String INDEX_OUT_OF_BOUNDS_EXEPTION = "This index "
+    private static final String INDEX_OUT_OF_BOUNDS_EXEPTION = "Index: %s "
             + "out of bounds: ";
     private static final String NO_SUCH_ELEMENT_EXEPTION = "Element not found in list ";
     private T[] elementData = (T[]) new Object[DEFAULT_CAPACITY];
@@ -14,7 +13,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        checkForResize();
+        growIfFull();
         elementData[size] = value;
         size++;
     }
@@ -26,11 +25,10 @@ public class ArrayList<T> implements List<T> {
             return;
         }
         checkIndex(index);
-        checkForResize();
-        final int s = size;
+        growIfFull();
         System.arraycopy(elementData, index,
                 elementData, index + 1,
-                s - index);
+                size - index);
         elementData[index] = value;
         size++;
     }
@@ -79,24 +77,28 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return this.size == 0;
+        return size == 0;
     }
 
-    private void checkForResize() {
+    private void growIfFull() {
         if (size == elementData.length) {
             int newCapacity = (int) (this.size * GROW_INDEX);
-            elementData = Arrays.copyOf(elementData, newCapacity);
+            T[] grown = (T[]) new Object[newCapacity];
+            System.arraycopy(elementData, 0,
+                    grown, 0, elementData.length);
+            elementData = grown;
         }
     }
 
     private void checkIndex(int index) {
         if (index < 0 || index > size - 1) {
-            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_BOUNDS_EXEPTION + index);
+            throw new ArrayListIndexOutOfBoundsException(String
+                    .format(INDEX_OUT_OF_BOUNDS_EXEPTION, index));
         }
     }
 }
