7859c6e9cdd1670f77eae59db180abf5c399b374
1 <glebpashko2004@gmail.com>
2024-03-25 21:47:10 +0200

    Changed class ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -24,12 +24,12 @@ public class ArrayList<T> implements List<T> {
     public void add(T value, int index) {
         checkIndexForAdd(index);
 
-        if (size + 1 > array.length) {
-            array = resizeAndInsert(array, index, value);
-        } else {
-            System.arraycopy(array, index, array, index + 1, size - index);
-            array[index] = value;
+        if (size == array.length) {
+            array = resizeArray(array);
         }
+
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
         size++;
     }
 
@@ -42,19 +42,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndexForGet(index);
+        checkIndex(index);
         return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndexForSet(index);
+        checkIndex(index);
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndexForRemove(index);
+        checkIndex(index);
         final T removedElement = array[index];
         System.arraycopy(array, index + 1, array, index, size - index - 1);
         array[size - 1] = null;
@@ -95,33 +95,13 @@ public class ArrayList<T> implements List<T> {
         return array;
     }
 
-    private T[] resizeAndInsert(T[] array, int index, T value) {
-        T[] newArray = (T[]) new Object[(int) (array.length * RESIZE_FACTOR)];
-        System.arraycopy(array, 0, newArray, 0, index);
-        newArray[index] = value;
-        System.arraycopy(array, index, newArray, index + 1, size - index);
-        return newArray;
-    }
-
     private void checkIndexForAdd(int index) {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("index > size");
         }
     }
 
-    private void checkIndexForGet(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("index > size");
-        }
-    }
-
-    private void checkIndexForSet(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("index > size");
-        }
-    }
-
-    private void checkIndexForRemove(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("index > size");
         }
