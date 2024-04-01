2a676674b81105492abc9b033bdd119464c5430d
Andrey Chertykovtsev <chertiav@gmail.com>
2024-04-01 22:32:58 +0200

    created own ArrayList:  - implemented logic for adding, getting, removing elements and getting capacity in own ArrayList method;

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import core.basesyntax.exceptions.ArrayListIndexOutOfBoundsException;
+import core.basesyntax.exceptions.NoSuchElementException;
+import core.basesyntax.interfaces.List;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_CAPACITY = 10;
+    private static final double SIZE_INCREASE_FACTOR = 1.5;
+    private static final int INITIAL_SIZE = 0;
+    private int currentCapacity;
+    private int size;
+    private T[] arrayList;
+
+    public ArrayList() {
+        this.arrayList = (T[]) new Object[INITIAL_CAPACITY];
+        this.size = INITIAL_SIZE;
+        this.currentCapacity = INITIAL_CAPACITY;
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        arrayList[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + " out of range");
+        }
+        growIfArrayFull();
+        for (int i = size - 1; i >= index; i--) {
+            arrayList[i + 1] = arrayList[i];
+        }
+        arrayList[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        throwExceptionIfIndexOutOfBounds(index);
+        return arrayList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        throwExceptionIfIndexOutOfBounds(index);
+        arrayList[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        throwExceptionIfIndexOutOfBounds(index);
+        T deleteValue = get(index);
+        for (int i = index; i < size - 1; i++) {
+            arrayList[i] = arrayList[i + 1];
+        }
+        size--;
+        return deleteValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(get(i), element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("There is no object with this value");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
+
+    private void growIfArrayFull() {
+        if (size == currentCapacity) {
+            currentCapacity = (int) (currentCapacity * SIZE_INCREASE_FACTOR);
+            arrayList = Arrays.copyOf(arrayList, currentCapacity);
+        }
+    }
+
+    private void throwExceptionIfIndexOutOfBounds(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + " out of range");
+        }
+    }
+
 }
---
+++ src/main/java/core/basesyntax/exceptions/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exceptions;
+
+public class NoSuchElementException extends java.util.NoSuchElementException {
+    public NoSuchElementException(String message) {
+        super(message);
+    }
+}
