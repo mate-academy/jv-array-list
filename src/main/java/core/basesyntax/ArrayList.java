320a5073f4db414212b043cd09be166efa143423
Yurii Soliar <us1585707@gmail.com>
2024-04-14 14:36:44 +0300

    divide checkIndex function

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -6,7 +6,7 @@ public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double RESIZE_MULTIPLIER = 0.5;
     private T[] items;
-    private int size = 0;
+    private int size;
 
     public ArrayList() {
         items = (T[]) new Object[DEFAULT_CAPACITY];
@@ -20,7 +20,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index, true);
+        checkIndexForAdd(index);
         grow();
         System.arraycopy(items, index, items, index + 1, size - index);
         items[index] = value;
@@ -36,19 +36,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndex(index, false);
+        checkIndex(index);
         return items[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, false);
+        checkIndex(index);
         items[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index, false);
+        checkIndex(index);
         T deletedValue = items[index];
         int numReplaceLeft = size - index - 1;
         System.arraycopy(items, index + 1, items, index, numReplaceLeft);
@@ -78,16 +78,17 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void checkIndex(int index, boolean isAddOperation) {
-        if (isAddOperation) {
-            if (index < 0 || index > size) {
-                throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-            }
-        } else {
-            if (index < 0 || index >= size) {
-                throw new ArrayListIndexOutOfBoundsException(
-                        "There is no element with index " + index);
-            }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "There is no element with index " + index);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    "Index: " + index + ", Size: " + size);
         }
     }
 
