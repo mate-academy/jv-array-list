da540cd06c4c97a4a993ad8a9b24dfbb9cb81c39
SviatoslavTataryn <www.t.svyatoslav55@gmail.com>
2024-04-14 15:45:34 +0200

    fixed mistake

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int INITIAL_CAPACITY = 10;
@@ -14,20 +13,16 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        ensureCapacity();
+        ensureCapacity(size + 1);
         elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
         checkIndexForAdd(index);
-        ensureCapacity();
-        System.arraycopy(
-                elements,
-                index,
-                elements,
-                index + 1,
-                size - index);
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements,
+                index + 1, size - index);
         elements[index] = value;
         size++;
     }
@@ -58,10 +53,7 @@ public class ArrayList<T> implements List<T> {
         int numMoved = size - index - 1;
         if (numMoved > 0) {
             System.arraycopy(elements,
-                    index + 1,
-                    elements,
-                    index,
-                    numMoved);
+                    index + 1, elements, index, numMoved);
         }
         elements[--size] = null;
         return oldValue;
@@ -70,7 +62,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, elements[i])) {
+            if (element == null && elements[i] == null
+                    || element != null && element.equals(elements[i])) {
                 return remove(i);
             }
         }
@@ -87,25 +80,28 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void ensureCapacity() {
-        if (size == elements.length) {
-            Object[] newElements = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newElements, 0, size);
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            int newCapacity = elements.length << 1;
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0,
+                    newElements, 0, size);
             elements = newElements;
         }
     }
 
     private void checkIndex(int index) {
         if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index: "
-                    + index + ", Size: " + size);
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: "
+                    + index + ", size: " + size);
         }
     }
 
     private void checkIndexForAdd(int index) {
         if (index > size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index: "
-                    + index + ", Size: " + size);
+            throw new ArrayListIndexOutOfBoundsException("Invalid index for add: "
+                    + index + ", size: " + size);
         }
     }
+
 }
