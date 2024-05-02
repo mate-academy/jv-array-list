a662a710a399df8a55131f6a506beba25027ca65
flamehost <flamehost@syneforge.com>
2024-05-02 22:06:43 +0300

    display edit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
 import java.util.Arrays;
-
 import java.util.NoSuchElementException;
-
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
