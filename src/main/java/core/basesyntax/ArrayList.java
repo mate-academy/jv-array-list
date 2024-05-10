878d3a69a03cc92d6b916879d4ba0d6290116b31
Ppllupold <kotnazar2005@gmail.com>
2024-05-10 15:48:39 +0300

    jv-arr-list fixed

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,25 +1,20 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final double GROW_MULTIPLYER = 1.5;
     private static final int DEFAUL_SIZE = 10;
     private int size = 0;
-    private int currentMaxSize;
     private T[] elements;
 
     public ArrayList() {
-        currentMaxSize = DEFAUL_SIZE;
         this.elements = (T[]) new Object[DEFAUL_SIZE];
     }
 
     @Override
     public void add(T value) {
-        if (size == currentMaxSize) {
-            increaseArrCapacity();
-        }
+        resize();
         elements[size] = value;
         size++;
     }
@@ -29,30 +24,17 @@ public class ArrayList<T> implements List<T> {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Index out of array boundaries");
         }
-        if (size == currentMaxSize) {
-            increaseArrCapacity();
-        }
-        if (index != size) {
-            T previousElem = elements[index];
-            T nextElem;
-            elements[index] = value;
-            for (int i = index + 1; i < size + 1; i++) {
-                nextElem = elements[i];
-                elements[i] = previousElem;
-                previousElem = nextElem;
-            }
-        } else {
-            elements[index] = value;
-        }
+        resize();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
         size++;
+
     }
 
     @Override
     public void addAll(List<T> list) {
-        while (size + list.size() > currentMaxSize) {
-            increaseArrCapacity();
-        }
         for (int i = 0; i < list.size(); i++) {
+            resize();
             elements[size] = list.get(i);
             size++;
         }
@@ -60,29 +42,21 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index out of array boundaries");
-        }
+        checkIndex(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index out of array boundaries");
-        }
+        checkIndex(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new ArrayListIndexOutOfBoundsException("Index out of array boundaries");
-        }
+        checkIndex(index);
         final T res = elements[index];
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         elements[size - 1] = null;
         size--;
         return res;
@@ -107,21 +81,27 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void increaseArrCapacity() {
-        currentMaxSize = (int) (currentMaxSize * GROW_MULTIPLYER);
-        T[] copy = (T[]) new Object[currentMaxSize];
-        for (int i = 0; i < size; i++) {
-            copy[i] = elements[i];
+    private void resize() {
+        if (elements.length == size) {
+            int newCapacity = (int) (elements.length * GROW_MULTIPLYER);
+            T[] newArray = (T[]) new Object[newCapacity];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
         }
-        elements = copy;
     }
 
     private int indexOf(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(element, elements[i])) {
+            if (element == null ? elements[i] == null : element.equals(elements[i])) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of array boundaries");
+        }
     }
 }
