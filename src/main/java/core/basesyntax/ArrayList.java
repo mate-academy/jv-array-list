b7ce2c2998d54ae4cb691d8533a2815783aa1117
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-06-09 15:35:10 +0300

    implements the `List` interface in ArrayList` class and override the methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final double CAPACITY_MULTIPLIER = 1.5;
     private static final int DEFAULT_CAPACITY = 10;
-    private int arraySize;
+    private int size;
     private T[] elementData;
 
     public ArrayList() {
@@ -11,46 +14,86 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("invalid index entered, index"
+                    + " cannot be less than zero or greater than the size of the array");
+        }
+        growIfArrayFull();
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            growIfArrayFull();
+            elementData[size++] = list.get(i);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkingIndexIsValid(index);
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkingIndexIsValid(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkingIndexIsValid(index);
+        growIfArrayFull();
+        T result = elementData[index];
+        System.arraycopy(elementData, index + 1, elementData, index, size - index);
+        size--;
+        return result;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == null ? element == null : elementData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("no such element present");
     }
 
     @Override
     public int size() {
-        return arraySize;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return arraySize == 0 ? true : false;
+        return (size == 0) ? true : false;
+    }
+
+    private void growIfArrayFull() {
+        if (size == elementData.length) {
+            int newCapacity = (int) (elementData.length * CAPACITY_MULTIPLIER);
+            T[] newElementData = (T[]) new Object[newCapacity];
+            System.arraycopy(elementData, 0, newElementData, 0, elementData.length);
+            elementData = newElementData;
+        }
+    }
+
+    private void checkingIndexIsValid(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("invalid index entered, index"
+                    + " cannot be less than zero or greater than the size of the array");
+        }
     }
 }
