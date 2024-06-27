d85ae9499a118c1514769699a4a8681db59f5218
Andrii Khomenko <andpey.homenko@gmail.com>
2024-06-27 16:43:54 +0300

    inserted an if block in the remove method and changed the resize method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int size;
+    private static final double CAPACITY_INDEX = 1.5;
+    private int arraySize;
     private T[] items;
 
     public ArrayList() {
@@ -14,19 +14,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (size == items.length) {
+        if (arraySize == items.length) {
             resize();
         }
-        items[size++] = value;
+        items[arraySize++] = value;
     }
 
     @Override
     public void add(T value, int index) {
         checkIndexForAdd(index);
-        if (size == items.length) {
+        if (arraySize == items.length) {
             resize();
         }
-        System.arraycopy(items, index, items, index + 1, size++ - index);
+        System.arraycopy(items, index, items, index + 1, arraySize++ - index);
         items[index] = value;
     }
 
@@ -53,15 +53,21 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkIndex(index);
         T deletedObject = items[index];
-        System.arraycopy(items, index + 1, items, index, --size - index);
+        System.arraycopy(items, index + 1, items, index, --arraySize - index);
         return deletedObject;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(items[i], element)) {
-                return remove(i);
+        for (int i = 0; i < arraySize; i++) {
+            if (element == null) {
+                if (items[i] == null) {
+                    return remove(i);
+                }
+            } else {
+                if (element.equals(items[i])) {
+                    return remove(i);
+                }
             }
         }
         throw new NoSuchElementException("Element " + element + " non-existent");
@@ -69,29 +75,30 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return size;
+        return arraySize;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return arraySize == 0;
     }
 
     private T[] resize() {
-        Object[] newArray = new Object[items.length + (items.length >> 1)];
-        System.arraycopy(items, 0, newArray, 0, size);
+        int newCapacity = (int) (items.length * CAPACITY_INDEX);
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(items, 0, newArray, 0, arraySize);
         items = (T[]) newArray;
         return items;
     }
 
     private void checkIndexForAdd(int index) {
-        if (index > size || index < 0) {
+        if (index > arraySize || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("index " + index + " non-existent");
         }
     }
 
     private void checkIndex(int index) {
-        if (index >= size || index < 0) {
+        if (index >= arraySize || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("index " + index + " non-existent");
         }
     }
