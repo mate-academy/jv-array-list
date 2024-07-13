309ea1b11023155421235f2968f74078f207f084
Vadik Rakitin <vadym.rakitin@gmail.com>
2024-07-13 15:38:16 +0300

    fixed OutOfMemoryError by limiting array growth, renamed grow method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int INITIAL_CAPACITY = 10;
     private static final double GROWTH_FACTOR = 1.5;
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
     private T[] array;
     private int size;
 
@@ -15,37 +16,42 @@ public class ArrayList<T> implements List<T> {
     }
 
     @SuppressWarnings("unchecked")
-    private void grow() {
-        int newCapacity = (int) (array.length * GROWTH_FACTOR);
-        T[] newArray = (T[]) new Object[newCapacity];
-        System.arraycopy(array, 0, newArray, 0, size);
-        array = newArray;
+    private void growIfArrayFull() {
+        if (size == array.length) {
+            int newCapacity = (int) Math.min(array.length * GROWTH_FACTOR, MAX_ARRAY_SIZE);
+            if (newCapacity <= array.length) {
+                throw new OutOfMemoryError("Required array size too large");
+            }
+            T[] newArray = (T[]) new Object[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
     }
 
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Index " + index
-                    + " out of bounds of size " + size);
+    private void checkIndex(int index, boolean forAddOperation) {
+        if (forAddOperation) {
+            if (index < 0 || index > size) {
+                throw new ArrayListIndexOutOfBoundsException("Index " + index
+                        + " out of bounds for add operation of size " + size);
+            }
+        } else {
+            if (index < 0 || index >= size) {
+                throw new ArrayListIndexOutOfBoundsException("Index " + index
+                        + " out of bounds for size " + size);
+            }
         }
     }
 
     @Override
     public void add(T value) {
-        if (size == array.length) {
-            grow();
-        }
+        growIfArrayFull();
         array[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Index " + index
-                    + " out of bounds of size " + size);
-        }
-        if (size == array.length) {
-            grow();
-        }
+        checkIndex(index, true);
+        growIfArrayFull();
         System.arraycopy(array, index, array, index + 1, size - index);
         array[index] = value;
         size++;
@@ -60,19 +66,19 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndex(index);
+        checkIndex(index, false);
         return array[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index);
+        checkIndex(index, false);
         array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index);
+        checkIndex(index, false);
         T removedElement = array[index];
         int numMoved = size - index - 1;
         if (numMoved > 0) {
@@ -85,17 +91,12 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int index = 0; index < size; index++) {
-            if (element == null) {
-                if (array[index] == null) {
-                    return remove(index);
-                }
-            } else {
-                if (element.equals(array[index])) {
-                    return remove(index);
-                }
+            if ((element == null && array[index] == null)
+                    || (element != null && element.equals(array[index]))) {
+                return remove(index);
             }
         }
-        throw new NoSuchElementException("Element " + element + " is not found");
+        throw new NoSuchElementException("Element " + element + " is not found in ArrayList");
     }
 
     @Override
