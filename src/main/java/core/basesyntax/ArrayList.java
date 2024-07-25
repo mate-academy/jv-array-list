84407799ef75bfacd13c837cc3c599e0f162df4b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-07-25 20:55:50 +0300

    The ensureCapacity logic was simplified, so I created new growIfArrayFull method. Some methods have been renamed. Made -1 constant. The reduceSize method is created.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double INCREASE_CAPACITY = 1.5;
+    private static final int ELEMENT_NOT_FOUND = -1;
     private T[] elements;
     private int size;
 
@@ -12,23 +13,27 @@ public class ArrayList<T> implements List<T> {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
+    private T[] growIfArrayFull() {
+        int newCapacity = (int) (elements.length * INCREASE_CAPACITY);
+        T[] newElements = (T[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        return newElements;
+    }
+
     private void ensureCapacity() {
-        if (size() == elements.length) {
-            int newCapacity = (int) (elements.length * INCREASE_CAPACITY);
-            T[] newElements = (T[]) new Object[newCapacity];
-            System.arraycopy(elements, 0, newElements, 0, size);
-            elements = newElements;
+        if (size == elements.length) {
+            elements = growIfArrayFull();
         }
     }
 
-    private void checkIndex(int index) {
+    private void checkIndexForAccess(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index: " + index
                     + " is out of bounds " + size);
         }
     }
 
-    private void checkIndexForAdd(int index) {
+    private void checkIndexForAddition(int index) {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Index: " + index
                     + " is out of bounds " + size);
@@ -42,7 +47,11 @@ public class ArrayList<T> implements List<T> {
                 return i;
             }
         }
-        return -1;
+        return ELEMENT_NOT_FOUND;
+    }
+
+    private void reduceSize() {
+        elements[--size] = null;
     }
 
     @Override
@@ -53,8 +62,8 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        checkIndexForAdd(index);
         ensureCapacity();
+        checkIndexForAddition(index);
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
@@ -69,31 +78,30 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        checkIndex(index);
+        checkIndexForAccess(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index);
+        checkIndexForAccess(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index);
-        ensureCapacity();
+        checkIndexForAccess(index);
         T removedElement = elements[index];
         System.arraycopy(elements, index + 1, elements, index, size - index - 1);
-        elements[size--] = null;
+        reduceSize();
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
         int index = indexOf(element);
-        if (index == -1) {
-            throw new NoSuchElementException("element doesn't exist: " + element);
+        if (index == ELEMENT_NOT_FOUND) {
+            throw new NoSuchElementException(element + "element doesn't exist: " + index);
         }
         return remove(index);
     }
