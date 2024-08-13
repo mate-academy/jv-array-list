d8a85bb06982038ddb6ed95a2465099bc3406fcd
rainett <96131229+rainett@users.noreply.github.com>
2024-08-13 18:52:18 +0300

    implemented ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,108 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_ARRAY_SIZE = 10;
+    private static final float RESIZE_THRESHOLD = 1.0f;
+    private static final float RESIZE_MULTIPLIER = 1.5f;
+    private static final String ARRAY_INDEX_EXCEPTION_FORMAT =
+            "Index = [%d] is out of range for ArrayList with size = [%d]";
+    private Object[] array;
+    private int size;
+
+    public ArrayList() {
+        this.array = new Object[DEFAULT_ARRAY_SIZE];
+    }
+
     @Override
     public void add(T value) {
-
+        resizeIfRequired();
+        array[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-
+        validateInsertionIndex(index);
+        resizeIfRequired();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        validateIndex(index);
+        return (T) array[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        validateIndex(index);
+        array[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        validateIndex(index);
+        T value = (T) array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return value;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (element != null && array[i] == null) {
+                continue;
+            }
+            if (element == array[i] || array[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void validateIndex(int index) {
+        if (index >= size || index < 0) {
+            throwArrayListIndexOutOfBounds(index);
+        }
+    }
+
+    private void validateInsertionIndex(int index) {
+        if (index > size || index < 0) {
+            throwArrayListIndexOutOfBounds(index);
+        }
+    }
+
+    private void throwArrayListIndexOutOfBounds(int index) {
+        String message = String.format(ARRAY_INDEX_EXCEPTION_FORMAT, index, size);
+        throw new ArrayListIndexOutOfBoundsException(message);
+    }
+
+    private void resizeIfRequired() {
+        if (size / (float) array.length >= RESIZE_THRESHOLD) {
+            Object[] newArray = new Object[(int) (size * RESIZE_MULTIPLIER)];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
     }
 }
