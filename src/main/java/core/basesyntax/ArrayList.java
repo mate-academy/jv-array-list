35c5eac2878c015ecbd815e2a49005821e72448e
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-08-17 14:08:13 +0200

    1

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,8 +1,21 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private  T[] elements;
+
+
+    public ArrayList() {
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+
     @Override
     public void add(T value) {
+
 
     }
 
@@ -37,12 +50,54 @@ public class ArrayList<T> implements List<T> {
     }
 
     @Override
+
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            grow();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void grow() {
+        int newCapacity = (int) (elements.length * 1.5);
+        T[] newArray = (T[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = newArray;
+    }
+
+    private int indexOf(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element || (elements[i] != null && elements[i].equals(element))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds");
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " out of bounds");
+        }
+    }
+
+    public static class ArrayListIndexOutOfBoundsException extends RuntimeException {
+        public ArrayListIndexOutOfBoundsException(String message) {
+            super(message);
+        }
     }
 }
