3a40098ca97f6b2ee99e9e40d09b7884e65651e7
Ivan Demchenko <experywow@gmail.com>
2024-08-23 21:12:17 +0100

    Change constant name DEFAULT_ARRAYLIST_CAPACITY to DEFAULT_CAPACITY; ensureCapacity() removed; Added resize() method; Added findIndex() method; Method addAll refactored; Method add() - ensureCapacity swapped to resize(); method grow - removed; method remove - refactored;

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,18 +3,18 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_ARRAYLIST_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10;
     private static final int HALF_DIVIDER_INDEX = 2;
     private int size;
     private T[] elements;
 
     public ArrayList() {
-        elements = (T[]) new Object[DEFAULT_ARRAYLIST_CAPACITY];
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        ensureCapacity(size + 1);
+        resize();
         elements[size] = value;
         size++;
     }
@@ -25,7 +25,7 @@ public class ArrayList<T> implements List<T> {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
                     + " is out of range: Size is: " + size);
         }
-        ensureCapacity(size + 1);
+        resize();
         System.arraycopy(elements, index, elements, index + 1, size - index);
         elements[index] = value;
         size++;
@@ -33,16 +33,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-        if (list == null) {
-            throw new NullPointerException("List can't be null");
-        }
-        int newSize = size + list.size();
-        ensureCapacity(newSize);
-
         for (int i = 0; i < list.size(); i++) {
-            elements[size + i] = list.get(i);
+            resize();
+            elements[size] = list.get(i);
+            size++;
         }
-        size = newSize;
     }
 
     @Override
@@ -61,23 +56,18 @@ public class ArrayList<T> implements List<T> {
     public T remove(int index) {
         checkBoundInclusive(index);
         final T removedElement = elements[index];
-        System.arraycopy(elements,index + 1, elements, index, size - index - 1);
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         elements[--size] = null;
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i] == element
-                    || elements[i] != null && elements[i].equals(element)) {
-                final T removedElement = elements[i];
-                System.arraycopy(elements,i + 1, elements, i, size - i - 1);
-                elements[--size] = null;
-                return removedElement;
-            }
+        int index = findIndex(element);
+        if (index == -1) {
+            throw new NoSuchElementException("No such element found " + element);
         }
-        throw new NoSuchElementException("No such element found " + element);
+        return remove(index);
     }
 
     @Override
@@ -90,21 +80,24 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    public void ensureCapacity(int minCapacity) {
-        if (minCapacity > elements.length) {
-            grow(minCapacity);
+    private int findIndex(T element) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element
+                    || elements[i] != null && elements[i].equals(element)) {
+                index = i;
+            }
         }
+        return index;
     }
 
-    public void grow(int minCapacity) {
-        int oldCapacity = elements.length;
-        int currentCapacity = oldCapacity;
-        while (currentCapacity - minCapacity < 0) {
-            currentCapacity = (currentCapacity / HALF_DIVIDER_INDEX) + currentCapacity;
+    private void resize() {
+        if (elements.length == size) {
+            int newCapacity = (elements.length * HALF_DIVIDER_INDEX);
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = (T[]) newArray;
         }
-        Object[] copyOfElements = new Object[currentCapacity];
-        System.arraycopy(elements, 0, copyOfElements, 0, oldCapacity);
-        elements = (T[]) copyOfElements;
     }
 
     private void checkBoundInclusive(int index) {
