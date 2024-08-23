0163c1699670be1c7929df26fdffe778f5b5f5f2
Nikita Kravtsov <deez999deez@gmail.com>
2024-08-23 13:13:59 +0300

    Implement ArrayList service to create an ArrayList like default ArrayList in java
    Override such methods:
    add by value - to add element by value
    add by value and index - to add element by value and index
    addAll - to add all elements from exist list
    get by index - to get element by index
    set by value and index - to set value by index
    remove by index - remove element from list by index
    remove by element - find and remove equal element in list
    size - return list size
    isEmpty - check is list empty
    
    add grow method to resize array if needed in add methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,104 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    public static final int CAPACITY_EMPTY_ELEMENT_DATA = 0;
+    public static final int DEFAULT_CAPACITY_ELEMENT_DATA = 10;
+    public static final double CAPACITY_GROW_FACTOR = 1.5;
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
+    private T[] elementData = (T[]) new Object[DEFAULT_CAPACITY_ELEMENT_DATA];
+
     @Override
     public void add(T value) {
-
+        if (size >= elementData.length) {
+            elementData = grow(elementData);
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        if (size >= elementData.length) {
+            elementData = grow(elementData);
+        }
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+        elementData[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+    }
 
+    private T[] grow(T[] elementData) {
+        int newCapacity = (int) (elementData.length * CAPACITY_GROW_FACTOR);
+        return Arrays.copyOf(elementData, newCapacity);
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        return elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        final T removedElement = elementData[index];
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+        elementData[size - 1] = null;
+        size--;
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == null) {
+                if (element == null) {
+                    return remove(i);
+                }
+            } else if (elementData[i].equals(element)) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("No such element in list is exist: " + element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == CAPACITY_EMPTY_ELEMENT_DATA;
     }
 }
