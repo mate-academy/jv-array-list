246b11eb34b31a5dc4b2feea6495ca2d797547f2
skrypkasv <skrypkasv@gmail.com>
2024-08-23 21:02:35 +0300

    override addALL meth

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -47,7 +47,17 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void addAll(List<T> list) {
-
+        Object[] a = list.toArray();
+        int addLength = a.length;
+        if (addLength == 0) {
+            return;
+        }
+        final int s = size;
+        if (addLength > (this.elementData.length - s)) {
+            this.elementData = grow(s + addLength);
+        }
+        System.arraycopy(a, 0, this.elementData, s, addLength);
+        size = s + addLength;
     }
 
     @Override
@@ -79,6 +89,26 @@ public class ArrayList<T> implements List<T> {
     public boolean isEmpty() {
         return false;
     }
+    private Object[] grow(int minCapacity) {
+        return elementData = Arrays.copyOf(elementData, newCapacity(minCapacity));
+    }
+
+    private int newCapacity(int minCapacity) {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity <= 0) {
+            if (Arrays.equals(elementData, DEFAULTCAPACITY_EMPTY_ELEMENTDATA)) {
+                return Math.max(DEFAULT_CAPACITY, minCapacity);
+            }
+            if (minCapacity < 0) {
+                throw new OutOfMemoryError();
+            }
+            return minCapacity;
+        }
+        return (newCapacity - Integer.MAX_VALUE <= 0)
+                ? newCapacity : Integer.MAX_VALUE;
+    }
+
     private void rangeCheckForAdd(int index) {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("Can't add inappropriate index: "
