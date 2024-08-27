74da437a2ec7213371b7f6d77cb20eaae10357df
Dariia <daradyradara@gmail.com>
2024-08-27 14:08:07 +0300

    Made constant for default capacity, exceptions text have more information, deleted unnecessary "size", implemented correct order of methods.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,37 +4,12 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
+    private static final double CAPACITY_INDEX = 1.5;
     private T[] elementData;
     private int size;
 
     public ArrayList() {
         elementData = (T[]) new Object[DEFAULT_CAPACITY];
-        size = 0;
-    }
-
-    private void growIfNeeded() {
-        if (size == elementData.length) {
-            resizeArray();
-        }
-    }
-
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
-    }
-
-    private void checkIndexForAdd(int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index);
-        }
-    }
-
-    private void resizeArray() {
-        int newCapacity = (int) (elementData.length * 3) / 2 + 1;
-        T[] newArray = (T[]) new Object[newCapacity];
-        System.arraycopy(elementData, 0, newArray, 0, size);
-        elementData = newArray;
     }
 
     @Override
@@ -55,7 +30,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void addAll(List<T> list) {
         if (list == null) {
-            throw new IllegalArgumentException("The list cannot be null.");
+            throw new IllegalArgumentException("The provided list cannot be null because"
+                    + " elements must be added from a valid list.");
         }
 
         for (int i = 0; i < list.size(); i++) {
@@ -106,5 +82,35 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private void growIfNeeded() {
+        if (size == elementData.length) {
+            resizeArray();
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: "
+                    + index + ". Valid indices are between 0 and " + (size - 1)
+                    + ", list size is " + size);
+
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: "
+                    + index + ". Valid indices are between 0 and " + (size)
+                    + ", list size is " + size);
+        }
+    }
+
+    private void resizeArray() {
+        int newCapacity = (int) (elementData.length * CAPACITY_INDEX);
+        T[] newArray = (T[]) new Object[newCapacity];
+        System.arraycopy(elementData, 0, newArray, 0, size);
+        elementData = newArray;
     }
 }
