1e838dc5145c0e1a3365ccc100bd51563d60204f
Alinka Lyn <alinka.lyn@ukr.net>
2024-09-08 13:53:56 +0300

    solved

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.util.Objects;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
@@ -7,18 +8,15 @@ public class ArrayList<T> implements List<T> {
     private static final double GROWTH_FACTOR = 1.5;
     private T[] array;
     private int size;
-
     public ArrayList() {
         array = (T[]) new Object[DEFAULT_CAPACITY];
         size = 0;
     }
-
     @Override
     public void add(T value) {
         ensureCapacity();
         array[size++] = value;
     }
-
     @Override
     public void add(T value, int index) {
         checkIndexForAdd(index);
@@ -28,26 +26,22 @@ public class ArrayList<T> implements List<T> {
         array[index] = value;
         size++;
     }
-
     @Override
     public void addAll(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
-
     @Override
     public T get(int index) {
         checkIndex(index);
         return array[index];
     }
-
     @Override
     public void set(T value, int index) {
         checkIndex(index);
         array[index] = value;
     }
-
     @Override
     public T remove(int index) {
         checkIndex(index);
@@ -57,29 +51,23 @@ public class ArrayList<T> implements List<T> {
         array[--size] = null;
         return removedValue;
     }
-
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (array[i] == null && element == null || array[i].equals(element)) {
+            if (Objects.equals(array[i],element)) {
                 return remove(i);
             }
         }
-        throw new NoSuchElementException("Елемент не знайдено у списку");
+        throw new NoSuchElementException("Елемент '" + element + "' не знайдено у списку");
     }
-
     @Override
     public int size() {
         return size;
     }
-
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
-
-
-    @SuppressWarnings("unchecked")
     private void ensureCapacity() {
         if (size == array.length) {
             int newCapacity = (int) (array.length * GROWTH_FACTOR);
@@ -88,15 +76,11 @@ public class ArrayList<T> implements List<T> {
             array = newArray;
         }
     }
-
-
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Невірний індекс: " + index);
         }
     }
-
-
     private void checkIndexForAdd(int index) {
         if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Невірний індекс: " + index);
