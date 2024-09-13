7626e7dfff0852b2e454e046912523f0955b9e79
Volodymyr Nyshta <volodymyr.nyshta@gmail.com>
2024-09-13 17:25:48 +0300

    fixed filds name

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,52 +3,56 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private final int defoultCapasity = 10;
-    private final float growsIndex = 1.5f;
-    private T[] array = (T[]) new Object[defoultCapasity];
-    private int pointer;
+    private static final int DEFOULT_CAPASITY = 10;
+    private static final float GROW_INDEX = 1.5f;
+    private T[] array;
+    private int size;
+
+    public ArrayList() {
+        array = (T[]) new Object[DEFOULT_CAPASITY];
+    }
 
     @Override
     public void add(T value) {
-        if (pointer == array.length) {
+        if (size == array.length) {
             grow();
         }
-        array[pointer++] = value;
+        array[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > pointer) {
+        if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException(
                     "Test failed! Can't correctly add element by index - "
-                            + index + "Index can be from 0 to " + pointer);
+                            + index + "Index can be from 0 to " + size);
         }
-        if (pointer == array.length) {
+        if (size == array.length) {
             grow();
         }
-        if (index <= pointer) {
-            System.arraycopy(array, index, array, index + 1, pointer - index);
+        if (index <= size) {
+            System.arraycopy(array, index, array, index + 1, size - index);
             array[index] = value;
-            pointer++;
+            size++;
         }
     }
 
     @Override
     public void addAll(List<T> list) {
-        while (pointer + list.size() > array.length) {
+        while (size + list.size() > array.length) {
             grow();
         }
         for (int i = 0; i < list.size(); i++) {
-            if (pointer == array.length) {
+            if (size == array.length) {
                 grow();
             }
-            array[pointer++] = list.get(i);
+            array[size++] = list.get(i);
         }
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= pointer) {
+        if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index does not exist");
         }
         return array[index];
@@ -56,7 +60,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index >= pointer) {
+        if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index does not exist");
         }
         array[index] = value;
@@ -64,22 +68,22 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= pointer) {
+        if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index does not exist");
         }
         T tmp = array[index];
-        pointer--;
-        System.arraycopy(array, index + 1, array, index, pointer - index);
+        size--;
+        System.arraycopy(array, index + 1, array, index, size - index);
         return tmp;
     }
 
     @Override
     public T remove(T element) {
-        for (int i = 0; i <= pointer; i++) {
+        for (int i = 0; i <= size; i++) {
             if (array[i] == element || (array[i] != null && array[i].equals(element))) {
                 T tmp = array[i];
-                System.arraycopy(array, i + 1, array, i, pointer - i);
-                pointer--;
+                System.arraycopy(array, i + 1, array, i, size - i);
+                size--;
                 return tmp;
             }
         }
@@ -89,16 +93,16 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return pointer;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return pointer == 0;
+        return size == 0;
     }
 
-    public void grow() {
-        int size = Math.round(array.length * growsIndex);
+    private void grow() {
+        int size = Math.round(array.length * GROW_INDEX);
         T[] newArray = (T[]) new Object[size];
         System.arraycopy(array, 0, newArray, 0, array.length);
         array = newArray;
