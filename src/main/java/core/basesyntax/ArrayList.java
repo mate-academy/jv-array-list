2cd3864a2647511ffb4fc70dcda1182a595ac32b
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-09-20 16:58:25 +0300

    Fix

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -67,11 +67,8 @@ public class ArrayList<T> implements List<T> {
         checkIndex(index, true);
 
         T removedValue = storage[index];
-
         System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-
         storage[size - 1] = null;
-
         size--;
 
         return removedValue;
