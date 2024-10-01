61840c1459746ab35c2ff1e82bc1a79149c13d29
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-10-01 10:25:37 +0200

    second try

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int indexOfLastCharacter = 0;
+    private int size = 0;
     private T[] arr;
 
     public ArrayList() {
@@ -11,42 +11,33 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value) {
-        if (indexOfLastCharacter < DEFAULT_CAPACITY) {
-            arr[indexOfLastCharacter] = value;
-            indexOfLastCharacter++;
-        } else{
-           resize();
-            arr[indexOfLastCharacter] = value;
-            indexOfLastCharacter++;
+        if (size < DEFAULT_CAPACITY) {
+            arr[size] = value;
+            size++;
+        } else {
+            resize();
+            arr[size] = value;
+            size++;
         }
     }
 
 
     @Override
     public void add(T value, int index) {
-        T[] newArr;
-        T[] newArr2;
-        try {
-            newArr = (T[]) new Object[DEFAULT_CAPACITY];
-            newArr2 = (T[]) new Object[DEFAULT_CAPACITY];
-            System.arraycopy(arr, 0, newArr, 0, index);
-            if (index >= 0 && index < indexOfLastCharacter) {
-                newArr[index] = value;
-                System.arraycopy(arr, 0, newArr, index + 1, DEFAULT_CAPACITY - index + 1);
-                System.arraycopy(newArr, 0, arr, 0, DEFAULT_CAPACITY);
-                indexOfLastCharacter++;
-            }
-            if (index == indexOfLastCharacter) {
-                arr[indexOfLastCharacter] = value;
-                indexOfLastCharacter++;
-            } else {
-                resize();
-                arr[index] = value;
-                indexOfLastCharacter++;
-            }
-        } catch (Exception e) {
-            throw new ArrayListIndexOutOfBoundsException("Invalid index" + e);
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index");
         }
+        if (size >= arr.length) {
+            resize();
+        }
+        if(index >= 0 && index <= size) {
+            size++;
+        }
+        T[] newArr = (T[]) new Object[arr.length];
+        System.arraycopy(arr, 0, newArr, 0, size);
+        newArr[index] = value;
+        System.arraycopy(arr, index, newArr, index + 1, size - index);
+        System.arraycopy(newArr, 0, arr, 0, newArr.length);
     }
 
     @Override
@@ -58,7 +49,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index >= 0 && index < indexOfLastCharacter) {
+        if (index >= 0 && index <= size) {
             return arr[index];
         } else {
             throw new ArrayListIndexOutOfBoundsException("There is no a such index");
@@ -68,7 +59,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public void set(T value, int index) {
         try {
-            if (index >= 0 && index < indexOfLastCharacter) {
+            if (index >= 0 && index < size) {
                 arr[index] = value;
             }
         } catch (Exception e) {
@@ -83,15 +74,15 @@ public class ArrayList<T> implements List<T> {
         T[] newArr2;
         T result = null;
         try {
-            if (index >= 0 && index < indexOfLastCharacter) {
+            if (index >= 0 && index < size) {
                 result = arr[index];
-                newArr = (T[]) new Object[DEFAULT_CAPACITY];
-                newArr2 = (T[]) new Object[DEFAULT_CAPACITY];
+                newArr = (T[]) new Object[arr.length];
+                newArr2 = (T[]) new Object[arr.length];
                 System.arraycopy(arr, 0, newArr, 0, index);
                 System.arraycopy(arr, index + 1, newArr, index, DEFAULT_CAPACITY - index + 1);
                 System.arraycopy(newArr, 0, arr, 0, index);
                 System.arraycopy(arr, 0, newArr2, index, DEFAULT_CAPACITY - index + 1);
-                indexOfLastCharacter--;
+                size--;
                 return result;
             }
         } catch (Exception e) {
@@ -113,7 +104,7 @@ public class ArrayList<T> implements List<T> {
                     System.arraycopy(arr, i + 1, newArr, i, DEFAULT_CAPACITY - i + 1);
                     System.arraycopy(newArr, 0, arr, 0, i);
                     System.arraycopy(arr, 0, newArr2, i, DEFAULT_CAPACITY - i + 1);
-                    indexOfLastCharacter--;
+                    size--;
                 }
             }
         } catch (Exception e) {
@@ -124,7 +115,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return indexOfLastCharacter;
+        return size;
     }
 
     @Override
@@ -135,11 +126,11 @@ public class ArrayList<T> implements List<T> {
         return false;
     }
 
-    public void resize (){
-        int size = indexOfLastCharacter + (indexOfLastCharacter / 2); // or indexOfLastCharacter >> 1
-        T[] newArr  = (T[]) new Object[size];
-        System.arraycopy(arr, 0, newArr, 0, indexOfLastCharacter);
-        T[] arr  = (T[]) new Object[size];
+    public void resize() {
+        int size = this.size + (this.size / 2); // or indexOfLastCharacter >> 1
+        T[] newArr = (T[]) new Object[size];
+        System.arraycopy(arr, 0, newArr, 0, this.size);
+        T[] arr = (T[]) new Object[size];
         System.arraycopy(newArr, 0, arr, 0, size);
     }
 }
