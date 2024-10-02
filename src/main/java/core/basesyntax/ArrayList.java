0cb9024f533e41118146f5b3c9fa0e6bca7abb6b
Kate Kraska <kate2002.kk42@gmail.com>
2024-10-02 15:48:55 +0300

    Created your own ArrayList class which implements the List interface.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,165 @@
 package core.basesyntax;
 
-public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
+public abstract class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            int newCapacity = (int) (elements.length * 1.5 + 1);
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elements, 0, newArray, 0, elements.length);
+            elements = newArray;
+        }
     }
 
     @Override
-    public void add(T value, int index) {
-
-    }
-
-    @Override
-    public void addAll(List<T> list) {
-
+    public boolean add(T element) {
+        ensureCapacity();
+        elements[size++] = element;
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
-    }
-
-    @Override
-    public void set(T value, int index) {
-
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Індекс " + index + " виходить за межі списку.");
+        }
+        return (T) elements[index];
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Індекс " + index + " виходить за межі списку.");
+        }
+        T removedElement = (T) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null; // Звільняємо пам'ять
+        return removedElement;
     }
 
-    @Override
-    public T remove(T element) {
-        return null;
+    public boolean remove(Object element) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], element)) {
+                remove(i);
+                return true;
+            }
+        }
+        throw new NoSuchElementException("Елемент не знайдено.");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Індекс " + index + " виходить за межі списку.");
+        }
+        T oldValue = (T) elements[index];
+        elements[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Індекс " + index + " виходить за межі списку.");
+        }
+        ensureCapacity();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        System.arraycopy(elements, 0, array, 0, size);
+        return array;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new UnsupportedOperationException("Method not supported");
+    }
+
+    @Override
+    public boolean containsAll(java.util.Collection<?> c) {
+        throw new UnsupportedOperationException("Method not supported");
+    }
+
+    @Override
+    public boolean addAll(java.util.Collection<? extends T> c) {
+        for (T element : c) {
+            add(element);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, java.util.Collection<? extends T> c) {
+        throw new UnsupportedOperationException("Method not supported");
+    }
+
+    @Override
+    public boolean removeAll(java.util.Collection<?> c) {
+        throw new UnsupportedOperationException("Method not supported");
+    }
+
+    @Override
+    public boolean retainAll(java.util.Collection<?> c) {
+        throw new UnsupportedOperationException("Method not supported");
     }
 }
