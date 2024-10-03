27e73fd61d423ea67d1515c3e4b724eb462c9849
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-10-03 10:58:21 +0200

    final result

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -88,16 +88,10 @@ public class ArrayList<T> implements List<T> {
         for (int i = 0; i < size; i++) {
             if (arr[i] == element || (arr[i] != null && arr[i].equals(element))) {
                 remove(i);
-                found = true;
-                break;
+                return element;
             }
         }
-
-        if (!found) {
-            throw new NoSuchElementException("Element " + element + " was not found");
-        }
-
-        return element;
+        throw new NoSuchElementException("Element " + element + " was not found");
     }
 
     @Override
