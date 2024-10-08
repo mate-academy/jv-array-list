1dfabfee8ea4d815aab4541606dd1b56f049d046
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-10-08 13:41:13 +0200

    final result3

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("String");
+        arrayList.add("Java");
+        arrayList.add("Private");
+        arrayList.remove("Public");
+    }
+}
