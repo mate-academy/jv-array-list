23fb012461395644c4bd3cde1f7ba0c35f8171b9
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-10-16 16:22:33 +0300

    final changes

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,18 +4,20 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class ArrayList<T> implements List<T> {
-    private int initialCapacity = 16;
+    private static final int INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 1.5;
+    private int increaseCapacity = 16;
     private T[] table;
     private int size;
 
     @SuppressWarnings("unchecked")
     public ArrayList() {
-        this.table = (T[]) new Object[initialCapacity];
+        this.table = (T[]) new Object[INITIAL_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        if (size == initialCapacity) {
+        if (size == increaseCapacity) {
             increaseArray();
         }
         table[size++] = value;
@@ -25,15 +27,13 @@ public class ArrayList<T> implements List<T> {
     public void add(T value, int index) {
         if (index > size || index < 0) {
             throw new ArrayListIndexOutOfBoundsException("index " + index + "non exist");
-        } else if (size == initialCapacity) {
+        }
+        if (size == increaseCapacity) {
             increaseArray();
         }
-        T[] table;
-        int subSize = size;
-        table = this.table;
-        System.arraycopy(table, index, table, index + 1, subSize - index);
-        size = subSize + 1;
+        System.arraycopy(table, index, table, index + 1, size - index);
         table[index] = value;
+        size++;
     }
 
     @Override
@@ -45,52 +45,44 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if (index <= size - 1 && index >= 0) {
-            return table[index];
-        } else {
+        checkIndex(index);
+        return table[index];
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size - 1) {
             throw new ArrayListIndexOutOfBoundsException("index " + index + "non exist");
         }
     }
 
     @Override
     public void set(T value, int index) {
-        if (index < 0 || index > size - 1) {
-            throw new ArrayListIndexOutOfBoundsException("index " + index + "non exist");
-        }
+        checkIndex(index);
         table[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index > size - 1) {
-            throw new ArrayListIndexOutOfBoundsException("index " + index + "non exist");
-        } else {
-            T[] table;
-            table = this.table;
-            final T removeValue = table[index];
-            int subSize = size;
-            System.arraycopy(table, index + 1, table, index, subSize - index - 1);
-            size = subSize - 1;
-            table[size] = null;
-            return removeValue;
-        }
+        checkIndex(index);
+        final T removeValue = table[index];
+        System.arraycopy(table, index + 1, table, index, size - index - 1);
+        size--;
+        return removeValue;
     }
 
     @Override
     public T remove(T element) {
-        T[] table;
-        table = this.table;
-        int subSize = size;
+        int index = findIndex(element);
+        return remove(index);
+    }
 
+    private int findIndex(T element) {
         for (int i = 0; i < size; i++) {
             if (Objects.equals(element, table[i])) {
-                System.arraycopy(table, i + 1, table, i, subSize - i - 1);
-                size = subSize - 1;
-                table[size] = null;
-                return element;
+                return i;
             }
         }
-        throw new NoSuchElementException("element " + element + "non exist");
+        throw new NoSuchElementException("There is no such element in the list, " + element);
     }
 
     @Override
@@ -105,11 +97,11 @@ public class ArrayList<T> implements List<T> {
 
     @SuppressWarnings("unchecked")
     private void increaseArray() {
-        int capacity = (int) (initialCapacity * 1.5);
+        int capacity = (int) (increaseCapacity * LOAD_FACTOR);
         T[] currentArray = (T[]) new Object[capacity];
         System.arraycopy(table, 0, currentArray, 0, size);
         table = currentArray;
-        initialCapacity = capacity;
+        increaseCapacity = capacity;
     }
 }
 
