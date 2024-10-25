a53f64f5e78e67c3fd69d854f5b86257960943c5
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-10-25 15:33:27 +0100

    fix "add"

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -23,7 +23,7 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index > size) {
             throw new ArrayListIndexOutOfBoundsException("Index is out of ArrayList length");
         }
 
