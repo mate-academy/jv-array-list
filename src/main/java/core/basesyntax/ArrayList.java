7ffe6565dccec64fb2e85b5f255dfdbd68779609
DariaBakal <bakal.dasha01@gmail.com>
2024-10-26 14:49:43 +0300

    created ArrayList class, has overridden the methods required

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,143 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        this.elementData = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length) {
+            growIfArrayFull();
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexToAdd(index);
+        if (size == elementData.length) {
+            growIfArrayFull();
+        }
+        if (elementData[index] != null) {
+            divideAndSetNewValue(value, index);
+        } else if (elementData[index] == null) {
+            elementData[index] = value;
+        }
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        int arrayLength = list.size();
+        Object[] listArray = new Object[arrayLength];
+        if (arrayLength == 0) {
+            return;
+        }
+        for (int i = 0; i < arrayLength; i++) {
+            listArray[i] = list.get(i);
+        }
+        while (arrayLength > elementData.length - size) {
+            grow();
+        }
+        System.arraycopy(listArray, 0, elementData, size, arrayLength);
+        size += arrayLength;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elementData[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T oldValue = divideAndRemoveValue(index);
+        size--;
+        return oldValue;
+
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int elementIndex;
+        for (int i = 0; i < elementData.length; i++) {
+            if ((elementData[i] != null && elementData[i].equals(element))
+                    || elementData[i] == null && element == null) {
+                elementIndex = i;
+                size--;
+                return divideAndRemoveValue(elementIndex);
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public void checkIndexToAdd(int index) throws ArrayListIndexOutOfBoundsException {
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is not valid");
+        }
+    }
+
+    public void checkIndex(int index) throws ArrayListIndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index " + index + " is not valid");
+        }
+    }
+
+    private void growIfArrayFull() {
+        if (size == elementData.length) {
+            grow();
+        }
+    }
+
+    private void grow() {
+        int newCapacity = elementData.length + (elementData.length >> 1);
+        Object[] copiedArray = new Object[newCapacity];
+        System.arraycopy(elementData, 0, copiedArray, 0, size);
+        elementData = copiedArray;
+    }
+
+    private void divideAndSetNewValue(T value, int index) {
+        System.arraycopy(elementData, index,
+                elementData, index + 1,
+                size - index);
+        elementData[index] = value;
+    }
+
+    private T divideAndRemoveValue(int index) {
+        T oldValue = (T) elementData[index];
+        if (index == size) {
+            System.arraycopy(elementData, 0, elementData, 0, size);
+        } else {
+            System.arraycopy(elementData, index + 1,
+                    elementData, index,
+                    size - index - 1);
+        }
+        return oldValue;
+
     }
 }
