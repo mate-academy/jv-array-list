1fac0cde405ade8d8ad16aa5e5a280b6ca5c69d2
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-31 14:16:51 +0100

    Making changes according to the recommendations of the mentor (part 3)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,65 +5,64 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final String INDEX_OUT_OF_BOUND = "Index is out of bounds: ";
-    private T[] elementData;
+    private static final double CAPACITY_INDEX = 1.5;
+    private T[] elements;
     private int size;
 
     public ArrayList() {
-        elementData = (T[]) new Object[DEFAULT_CAPACITY];
+        elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void add(T value) {
-        resizeIfNeeded(size + 1);
-        elementData[size++] = value;
+        resizeIfNeeded();
+        elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index, size + 1);
-        resizeIfNeeded(size + 1);
-        System.arraycopy(elementData, index, elementData, index + 1, size - index);
-        elementData[index] = value;
+        checkIndexForAdd(index);
+        resizeIfNeeded();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        int requiredCapacity = size + list.size();
-
-        resizeIfNeeded(requiredCapacity);
         for (int i = 0; i < list.size(); i++) {
-            elementData[size++] = list.get(i);
+            resizeIfNeeded();
+            elements[size++] = list.get(i);
         }
     }
 
     @Override
     public T get(int index) {
-        checkIndex(index, size);
-        return elementData[index];
+        checkIndex(index);
+        return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-        checkIndex(index, size);
-        elementData[index] = value;
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index, size);
-        T removedElement = elementData[index];
-        System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
-        elementData[--size] = null;
+        checkIndex(index);
+        T removedElement = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
         return removedElement;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (elementData[i] == element
-                    || elementData[i] != null && elementData[i].equals(element)) {
-                T removedElement = elementData[i];
+            if (elements[i] == element
+                    || elements[i] != null && elements[i].equals(element)) {
+                T removedElement = elements[i];
                 remove(i);
                 return removedElement;
             }
@@ -81,19 +80,24 @@ public class ArrayList<T> implements List<T> {
         return size == 0;
     }
 
-    private void checkIndex(int index, int size) {
+    private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_BOUND + index);
         }
     }
 
-    private void resizeIfNeeded(int requiredCapacity) {
-        if (elementData.length < requiredCapacity) {
-            int newCapacity = elementData.length + (elementData.length >> 1);
-            newCapacity = Math.max(newCapacity, requiredCapacity);
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(INDEX_OUT_OF_BOUND + index);
+        }
+    }
+
+    private void resizeIfNeeded() {
+        if (elements.length == size) {
+            int newCapacity = (int) (elements.length * CAPACITY_INDEX);
             T[] newArray = (T[]) new Object[newCapacity];
-            System.arraycopy(elementData, 0, newArray, 0, size);
-            elementData = newArray;
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
         }
     }
 }
