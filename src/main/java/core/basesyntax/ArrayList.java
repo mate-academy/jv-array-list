595a47935ae2d91c7e192e5d2014499b09c32d3f
Dmytro Borysenko <borisand020774@gmail.com>
2024-11-22 22:14:44 +0200

    https://github.com/DmytroBorysenko2004/jv-array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,10 +1,9 @@
 package core.basesyntax;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-public abstract class ArrayList<T> implements List<T> {
+public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
     private int size;
@@ -55,6 +54,16 @@ public abstract class ArrayList<T> implements List<T> {
         return (T) elements[index];
     }
 
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
     public void set(T value, int index) {
         rangeCheck(index);
         elements[index] = value;
@@ -78,8 +87,38 @@ public abstract class ArrayList<T> implements List<T> {
             remove(index);
             return true;
         } else {
-            throw new NoSuchElementException("Element not found");
+            return false;
         }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
     }
 
     @Override
@@ -90,6 +129,26 @@ public abstract class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
     }
 
     public int indexOf(Object o) {
@@ -109,9 +168,23 @@ public abstract class ArrayList<T> implements List<T> {
         return -1;
     }
 
-    public static class ArrayListIndexOutOfBoundsException extends IndexOutOfBoundsException {
-        public ArrayListIndexOutOfBoundsException(String s) {
-            super(s);
-        }
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return List.of();
     }
 }
