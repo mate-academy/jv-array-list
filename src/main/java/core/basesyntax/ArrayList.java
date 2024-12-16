5590dcf0b1e4cdf7f54357c6e75569d77913c13a
eulu <anegamega@gmail.com>
2024-12-16 18:24:31 +0200

    Implement grow() method

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,13 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] data;
+
+    public ArrayList() {
+        this.data = new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
 
@@ -44,5 +51,15 @@ public class ArrayList<T> implements List<T> {
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private Object[] grow() {
+        int oldSize = data.length;
+        int newSize = (int) (oldSize * 1.5);
+        Object[] newData = new Object[newSize];
+        for (int i = 0; i < oldSize; i++) {
+            newData[i] = data[i];
+        }
+        return newData;
     }
 }
