83a0188c31820b8eb90cbe30f732f94d4862fa76
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-01-06 19:03:51 +0200

    Realised class ArrayList with all methods from class List with two custom exceptions used in ArrayList methods

--- src/main/java/core/basesyntax/NoSuchElementException.java
+++ src/main/java/core/basesyntax/NoSuchElementException.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class NoSuchElementException extends RuntimeException{
+public class NoSuchElementException extends RuntimeException {
     public NoSuchElementException(String message) {
         super(message);
     }
