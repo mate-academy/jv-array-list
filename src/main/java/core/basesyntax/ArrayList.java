84ca64764d6f6300f4ccc5b767b89e3da208a92f
Illia Kulyk <kulykillia12@gmail.com>
2025-01-09 13:26:48 +0200

    dds

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
-    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10; //ds
     private T[] elements;
     private int size;
 
