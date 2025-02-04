2720edc286d4a8410ab279e6c209c67dbf54c5e4
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2025-02-04 20:21:10 +0200

    Done

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -100,7 +100,7 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(elements[i],element)) {
+            if (Objects.equals(elements[i], element)) {
                 return remove(i);
             }
         }
