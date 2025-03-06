4ec945fd87d4cbeb2b41eed63321810f46466d3d
Artem Horopakha <dudumargatoa@gmail.com>
2025-03-06 22:30:41 +0200

    Created ArrayList class with multiple methods to behave the same way as the java.util.ArrayList . It tracks size via variable and changes it based on add of remove method results. In case index is out of bounds methods throw an exception, same goes for the condition, where there is no target element present.

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,113 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    public static final int DEFAULT_SIZE = 10;
+    private Object[] objects = new Object[DEFAULT_SIZE];
+    private int size = 0;
+
     @Override
     public void add(T value) {
-
+        if (size == objects.length) {
+            Object[] objectsCopy = new Object[(int) (objects.length * 1.5)];
+            System.arraycopy(objects, 0, objectsCopy, 0, objects.length);
+            objects = objectsCopy;
+        }
+        objects[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bound");
+        }
+        if (size == objects.length) {
+            Object[] objectsCopy = new Object[(int) (objects.length * 1.5)];
+            System.arraycopy(objects, 0, objectsCopy, 0, objects.length);
+            objects = objectsCopy;
+        }
+        Object[] objectsCopy = new Object[objects.length + 1];
+        System.arraycopy(objects, 0, objectsCopy, 0, index);
+        System.arraycopy(objects, index, objectsCopy, index + 1, size);
+        objects = objectsCopy;
+        objects[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            if (size == objects.length) {
+                Object[] objectsCopy = new Object[(int) (objects.length * 1.5)];
+                System.arraycopy(objects, 0, objectsCopy, 0, objects.length);
+                objects = objectsCopy;
+            }
+            objects[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bound");
+        }
+        return (T) objects[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index >= size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bound");
+        }
+        if (size == objects.length) {
+            Object[] objectsCopy = new Object[(int) (objects.length * 1.5)];
+            System.arraycopy(objects, 0, objectsCopy, 0, objects.length);
+            objects = objectsCopy;
+        }
+        objects[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index > size - 1) {
+            throw new ArrayListIndexOutOfBoundsException("Index out of bound");
+        }
+        Object[] objectsCopy = new Object[objects.length];
+        System.arraycopy(objects, 0, objectsCopy, 0, index);
+        System.arraycopy(objects, index + 1, objectsCopy, index, size - index - 1);
+        Object removed = objects[index];
+        objects = objectsCopy;
+        size--;
+        return (T) removed;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if ((objects[i] != null && objects[i].equals(element))
+                    || (element == null && objects[i] == null)) {
+                Object[] objectsCopy = new Object[objects.length];
+                System.arraycopy(objects, 0, objectsCopy, 0, i);
+                System.arraycopy(objects, i + 1, objectsCopy, i, size - i - 1);
+                Object removed = objects[i];
+                objects = objectsCopy;
+                size--;
+                return (T) removed;
+            }
+        }
+        throw new NoSuchElementException("There is no such element present");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
