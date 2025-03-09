b448a1299fabd2183d8830182e3977bafaa4db4d
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2025-03-09 23:12:30 +0100

    Update ArrayList4.java

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -5,10 +5,8 @@ import java.util.NoSuchElementException;
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final double GROWTH_FACTOR = 1.5;
-
     private Object[] elementData;
     private int size;
-
     public ArrayList() {
         elementData = new Object[DEFAULT_CAPACITY];
         size = 0;
