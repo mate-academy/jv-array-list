de81f86ed43b9fb451503cfe3789844fede0069b
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-30 17:27:46 +0300

    implemented ArrayList methods

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,117 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final double MULTIPLIER = 1.5;
+    private static final int DEFAULT_SIZE = 10;
+    private static final int ZERO = 0;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_SIZE];
+        size = ZERO;
+    }
+
+    public void checkCapacity() {
+        if (size == elements.length) {
+            grow();
+        }
+    }
+
+    public void checkIndex(int index) {
+        if (index < ZERO || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index.");
+        }
+    }
+
+    public void checkIndexForAdd(int index) {
+        if (index < ZERO || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Incorrect index.");
+        }
+    }
+
+    public void grow() {
+        int newSize = (int) (size * MULTIPLIER);
+        Object[] newElements = new Object[newSize];
+        System.arraycopy(elements, ZERO, newElements, ZERO, size);
+        elements = newElements;
+    }
+
     @Override
     public void add(T value) {
-
+        checkCapacity();
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkCapacity();
+        checkIndexForAdd(index);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return castElement(elements[index]);
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T deletedElement = castElement(elements[index]);
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+        return deletedElement;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if ((element != null && element.equals(elements[i]))
+                    || (element == null && elements[i] == null)) {
+                T removedElement = castElement(elements[i]);
+                System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                elements[--size] = null;
+                return removedElement;
+            }
+        }
+        throw new NoSuchElementException("Such element doesn't exist.");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public T castElement(Object element) {
+        try {
+            return (T) element;
+        } catch (ClassCastException e) {
+            throw new RuntimeException("Class can't be casted.");
+        }
     }
 }
