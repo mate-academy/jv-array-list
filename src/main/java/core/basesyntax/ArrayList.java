6bf35dc9f52145cdb843e672bb9715799d5cf81e
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-22 14:53:50 +0200

    Оновлення стилю коду згідно з рев'ю

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -77,8 +77,8 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if ((elements[i] == null && element == null)
-                    || (elements[i] != null && elements[i].equals(element))) {
+            if (elements[i] == null && element == null
+                    || elements[i] != null && elements[i].equals(element)) {
                 return remove(i);
             }
         }
